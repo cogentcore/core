99f1747b108a0c81a951132b9967ec4d576b60ee
Randall C. O'Reilly <randy.oreilly@colorado.edu>
2018-04-24 04:21:55 -0600

    added HSL and float32 color objects, and cleaned up basic Color -- now has a Lighter / darker function -- use that exclusively in styling so base color can be whatever

--- color.go
+++ color.go
@@ -7,18 +7,23 @@ package gi
 import (
 	"fmt"
 
-	"github.com/rcoreilly/goki/ki"
-	"github.com/rcoreilly/goki/ki/kit"
-	"golang.org/x/image/colornames"
-	// "gopkg.in/go-playground/colors.v1"
 	"image/color"
 	"log"
 	"strings"
+
+	"github.com/chewxy/math32"
+	"github.com/rcoreilly/goki/ki"
+	"github.com/rcoreilly/goki/ki/kit"
+	"golang.org/x/image/colornames"
 )
 
-// our color object
+// Color defines a standard color object for GUI use, with RGBA values, and
+// all the usual necessary conversion functions to / from names, strings, etc
+
+// Color extends image/color.RGBA with more methods for converting to / from
+// strings etc -- it has standard uint8 0..255 color values
 type Color struct {
-	Rgba color.RGBA
+	R, G, B, A uint8
 }
 
 var KiT_Color = kit.Types.AddType(&Color{}, ColorProps)
@@ -27,25 +32,34 @@ var ColorProps = ki.Props{
 	"style-prop": true,
 }
 
-var NilColor Color
-
-func (c *Color) RGBA() (r, g, b, a uint32) {
-	return c.Rgba.RGBA()
+// implements color.Color interface -- returns values in range 0x0000 - 0xffff
+func (c Color) RGBA() (r, g, b, a uint32) {
+	r = uint32(c.R)
+	r |= r << 8
+	g = uint32(c.G)
+	g |= g << 8
+	b = uint32(c.B)
+	b |= b << 8
+	a = uint32(c.A)
+	a |= a << 8
+	return
 }
+
+var NilColor Color
 
 // check if color is the nil initial default color -- a = 0 means fully transparent black
 func (c *Color) IsNil() bool {
-	if c.Rgba.R == 0 && c.Rgba.G == 0 && c.Rgba.B == 0 && c.Rgba.A == 0 {
+	if c.R == 0 && c.G == 0 && c.B == 0 && c.A == 0 {
 		return true
 	}
 	return false
 }
 
 func (c *Color) SetToNil() {
-	c.Rgba.R = 0
-	c.Rgba.G = 0
-	c.Rgba.B = 0
-	c.Rgba.A = 0
+	c.R = 0
+	c.G = 0
+	c.B = 0
+	c.A = 0
 }
 
 func (c *Color) SetColor(ci color.Color) {
@@ -55,31 +69,74 @@ func (c *Color) SetColor(ci color.Color) {
 }
 
 func (c *Color) SetUInt8(r, g, b, a uint8) {
-	c.Rgba.R = r
-	c.Rgba.G = g
-	c.Rgba.B = b
-	c.Rgba.A = a
+	c.R = r
+	c.G = g
+	c.B = b
+	c.A = a
 }
 
 func (c *Color) SetUInt32(r, g, b, a uint32) {
-	c.Rgba.R = uint8(r / 0x101) // convert back to uint8
-	c.Rgba.G = uint8(g / 0x101)
-	c.Rgba.B = uint8(b / 0x101)
-	c.Rgba.A = uint8(a / 0x101)
+	c.R = uint8(r >> 8) // convert back to uint8
+	c.G = uint8(g >> 8)
+	c.B = uint8(b >> 8)
+	c.A = uint8(a >> 8)
 }
 
 func (c *Color) SetInt(r, g, b, a int) {
 	c.SetUInt32(uint32(r), uint32(g), uint32(b), uint32(a))
 }
 
-// from 0-1 normalized floating point numbers
+// Convert from 0-1 normalized floating point numbers
 func (c *Color) SetFloat64(r, g, b, a float64) {
 	c.SetUInt8(uint8(r*255.0), uint8(g*255.0), uint8(b*255.0), uint8(a*255.0))
 }
 
-// from 0-1 normalized floating point numbers
+// Convert from 0-1 normalized floating point numbers
 func (c *Color) SetFloat32(r, g, b, a float32) {
 	c.SetUInt8(uint8(r*255.0), uint8(g*255.0), uint8(b*255.0), uint8(a*255.0))
+}
+
+// Convert from 0-1 normalized floating point numbers, non alpha-premultiplied
+func (c *Color) SetNPFloat32(r, g, b, a float32) {
+	r *= a
+	g *= a
+	b *= a
+	c.SetFloat32(r, g, b, a)
+}
+
+// Convert to 0-1 normalized floating point numbers, still alpha-premultiplied
+func (c Color) ToFloat32() (r, g, b, a float32) {
+	r = float32(c.R) / 255.0
+	g = float32(c.G) / 255.0
+	b = float32(c.B) / 255.0
+	a = float32(c.A) / 255.0
+	return
+}
+
+// Convert to 0-1 normalized floating point numbers, not alpha premultiplied
+func (c Color) ToNPFloat32() (r, g, b, a float32) {
+	r, g, b, a = c.ToFloat32()
+	if a != 0 {
+		r /= a
+		g /= a
+		b /= a
+	}
+	return
+}
+
+// Convert from HSLA: [0..360], Saturation [0..1], and Luminance
+// (lightness) [0..1] of the color using float32 values
+func (c *Color) SetHSLA(h, s, l, a float32) {
+	r, g, b := HSLtoRGBf32(h, s, l)
+	c.SetNPFloat32(r, g, b, a)
+}
+
+// Convert to HSLA: [0..360], Saturation [0..1], and Luminance
+// (lightness) [0..1] of the color using float32 values
+func (c *Color) ToHSLA() (h, s, l, a float32) {
+	r, g, b, a := c.ToNPFloat32()
+	h, s, l = RGBtoHSLf32(r, g, b)
+	return
 }
 
 func (c *Color) SetFromString(nm string) error {
@@ -104,7 +161,7 @@ func (c *Color) SetFromString(nm string) error {
 				log.Printf("%v\n", err)
 				return err
 			} else {
-				c.Rgba = nc
+				c.SetUInt8(nc.R, nc.G, nc.B, nc.A)
 			}
 		}
 	}
@@ -145,10 +202,229 @@ func (c *Color) ParseHex(x string) error {
 	}
 
 	if got {
-		c.Rgba.R = uint8(r)
-		c.Rgba.G = uint8(g)
-		c.Rgba.B = uint8(b)
-		c.Rgba.A = uint8(a)
+		c.R = uint8(r)
+		c.G = uint8(g)
+		c.B = uint8(b)
+		c.A = uint8(a)
 	}
 	return nil
+}
+
+// Lighter returns a color that is lighter (factor > 1) or darker (factor < 1) -- converts to HSL and back
+func (c *Color) Lighter(factor float32) Color {
+	hsl := HSLAf32Model.Convert(*c).(HSLAf32)
+	hsl.L *= factor
+	if hsl.L > 1.0 {
+		hsl.L = 1.0
+	}
+	if hsl.L < 0.0 {
+		hsl.L = 0.0
+	}
+	return ColorModel.Convert(hsl).(Color)
+}
+
+/////////////////////////////////////////////////////////////////////////////
+//  float32 RGBA color
+
+// RGBAf32 stores alpha-premultiplied RGBA values in float32 0..1 normalized
+// format -- more useful for converting to other spaces
+type RGBAf32 struct {
+	R, G, B, A float32
+}
+
+// Implements the color.Color interface
+func (c RGBAf32) RGBA() (r, g, b, a uint32) {
+	r = uint32(c.R*65535.0 + 0.5)
+	g = uint32(c.G*65535.0 + 0.5)
+	b = uint32(c.B*65535.0 + 0.5)
+	a = uint32(c.A*65535.0 + 0.5)
+	return
+}
+
+// NRGBAf32 stores non-alpha-premultiplied RGBA values in float32 0..1
+// normalized format -- more useful for converting to other spaces
+type NRGBAf32 struct {
+	R, G, B, A float32
+}
+
+// Implements the color.Color interface
+func (c NRGBAf32) RGBA() (r, g, b, a uint32) {
+	r = uint32(c.R*c.A*65535.0 + 0.5)
+	g = uint32(c.G*c.A*65535.0 + 0.5)
+	b = uint32(c.B*c.A*65535.0 + 0.5)
+	a = uint32(c.A*65535.0 + 0.5)
+	return
+}
+
+/////////////////////////////////////////////////////////////////////////////
+//  HSLA color -- HSL is proposed to be supported in CSS3 and seems better than HSV
+
+// Hsl returns the Hue [0..360], Saturation [0..1], and Luminance (lightness) [0..1] of the color.
+
+// HSLA represents the Hue [0..360], Saturation [0..1], and Luminance
+// (lightness) [0..1] of the color using float32 values
+type HSLAf32 struct {
+	H, S, L, A float32
+}
+
+// Implements the color.Color interface
+func (c HSLAf32) RGBA() (r, g, b, a uint32) {
+	fr, fg, fb := HSLtoRGBf32(c.H, c.S, c.L)
+	r = uint32(fr*c.A*65535.0 + 0.5)
+	g = uint32(fg*c.A*65535.0 + 0.5)
+	b = uint32(fb*c.A*65535.0 + 0.5)
+	a = uint32(c.A*65535.0 + 0.5)
+	return
+}
+
+// HSLtoRGBf32 converts HSL values to RGB float32 0..1 values (non alpha-premultiplied) -- based on https://stackoverflow.com/questions/2353211/hsl-to-rgb-color-conversion, https://www.w3.org/TR/css-color-3/ and github.com/lucasb-eyer/go-colorful
+func HSLtoRGBf32(h, s, l float32) (r, g, b float32) {
+	if s == 0 {
+		r = l
+		g = l
+		b = l
+		return
+	}
+
+	h = h / 360.0 // convert to normalized 0-1 h
+	var q float32
+	if l < 0.5 {
+		q = l * (1.0 + s)
+	} else {
+		q = l + s - l*s
+	}
+	p := 2.0*l - q
+	r = hueToRGBf32(p, q, h+1.0/3.0)
+	g = hueToRGBf32(p, q, h)
+	b = hueToRGBf32(p, q, h-1.0/3.0)
+	return
+}
+
+func hueToRGBf32(p, q, t float32) float32 {
+	if t < 0 {
+		t += 1
+	}
+	if t > 1 {
+		t -= 1
+	}
+	if t < 1.0/6.0 {
+		return p + (q-p)*6.0*t
+	}
+	if t < .5 {
+		return q
+	}
+	if t < 2.0/3.0 {
+		return p + (q-p)*(2.0/3.0-t)*6.0
+	}
+	return p
+}
+
+// RGBtoHSLf32 converts RGB 0..1 values (non alpha-premultiplied) to HSL -- based on https://stackoverflow.com/questions/2353211/hsl-to-rgb-color-conversion, https://www.w3.org/TR/css-color-3/ and github.com/lucasb-eyer/go-colorful
+func RGBtoHSLf32(r, g, b float32) (h, s, l float32) {
+	min := math32.Min(math32.Min(r, g), b)
+	max := math32.Max(math32.Max(r, g), b)
+
+	l = (max + min) / 2.0
+
+	if min == max {
+		s = 0
+		h = 0
+	} else {
+		d := max - min
+		if l > 0.5 {
+			s = d / (2.0 - max - min)
+		} else {
+			s = d / (max + min)
+		}
+		switch max {
+		case r:
+			h = (g - b) / d
+			if g < b {
+				h += 6.0
+			}
+		case g:
+			h = 2.0 + (b-r)/d
+		case b:
+			h = 4.0 + (r-g)/d
+		}
+
+		h *= 60
+
+		if h < 0 {
+			h += 360
+		}
+	}
+	return
+}
+
+///////////////////////////////////////////////////////////////////////
+// Models for conversion
+
+var (
+	ColorModel    color.Model = color.ModelFunc(colorModel)
+	RGBAf32Model  color.Model = color.ModelFunc(rgbaf32Model)
+	NRGBAf32Model color.Model = color.ModelFunc(nrgbaf32Model)
+	HSLAf32Model  color.Model = color.ModelFunc(hslaf32Model)
+)
+
+func colorModel(c color.Color) color.Color {
+	if rg, ok := c.(color.RGBA); ok {
+		return Color{rg.R, rg.G, rg.B, rg.A}
+	}
+	if _, ok := c.(Color); ok {
+		return c
+	}
+	r, g, b, a := c.RGBA()
+	return Color{uint8(r >> 8), uint8(g >> 8), uint8(b >> 8), uint8(a >> 8)}
+}
+
+func rgbaf32Model(c color.Color) color.Color {
+	if _, ok := c.(RGBAf32); ok {
+		return c
+	}
+	r, g, b, a := c.RGBA()
+	return RGBAf32{float32(r) / 65535.0, float32(g) / 65535.0, float32(b) / 65535.0, float32(a) / 65535.0}
+}
+
+func nrgbaf32Model(c color.Color) color.Color {
+	if _, ok := c.(NRGBAf32); ok {
+		return c
+	}
+	r, g, b, a := c.RGBA()
+	if a > 0 {
+		// Since color.Color is alpha pre-multiplied, we need to divide the
+		// RGB values by alpha again in order to get back the original RGB.
+		r *= 0xffff
+		r /= a
+		g *= 0xffff
+		g /= a
+		b *= 0xffff
+		b /= a
+	}
+	return NRGBAf32{float32(r) / 65535.0, float32(g) / 65535.0, float32(b) / 65535.0, float32(a) / 65535.0}
+}
+
+func hslaf32Model(c color.Color) color.Color {
+	if _, ok := c.(HSLAf32); ok {
+		return c
+	}
+	r, g, b, a := c.RGBA()
+	if a > 0 {
+		// Since color.Color is alpha pre-multiplied, we need to divide the
+		// RGB values by alpha again in order to get back the original RGB.
+		r *= 0xffff
+		r /= a
+		g *= 0xffff
+		g /= a
+		b *= 0xffff
+		b /= a
+	}
+	fr := float32(r) / 65535.0
+	fg := float32(g) / 65535.0
+	fb := float32(b) / 65535.0
+	fa := float32(a) / 65535.0
+
+	h, s, l := RGBtoHSLf32(fr, fg, fb)
+
+	return HSLAf32{h, s, l, fa}
 }
--- examples/colors/colors.go
+++ examples/colors/colors.go
@@ -30,7 +30,7 @@ func mainrun() {
 	rec := ki.Node{}          // receiver for events
 	rec.InitName(&rec, "rec") // this is essential for root objects not owned by other Ki tree nodes
 
-	win := gi.NewWindow2D("GoGi Icons Window", width, height, true)
+	win := gi.NewWindow2D("GoGi Colors Window", width, height, true)
 	vp := win.WinViewport2D()
 	updt := vp.UpdateStart()
 
@@ -52,47 +52,61 @@ func mainrun() {
 
 	row1.AddNewChild(gi.KiT_Stretch, "str1")
 	lab1 := row1.AddNewChild(gi.KiT_Label, "lab1").(*gi.Label)
-	lab1.Text = "These are all the GoGi Icons, in a small and large size"
+	lab1.Text = "These are tests of the various GoGi Color functions"
 	lab1.SetProp("max-width", -1)
 	lab1.SetProp("text-align", "center")
 	row1.AddNewChild(gi.KiT_Stretch, "str2")
 
 	grid := vlay.AddNewChild(gi.KiT_Layout, "grid").(*gi.Layout)
 	grid.Lay = gi.LayoutGrid
-	grid.SetProp("columns", 5)
+	grid.SetProp("columns", 11)
 	grid.SetProp("align-vert", "center")
 	grid.SetProp("align-horiz", "center")
 	grid.SetProp("margin", 2.0)
 	grid.SetStretchMaxWidth()
 	grid.SetStretchMaxHeight()
 
-	il := gi.IconListSorted(*gi.CurIconSet)
+	// first test the HSL color scheme
+	var hues = [...]float32{0, 60, 120, 180, 240, 300}
+	sat := float32(1.0)
 
-	for _, ic := range il {
-		vb := grid.AddNewChild(gi.KiT_Layout, "vb").(*gi.Layout)
-		vb.Lay = gi.LayoutCol
-
-		nm := ic.Name()
-
-		smico := vb.AddNewChild(gi.KiT_Icon, nm).(*gi.Icon)
-		smico.CopyFrom(ic)
-		smico.SetMinPrefWidth(units.NewValue(20, units.Px))
-		smico.SetMinPrefHeight(units.NewValue(20, units.Px))
-		smico.SetProp("background-color", "transparent")
-		smico.SetProp("fill", "blue")
-		smico.SetProp("stroke", "black")
-		smico.SetProp("align-horiz", gi.AlignCenter)
-
-		ico := vb.AddNewChild(gi.KiT_Icon, nm).(*gi.Icon)
-		ico.CopyFrom(ic)
-		ico.SetMinPrefWidth(units.NewValue(100, units.Px))
-		ico.SetMinPrefHeight(units.NewValue(100, units.Px))
-		ico.SetProp("background-color", "transparent")
-		ico.SetProp("fill", "blue")
-		ico.SetProp("stroke", "black")
-		ico.SetProp("align-horiz", gi.AlignCenter)
-		nmlbl := vb.AddNewChild(gi.KiT_Label, "lab1").(*gi.Label)
-		nmlbl.Text = nm
+	for _, hu := range hues {
+		for lt := float32(0.0); lt <= 1.01; lt += 0.1 {
+			fr := grid.AddNewChild(gi.KiT_Frame, "fr").(*gi.Frame)
+			fr.SetProp("background-color", gi.HSLAf32{hu, sat, lt, 1.0})
+			fr.SetProp("max-width", -1)
+			fr.SetProp("max-height", -1)
+		}
+	}
+	// try again with alpha
+	for _, hu := range hues {
+		for lt := float32(0.0); lt <= 1.01; lt += 0.1 {
+			fr := grid.AddNewChild(gi.KiT_Frame, "fr").(*gi.Frame)
+			fr.SetProp("background-color", gi.HSLAf32{hu, sat, lt, 0.5})
+			fr.SetProp("max-width", -1)
+			fr.SetProp("max-height", -1)
+		}
+	}
+	// then sats
+	lt := float32(0.5)
+	for _, hu := range hues {
+		for sat := float32(0.0); sat <= 1.01; sat += 0.1 {
+			fr := grid.AddNewChild(gi.KiT_Frame, "fr").(*gi.Frame)
+			fr.SetProp("background-color", gi.HSLAf32{hu, sat, lt, 1.0})
+			fr.SetProp("max-width", -1)
+			fr.SetProp("max-height", -1)
+		}
+	}
+	// then doing it with colors -- tests the "there and back again" round trip..
+	for _, hu := range hues {
+		clr := gi.Color{}
+		clr.SetHSLA(hu, 1.0, 0.5, 1.0)
+		for lt := float32(0.0); lt <= 2.01; lt += 0.2 {
+			fr := grid.AddNewChild(gi.KiT_Frame, "fr").(*gi.Frame)
+			fr.SetProp("background-color", clr.Lighter(lt))
+			fr.SetProp("max-width", -1)
+			fr.SetProp("max-height", -1)
+		}
 	}
 
 	vp.UpdateEndNoSig(updt)
