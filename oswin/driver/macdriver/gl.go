7e0c9297f4e2373ad88181b444de845665e6ebcc
Randall C. O'Reilly <randy.oreilly@colorado.edu>
2018-08-26 23:06:14 -0600

    +build darwin on all macdriver - all those shiny files didn't have proper +build directives..

--- oswin/driver/macdriver/gl.go
+++ oswin/driver/macdriver/gl.go
@@ -7,7 +7,8 @@
 // Use of this source code is governed by a BSD-style
 // license that can be found in the LICENSE file.
 
-// Package macdriver provides an OpenGL-based driver for MacOS platform.
+// +build darwin
+
 package macdriver
 
 import (
--- oswin/driver/macdriver/macdriver.go
+++ oswin/driver/macdriver/macdriver.go
@@ -7,6 +7,8 @@
 // Use of this source code is governed by a BSD-style
 // license that can be found in the LICENSE file.
 
+// +build darwin
+
 // Package macdriver provides an OpenGL-based driver for MacOS platform.
 package macdriver
 
