bedc7fea460cdde8aa84863da7fda04fa902c8fa
rohrlich <john.rohrlich@colorado.edu>
2018-10-27 21:28:03 -0600

    Refactor spell code. Add gi.spell which removes any gi code from spell.spell and serves as a wrapper to isolate gide (or other app) from the implementation of the check/suggest.

---
+++ spell.go
@@ -0,0 +1,163 @@
+// Copyright (c) 2018, The GoKi Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style
+// license that can be found in the LICENSE file.
+
+package gi
+
+import (
+	"io/ioutil"
+	"log"
+	"os"
+	"path/filepath"
+	"regexp"
+	"strings"
+
+	"github.com/goki/gi/oswin"
+	"github.com/goki/gi/spell"
+	"github.com/goki/ki/kit"
+)
+
+////////////////////////////////////////////////////////////////////////////////////////
+// Spell
+
+// TextWord represents one word of the input text - used with fuzzy implementation
+type TextWord struct {
+	Word     string
+	Line     int `desc:"the line number"`
+	StartPos int `desc:"the starting character position"`
+	EndPos   int `desc:"the ending character position"`
+}
+
+var input []TextWord
+var inputidx int = 0
+
+// InitSpell tries to load the saved fuzzy.spell model.
+// If unsuccessful tries to create a new model from a text file used as input
+func InitSpell() {
+	if spell.Initialized() {
+		return
+	}
+	pdir := oswin.TheApp.AppPrefsDir()
+	openpath := filepath.Join(pdir, "spell_en_us_plain.json")
+	b, err := ioutil.ReadFile(openpath)
+	if err == nil {
+		loaded := spell.LoadModel(b)
+		if !loaded {
+			// try creating a new model from corpus
+			bigdatapath, err := kit.GoSrcDir("github.com/goki/gi/spell")
+			if err != nil {
+				log.Printf("Error getting path to corpus directory: %v.\n", err)
+			}
+			bigdatafile := filepath.Join(bigdatapath, "big.txt")
+			file, err := os.Open(bigdatafile)
+			if err == nil {
+				err := spell.ModelFromCorpus(*file)
+				if err != nil {
+					log.Printf("Failed building model from corpus file: %v. This file is used to create the spelling model.\n", err)
+				}
+			} else {
+				log.Printf("Could not open corpus file: %v. This file is used to create the spelling model.\n", err)
+
+			}
+		}
+	} else {
+		log.Printf("Could not open spelling model file: %v. Creating new default spell model.\n", err)
+	}
+}
+
+// SaveSpell saves the spelling model which includes the data and parameters
+func SaveSpell() error {
+	pdir := oswin.TheApp.AppPrefsDir()
+	path := filepath.Join(pdir, "spell_en_us_plain.json")
+	err := spell.SaveModel(path)
+	if err != nil {
+		log.Printf("Could not save spelling model to file: %v.\n", err)
+	}
+	return err
+}
+
+// InitNewCheck builds the input list, i.e. the words to check
+func InitNewCheck(text []byte) {
+	input = input[:0] // clear past input
+	inputidx = 0
+	TextToWords(text)
+}
+
+// TextToWords generates a slice of words from text
+// removes various non-word input, trims symbols, etc
+func TextToWords(text []byte) {
+	notwordchar, err := regexp.Compile(`[^0-9A-Za-z]`)
+	if err != nil {
+		panic(err)
+	}
+	allnum, err := regexp.Compile(`^[0-9]*$`)
+	if err != nil {
+		panic(err)
+	}
+	wordbounds, err := regexp.Compile(`\b`)
+	if err != nil {
+		panic(err)
+	}
+
+	textstr := string(text)
+
+	var words []TextWord
+	for l, line := range strings.Split(textstr, "\n") {
+		line = notwordchar.ReplaceAllString(line, " ")
+		bounds := wordbounds.FindAllStringIndex(line, -1)
+		words = words[:0] // reset for new line
+		splits := strings.Fields(line)
+		for i, w := range splits {
+			if allnum.MatchString(w) {
+				break
+			}
+			if len(w) > 1 {
+				tw := TextWord{Word: w, Line: l, StartPos: bounds[i*2][0], EndPos: bounds[i*2+1][0]}
+				words = append(words, tw)
+			}
+		}
+		input = append(input, words...)
+	}
+	//for _, w := range input {
+	//	fmt.Println(w)
+	//}
+}
+
+// NextUnknownWord returns the next unknown word, i.e. not found in corpus
+func NextUnknownWord() (unknown TextWord, suggests []string, err error) {
+	var tw TextWord
+
+	for {
+		tw = NextWord()
+		if tw.Word == "" { // we're done!
+			break
+		}
+		var known = false
+		suggests, known, err = spell.CheckWord(tw.Word)
+		if !known {
+			break
+		}
+	}
+	return tw, suggests, err
+}
+
+// NextWord returns the next word of the input words
+func NextWord() TextWord {
+	tw := TextWord{}
+	if inputidx < len(input) {
+		tw = input[inputidx]
+		inputidx += 1
+		return tw
+	}
+	return tw
+}
+
+// CheckWord calls the implementation so the app isn't tied to a particular implementation
+func CheckWord(w string) (suggests []string, known bool, err error) {
+	return spell.CheckWord(w)
+}
+
+// LearnWord calls the implementation so the app isn't tied to a particular implementation
+func LearnWord(w string) {
+	spell.LearnWord(w)
+}
--- spell/spell.go
+++ spell/spell.go
@@ -11,149 +11,46 @@ package spell
 import (
 	"bufio"
 	"encoding/json"
-	"fmt"
+	"errors"
 	"io/ioutil"
 	"log"
 	"os"
-	"path/filepath"
 	"regexp"
 	"strings"
 
-	"github.com/goki/gi"
-	"github.com/goki/gi/oswin"
-	"github.com/goki/ki/kit"
 	"github.com/sajari/fuzzy"
 )
 
 ///////////////////////////////////////////////////////////////////////////////
-//  spell check (no suggestions) github.com/client9/gospell
-
-// UnknownWord is an unknown word instance - used with the gospell implementation
-type UnknownWord struct {
-	Filename    string   `desc:"filename only, no path"` // gospell.Diff.Filename
-	Path        string   `desc:"path only, no filename"` // gospell.Diff.Path
-	Word        string   `desc:"unknown word"`           // gospell.Diff.Original
-	Suggestions []string `desc:"a list of suggestions for the unknown word"`
-	Text        string   `desc:"full line of text containing unknown word"` // gospell.Diff.Line
-	LineNo      int      `desc:"the line number"`                           // gospell.Diff.LineNum
-}
-
-// CheckFile makes calls on the gospell package and returns a slice of words not found in dictionary
-// Not used at this time
-//func CheckFile(fullpath string) ([]UnknownWord, error) {
-//	var unknowns []UnknownWord
-//
-//	path, filename := filepath.Split(fullpath)
-//
-//	// TODO: comment from gospell authoer -  based on OS (Windows vs. Linux)
-//	//dictPath := flag.String("path", ".:/usr/local/share/hunspell:/usr/share/hunspell", "Search path for dictionaries")
-//	dictPath := "/usr/local/share/hunspell"
-//
-//	// TODO : comment from gospell authoer -  based on environment variable settings
-//	//dicts := flag.String("d", "en_US", "dictionaries to load")
-//	dicts := "en_US"
-//
-//	//personalDict := flag.String("p", "", "personal wordlist file")
-//	personalDict := ""
-//
-//	affFile := ""
-//	dicFile := ""
-//	for _, base := range filepath.SplitList(dictPath) {
-//		affFile = filepath.Join(base, dicts+".aff")
-//		dicFile = filepath.Join(base, dicts+".dic")
-//		//log.Printf("Trying %s", affFile)
-//		_, err1 := os.Stat(affFile)
-//		_, err2 := os.Stat(dicFile)
-//		if err1 == nil && err2 == nil {
-//			break
-//		}
-//		affFile = ""
-//		dicFile = ""
-//	}
-//
-//	if affFile == "" {
-//		ur := "https://sourceforge.net/projects/hunspell/files/Spelling%20dictionaries/en_US/"
-//		return unknowns, fmt.Errorf("Unable to load %s. Download en_us.zip from \n\n %v \n\n Unzip into %v", dicts, ur, dictPath)
-//	}
-//
-//	log.Printf("Loading %s %s", affFile, dicFile)
-//	timeStart := time.Now()
-//	h, err := gospell.NewGoSpell(affFile, dicFile)
-//	timeEnd := time.Now()
-//
-//	// note: 10x too slow
-//	log.Printf("Loaded in %v", timeEnd.Sub(timeStart))
-//	if err != nil {
-//		log.Fatalf("%s", err)
-//	}
-//
-//	if personalDict != "" {
-//		raw, err := ioutil.ReadFile(personalDict)
-//		if err != nil {
-//			log.Fatalf("Unable to load personal dictionary %s: %s", personalDict, err)
-//		}
-//		duplicates, err := h.AddWordList(bytes.NewReader(raw))
-//		if err != nil {
-//			log.Fatalf("Unable to process personal dictionary %s: %s", personalDict, err)
-//		}
-//		if len(duplicates) > 0 {
-//			for _, word := range duplicates {
-//				log.Printf("Word %q in personal dictionary already exists in main dictionary", word)
-//			}
-//		}
-//	}
-//
-//	// todo: this will read what is on disk - do we want to pass in the bytes rather than the filename?
-//	raw, err := ioutil.ReadFile(fullpath)
-//	if err != nil {
-//		log.Fatalf("Unable to read Stdin: %s", err)
-//	}
-//	pt, _ := plaintext.NewIdentity()
-//	results := gospell.SpellFile(h, pt, raw)
-//	for _, diff := range results {
-//		unknown := UnknownWord{
-//			Filename: filename,
-//			Path:     path,
-//			Text:     diff.Line,
-//			LineNo:   diff.LineNum,
-//			Word:     diff.Original,
-//		}
-//		unknowns = append(unknowns, unknown)
-//	}
-//	return unknowns, nil
-//}
-
-///////////////////////////////////////////////////////////////////////////////
 //  spell check returning suggestions using github.com/sajari/fuzzy
 
-// TextWord represents one word of the input text - used with fuzzy implementation
-type TextWord struct {
-	Word     string
-	Line     int `desc:"the line number"`
-	StartPos int `desc:"the starting character position"`
-	EndPos   int `desc:"the ending character position"`
+var inited bool
+var model *fuzzy.Model
+
+func Initialized() bool {
+	return inited
 }
 
-var model *fuzzy.Model
-var input []TextWord
-var inputidx int = 0
+// Load Model loads the save model stored in json format
+func LoadModel(b []byte) bool {
+	model = fuzzy.NewModel()
+	err := json.Unmarshal(b, model)
+	if err == nil {
+		model.SetThreshold(1)
+		inited = true
+		return true
+	} else {
+		log.Printf("Failed loading model from saved json: %v.\n", err)
+		return false
+	}
+}
 
-func GetCorpus() []string {
+// ModelFromCorpus builds a new fuzzy.model from a text file
+func ModelFromCorpus(file os.File) error {
 	var out []string
+	var err error
 
-	bigdatapath, err := kit.GoSrcDir("github.com/goki/gi/spell")
-	if err != nil {
-		log.Println(err)
-		return out
-	}
-	bigdatafile := filepath.Join(bigdatapath, "big.txt")
-	file, err := os.Open(bigdatafile)
-	if err != nil {
-		gi.PromptDialog(nil, gi.DlgOpts{Title: "Could Not Open Spell File", Prompt: "Creating new default spell file."}, true, false, nil, nil)
-		fmt.Println(err)
-		return out
-	}
-	reader := bufio.NewReader(file)
+	reader := bufio.NewReader(&file)
 	scanner := bufio.NewScanner(reader)
 	scanner.Split(bufio.ScanLines)
 	// Count the words.
@@ -169,81 +66,35 @@ func GetCorpus() []string {
 		}
 	}
 	if err := scanner.Err(); err != nil {
-		fmt.Fprintln(os.Stderr, "reading input:", err)
+		log.Println(os.Stderr, "reading input: ", err)
+		return err
 	}
 
-	return out
-}
-
-func InitModel() {
-	model = fuzzy.NewModel()
-	pdir := oswin.TheApp.AppPrefsDir()
-	openpath := filepath.Join(pdir, "spell_en_us_plain.json")
-	b, err := ioutil.ReadFile(openpath)
-	if err != nil {
-		gi.PromptDialog(nil, gi.DlgOpts{Title: "Could Not Open Spell File", Prompt: "Creating new default spell file."}, true, false, nil, nil)
-		log.Println(err)
-		words := GetCorpus() // use the default corpus
-		model.Train(words)
-	} else {
-		json.Unmarshal(b, model)
-	}
+	model.Train(out)
 	model.SetThreshold(1)
-}
-
-// NewSpellCheck builds the input list, i.e. the words to check
-func NewSpellCheck(text []byte) {
-	input = input[:0] // clear past input
-	inputidx = 0
-	TextToWords(text)
-}
-
-// returns the next word not found in corpus
-func NextUnknownWord() (unknown TextWord, suggests []string) {
-	var tw TextWord
-	for {
-		tw = NextWord()
-		if tw.Word == "" { // we're done!
-			break
-		}
-		var known = false
-		suggests, known = CheckWord(tw)
-		if !known {
-			break
-		}
-	}
-	return tw, suggests
-}
-
-// returns the next word of the input words
-func NextWord() TextWord {
-	tw := TextWord{}
-	if inputidx < len(input) {
-		tw = input[inputidx]
-		inputidx += 1
-		return tw
-	}
-	return tw
+	inited = true
+	return err
 }
 
 // CheckWord checks a single word and returns suggestions if word is unknown
-func CheckWord(tw TextWord) (suggests []string, known bool) {
+// Programs should call gi.CheckWord - all program calls should be done through that single API
+func CheckWord(w string) (suggests []string, known bool, err error) {
 	if model == nil {
-		InitModel()
+		err = errors.New("Model not initialized")
+		return suggests, false, err
 	}
 
-	w := tw.Word
 	known = false
 	w = strings.Trim(w, "`'*.,?[]():;")
 	w = strings.ToLower(w)
 	suggests = model.SpellCheckSuggestions(w, 2)
 	if suggests == nil {
-		return nil, known // known is false
+		return nil, known, err // known is false
 	}
 	if len(suggests) > 0 && suggests[0] == w {
 		known = true
 	}
-	return suggests[1:], known
+	return suggests[1:], known, err
 }
 
 // LearnWord adds a single word to the corpus
@@ -251,62 +102,30 @@ func LearnWord(word string) {
 	model.TrainWord(word)
 }
 
-// TextToWords generates a slice of words from text
-// removes various non-word input, trims symbols, etc
-func TextToWords(text []byte) {
-	notwordchar, err := regexp.Compile(`[^0-9A-Za-z]`)
-	if err != nil {
-		panic(err)
-	}
-	allnum, err := regexp.Compile(`^[0-9]*$`)
-	if err != nil {
-		panic(err)
-	}
-	wordbounds, err := regexp.Compile(`\b`)
-	if err != nil {
-		panic(err)
-	}
-
-	textstr := string(text)
-
-	var words []TextWord
-	for l, line := range strings.Split(textstr, "\n") {
-		line = notwordchar.ReplaceAllString(line, " ")
-		bounds := wordbounds.FindAllStringIndex(line, -1)
-		words = words[:0] // reset for new line
-		splits := strings.Fields(line)
-		for i, w := range splits {
-			if allnum.MatchString(w) {
-				break
-			}
-			if len(w) > 1 {
-				tw := TextWord{Word: w, Line: l, StartPos: bounds[i*2][0], EndPos: bounds[i*2+1][0]}
-				words = append(words, tw)
-			}
-		}
-		input = append(input, words...)
-	}
-	//for _, w := range input {
-	//	fmt.Println(w)
-	//}
-}
-
-// SaveModel saves the spelling model which includes the dictionary and parameters
-func SaveModel() error {
+// SaveModel saves the spelling model which includes the data and parameters
+func SaveModel(path string) error {
 	if model != nil {
 		b, err := json.MarshalIndent(model, "", "  ")
 		if err != nil {
 			log.Println(err) // unlikely
 			return err
 		}
-		pdir := oswin.TheApp.AppPrefsDir()
-		savepath := filepath.Join(pdir, "spell_en_us_plain.json")
-		err = ioutil.WriteFile(savepath, b, 0644)
+		err = ioutil.WriteFile(path, b, 0644)
 		if err != nil {
-			gi.PromptDialog(nil, gi.DlgOpts{Title: "Could not Save to File", Prompt: err.Error()}, true, false, nil, nil)
-			log.Println(err)
+			log.Printf("Could not save spelling model to file: %v.\n", err)
 		}
 		return err
 	}
 	return nil
 }
+
+//func Complete(s string) []string {
+//	if model == nil {
+//		Init()
+//	}
+//	results, err := model.Autocomplete(s)
+//	if err == nil {
+//		fmt.Println(results)
+//	}
+//	return results
+//}
