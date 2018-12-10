1f33ebf5772fec281ce26daa4a06249a90fb6475
Randall C. O'Reilly <randy.oreilly@colorado.edu>
2018-12-10 04:28:38 -0700

    actually doesn't make sense to push timing printout -- I will just look at later.

--- giv/himarkup.go
+++ giv/himarkup.go
@@ -5,11 +5,9 @@
 package giv
 
 import (
-	"fmt"
 	htmlstd "html"
 	"log"
 	"strings"
-	"time"
 
 	"github.com/alecthomas/chroma"
 	"github.com/alecthomas/chroma/formatters/html"
@@ -122,13 +120,13 @@ func (hm *HiMarkup) MarkupTagsAll(txt []byte) ([]lex.Line, error) {
 		return nil, nil
 	}
 	if hm.PiParser != nil {
-		stt := time.Now()
+		// stt := time.Now()
 		hm.PiParser.InitAll(hm.PiState)
 		hm.PiParser.LexAll(hm.PiState)
-		lxdur := time.Now().Sub(stt)
+		// lxdur := time.Now().Sub(stt)
 		hm.PiParser.ParseAll(hm.PiState)
-		prdur := time.Now().Sub(stt)
-		fmt.Printf("lex: %v full parse: %v\n", lxdur, prdur)
+		// prdur := time.Now().Sub(stt)
+		// fmt.Printf("lex: %v full parse: %v\n", lxdur, prdur)
 		return hm.PiState.Src.Lexs, nil
 	} else if hm.lexer != nil {
 		return hm.ChromaTagsAll(txt)
