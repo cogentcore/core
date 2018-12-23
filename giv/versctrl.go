cd222bafb3a96950c8f968c7908725211d160073
rohrlich <john.rohrlich@colorado.edu>
2018-12-22 19:50:58 -0700

    Remove ChangeLog

--- giv/versctrl.go
+++ giv/versctrl.go
@@ -4,31 +4,6 @@
 
 package giv
 
-// ChangeRec is version control change-log record
-type ChangeRec struct {
-	Date     FileTime `desc:"date/time when change made"`
-	CommitID string   `desc:"unique identifier for the commit (git SHA, svn rev)"`
-	Author   string   `desc:"author name"`
-	Email    string   `desc:"author email"`
-	Message  string   `desc:"commit message summarizing changes"`
-	Files    string   `desc:"files changed in this commit"`
-}
-
-// ChangeLog is a record of changes committed from within the Gide system to
-// the version control system for this project.  Use the Log command for your
-// VCS to see all changes.
-type ChangeLog []ChangeRec
-
-// Add adds a record to the change log, at the top of the log
-func (cl *ChangeLog) Add(cr ChangeRec) {
-	sz := len(*cl)
-	*cl = append(*cl, cr)
-	if sz > 0 {
-		copy((*cl)[1:], (*cl)[0:sz])
-	}
-	(*cl)[0] = cr
-}
-
 // VersCtrlSystems is a list of supported Version Control Systems -- use these
 // names in commands to select commands for the current VCS for this project
 // (i.e., use shortest version of name, typically three letters)
