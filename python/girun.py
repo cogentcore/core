6b5a269c1454cf7d7e1632bbbd454a792c8fa156
Randall C. O'Reilly <randy.oreilly@colorado.edu>
2019-03-01 17:26:57 -0700

    updated python makefile -- now uses exe mode to avoid all issues of dead interpreter..

--- python/girun.py
+++
@@ -1,9 +0,0 @@
-# girun runs file given in first argument, starting up gi gui event loop
-
-import sys
-from gi import go
-
-# run filename given in first argument
-go.InitRunFileSet(sys.argv[1])
-go.Init()
-
