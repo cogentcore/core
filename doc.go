356c12b449e302a3366e9571b4163382d6db140f
Randall C. O'Reilly <randy.oreilly@colorado.edu>
2019-05-07 01:42:42 -0600

    add top-level doc.go and .gitignore

---
+++ doc.go
@@ -0,0 +1,22 @@
+// Copyright (c) 2018, The GoKi Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style
+// license that can be found in the LICENSE file.
+
+/*
+Package ki provides the top-level repository for GoKi Trees: Ki = Tree in Japanese, and
+"Key" in English -- powerful tree structures supporting scenegraphs, programs,
+parsing, etc.
+
+The sub-packages contain all the relevant code:
+
+* ki: is the main Ki interface and Node implementation thereof.
+
+* kit: is a type registry that ki uses in various ways and provides
+useful type-level properties that are used in the GoGi GUI.  It also
+is a powerful 'kit for dealing with Go's reflect sytem.
+
+* ints, floats, dirs, bitflag, atomctr, indent all provide basic
+Go infrastructure that one could argue should have been in the
+standard library, but isn't..
+*/
+package ki
