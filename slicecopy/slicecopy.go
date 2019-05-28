2dad8368ee8ea1f0b8cc9f1ee67acddfa062a784
Randall C. O'Reilly <randy.oreilly@colorado.edu>
2019-05-28 03:20:47 -0600

    added slicecopy package to copy basic slice types..

---
+++ slicecopy/slicecopy.go
@@ -0,0 +1,93 @@
+// Copyright (c) 2019, The GoKi Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style
+// license that can be found in the LICENSE file.
+
+/*
+package slicecopy provides those basic slice copy methods that I finally got tired of
+rewriting all the time.
+*/
+package slicecopy
+
+// String returns a copied version of the given string slice -- returns nil
+// if slice has zero length
+func String(sl []string) []string {
+	sz := len(sl)
+	if sz == 0 {
+		return nil
+	}
+	cp := make([]string, sz)
+	copy(cp, sl)
+	return cp
+}
+
+// Bool returns a copied version of the given bool slice -- returns nil
+// if slice has zero length
+func Bool(sl []bool) []bool {
+	sz := len(sl)
+	if sz == 0 {
+		return nil
+	}
+	cp := make([]bool, sz)
+	copy(cp, sl)
+	return cp
+}
+
+// Int returns a copied version of the given int slice -- returns nil
+// if slice has zero length
+func Int(sl []int) []int {
+	sz := len(sl)
+	if sz == 0 {
+		return nil
+	}
+	cp := make([]int, sz)
+	copy(cp, sl)
+	return cp
+}
+
+// Int32 returns a copied version of the given int32 slice -- returns nil
+// if slice has zero length
+func Int32(sl []int32) []int32 {
+	sz := len(sl)
+	if sz == 0 {
+		return nil
+	}
+	cp := make([]int32, sz)
+	copy(cp, sl)
+	return cp
+}
+
+// Int64 returns a copied version of the given int64 slice -- returns nil
+// if slice has zero length
+func Int64(sl []int64) []int64 {
+	sz := len(sl)
+	if sz == 0 {
+		return nil
+	}
+	cp := make([]int64, sz)
+	copy(cp, sl)
+	return cp
+}
+
+// Float64 returns a copied version of the given float64 slice -- returns nil
+// if slice has zero length
+func Float64(sl []float64) []float64 {
+	sz := len(sl)
+	if sz == 0 {
+		return nil
+	}
+	cp := make([]float64, sz)
+	copy(cp, sl)
+	return cp
+}
+
+// Float32 returns a copied version of the given float32 slice -- returns nil
+// if slice has zero length
+func Float32(sl []float32) []float32 {
+	sz := len(sl)
+	if sz == 0 {
+		return nil
+	}
+	cp := make([]float32, sz)
+	copy(cp, sl)
+	return cp
+}
