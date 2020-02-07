10fdb21b4581b6d0c29ee5ea3a983e6e29ee1784
Randall C. O'Reilly <randy.oreilly@colorado.edu>
2020-02-07 05:37:32 -0800

    add ReflectKindMap

--- syms/kinds.go
+++ syms/kinds.go
@@ -4,7 +4,11 @@
 
 package syms
 
-import "github.com/goki/ki/kit"
+import (
+	"reflect"
+
+	"github.com/goki/ki/kit"
+)
 
 // Kinds is a complete set of basic type categories and sub(sub..) categories -- these
 // describe builtin types -- user-defined types must be some combination / version
@@ -307,4 +311,38 @@ func InitSub2CatMap() {
 			}
 		}
 	}
+}
+
+///////////////////////////////////////////////////////////////////////
+//   Reflect map
+
+// ReflectKindMap maps reflect kinds to syms.Kinds
+
+var ReflectKindMap = map[reflect.Kind]Kinds{
+	reflect.Invalid:       Unknown,
+	reflect.Int:           Int,
+	reflect.Int8:          Int8,
+	reflect.Int16:         Int16,
+	reflect.Int32:         Int32,
+	reflect.Int64:         Int64,
+	reflect.Uint:          Uint,
+	reflect.Uint8:         Uint8,
+	reflect.Uint16:        Uint16,
+	reflect.Uint32:        Uint32,
+	reflect.Uint64:        Uint64,
+	reflect.Uintptr:       Uintptr,
+	reflect.Float32:       Float32,
+	reflect.Float64:       Float64,
+	reflect.Complex64:     Complex64,
+	reflect.Complex128:    Complex128,
+	reflect.Array:         Array,
+	reflect.Chan:          Chan,
+	reflect.Func:          Func,
+	reflect.Interface:     Interface,
+	reflect.Map:           Map,
+	reflect.Ptr:           Ptr,
+	reflect.Slice:         List,
+	reflect.String:        String,
+	reflect.Struct:        Struct,
+	reflect.UnsafePointer: Ptr,
 }
