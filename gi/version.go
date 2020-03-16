d7bb4b3e12a3ef388a44c50cc171130eb7750d2a
Randall C. O'Reilly <randy.oreilly@colorado.edu>
2020-03-15 18:39:33 -0700

    v0.9.14 release -- version.go updated

--- gi/version.go
+++ gi/version.go
@@ -3,8 +3,8 @@
 package gi
 
 const (
-	Version = "v0.9.13"
-	GitCommit = "261c5de" // the commit JUST BEFORE the release
-	VersionDate = "2020-03-03 11:04" // UTC
+	Version = "v0.9.14"
+	GitCommit = "1d43523" // the commit JUST BEFORE the release
+	VersionDate = "2020-03-16 01:39" // UTC
 )
 
