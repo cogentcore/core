4588d43bba2703208b20f7fadc8cea1672832d9e
Randall C. O'Reilly <randy.oreilly@colorado.edu>
2020-03-25 05:13:22 -0700

    restore layout examples

--- examples/layout/layout.go
+++ examples/layout/layout.go
@@ -50,56 +50,54 @@ func mainrun() {
 
 	mfr := win.SetMainFrame()
 
-	/*
-		row1 := gi.AddNewLayout(mfr, "row1", gi.LayoutHoriz)
+	row1 := gi.AddNewLayout(mfr, "row1", gi.LayoutHoriz)
 
-		row1.SetProp("vertical-align", "top")
-		// row1.SetProp("horizontal-align", "justify")
-		row1.SetProp("horizontal-align", "left")
-		row1.SetProp("margin", 4.0)
-		row1.SetProp("max-width", -1) // always stretch width
-		row1.SetProp("spacing", 6.0)
+	row1.SetProp("vertical-align", "top")
+	// row1.SetProp("horizontal-align", "justify")
+	row1.SetProp("horizontal-align", "left")
+	row1.SetProp("margin", 4.0)
+	row1.SetProp("max-width", -1) // always stretch width
+	row1.SetProp("spacing", 6.0)
 
-		for i, sz := range frsz {
-			nm := fmt.Sprintf("fr%v", i)
-			fr := gi.AddNewFrame(row1, nm, gi.LayoutHoriz)
-			fr.SetProp("width", sz.X)
-			fr.SetProp("height", sz.Y)
-			fr.SetProp("vertical-align", "inherit")
-			// fr.SetProp("horizontal-align", "inherit")
-			fr.SetProp("margin", "inherit")
-			if i == 2 {
-				fr.SetFixedWidth(units.NewEm(20))
-				spc := row1.AddNewChild(gi.KiT_Space, "spc").(*gi.Space)
-				spc.SetFixedWidth(units.NewEm(4))
-			} else {
-				fr.SetProp("max-width", -1) // spacer
-			}
+	for i, sz := range frsz {
+		nm := fmt.Sprintf("fr%v", i)
+		fr := gi.AddNewFrame(row1, nm, gi.LayoutHoriz)
+		fr.SetProp("width", sz.X)
+		fr.SetProp("height", sz.Y)
+		fr.SetProp("vertical-align", "inherit")
+		// fr.SetProp("horizontal-align", "inherit")
+		fr.SetProp("margin", "inherit")
+		if i == 2 {
+			fr.SetFixedWidth(units.NewEm(20))
+			spc := row1.AddNewChild(gi.KiT_Space, "spc").(*gi.Space)
+			spc.SetFixedWidth(units.NewEm(4))
+		} else {
+			fr.SetProp("max-width", -1) // spacer
 		}
+	}
 
-		row2 := gi.AddNewLayout(mfr, "row2", gi.LayoutHoriz)
-		row2.SetProp("text-align", "center")
-		row2.SetProp("max-width", -1) // always stretch width
+	row2 := gi.AddNewLayout(mfr, "row2", gi.LayoutHoriz)
+	row2.SetProp("text-align", "center")
+	row2.SetProp("max-width", -1) // always stretch width
 
-		row2.SetProp("vertical-align", "center")
-		// row2.SetProp("horizontal-align", "justify")
-		row2.SetProp("horizontal-align", "left")
-		row2.SetProp("margin", 4.0)
-		row2.SetProp("spacing", 6.0)
+	row2.SetProp("vertical-align", "center")
+	// row2.SetProp("horizontal-align", "justify")
+	row2.SetProp("horizontal-align", "left")
+	row2.SetProp("margin", 4.0)
+	row2.SetProp("spacing", 6.0)
 
-		for i, sz := range frsz {
-			nm := fmt.Sprintf("fr%v", i)
-			fr := gi.AddNewFrame(row2, nm, gi.LayoutHoriz)
-			fr.SetProp("width", sz.X)
-			fr.SetProp("height", sz.Y)
-			fr.SetProp("vertical-align", "inherit")
-			// fr.SetProp("horizontal-align", "inherit")
-			fr.SetProp("margin", "inherit")
-			// if i == 2 {
-			// 	gi.AddNewStretch(row2, "str")
-			// }
-		}
-	*/
+	for i, sz := range frsz {
+		nm := fmt.Sprintf("fr%v", i)
+		fr := gi.AddNewFrame(row2, nm, gi.LayoutHoriz)
+		fr.SetProp("width", sz.X)
+		fr.SetProp("height", sz.Y)
+		fr.SetProp("vertical-align", "inherit")
+		// fr.SetProp("horizontal-align", "inherit")
+		fr.SetProp("margin", "inherit")
+		// if i == 2 {
+		// 	gi.AddNewStretch(row2, "str")
+		// }
+	}
 
 	row3 := gi.AddNewLayout(mfr, "row3", gi.LayoutHorizFlow)
 	// row3.SetProp("text-align", "center")
@@ -115,38 +113,36 @@ func mainrun() {
 	for i, sz := range frsz {
 		nm := fmt.Sprintf("fr%v", i)
 		fr := gi.AddNewFrame(row3, nm, gi.LayoutHoriz)
-		fr.SetProp("width", sz.X)
+		fr.SetProp("width", 5*sz.X)
 		fr.SetProp("height", sz.Y)
 		fr.SetProp("min-height", sz.Y)
-		fr.SetProp("min-width", sz.X)
+		fr.SetProp("min-width", 5*sz.X)
 		// fr.SetProp("vertical-align", "inherit")
 		// fr.SetProp("horizontal-align", "inherit")
 		fr.SetProp("margin", "inherit")
 		// fr.SetProp("max-width", -1) // spacer
 	}
 
-	/*
-		row4 := gi.AddNewLayout(mfr, "row4", gi.LayoutGrid)
-		row4.SetProp("columns", 2)
-		// row4.SetProp("max-width", -1)
+	row4 := gi.AddNewLayout(mfr, "row4", gi.LayoutGrid)
+	row4.SetProp("columns", 2)
+	// row4.SetProp("max-width", -1)
 
-		row4.SetProp("vertical-align", "top")
-		// row4.SetProp("horizontal-align", "justify")
-		row4.SetProp("horizontal-align", "left")
-		row4.SetProp("margin", 6.0)
+	row4.SetProp("vertical-align", "top")
+	// row4.SetProp("horizontal-align", "justify")
+	row4.SetProp("horizontal-align", "left")
+	row4.SetProp("margin", 6.0)
 
-		for i, sz := range frsz {
-			nm := fmt.Sprintf("fr%v", i)
-			fr := gi.AddNewFrame(row4, nm, gi.LayoutHoriz)
-			fr.SetProp("width", sz.X)
-			fr.SetProp("height", sz.Y)
-			// fr.SetProp("min-height", sz.Y)
-			fr.SetProp("vertical-align", "inherit")
-			fr.SetProp("horizontal-align", "inherit")
-			fr.SetProp("margin", 2.0)
-			// fr.SetProp("max-width", -1) // spacer
-		}
-	*/
+	for i, sz := range frsz {
+		nm := fmt.Sprintf("fr%v", i)
+		fr := gi.AddNewFrame(row4, nm, gi.LayoutHoriz)
+		fr.SetProp("width", sz.X)
+		fr.SetProp("height", sz.Y)
+		// fr.SetProp("min-height", sz.Y)
+		fr.SetProp("vertical-align", "inherit")
+		fr.SetProp("horizontal-align", "inherit")
+		fr.SetProp("margin", 2.0)
+		// fr.SetProp("max-width", -1) // spacer
+	}
 
 	// main menu
 	appnm := gi.AppName()
