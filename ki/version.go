a18ba19d33fb87e079dc436e142350efdaa33d93
Randall C. O'Reilly <randy.oreilly@colorado.edu>
2020-04-07 05:32:21 -0700

    v1.0.0 release -- version.go updated

--- ki/version.go
+++ ki/version.go
@@ -3,8 +3,8 @@
 package ki
 
 const (
-	Version = "v0.9.13"
-	GitCommit = "1a45079" // the commit JUST BEFORE the release
-	VersionDate = "2020-03-25 12:06" // UTC
+	Version = "v1.0.0"
+	GitCommit = "3271301" // the commit JUST BEFORE the release
+	VersionDate = "2020-04-07 12:32" // UTC
 )
 
