cb620ad1f39b9c86ce4e57bccbc014b2f70d30b4
Randall C. O'Reilly <randy.oreilly@colorado.edu>
2021-04-20 23:01:25 -0700

    fast exponential function FastExp - is actually reasonably accurate (to e-5) too and much faster, mostly due to avoiding sanity checks perhaps.

---
+++ fastexp.go
@@ -0,0 +1,80 @@
+// Copyright 2021 The GoKi Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style
+// license that can be found in the LICENSE file.
+
+package mat32
+
+import (
+	"math"
+)
+
+// This is a fast version of the natural exponential function, for highly
+// time-sensitive uses where precision is less important.
+
+// based on: N. N. Schraudolph. "A fast, compact approximation of the exponential function." Neural Computation, 11(4), May 1999, pp.853-862.
+
+// as discussed and elaborated here: https://stackoverflow.com/questions/47025373/fastest-implementation-of-the-natural-exponential-function-using-sse
+
+/*
+// FastExpBad is the basic original N.N. Schraudolph version
+// which has bad error and is no faster than the better cubic
+// and quadratic cases.
+func FastExpBad(x float32) float32 {
+	i := int32(1512775*x + 1072632447)
+	if x <= -88.76731 { // this doesn't add anything and -exp is main use-case anyway
+		return 0
+	}
+	return math.Float32frombits(uint32(i << 32))
+}
+
+// FastExp3 is less accurate and no faster than quartic version.
+
+// FastExp3 is a cubic spline approximation to the Exp function, by N.N. Schraudolph
+// It does not have any of the sanity checking of a standard method -- returns
+// nonsense when arg is out of range.  Runs in .24ns vs. 8.7ns for 64bit which is faster
+// than math32.Exp actually.
+func FastExp3(x float32) float32 {
+	// const (
+	// 	Overflow  = 88.43114
+	// 	Underflow = -88.76731
+	// 	NearZero  = 1.0 / (1 << 28) // 2**-28
+	// )
+
+	// special cases
+	// switch {
+	// these "sanity check" cases cost about 1 ns
+	// case math32.IsNaN(x) || math32.IsInf(x, 1): /
+	// 	return x
+	// case math32.IsInf(x, -1):
+	// 	return 0
+	// these cases cost about 4+ ns
+	// case x >= Overflow:
+	// 	return math32.Inf(1)
+	// case x <= Underflow:
+	// 	return 0
+	// case -NearZero < x && x < NearZero:
+	// 	return 1 + x
+	// }
+	if x <= -88.76731 { // this doesn't add anything and -exp is main use-case anyway
+		return 0
+	}
+	i := int32(12102203*x) + 127*(1<<23)
+	m := i >> 7 & 0xFFFF // copy mantissa
+	i += ((((((((1277 * m) >> 14) + 14825) * m) >> 14) - 79749) * m) >> 11) - 626
+	return math.Float32frombits(uint32(i))
+}
+*/
+
+// FastExp is a quartic spline approximation to the Exp function, by N.N. Schraudolph
+// It does not have any of the sanity checking of a standard method -- returns
+// nonsense when arg is out of range.  Runs in .24ns vs. 8.7ns for 64bit which is faster
+// than math32.Exp actually.
+func FastExp(x float32) float32 {
+	if x <= -88.76731 { // this doesn't add anything and -exp is main use-case anyway
+		return 0
+	}
+	i := int32(12102203*x) + 127*(1<<23)
+	m := i >> 7 & 0xFFFF // copy mantissa
+	i += (((((((((((3537 * m) >> 16) + 13668) * m) >> 18) + 15817) * m) >> 14) - 80470) * m) >> 11)
+	return math.Float32frombits(uint32(i))
+}
---
+++ fastexp_test.go
@@ -0,0 +1,41 @@
+// Copyright 2021 The GoKi Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style
+// license that can be found in the LICENSE file.
+
+package mat32
+
+import (
+	"fmt"
+	"math"
+	"testing"
+
+	"github.com/chewxy/math32"
+)
+
+func TestFastExp(t *testing.T) {
+	for x := float32(-87); x <= 88.43114; x += 1.0e-01 {
+		fx := FastExp(x)
+		sx := float32(math.Exp(float64(x)))
+		if math32.Abs((fx-sx)/sx) > 1.0e-5 {
+			fmt.Printf("Exp4 at: %g  err from cx: %g  vs  %g\n", x, fx, sx)
+		}
+	}
+}
+
+func BenchmarkFastExp(b *testing.B) {
+	for n := 0; n < b.N; n++ {
+		FastExp(float32(n%40 - 20))
+	}
+}
+
+func BenchmarkExpStd64(b *testing.B) {
+	for n := 0; n < b.N; n++ {
+		math.Exp(float64(n%40 - 20))
+	}
+}
+
+func BenchmarkExp32(b *testing.B) {
+	for n := 0; n < b.N; n++ {
+		math32.Exp(float32(n%40 - 20))
+	}
+}
