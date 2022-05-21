6f03ad724296237802d964b61cfa94c1b965ca6a
Randall C. O'Reilly <randy.oreilly@colorado.edu>
2022-05-21 00:50:13 -0700

    better impl of color

--- color.go
+++ color.go
@@ -6,33 +6,36 @@ package mat32
 
 import "image/color"
 
-// NewGoColor3 returns a Vec3 from Go standard color.Color
-func NewGoColor3(clr color.Color) Vec3 {
+// NewVec3Color returns a Vec3 from Go standard color.Color
+// (R,G,B components only)
+func NewVec3Color(clr color.Color) Vec3 {
 	v3 := Vec3{}
-	SetGoColor3(&v3, clr)
+	v3.SetColor(clr)
 	return v3
 }
 
-// SetGoColor3 sets a Vec3 from Go standard color.Color
-func SetGoColor3(v3 *Vec3, clr color.Color) {
+// SetColor sets from Go standard color.Color
+// (R,G,B components only)
+func (v *Vec3) SetColor(clr color.Color) {
 	r, g, b, _ := clr.RGBA()
-	v3.X = float32(r) / 0xffff
-	v3.Y = float32(g) / 0xffff
-	v3.Z = float32(b) / 0xffff
+	v.X = float32(r) / 0xffff
+	v.Y = float32(g) / 0xffff
+	v.Z = float32(b) / 0xffff
 }
 
-// NewGoColor4 returns a Vec4 from Go standard color.Color
-func NewGoColor4(clr color.Color) Vec4 {
+// NewVec4Color returns a Vec4 from Go standard color.Color
+// (full R,G,B,A components)
+func NewVec4Color(clr color.Color) Vec4 {
 	v4 := Vec4{}
-	SetGoColor4(&v4, clr)
+	v4.SetColor(clr)
 	return v4
 }
 
-// SetGoColor4 sets a Vec4 from Go standard color.Color
-func SetGoColor4(v4 *Vec4, clr color.Color) {
+// SetColor sets a Vec4 from Go standard color.Color
+func (v *Vec4) SetColor(clr color.Color) {
 	r, g, b, a := clr.RGBA()
-	v4.X = float32(r) / 0xffff
-	v4.Y = float32(g) / 0xffff
-	v4.Z = float32(b) / 0xffff
-	v4.W = float32(a) / 0xffff
+	v.X = float32(r) / 0xffff
+	v.Y = float32(g) / 0xffff
+	v.Z = float32(b) / 0xffff
+	v.W = float32(a) / 0xffff
 }
