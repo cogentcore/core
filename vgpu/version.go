291aeb3fb9a2de47db093aa75a8d1a7f16e520a1
Randall C. O'Reilly <randy.oreilly@colorado.edu>
2022-08-12 14:11:33 -0700

    v1.0.5 release -- version.go updated

--- vgpu/version.go
+++ vgpu/version.go
@@ -3,7 +3,7 @@
 package vgpu
 
 const (
-	Version     = "v1.0.4"
-	GitCommit   = "c64a6ce"          // the commit JUST BEFORE the release
-	VersionDate = "2022-06-10 11:50" // UTC
+	Version     = "v1.0.5"
+	GitCommit   = "9edc904"          // the commit JUST BEFORE the release
+	VersionDate = "2022-08-12 21:11" // UTC
 )
