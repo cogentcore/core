5bbdbc2ffb05c665dfb3b012bbccb492e93bcd26
Randall C. O'Reilly <randy.oreilly@colorado.edu>
2023-01-15 23:38:57 -0800

    v1.0.14 release -- version.go updated

--- version.go
+++ version.go
@@ -3,8 +3,8 @@
 package mat32
 
 const (
-	Version = "v1.0.13"
-	GitCommit = "8e5e626" // the commit JUST BEFORE the release
-	VersionDate = "2023-01-05 19:40" // UTC
+	Version = "v1.0.14"
+	GitCommit = "1219b65" // the commit JUST BEFORE the release
+	VersionDate = "2023-01-16 07:38" // UTC
 )
 
