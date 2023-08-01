ab5cf7efa160e6859a2f48f2e5d6691d2a42c061
Kai O'Reilly <koreilly5297@gmail.com>
2023-08-01 17:05:17 -0600

    started making changes to color scheme to be more like material (not working yet)

--- gi/color.go
+++ gi/color.go
@@ -71,11 +71,23 @@ var TypeColorSchemeTypes = kit.Enums.AddEnumAltLower(ColorSchemesN, kit.NotBitFl
 // ColorScheme contains the colors for
 // one color scheme (ex: light or dark).
 type ColorScheme struct {
-	Text       gist.Color `desc:"default text color"`
-	Background gist.Color `desc:"default background color"`
-	Primary    gist.Color `desc:"the primary button color"`
-	Secondary  gist.Color `desc:"the secondary button color"`
-	Accent     gist.Color `desc:"the accent color, typically used for selected elements"`
-	Success    gist.Color `desc:"a semantic color that indicates success"`
-	Failure    gist.Color `desc:"a semantic color that indicates failure or danger"`
+	Primary            gist.Color `desc:"Primary is the base primary color applied to important elements"`
+	OnPrimary          gist.Color `desc:"OnPrimary is the color applied to content on top of Primary. It defaults to the contrast color of Primary."`
+	PrimaryContainer   gist.Color `desc:"PrimaryContainer is the color applied to elements with less emphasis than Primary"`
+	OnPrimaryContainer gist.Color `desc:"OnPrimaryContainer is the color applied to content on top of PrimaryContainer. It defaults to the contrast color of PrimaryContainer."`
+
+	Secondary            gist.Color `desc:"Secondary is the base secondary color applied to less important elements"`
+	OnSecondary          gist.Color `desc:"OnSecondary is the color applied to content on top of Secondary. It defaults to the contrast color of Secondary."`
+	SecondaryContainer   gist.Color `desc:"SecondaryContainer is the color applied to elements with less emphasis than Secondary"`
+	OnSecondaryContainer gist.Color `desc:"OnSecondaryContainer is the color applied to content on top of SecondaryContainer. It defaults to the contrast color of SecondaryContainer."`
+
+	Tertiary            gist.Color `desc:"Tertiary is the base tertiary color applied as an accent to highlight elements and screate contrast between other colors"`
+	OnTertiary          gist.Color `desc:"OnTertiary is the color applied to content on top of Tertiary. It defaults to the contrast color of Tertiary."`
+	TertiaryContainer   gist.Color `desc:"TertiaryContainer is the color applied to elements with less emphasis than Tertiary"`
+	OnTertiaryContainer gist.Color `desc:"OnTertiaryContainer is the color applied to content on top of TertiaryContainer. It defaults to the contrast color of TertiaryContainer."`
+
+	Error            gist.Color `desc:"Error is the base error color applied to elements that indicate an error or danger"`
+	OnError          gist.Color `desc:"OnError is the color applied to content on top of Error. It defaults to the contrast color of Error."`
+	ErrorContainer   gist.Color `desc:"ErrorContainer is the color applied to elements with less emphasis than Error"`
+	OnErrorContainer gist.Color `desc:"OnErrorContainer is the color applied to content on top of ErrorContainer. It defaults to the contrast color of ErrorContainer."`
 }
