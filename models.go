74a56c20f639ffa746b6928e9e8e3e6c66ef03f9
Kai O'Reilly <koreilly5297@gmail.com>
2023-08-18 16:11:27 -0700

    added models

---
+++ models.go
@@ -0,0 +1,68 @@
+// Copyright (c) 2023, The GoKi Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style
+// license that can be found in the LICENSE file.
+
+package colors
+
+import "image/color"
+
+// RGBAf32 stores alpha-premultiplied RGBA values in float32 0..1 normalized
+// format -- more useful for converting to other spaces
+type RGBAf32 struct {
+	R, G, B, A float32
+}
+
+// Implements the color.Color interface
+func (c RGBAf32) RGBA() (r, g, b, a uint32) {
+	r = uint32(c.R*65535.0 + 0.5)
+	g = uint32(c.G*65535.0 + 0.5)
+	b = uint32(c.B*65535.0 + 0.5)
+	a = uint32(c.A*65535.0 + 0.5)
+	return
+}
+
+// NRGBAf32 stores non-alpha-premultiplied RGBA values in float32 0..1
+// normalized format -- more useful for converting to other spaces
+type NRGBAf32 struct {
+	R, G, B, A float32
+}
+
+// Implements the color.Color interface
+func (c NRGBAf32) RGBA() (r, g, b, a uint32) {
+	r = uint32(c.R*c.A*65535.0 + 0.5)
+	g = uint32(c.G*c.A*65535.0 + 0.5)
+	b = uint32(c.B*c.A*65535.0 + 0.5)
+	a = uint32(c.A*65535.0 + 0.5)
+	return
+}
+
+var (
+	RGBAf32Model  color.Model = color.ModelFunc(rgbaf32Model)
+	NRGBAf32Model color.Model = color.ModelFunc(nrgbaf32Model)
+)
+
+func rgbaf32Model(c color.Color) color.Color {
+	if _, ok := c.(RGBAf32); ok {
+		return c
+	}
+	r, g, b, a := c.RGBA()
+	return RGBAf32{float32(r) / 65535.0, float32(g) / 65535.0, float32(b) / 65535.0, float32(a) / 65535.0}
+}
+
+func nrgbaf32Model(c color.Color) color.Color {
+	if _, ok := c.(NRGBAf32); ok {
+		return c
+	}
+	r, g, b, a := c.RGBA()
+	if a > 0 {
+		// Since color.Color is alpha pre-multiplied, we need to divide the
+		// RGB values by alpha again in order to get back the original RGB.
+		r *= 0xffff
+		r /= a
+		g *= 0xffff
+		g /= a
+		b *= 0xffff
+		b /= a
+	}
+	return NRGBAf32{float32(r) / 65535.0, float32(g) / 65535.0, float32(b) / 65535.0, float32(a) / 65535.0}
+}
