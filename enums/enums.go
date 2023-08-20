4c0528fa3659ded104346c3423c03ea9fe9b0220
Kai O'Reilly <koreilly5297@gmail.com>
2023-08-20 10:42:07 -0700

    added Int64, SetInt64, Desc, and Desc methods to Enumer and updated arg types (close #2)

--- enums/enums.go
+++ enums/enums.go
@@ -5,21 +5,38 @@ package enums
 import "fmt"
 
 // Enumer is the interface that all enum types satisfy.
-// Enum types must be convertable to and from strings,
-// and must be able to return all possible enum values
-// and string representations.
+// Enum types must be convertable to and from strings
+// and int64s, and must be able to return all possible
+// enum values and string representations.
 type Enumer interface {
 	fmt.Stringer
 	// SetString sets the enum value from its
 	// string representation, and returns an
 	// error if the string is invalid.
 	SetString(s string) error
+	// Int64 returns the enum value as an int64.
+	Int64() int64
+	// SetInt64 sets the enum value from an int64.
+	SetInt64(i int64)
+	// Desc returns the description of the enum value.
+	Desc() string
 	// Values returns all possible values this
-	// enum type has.
-	Values() []int64
+	// enum type has. This slice will be in the
+	// same order as those returned by [Enumer.Strings]
+	// and [Enumer.Descs].
+	Values() []Enumer
 	// Strings returns the string encodings of
 	// all possible values this enum type has.
+	// This slice will be in the same order as
+	// those returned by [Enumer.Values]
+	// and [Enumer.Descs].
 	Strings() []string
+	// Descs returns the descriptions of all
+	// possible values this enum type has.
+	// This slice will be in the same order as
+	// those returned by [Enumer.Values]
+	// and [Enumer.Strings].
+	Descs() []string
 }
 
 // BitEnumer is the interface that all bit flag enum types
@@ -30,8 +47,8 @@ type BitEnumer interface {
 	Enumer
 	// HasBitFlag returns whether these flags
 	// have the given flag set.
-	HasBitFlag(f int64) bool
+	HasBitFlag(f BitEnumer) bool
 	// SetBitFlag sets the value of the given
 	// flag in these flags to the given value.
-	SetBitFlag(f int64, b bool)
+	SetBitFlag(f BitEnumer, b bool)
 }
