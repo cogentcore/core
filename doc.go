19dcaedc6ae76bbcf278a7ae67f914c66ea157b8
Kai O'Reilly <koreilly5297@gmail.com>
2023-08-21 16:47:17 -0700

    added doc and makefile

---
+++ doc.go
@@ -0,0 +1,7 @@
+// Copyright (c) 2023, The GoKi Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style
+// license that can be found in the LICENSE file.
+
+// Package enums provides utilities for creating
+// and using enumerated types in Go.
+package enums
