9e9882834f4ad23ed58ce068d16cbcfec889d1db
Kai O'Reilly <koreilly5297@gmail.com>
2023-08-21 20:06:17 -0700

    v0.9.1 release -- version.go updated

--- version.go
+++ version.go
@@ -3,8 +3,8 @@
 package enums
 
 const (
-	Version = "v0.9.0"
-	GitCommit = "19dcaed" // the commit JUST BEFORE the release
-	VersionDate = "2023-08-21 23:47" // UTC
+	Version = "v0.9.1"
+	GitCommit = "1c2535d" // the commit JUST BEFORE the release
+	VersionDate = "2023-08-22 03:06" // UTC
 )
 
