666eb458da8e9fe037089c5f16cc43d678cc1132
Kai O'Reilly <koreilly5297@gmail.com>
2023-08-25 20:57:59 -0700

    added init function

--- tools/tools.go
+++ tools/tools.go
@@ -6,3 +6,22 @@
 // for developing apps and libraries using
 // the GoKi framework.
 package tools
+
+import (
+	"fmt"
+	"os"
+)
+
+// Init initializes the ".goki" directory
+// and a "config.toml" file inside it.
+func Init() error {
+	err := os.Mkdir(".goki", 0750)
+	if err != nil {
+		return fmt.Errorf("error creating %q directory: %w", ".goki", err)
+	}
+	err = os.WriteFile(".goki/config.toml", []byte(`name = "goki"`), 0666)
+	if err != nil {
+		return fmt.Errorf("error writing to configuration file: %w", err)
+	}
+	return nil
+}
