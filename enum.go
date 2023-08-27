51dbddf35fdd94490839cda948dbaf1da98a57a5
Kai O'Reilly <koreilly5297@gmail.com>
2023-08-26 17:05:49 -0700

    use new enums package for enum

--- enum.go
+++ enum.go
@@ -4,12 +4,10 @@
 
 package gear
 
-import (
-	"github.com/goki/ki/kit"
-)
+//go:generate enumgen
 
 // TestEnum is an enum type for testing
-type TestEnum int32
+type TestEnum int32 //enums:enum
 
 // note: we need to add the Layer extension to avoid naming
 // conflicts between layer, projection and other things.
@@ -18,20 +16,4 @@ const (
 	TestValue1 TestEnum = iota
 
 	TestValue2
-
-	TestEnumN
 )
-
-// important: must use 'go install github.com/goki/stringer@latest' with FromString method
-
-//go:generate stringer -type=TestEnum
-
-// This registers the enum with Kit which then allows full set / get from strings.
-var KiT_TestEnum = kit.Enums.AddEnum(TestEnumN, kit.NotBitFlag, nil)
-
-func (ev TestEnum) MarshalJSON() ([]byte, error)  { return kit.EnumMarshalJSON(ev) }
-func (ev *TestEnum) UnmarshalJSON(b []byte) error { return kit.EnumUnmarshalJSON(ev, b) }
-
-// TOML uses Text interface
-func (ev TestEnum) MarshalText() ([]byte, error)  { return kit.EnumMarshalText(ev) }
-func (ev *TestEnum) UnmarshalText(b []byte) error { return kit.EnumUnmarshalText(ev, b) }
--- go.mod
+++ go.mod
@@ -6,6 +6,7 @@ require (
 	github.com/goki/gi v1.3.22
 	github.com/goki/ki v1.1.16
 	github.com/iancoleman/strcase v0.3.0
+	goki.dev/enums v0.9.2
 	golang.org/x/exp v0.0.0-20230817173708-d852ddb80c63
 )
 
