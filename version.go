11d28f3c377c83ecfadf2df1fcd4c8762428534b
Kai O'Reilly <koreilly5297@gmail.com>
2023-08-28 17:55:46 -0700

    better version comments

--- version.go
+++ version.go
@@ -3,8 +3,7 @@
 package main
 
 const (
-	Version = "v0.1.4" // Version is the version of this package being used
-	GitCommit = "3caac96" // GitCommit is the commit just before the release
+	Version     = "v0.1.4"           // Version is the version of this package being used
+	GitCommit   = "3caac96"          // GitCommit is the commit just before the release
 	VersionDate = "2023-08-29 00:53" // VersionDate is the date-time of the release in UTC (in the format 'YYYY-MM-DD HH:MM', which is the Go format '2006-01-02 15:04')
 )
-
