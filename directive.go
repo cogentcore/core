38d92a9bcec4afc3e33a299bedbe1b760c0177c7
Kai O'Reilly <koreilly5297@gmail.com>
2023-09-02 11:38:22 -0700

    use shellwords for directive

--- directive.go
+++ directive.go
@@ -4,15 +4,20 @@
 
 package grease
 
-import "strings"
+import (
+	"fmt"
+	"strings"
+
+	"github.com/mattn/go-shellwords"
+)
 
 // ParseDirective parses a comment directive and returns
 // the tool, the directive, and true if the comment is
 // a directive, and "", "", false if it is not. Directives
 // are of the following form (the slashes are optional):
 //
-//	//tool:directive...
-func ParseDirective(comment string) (tool, directive string, has bool) {
+//	//tool:directive arg0 -arg1=go ...
+func ParseDirective(comment string) (tool, directive string, args []string, has bool, err error) {
 	comment = strings.TrimPrefix(comment, "//")
 	before, after, found := strings.Cut(comment, ":")
 	if !found {
@@ -20,9 +25,14 @@ func ParseDirective(comment string) (tool, directive string, has bool) {
 	}
 	has = true
 	tool = before
-	fields := strings.Fields(after)
-	if len(fields) != 0 {
-		directive = fields[0]
+	args, err = shellwords.Parse(after)
+	if err != nil {
+		err = fmt.Errorf("error parsing args %w", err)
+		return
+	}
+	if len(args) > 0 {
+		directive = args[0]
+		args = args[1:]
 	}
 	return
 }
--- go.mod
+++ go.mod
@@ -5,6 +5,7 @@ go 1.18
 require (
 	github.com/goki/ki v1.1.16
 	github.com/iancoleman/strcase v0.3.0
+	github.com/mattn/go-shellwords v1.0.12
 	goki.dev/enums v0.9.2
 	golang.org/x/exp v0.0.0-20230817173708-d852ddb80c63
 )
