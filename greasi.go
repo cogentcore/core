ea72618aa24f6aad2b21124626a90e60128f0d80
Kai O'Reilly <koreilly5297@gmail.com>
2023-09-02 09:21:49 -0700

    copied over from grease

---
+++ greasi.go
@@ -0,0 +1,43 @@
+// Copyright (c) 2023, The GoKi Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style
+// license that can be found in the LICENSE file.
+
+package greasi
+
+import (
+	"fmt"
+
+	"goki.dev/grease"
+)
+
+// Run runs the given app with the given default
+// configuration file paths. It is similar to
+// [grease.Run], but it also runs the GUI if no
+// arguments were provided. The app should be
+// a pointer, and configuration options should
+// be defined as fields on the app type. Also,
+// commands should be defined as methods on the
+// app type with the suffix "Cmd"; for example,
+// for a command named "build", there should be
+// the method:
+//
+//	func (a *App) BuildCmd() error
+//
+// Run uses [os.Args] for its arguments.
+func Run(app any, defaultFile ...string) error {
+	leftovers, err := grease.Config(app, defaultFile...)
+	if err != nil {
+		return fmt.Errorf("error configuring app: %w", err)
+	}
+	if len(leftovers) == 0 {
+		GUI(app)
+		return nil
+	}
+	cmd := leftovers[0]
+
+	err = grease.RunCommand(app, cmd)
+	if err != nil {
+		return fmt.Errorf("error running command %q: %w", cmd, err)
+	}
+	return nil
+}
---
+++ gui.go
@@ -0,0 +1,77 @@
+// Copyright (c) 2023, The GoKi Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style
+// license that can be found in the LICENSE file.
+
+package greasi
+
+import (
+	"fmt"
+
+	"github.com/goki/gi/gi"
+	"github.com/goki/gi/gimain"
+	"github.com/goki/gi/giv"
+	"github.com/goki/ki/ki"
+	"goki.dev/grease"
+)
+
+// GUI starts the GUI for the given
+// Grease app, which must be passed as
+// a pointer.
+func GUI(app any) {
+	gimain.Main(func() {
+		mainrun(app)
+	})
+}
+
+func mainrun(app any) {
+	gi.SetAppName(grease.AppName)
+	gi.SetAppAbout(grease.AppAbout)
+
+	win := gi.NewMainWindow(grease.AppName, grease.AppTitle, 1024, 768)
+	vp := win.WinViewport2D()
+	updt := vp.UpdateStart()
+	mfr := win.SetMainFrame()
+
+	sv := giv.AddNewStructView(mfr, "sv")
+	sv.Viewport = vp
+	sv.SetStruct(app)
+
+	// Main Menu
+
+	appnm := gi.AppName()
+	mmen := win.MainMenu
+	mmen.ConfigMenus([]string{appnm, "File", "Edit", "Window"})
+
+	amen := win.MainMenu.ChildByName(appnm, 0).(*gi.Action)
+	amen.Menu.AddAppMenu(win)
+
+	fmen := win.MainMenu.ChildByName("File", 0).(*gi.Action)
+	fmen.Menu.AddAction(gi.ActOpts{Label: "New", ShortcutKey: gi.KeyFunMenuNew},
+		fmen.This(), func(recv, send ki.Ki, sig int64, data any) {
+			fmt.Println("File:New menu action triggered")
+		})
+	fmen.Menu.AddAction(gi.ActOpts{Label: "Open", ShortcutKey: gi.KeyFunMenuOpen},
+		fmen.This(), func(recv, send ki.Ki, sig int64, data any) {
+			fmt.Println("File:Open menu action triggered")
+		})
+	fmen.Menu.AddAction(gi.ActOpts{Label: "Save", ShortcutKey: gi.KeyFunMenuSave},
+		fmen.This(), func(recv, send ki.Ki, sig int64, data any) {
+			fmt.Println("File:Save menu action triggered")
+			grease.Save(app, grease.ConfigFile)
+		})
+	fmen.Menu.AddAction(gi.ActOpts{Label: "Save As..", ShortcutKey: gi.KeyFunMenuSaveAs},
+		fmen.This(), func(recv, send ki.Ki, sig int64, data any) {
+			fmt.Println("File:SaveAs menu action triggered")
+		})
+	fmen.Menu.AddSeparator("csep")
+	fmen.Menu.AddAction(gi.ActOpts{Label: "Close Window", ShortcutKey: gi.KeyFunWinClose},
+		win.This(), func(recv, send ki.Ki, sig int64, data any) {
+			win.CloseReq()
+		})
+
+	emen := win.MainMenu.ChildByName("Edit", 1).(*gi.Action)
+	emen.Menu.AddCopyCutPaste(win)
+
+	vp.UpdateEndNoSig(updt)
+	win.StartEventLoop()
+}
