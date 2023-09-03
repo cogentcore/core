8c171334a60b6dfad9e7ca3fd2a80bedda90ed40
Kai O'Reilly <koreilly5297@gmail.com>
2023-09-02 19:18:33 -0700

    run grease app

--- cmd/enumgen/enumgen.go
+++ cmd/enumgen/enumgen.go
@@ -7,50 +7,58 @@
 package main
 
 import (
-	"flag"
 	"fmt"
-	"log"
-	"os"
 
 	"goki.dev/enums/enumgen"
+	"goki.dev/grease"
 )
 
-// the configuration object we use
-var config enumgen.Config
-
-var ()
-
 func main() {
-	flag.StringVar(&config.Dir, "dir", ".", "the source directory to look for enums in")
-	flag.StringVar(&config.Output, "output", "enumgen.go", "the file name of the output file")
-	flag.BoolVar(&config.SQL, "sql", false, "whether to generate methods that implement the SQL Scanner and Valuer interfaces")
-	flag.BoolVar(&config.Text, "text", true, "whether to generate text marshaling methods")
-	flag.BoolVar(&config.JSON, "json", false, "whether to generate JSON marshaling methods (note that text marshaling methods will also work for JSON, so this should be unnecessary in almost all cases; see the text option)")
-	flag.BoolVar(&config.YAML, "yaml", false, "whether to generate YAML marshaling methods")
-	flag.BoolVar(&config.GQLGEN, "gqlgen", false, "whether to generate GraphQL marshaling methods for gqlgen")
-	flag.StringVar(&config.Transform, "transform", "noop", "if specified, the enum item transformation method (eg: snake_case)")
-	flag.StringVar(&config.TrimPrefix, "trimprefix", "", "if specified, the prefix to trim from each item")
-	flag.StringVar(&config.AddPrefix, "addprefix", "", "if specified, the prefix to add to each item")
-	flag.BoolVar(&config.LineComment, "linecomment", false, "whether to use line comment text as printed text when present")
-	flag.StringVar(&config.Comment, "comment", "", "a comment to include at the top of the generated code")
-
-	log.SetPrefix("enumgen")
-	flag.Usage = Usage
-	flag.Parse()
-	config.Defaults()
-	err := enumgen.Generate(config)
+	grease.AppName = "enumgen"
+	grease.AppTitle = "Enumgen"
+	grease.AppAbout = "Enumgen is a tool that generates helpful methods for Go enums."
+	err := grease.Run(enumgen.TheApp, "enumgen.toml")
 	if err != nil {
-		fmt.Println("error: " + err.Error())
+		fmt.Println(err)
 	}
 }
 
-// Usage is a replacement usage function for the flags package.
-func Usage() {
-	_, _ = fmt.Fprintf(os.Stderr, "Enumgen is a tool to generate Go code that adds helpful methods to Go enums.\n")
-	_, _ = fmt.Fprintf(os.Stderr, "Usage of %s:\n", os.Args[0])
-	_, _ = fmt.Fprintf(os.Stderr, "\tenumgen [flags]\n")
-	_, _ = fmt.Fprintf(os.Stderr, "For more information, see:\n")
-	_, _ = fmt.Fprintf(os.Stderr, "\thttps://goki.dev/enums\n")
-	_, _ = fmt.Fprintf(os.Stderr, "Flags:\n")
-	flag.PrintDefaults()
-}
+// // the configuration object we use
+// var config enumgen.Config
+
+// var ()
+
+// func main() {
+// 	flag.StringVar(&config.Dir, "dir", ".", "the source directory to look for enums in")
+// 	flag.StringVar(&config.Output, "output", "enumgen.go", "the file name of the output file")
+// 	flag.BoolVar(&config.SQL, "sql", false, "whether to generate methods that implement the SQL Scanner and Valuer interfaces")
+// 	flag.BoolVar(&config.Text, "text", true, "whether to generate text marshaling methods")
+// 	flag.BoolVar(&config.JSON, "json", false, "whether to generate JSON marshaling methods (note that text marshaling methods will also work for JSON, so this should be unnecessary in almost all cases; see the text option)")
+// 	flag.BoolVar(&config.YAML, "yaml", false, "whether to generate YAML marshaling methods")
+// 	flag.BoolVar(&config.GQLGEN, "gqlgen", false, "whether to generate GraphQL marshaling methods for gqlgen")
+// 	flag.StringVar(&config.Transform, "transform", "noop", "if specified, the enum item transformation method (eg: snake_case)")
+// 	flag.StringVar(&config.TrimPrefix, "trimprefix", "", "if specified, the prefix to trim from each item")
+// 	flag.StringVar(&config.AddPrefix, "addprefix", "", "if specified, the prefix to add to each item")
+// 	flag.BoolVar(&config.LineComment, "linecomment", false, "whether to use line comment text as printed text when present")
+// 	flag.StringVar(&config.Comment, "comment", "", "a comment to include at the top of the generated code")
+
+// 	log.SetPrefix("enumgen")
+// 	flag.Usage = Usage
+// 	flag.Parse()
+// 	config.Defaults()
+// 	err := enumgen.Generate(config)
+// 	if err != nil {
+// 		fmt.Println("error: " + err.Error())
+// 	}
+// }
+
+// // Usage is a replacement usage function for the flags package.
+// func Usage() {
+// 	_, _ = fmt.Fprintf(os.Stderr, "Enumgen is a tool to generate Go code that adds helpful methods to Go enums.\n")
+// 	_, _ = fmt.Fprintf(os.Stderr, "Usage of %s:\n", os.Args[0])
+// 	_, _ = fmt.Fprintf(os.Stderr, "\tenumgen [flags]\n")
+// 	_, _ = fmt.Fprintf(os.Stderr, "For more information, see:\n")
+// 	_, _ = fmt.Fprintf(os.Stderr, "\thttps://goki.dev/enums\n")
+// 	_, _ = fmt.Fprintf(os.Stderr, "Flags:\n")
+// 	flag.PrintDefaults()
+// }
--- go.mod
+++ go.mod
@@ -1,13 +1,17 @@
 module goki.dev/enums
 
-go 1.18
+go 1.21.0
 
 require (
 	github.com/iancoleman/strcase v0.3.0
-	golang.org/x/tools v0.12.0
+	goki.dev/grease v0.8.7
+	golang.org/x/tools v0.12.1-0.20230818130535-1517d1a3ba60
 )
 
 require (
+	github.com/BurntSushi/toml v1.3.2 // indirect
+	github.com/mattn/go-shellwords v1.0.12 // indirect
+	goki.dev/ki/v2 v2.0.0-dev0.0.3 // indirect
 	golang.org/x/mod v0.12.0 // indirect
 	golang.org/x/sys v0.12.0 // indirect
 )
