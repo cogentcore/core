41b0cacd5746c5903bc6ca8c0c5b3beec1d99ac2
Kai O'Reilly <koreilly5297@gmail.com>
2023-09-03 12:10:52 -0700

    added generate

---
+++ generate/generate.go
@@ -0,0 +1,17 @@
+// Copyright (c) 2023, The GoKi Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style
+// license that can be found in the LICENSE file.
+
+// Package generate provides the generation
+// of useful methods, variables, and constants
+// for GoKi code.
+package generate
+
+import "goki.dev/goki/config"
+
+// Generate is the main entry point to code generation
+// that does all of the generation according to the
+// given config info.
+func Generate(c *config.Config) error {
+	return nil
+}
