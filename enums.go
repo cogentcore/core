3e18ff27334b7bdc13febb8523895a61ff748198
Kai O'Reilly <koreilly5297@gmail.com>
2023-09-03 09:18:50 -0700

    added EnumSetter and BitFlagSetter interfaces

--- enums.go
+++ enums.go
@@ -9,21 +9,15 @@ package enums
 import "fmt"
 
 // Enum is the interface that all enum types satisfy.
-// Enum types must be convertable to and from strings
-// and int64s, must be able to return a description of
-// their value, must be able to report if they are valid,
-// and must be able to return all possible enum values
+// Enum types must be convertable to strings and int64s,
+// must be able to return a description of their value,
+// must be able to report if they are valid, and must
+// be able to return all possible enum values
 // and string and description representations.
 type Enum interface {
 	fmt.Stringer
-	// SetString sets the enum value from its
-	// string representation, and returns an
-	// error if the string is invalid.
-	SetString(s string) error
 	// Int64 returns the enum value as an int64.
 	Int64() int64
-	// SetInt64 sets the enum value from an int64.
-	SetInt64(i int64)
 	// Desc returns the description of the enum value.
 	Desc() string
 	// IsValid returns whether the value is a
@@ -45,15 +39,38 @@ type Enum interface {
 	Descs() []string
 }
 
+// EnumSetter is an expanded interface that all pointers
+// to enum types satisfy. Pointers to enum types must
+// satisfy all of the methods of [Enum], and must also
+// be settable from strings and int64s.
+type EnumSetter interface {
+	Enum
+	// SetString sets the enum value from its
+	// string representation, and returns an
+	// error if the string is invalid.
+	SetString(s string) error
+	// SetInt64 sets the enum value from an int64.
+	SetInt64(i int64)
+}
+
 // BitFlag is the interface that all bit flag enum types
 // satisfy. Bit flag enum types support all of the operations
-// that standard enums do, and additionally can get and set
-// bit flags.
+// that standard enums do, and additionally can check if they
+// have a given bit flag.
 type BitFlag interface {
 	Enum
 	// HasFlag returns whether these flags
 	// have the given flag set.
 	HasFlag(f BitFlag) bool
+}
+
+// BitFlagSetter is an expanded interface that all pointers
+// to bit flag enum types satisfy. Pointers to bit flag
+// enum types must  satisfy all of the methods of [EnumSetter]
+// and [BitFlag], and must also be able to set a given bit flag.
+type BitFlagSetter interface {
+	EnumSetter
+	BitFlag
 	// SetFlag sets the value of the given
 	// flags in these flags to the given value.
 	SetFlag(on bool, f ...BitFlag)
