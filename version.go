eca749d76462af660f8853ddd4a41b7890aa413a
Kai O'Reilly <koreilly5297@gmail.com>
2023-09-09 12:28:10 -0700

    updated version to v0.9.12

--- version.go
+++ version.go
@@ -4,9 +4,9 @@ package enums
 
 const (
 	// Version is the version of this package being used
-	Version = "v0.9.11"
+	Version = "v0.9.12"
 	// GitCommit is the commit just before the latest version commit
-	GitCommit = "8bbc001"
+	GitCommit = "e9f912f"
 	// VersionDate is the date-time of the latest version commit in UTC (in the format 'YYYY-MM-DD HH:MM', which is the Go format '2006-01-02 15:04')
-	VersionDate = "2023-09-09 18:30"
+	VersionDate = "2023-09-09 19:28"
 )
