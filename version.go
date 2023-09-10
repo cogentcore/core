35ec1c0f3d2914d883cbc24e87430f0b5d4197a0
Kai O'Reilly <koreilly5297@gmail.com>
2023-09-09 21:26:04 -0700

    updated version to v0.3.1

--- version.go
+++ version.go
@@ -4,9 +4,9 @@ package gengo
 
 const (
 	// Version is the version of this package being used
-	Version = "v0.3.0"
+	Version = "v0.3.1"
 	// GitCommit is the commit just before the latest version commit
-	GitCommit = "72aed99"
+	GitCommit = "180f181"
 	// VersionDate is the date-time of the latest version commit in UTC (in the format 'YYYY-MM-DD HH:MM', which is the Go format '2006-01-02 15:04')
-	VersionDate = "2023-09-10 03:54"
+	VersionDate = "2023-09-10 04:26"
 )
