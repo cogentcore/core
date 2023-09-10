19fcd496d63025f81e2297e8a6d4d04e186ce734
Kai O'Reilly <koreilly5297@gmail.com>
2023-09-09 22:08:07 -0700

    return gti.Directive in ParseDirective

--- directive.go
+++ directive.go
@@ -9,6 +9,7 @@ import (
 	"strings"
 
 	"github.com/mattn/go-shellwords"
+	"goki.dev/gti"
 )
 
 // ParseDirective parses a comment directive and returns
@@ -17,22 +18,22 @@ import (
 // are of the following form (the slashes are optional):
 //
 //	//tool:directive arg0 -arg1=go ...
-func ParseDirective(comment string) (tool, directive string, args []string, has bool, err error) {
+func ParseDirective(comment string) (directive gti.Directive, has bool, err error) {
 	comment = strings.TrimPrefix(comment, "//")
 	before, after, found := strings.Cut(comment, ":")
 	if !found {
 		return
 	}
 	has = true
-	tool = before
-	args, err = shellwords.Parse(after)
+	directive.Tool = before
+	directive.Args, err = shellwords.Parse(after)
 	if err != nil {
 		err = fmt.Errorf("error parsing args %w", err)
 		return
 	}
-	if len(args) > 0 {
-		directive = args[0]
-		args = args[1:]
+	if len(directive.Args) > 0 {
+		directive.Directive = directive.Args[0]
+		directive.Args = directive.Args[1:]
 	}
 	return
 }
--- go.mod
+++ go.mod
@@ -1,11 +1,12 @@
 module goki.dev/grease
 
-go 1.18
+go 1.21.0
 
 require (
 	github.com/iancoleman/strcase v0.3.0
 	github.com/mattn/go-shellwords v1.0.12
 	goki.dev/enums v0.9.4
+	goki.dev/gti v0.0.0-20230910050553-9577e693612d
 	goki.dev/ki/v2 v2.0.0-dev0.0.3
 	golang.org/x/exp v0.0.0-20230817173708-d852ddb80c63
 )
@@ -13,4 +14,5 @@ require (
 require (
 	github.com/BurntSushi/toml v1.3.2 // indirect
 	github.com/jinzhu/copier v0.4.0 // indirect
+	goki.dev/ordmap v0.5.0 // indirect
 )
