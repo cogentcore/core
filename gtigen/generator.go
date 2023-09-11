0876fc732780e2430e9efebb5f495927ca95c7b1
Kai O'Reilly <koreilly5297@gmail.com>
2023-09-10 20:13:47 -0700

    store methods, cleaned up code

--- gtigen/generator.go
+++ gtigen/generator.go
@@ -6,13 +6,8 @@ package gtigen
 
 import (
 	"bytes"
-	"errors"
 	"fmt"
 	"go/ast"
-	"go/constant"
-	"go/token"
-	"go/types"
-	"html"
 	"log"
 	"strings"
 	"text/template"
@@ -20,17 +15,19 @@ import (
 	"goki.dev/gengo"
 	"goki.dev/grease"
 	"goki.dev/gti"
+	"goki.dev/ordmap"
 	"golang.org/x/tools/go/packages"
 )
 
 // Generator holds the state of the generator.
 // It is primarily used to buffer the output.
 type Generator struct {
-	Config *Config             // The configuration information
-	Buf    bytes.Buffer        // The accumulated output.
-	Pkgs   []*packages.Package // The packages we are scanning.
-	Pkg    *packages.Package   // The packages we are currently on.
-	Types  []*Type             // The enum types
+	Config  *Config                        // The configuration information
+	Buf     bytes.Buffer                   // The accumulated output.
+	Pkgs    []*packages.Package            // The packages we are scanning.
+	Pkg     *packages.Package              // The packages we are currently on.
+	Types   []*Type                        // The types
+	Methods *ordmap.Map[ast.Expr, *Method] // The methods, keyed by their receiver types
 }
 
 // NewGenerator returns a new generator with the
@@ -63,6 +60,7 @@ func (g *Generator) PrintHeader() {
 // and adds them to [Generator.Types] and [Generator.Funcs]
 func (g *Generator) Find() error {
 	g.Types = []*Type{}
+	g.Methods = &ordmap.Map[ast.Expr, *Method]{}
 	err := gengo.Inspect(g.Pkg, g.Inspect)
 	if err != nil {
 		return fmt.Errorf("error while inspecting: %w", err)
@@ -150,7 +148,17 @@ func (g *Generator) InspectFuncDecl(fd *ast.FuncDecl) (bool, error) {
 	}
 	doc := strings.TrimSuffix(fd.Doc.Text(), "\n")
 
-	fmt.Println(dirs, doc, fd.Recv.List[0])
+	if fd.Recv == nil {
+		fmt.Println("func", fd)
+	} else {
+		method := &Method{
+			Name:       fd.Name.Name,
+			Doc:        doc,
+			Directives: dirs,
+			Config:     cfg,
+		}
+		g.Methods.Add(fd.Recv.List[0].Type, method)
+	}
 
 	return true, nil
 }
@@ -227,121 +235,15 @@ func (g *Generator) Generate() (bool, error) {
 	if len(g.Types) == 0 {
 		return false, nil
 	}
+	// for _, kv := range g.Methods.Order {
+	// 	for _, typ := range g.Types {
+	// 		typ.Type.Type
+	// 	}
+	// }
 	for _, typ := range g.Types {
 		g.ExecTmpl(TypeTmpl, typ)
-		// values := make([]Value, 0, 100)
-		// // for _, file := range g.Pkg.Syntax {
-		// // 	if ast.IsGenerated(file) {
-		// // 		continue
-		// // 	}
-		// // 	var terr error
-		// // 	ast.Inspect(file, func(n ast.Node) bool {
-		// // 		if terr != nil {
-		// // 			return false
-		// // 		}
-		// // 		vals, cont, err := g.GenDecl(n, file, typ)
-		// // 		if err != nil {
-		// // 			terr = err
-		// // 		} else {
-		// // 			values = append(values, vals...)
-		// // 		}
-		// // 		return cont
-		// // 	})
-		// // 	if terr != nil {
-		// // 		return true, fmt.Errorf("Generate: error parsing declaration clauses: %w", terr)
-		// // 	}
-		// }
-
-		// if len(values) == 0 {
-		// 	return true, errors.New("no values defined for type " + typ.Name)
-		// }
-
-		// TODO: build stuff
 	}
 	return true, nil
-}
-
-// GenDecl processes one declaration clause.
-// It returns whether the AST inspector should continue,
-// and an error if there is one. It should only be
-// called in [ast.Inspect].
-func (g *Generator) GenDecl(node ast.Node, file *ast.File, typ *Type) ([]Value, bool, error) {
-	decl, ok := node.(*ast.GenDecl)
-	if !ok || decl.Tok != token.CONST {
-		// We only care about const declarations.
-		return nil, true, nil
-	}
-	vals := []Value{}
-	// The name of the type of the constants we are declaring.
-	// Can change if this is a multi-element declaration.
-	typName := ""
-	// Loop over the elements of the declaration. Each element is a ValueSpec:
-	// a list of names possibly followed by a type, possibly followed by values.
-	// If the type and value are both missing, we carry down the type (and value,
-	// but the "go/types" package takes care of that).
-	for _, spec := range decl.Specs {
-		vspec := spec.(*ast.ValueSpec) // Guaranteed to succeed as this is CONST.
-		if vspec.Type == nil && len(vspec.Values) > 0 {
-			// "X = 1". With no type but a value, the constant is untyped.
-			// Skip this vspec and reset the remembered type.
-			typName = ""
-			continue
-		}
-		if vspec.Type != nil {
-			// "X T". We have a type. Remember it.
-			ident, ok := vspec.Type.(*ast.Ident)
-			if !ok {
-				continue
-			}
-			typName = ident.Name
-		}
-		if typName != typ.Name {
-			// This is not the type we're looking for.
-			continue
-		}
-		// We now have a list of names (from one line of source code) all being
-		// declared with the desired type.
-		// Grab their names and actual values and store them in f.values.
-		for _, n := range vspec.Names {
-			if n.Name == "_" {
-				continue
-			}
-			// This dance lets the type checker find the values for us. It's a
-			// bit tricky: look up the object declared by the n, find its
-			// types.Const, and extract its value.
-			obj, ok := g.Pkg.TypesInfo.Defs[n]
-			if !ok {
-				return nil, false, errors.New("no value for constant " + n.String())
-			}
-			info := obj.Type().Underlying().(*types.Basic).Info()
-			if info&types.IsInteger == 0 {
-				return nil, false, errors.New("can't handle non-integer constant type " + typName)
-			}
-			value := obj.(*types.Const).Val() // Guaranteed to succeed as this is CONST.
-			if value.Kind() != constant.Int {
-				return nil, false, errors.New("can't happen: constant is not an integer " + n.String())
-			}
-			i64, isInt := constant.Int64Val(value)
-			u64, isUint := constant.Uint64Val(value)
-			if !isInt && !isUint {
-				return nil, false, errors.New("internal error: value of " + n.String() + " is not an integer: " + value.String())
-			}
-			if !isInt {
-				u64 = uint64(i64)
-			}
-			v := Value{
-				OriginalName: n.Name,
-				Name:         n.Name,
-				Desc:         html.EscapeString(strings.Join(strings.Fields(vspec.Doc.Text()), " ")), // need to collapse whitespace and escape
-				Value:        u64,
-				Signed:       info&types.IsUnsigned == 0,
-				Str:          value.String(),
-			}
-
-			vals = append(vals, v)
-		}
-	}
-	return vals, false, nil
 }
 
 // ExecTmpl executes the given template with the given type and
--- gtigen/type.go
+++ gtigen/type.go
@@ -6,12 +6,11 @@ package gtigen
 
 import (
 	"go/ast"
-	"sort"
 
 	"goki.dev/gti"
 )
 
-// Type represents a parsed enum type.
+// Type represents a parsed type.
 type Type struct {
 	Name       string         // The name of the type in its package (eg: MyType)
 	FullName   string         // The fully-package-path-qualified name of the type (eg: goki.dev/ki/v2.MyType)
@@ -22,56 +21,10 @@ type Type struct {
 	Config     *Config // Configuration information set in the comment directive for the type; is initialized to generator config info first
 }
 
-// Value represents a declared constant.
-type Value struct {
-	OriginalName string // The name of the constant before transformation
-	Name         string // The name of the constant after transformation (i.e. camel case => snake case)
-	Desc         string // The comment description of the constant
-	// The Value is stored as a bit pattern alone. The boolean tells us
-	// whether to interpret it as an int64 or a uint64; the only place
-	// this matters is when sorting.
-	// Much of the time the str field is all we need; it is printed
-	// by Value.String.
-	Value  uint64 // Will be converted to int64 when needed.
-	Signed bool   // Whether the constant is a signed type.
-	Str    string // The string representation given by the "go/constant" package.
-}
-
-func (v *Value) String() string {
-	return v.Str
-}
-
-// SortValues sorts the values and ensures there
-// are no duplicates. The input slice is known
-// to be non-empty.
-func SortValues(values []Value) []Value {
-	// We use stable sort so the lexically first name is chosen for equal elements.
-	sort.Stable(ByValue(values))
-	// Remove duplicates. Stable sort has put the one we want to print first,
-	// so use that one. The String method won't care about which named constant
-	// was the argument, so the first name for the given value is the only one to keep.
-	// We need to do this because identical values would cause the switch or map
-	// to fail to compile.
-	j := 1
-	for i := 1; i < len(values); i++ {
-		if values[i].Value != values[i-1].Value {
-			values[j] = values[i]
-			j++
-		}
-	}
-	return values[:j]
-}
-
-// ByValue is a sorting method that sorts the constants into increasing order.
-// We take care in the Less method to sort in signed or unsigned order,
-// as appropriate.
-type ByValue []Value
-
-func (b ByValue) Len() int      { return len(b) }
-func (b ByValue) Swap(i, j int) { b[i], b[j] = b[j], b[i] }
-func (b ByValue) Less(i, j int) bool {
-	if b[i].Signed {
-		return int64(b[i].Value) < int64(b[j].Value)
-	}
-	return b[i].Value < b[j].Value
+// Method represents a parsed method.
+type Method struct {
+	Name       string         // The name of the method
+	Doc        string         // The documentation for the method
+	Directives gti.Directives // The directives for the method; guaranteed to be non-nil
+	Config     *Config        // Configuration information set in the comment directive for the method; is initialized to generator config info first
 }
