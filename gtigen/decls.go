d896a71c7cc341af6f7d3ba1f4ce8eeca51bbd0f
Kai O'Reilly <koreilly5297@gmail.com>
2023-09-10 18:48:06 -0700

    added ordmap code string

--- gtigen/decls.go
+++ gtigen/decls.go
@@ -4,14 +4,30 @@
 
 package gtigen
 
-import "text/template"
+import (
+	"fmt"
+	"text/template"
 
-var TypeTmpl = template.Must(template.New("Type").Parse(
+	"goki.dev/gti"
+)
+
+var TypeTmpl = template.Must(template.New("Type").Funcs(template.FuncMap{"OrdmapCodeString": OrdmapCodeString}).Parse(
 	`var {{if .Config.TypeVar}} {{.Name}}Type {{else}} _ {{end}} = &gti.Type{
 		Name: "{{.FullName}}",
 		Doc: ` + "`" + `{{.Doc}}` + "`" + `,
 		Directives: gti.Directives{ {{range .Directives}} {{printf "%#v" .}}, {{end}} },
-		{{if ne .Fields nil}} Fields: {{print "%#v" .Fields}}, {{end}}
+		{{if ne .Fields nil}} Fields: {{OrdmapCodeString .Fields}}, {{end}}
 		{{if .Config.Instance}} Instance: &{{.Name}}{}, {{end}}
 	}
-	`))
+	`)).Funcs(template.FuncMap{})
+
+// OrdmapCodeString returns the given ordered map as a string
+// of valid Go code that constructs the ordered map
+func OrdmapCodeString(omp *gti.Fields) string {
+	res := "ordmap.Make([]ordmap.KeyVal[string, *gti.Field]{"
+	for _, kv := range omp.Order {
+		res += fmt.Sprintf("{%q, %#v},", kv.Key, kv.Val)
+	}
+	res += "})"
+	return res
+}
