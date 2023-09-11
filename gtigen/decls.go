9e42f4347fd844d4e45bad5d2f2e32a2e338a1d8
Kai O'Reilly <koreilly5297@gmail.com>
2023-09-10 18:59:43 -0700

    made OrdmapCodeString generic

--- gtigen/decls.go
+++ gtigen/decls.go
@@ -11,22 +11,24 @@ import (
 	"goki.dev/gti"
 )
 
-var TypeTmpl = template.Must(template.New("Type").Funcs(template.FuncMap{"OrdmapCodeString": OrdmapCodeString}).Parse(
+var TypeTmpl = template.Must(template.New("Type").Funcs(template.FuncMap{"FieldsCodeString": OrdmapCodeString[string, *gti.Field]}).Parse(
 	`var {{if .Config.TypeVar}} {{.Name}}Type {{else}} _ {{end}} = &gti.Type{
 		Name: "{{.FullName}}",
 		Doc: ` + "`" + `{{.Doc}}` + "`" + `,
 		Directives: gti.Directives{ {{range .Directives}} {{printf "%#v" .}}, {{end}} },
-		{{if ne .Fields nil}} Fields: {{OrdmapCodeString .Fields}}, {{end}}
+		{{if ne .Fields nil}} Fields: {{FieldsCodeString .Fields}}, {{end}}
 		{{if .Config.Instance}} Instance: &{{.Name}}{}, {{end}}
 	}
-	`)).Funcs(template.FuncMap{})
+	`))
 
 // OrdmapCodeString returns the given ordered map as a string
 // of valid Go code that constructs the ordered map
-func OrdmapCodeString(omp *gti.Fields) string {
-	res := "ordmap.Make([]ordmap.KeyVal[string, *gti.Field]{"
+func OrdmapCodeString[K comparable, V any](omp *gti.Fields) string {
+	var zk K
+	var zv V
+	res := fmt.Sprintf("ordmap.Make([]ordmap.KeyVal[%T, %T]{", zk, zv)
 	for _, kv := range omp.Order {
-		res += fmt.Sprintf("{%q, %#v},", kv.Key, kv.Val)
+		res += fmt.Sprintf("{%#v, %#v},", kv.Key, kv.Val)
 	}
 	res += "})"
 	return res
