d8c5a2dcb9076cf08c36fc4809443d8c57714299
Kai O'Reilly <koreilly5297@gmail.com>
2023-09-11 20:04:17 -0700

    fixed command

--- cmd/gtigen/gtigen.go
+++ cmd/gtigen/gtigen.go
@@ -9,20 +9,10 @@ import (
 	"goki.dev/gti/gtigen"
 )
 
-// App is the main app type that handles
-// the logic for the enumgen tool
-type App struct{}
-
-// Cmd is the main command of enumgen
-// that generates the enum methods
-func (a *App) Cmd(cfg *gtigen.Config) error {
-	return gtigen.Generate(cfg)
-}
-
 func main() {
 	grease.AppName = "gtigen"
 	grease.AppTitle = "GTIGen"
 	grease.AppAbout = "GTIGen provides the generation of general purpose type information for Go types, methods, functions and variables"
 	grease.DefaultFiles = []string{"gtigen.toml"}
-	grease.Run(&App{}, &gtigen.Config{})
+	grease.Run(&gtigen.Config{}, gtigen.Generate)
 }
