db0f0ee5b99eea5fa52db3cb8b893550625b900b
Randall C. O'Reilly <oreilly@ucdavis.edu>
2023-09-14 00:40:04 -0700

    implements HasEmbed and CompileEmbeds -- tested in ki

--- type.go
+++ type.go
@@ -6,6 +6,7 @@ package gti
 
 import (
 	"reflect"
+	"strings"
 )
 
 // Type represents a type
@@ -29,18 +30,68 @@ type Type struct {
 	// Embeded fields for struct types
 	Embeds *Fields
 
-	// All embedded fields (including nested ones) for struct types;
-	// not set by gtigen but can be set manually by outside packages
-	AllEmbeds *Field
-
 	// Fields for struct types
 	Fields *Fields
 
 	// instance of the type
 	Instance any `desc:"instance of the type"`
+
+	// All embedded fields (including nested ones) for struct types;
+	// not set by gtigen -- HasEmbed automatically compiles it as needed.
+	// Key is the ID of the type.
+	AllEmbeds map[uint64]*Type
 }
 
-// ReflectType returns the [reflect.Type] of a given Ki Type
+// ReflectType returns the [reflect.Type] for this type, using the Instance
 func (tp *Type) ReflectType() reflect.Type {
 	return reflect.TypeOf(tp.Instance).Elem()
+}
+
+// HasEmbed returns true if this type has the given type
+// at any level of embedding depth, including if this type is
+// the given type.  The first time called it will Compile
+// a map of all embedded types so subsequent calls are very fast.
+func (tp *Type) HasEmbed(typ *Type) bool {
+	if tp.AllEmbeds == nil {
+		tp.CompileEmbeds()
+		if tp.AllEmbeds == nil {
+			return typ == tp
+		}
+	}
+	if tp == typ {
+		return true
+	}
+	_, has := tp.AllEmbeds[typ.ID]
+	return has
+}
+
+func (tp *Type) CompileEmbeds() {
+	if tp.Embeds == nil {
+		return
+	}
+	tp.AllEmbeds = make(map[uint64]*Type)
+	rt := tp.ReflectType()
+	for _, em := range tp.Embeds.Order {
+		enm := em.Val.Name
+		if idx := strings.LastIndex(enm, "."); idx >= 0 {
+			enm = enm[idx+1:]
+		}
+		etf, has := rt.FieldByName(enm)
+		if !has {
+			continue
+		}
+		etft := TypeName(etf.Type)
+		et := TypeByName(etft)
+		if et == nil {
+			continue
+		}
+		tp.AllEmbeds[et.ID] = et
+		et.CompileEmbeds()
+		if et.AllEmbeds == nil {
+			continue
+		}
+		for id, ct := range et.AllEmbeds {
+			tp.AllEmbeds[id] = ct
+		}
+	}
 }
