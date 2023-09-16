1ab14cf7627aa43e55c5667bc73222c9432f3630
Kai O'Reilly <koreilly5297@gmail.com>
2023-09-16 11:44:10 -0700

    went back to last index; splitn doesn't work for this situation

--- gtigen/generator.go
+++ gtigen/generator.go
@@ -98,13 +98,13 @@ func (g *Generator) GetInterfaces(pkgs []*types.Package) error {
 			if _, has := g.Interfaces.IdxByKeyTry(in); has {
 				continue
 			}
-			strs := strings.SplitN(in, ".", 2)
-			if len(strs) != 2 {
-				return fmt.Errorf("expected two strings from strings.SplitN(%q, %q, 2) (using the interface config fully-qualified type name)", in, ".")
+			li := strings.LastIndex(in, ".")
+			if li == -1 {
+				return fmt.Errorf("expected a dot (%q) in the interface config fully-qualified type name (%q)", ".", in)
 			}
-			pkgpath := strs[0]
+			pkgpath := in[:li]
 			if pkg.Path() == pkgpath {
-				typnm := strs[1]
+				typnm := in[li+1:] // need to get rid of the dot
 				typ := pkg.Scope().Lookup(typnm)
 				if typ == nil {
 					return fmt.Errorf("programmer error: internal error: could not find type %q in package %q (from interface config %q)", typnm, pkgpath, in)
