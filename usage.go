5579a4d536eab140d360ea92e8a667c2e06c000c
Kai O'Reilly <koreilly5297@gmail.com>
2023-09-15 19:40:09 -0700

    improved command usage

--- usage.go
+++ usage.go
@@ -22,14 +22,18 @@ import (
 func Usage[T any](opts *Options, cfg T, cmd string, cmds ...*Cmd[T]) string {
 	var b strings.Builder
 	if cmd == "" {
-		b.WriteString(opts.AppAbout)
-		b.WriteString("\n\n")
+		if opts.AppAbout != "" {
+			b.WriteString(opts.AppAbout)
+			b.WriteString("\n\n")
+		}
 	} else {
 		gotCmd := false
 		for _, c := range cmds {
 			if c.Name == cmd {
-				b.WriteString(c.Doc)
-				b.WriteString("\n\n")
+				if c.Doc != "" {
+					b.WriteString(c.Doc)
+					b.WriteString("\n\n")
+				}
 				gotCmd = true
 				break
 			}
@@ -44,16 +48,7 @@ func Usage[T any](opts *Options, cfg T, cmd string, cmds ...*Cmd[T]) string {
 		cmdName += " " + cmd
 	}
 	b.WriteString("Usage: " + cmdColor(cmdName+" [command] [arguments] [flags]\n\n"))
-	b.WriteString("The following commands are available:\n\n")
-	// for _, c := range cmds {
-	// 	if c.Root {
-	// 		b.WriteString(cmdColor("<default command>") + "\n\t" + c.Doc + "\n")
-	// 		break
-	// 	}
-	// }
-	b.WriteString(cmdColor("help") + "\n\tshow this usage message and exit\n")
 	CommandUsage(&b, cmd, cmds...)
-	b.WriteString("\n")
 
 	b.WriteString("The following flags are available. Flags are case-insensitive and\n")
 	b.WriteString("can be in kebab-case, snake_case, or CamelCase. Also, there can be\n")
@@ -66,11 +61,44 @@ func Usage[T any](opts *Options, cfg T, cmd string, cmds ...*Cmd[T]) string {
 	return b.String()
 }
 
-// CommandUsage adds the command usage info for
-// the given commands to the given [strings.Builder].
-// Typically, you should use [Usage] instead.
+// CommandUsage adds the command usage info for the given commands to the
+// given [strings.Builder]. Typically, end-user code should use [Usage] instead.
+//
+// To be a command that is included in the usage, we must be one command
+// nesting depth (subcommand) deeper than the current command (ie, if we
+// are on "x", we can see usage for commands of the form "x y"), and all
+// of our commands must be consistent with the current command. For example,
+// "" could generate usage for "help", "build", and "run", and "mod" could
+// generate usage for "mod init", "mod tidy", and "mod edit". This ensures
+// that only relevant commands are shown in the usage.
 func CommandUsage[T any](b *strings.Builder, cmd string, cmds ...*Cmd[T]) {
+	b.WriteString("The following commands are available:\n\n")
 	for _, c := range cmds {
+		if (c.Root && cmd == "") || c.Name == cmd {
+			b.WriteString(cmdColor("<default command> ("+c.Name+")") + "\n\t" + c.Doc + "\n")
+			break
+		}
+	}
+	if cmd == "" {
+		b.WriteString(cmdColor("help") + "\n\tshow this usage message and exit\n")
+	}
+
+	cmdstrs := strings.Fields(cmd)
+	// need this label so that we can continue outer loop when we have non-matching cmdstr
+outer:
+	for _, c := range cmds {
+		if (c.Root && cmd == "") || c.Name == cmd { // we already handled this case above, so skip
+			continue
+		}
+		cstrs := strings.Fields(c.Name)
+		if len(cstrs) != len(cmdstrs)+1 {
+			continue
+		}
+		for i, cmdstr := range cmdstrs {
+			if cmdstr != cstrs[i] {
+				continue outer
+			}
+		}
 		if c.Root {
 			b.WriteString(cmdColor(c.Name + " (default command)"))
 		} else {
@@ -81,6 +109,8 @@ func CommandUsage[T any](b *strings.Builder, cmd string, cmds ...*Cmd[T]) {
 		}
 		b.WriteString("\n")
 	}
+
+	b.WriteString("\n")
 }
 
 // FlagUsage adds the flag usage info for the
