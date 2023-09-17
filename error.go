be178eea22b27b761887082bffacdfbca2e68a3a
Kai O'Reilly <koreilly5297@gmail.com>
2023-09-17 10:58:22 -0700

    removed grr.go

--- error.go
+++ error.go
@@ -2,6 +2,7 @@
 // Use of this source code is governed by a BSD-style
 // license that can be found in the LICENSE file.
 
+// Package grr provides easy, context-wrapped error handling in Go.
 package grr
 
 import "strings"
