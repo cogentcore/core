e6fac0165a11bf634a4bf85471c600bf51d55c7c
Kai O'Reilly <koreilly5297@gmail.com>
2023-09-17 18:43:21 -0700

    added config

---
+++ config.go
@@ -0,0 +1,35 @@
+// Copyright (c) 2023, The GoKi Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style
+// license that can be found in the LICENSE file.
+
+package xe
+
+import (
+	"io"
+	"os"
+)
+
+// Config contains the configuration information that
+// controls the behavior of xe. It is passed to most
+// high-level functions, and a default version of it
+// can be easily constructed using [DefaultConfig].
+type Config struct {
+	// Stdout is the writer to write the standard output of called commands to.
+	// It can be set to nil to disable the writing of the standard output.
+	Stdout io.Writer
+	// Stderr is the writer to write the standard error of called commands to.
+	// It can be set to nil to disable the writing of the standard error.
+	Stderr io.Writer
+	// Commands is the writer to write the string representation of the called commands to.
+	// It can be set to nil to disable the writing of the string representations of the called commands.
+	Commands io.Writer
+}
+
+// DefaultConfig returns the default [Config] object.
+func DefaultConfig() *Config {
+	return &Config{
+		Stdout:   os.Stdout,
+		Stderr:   os.Stderr,
+		Commands: os.Stdout,
+	}
+}
