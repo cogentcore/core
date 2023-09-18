9bc3ea659de4777331bac848a9cad4d5ee6f71f4
Kai O'Reilly <koreilly5297@gmail.com>
2023-09-18 12:45:27 -0700

    added color scheme windows

---
+++ colorscheme_windows.go
@@ -0,0 +1,95 @@
+// Copyright (c) 2023, The GoKi Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style
+// license that can be found in the LICENSE file.
+
+// Code heavily based on https://gist.github.com/jerblack/1d05bbcebb50ad55c312e4d7cf1bc909
+// MIT License
+// Copyright 2022 Jeremy Black
+// Permission is hereby granted, free of charge, to any person obtaining a copy of this software and associated documentation files (the "Software"), to deal in the Software without restriction, including without limitation the rights to use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of the Software, and to permit persons to whom the Software is furnished to do so, subject to the following conditions:
+// The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.
+// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+
+//go:build windows
+
+package goosi
+
+import (
+	"log"
+	"syscall"
+
+	"golang.org/x/sys/windows/registry"
+)
+
+// ThemeIsDark is whether the system color scheme is dark (as opposed to light)
+var ThemeIsDark bool
+
+const (
+	themeRegKey  = `Software\Microsoft\Windows\CurrentVersion\Themes\Personalize` // in HKCU
+	themeRegName = `AppsUseLightTheme`                                            // <- For apps. Use SystemUsesLightTheme for taskbar and tray
+)
+
+// set the state of the theme and then monitor it on startup
+func init() {
+	react(isDark())
+	monitor(react)
+}
+
+// react reacts to a change in the color scheme
+func react(isDark bool) {
+	ThemeIsDark = isDark
+}
+
+// isDark returns whether we are currently in dark mode
+func isDark() bool {
+	k, err := registry.OpenKey(registry.CURRENT_USER, themeRegKey, registry.QUERY_VALUE)
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer k.Close()
+	val, _, err := k.GetIntegerValue(themeRegName)
+	if err != nil {
+		log.Fatal(err)
+	}
+	return val == 0
+}
+
+// monitor monitors the state of the dark mode and
+// calls the given function with the new value whenver it changes.
+// it does not return, so it should be called in a separate goroutine.
+func monitor(fn func(bool)) {
+	var regNotifyChangeKeyValue *syscall.Proc
+	changed := make(chan bool)
+
+	if advapi32, err := syscall.LoadDLL("Advapi32.dll"); err == nil {
+		if p, err := advapi32.FindProc("RegNotifyChangeKeyValue"); err == nil {
+			regNotifyChangeKeyValue = p
+		} else {
+			log.Fatal("Could not find function RegNotifyChangeKeyValue in Advapi32.dll")
+		}
+	}
+	if regNotifyChangeKeyValue != nil {
+		go func() {
+			k, err := registry.OpenKey(registry.CURRENT_USER, themeRegKey, syscall.KEY_NOTIFY|registry.QUERY_VALUE)
+			if err != nil {
+				log.Fatal(err)
+			}
+			var wasDark uint64
+			for {
+				regNotifyChangeKeyValue.Call(uintptr(k), 0, 0x00000001|0x00000004, 0, 0)
+				val, _, err := k.GetIntegerValue(themeRegName)
+				if err != nil {
+					log.Fatal(err)
+				}
+				if val != wasDark {
+					wasDark = val
+					changed <- val == 0
+				}
+			}
+		}()
+	}
+	for {
+		val := <-changed
+		fn(val)
+	}
+
+}
--- go.mod
+++ go.mod
@@ -1,3 +1,5 @@
 module goki.dev/goosi
 
 go 1.21.0
+
+require golang.org/x/sys v0.12.0
