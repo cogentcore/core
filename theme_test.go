9c836a87d499c627156b934a3fdef30c45d93e32
Kai O'Reilly <koreilly5297@gmail.com>
2023-09-18 17:50:12 -0700

    moved test

---
+++ theme_test.go
@@ -0,0 +1,36 @@
+// Copyright (c) 2023, The GoKi Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style
+// license that can be found in the LICENSE file.
+
+package goosi
+
+import (
+	"fmt"
+	"testing"
+)
+
+func TestIsDark(t *testing.T) {
+	isDark, err := IsDark()
+	if err != nil {
+		t.Fatal(err)
+	}
+	fmt.Println("IsDark:", isDark)
+}
+
+// func TestMonitor(t *testing.T) {
+// 	t.Skip("TODO: figure out how to do this well in a test, or just put it into an example")
+// 	isDark, err := IsDark()
+// 	if err != nil {
+// 		t.Fatal(err)
+// 	}
+// 	ec, err := Monitor(func(b bool) {
+// 		fmt.Println("IsDark changed to:", isDark)
+// 	})
+// 	if err != nil {
+// 		t.Fatal(err)
+// 	}
+// 	err = <-ec
+// 	if err != nil {
+// 		t.Fatal(err)
+// 	}
+// }
