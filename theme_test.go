324af3256482aa7695d450570ba19093f45490c7
Kai O'Reilly <koreilly5297@gmail.com>
2023-09-18 17:55:30 -0700

    added back monitor test

--- theme_test.go
+++ theme_test.go
@@ -17,20 +17,20 @@ func TestIsDark(t *testing.T) {
 	fmt.Println("IsDark:", isDark)
 }
 
-// func TestMonitor(t *testing.T) {
-// 	t.Skip("TODO: figure out how to do this well in a test, or just put it into an example")
-// 	isDark, err := IsDark()
-// 	if err != nil {
-// 		t.Fatal(err)
-// 	}
-// 	ec, err := Monitor(func(b bool) {
-// 		fmt.Println("IsDark changed to:", isDark)
-// 	})
-// 	if err != nil {
-// 		t.Fatal(err)
-// 	}
-// 	err = <-ec
-// 	if err != nil {
-// 		t.Fatal(err)
-// 	}
-// }
+func TestMonitor(t *testing.T) {
+	// t.Skip("TODO: figure out how to do this well in a test, or just put it into an example")
+	isDark, err := IsDark()
+	if err != nil {
+		t.Fatal(err)
+	}
+	ec, err := Monitor(func(b bool) {
+		fmt.Println("IsDark changed to:", isDark)
+	})
+	if err != nil {
+		t.Fatal(err)
+	}
+	err = <-ec
+	if err != nil {
+		t.Fatal(err)
+	}
+}
