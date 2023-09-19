e7463f4f188fcc0ba5fc64fae1e3ef80c0453ac7
Kai O'Reilly <koreilly5297@gmail.com>
2023-09-19 14:29:33 -0700

    monitor IsDark for 10 seconds

--- theme_test.go
+++ theme_test.go
@@ -20,6 +20,7 @@ func TestThemeIsDark(t *testing.T) {
 
 func TestMonitorTheme(t *testing.T) {
 	// t.Skip("comment this out to monitor theme changes (which uses a function that will never return)")
+	fmt.Println("monitoring IsDark for 10 seconds")
 	done := make(chan struct{})
 	ec, err := IsDarkMonitor(func(isDark bool) {
 		fmt.Println("IsDark changed to:", isDark)
@@ -28,7 +29,7 @@ func TestMonitorTheme(t *testing.T) {
 		t.Fatal(err)
 	}
 	go func() {
-		time.Sleep(time.Second)
+		time.Sleep(10 * time.Second)
 		close(done)
 	}()
 	select {
