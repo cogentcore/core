a242f2c588fdcb4c82a0ac7a4bb61e8e7bbb881c
Kai O'Reilly <koreilly5297@gmail.com>
2023-09-20 12:04:18 -0700

    added android and ios drivers

---
+++ driver/android/app.go
@@ -0,0 +1,8 @@
+// Copyright 2023 The GoKi Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style
+// license that can be found in the LICENSE file.
+
+//go:build android
+
+// Package android implements goosi interfaces on Android mobile devices
+package android
