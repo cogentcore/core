9ce5b691fccea172ffefe21bc054edd79f6f1125
Kai O'Reilly <koreilly5297@gmail.com>
2023-09-19 17:16:12 -0700

    added font paths for darwin and linux

---
+++ fontpaths_darwin.go
@@ -0,0 +1,10 @@
+// Copyright (c) 2023, The GoKi Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style
+// license that can be found in the LICENSE file.
+
+//go:build darwin
+
+package goosi
+
+// FontPaths contains the filepaths in which fonts are stored on Darwin (macOS/iOS).
+var FontPaths = []string{"/System/Library/Fonts", "/Library/Fonts"}
