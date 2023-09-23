5bc9260dd0f66c7b7b39fce6f372e58df97ab615
Kai O'Reilly <koreilly5297@gmail.com>
2023-09-23 16:28:14 -0700

    fixed install errors

--- mobile/install.go
+++ mobile/install.go
@@ -10,48 +10,38 @@ import (
 	"os/exec"
 	"path"
 	"strings"
+
+	"goki.dev/goki/config"
 )
 
-var cmdInstall = &command{
-	run:   runInstall,
-	Name:  "install",
-	Usage: "[-target android] [build flags] [package]",
-	Short: "compile android APK and install on device",
-	Long: `
-Install compiles and installs the app named by the import path on the
-attached mobile device.
-
-Only -target android is supported. The 'adb' tool must be on the PATH.
-
-The build flags -a, -i, -n, -x, -gcflags, -ldflags, -tags, -trimpath, and -work are
-shared with the build command.
-For documentation, see 'go help build'.
-`,
-}
-
-func runInstall(cmd *command) error {
-	if !strings.HasPrefix(buildTarget, "android") {
-		return fmt.Errorf("install is not supported for -target=%s", buildTarget)
+// Install compiles and installs the app named by the import path on the
+// attached mobile device.
+//
+// Only -target android is supported. The 'adb' tool must be on the PATH.
+func Install(c *config.Config) error {
+	// TODO: use install config fields, not build ones
+	if len(c.Build.Target) != 1 || c.Build.Target[0].OS != "android" {
+		return fmt.Errorf("target for install must be android, but got %v", c.Build.Target)
 	}
-	pkg, err := BuildImpl(cmd)
+	pkg, err := BuildImpl(c)
 	if err != nil {
 		return err
 	}
 
 	// Don't use runCmd as adb does not return a useful exit code.
-	c := exec.Command(
+	cmd := exec.Command(
 		`adb`,
 		`install`,
 		`-r`,
 		AndroidPkgName(path.Base(pkg.PkgPath))+`.apk`,
 	)
-	c.Stdout = os.Stdout
-	c.Stderr = os.Stderr
-	if buildX || buildN {
-		PrintCmd("%s", strings.Join(c.Args, " "))
+	cmd.Stdout = os.Stdout
+	cmd.Stderr = os.Stderr
+	if c.Build.Print || c.Build.PrintOnly {
+		PrintCmd("%s", strings.Join(cmd.Args, " "))
 	}
-	if buildN {
+	if c.Build.PrintOnly {
 		return nil
 	}
-	return c.Run()
+	return cmd.Run()
 }
