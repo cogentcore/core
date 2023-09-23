e6bceb9cba071d2d64c1ca6baa2d84c8334e7446
Kai O'Reilly <koreilly5297@gmail.com>
2023-09-22 17:05:14 -0700

    fixed icon urls

--- gen.go
+++ gen.go
@@ -38,7 +38,7 @@ type iconData struct {
 var iconTmpl = template.Must(template.New("icon").Parse(
 	`
 	// {{.Camel}} is the "{{.Snake}}" icon from Material Design Symbols,
-	// defined at https://goki.dev/gi/v2/blob/master/icons/{{.Dir}}{{.Snake}}.svg
+	// defined at https://github.com/goki/icons/blob/main/{{.Dir}}{{.Snake}}.svg
 	{{.Camel}} Icon = "{{.Snake}}"
 `,
 ))
