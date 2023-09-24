3bb81570bd7feabafb4e6ba01518978f47cfaddf
Kai O'Reilly <koreilly5297@gmail.com>
2023-09-24 13:16:54 -0700

    restructured api

--- directive.go
+++ directive.go
@@ -6,18 +6,17 @@
 package directive
 
 import (
+	"fmt"
 	"go/ast"
-	"sort"
 	"strings"
+	"unicode"
+
+	"github.com/mattn/go-shellwords"
 )
 
 // Directive represents a comment directive
 // that has been parsed or created in code.
 type Directive struct {
-
-	// Source is the source string of the
-	// comment directive.
-	Source string
 
 	// Tool is the name of the tool that
 	// the directive is for.
@@ -28,80 +27,60 @@ type Directive struct {
 	// name of the tool and a colon.
 	Directive string
 
-	// Args are the positional arguments
+	// Args are the arguments
 	// passed to the directive
 	Args []string
-
-	// NameValue are a map of name-value
-	// arguments given in the
-	// form `name=value`.
-	NameValue map[string]string
 }
 
-// String returns the directive as a
-// formatted string suitable for use in
-// code. It puts the positional arguments
-// before the name-value arguments, and it
-// includes two slashes (`//`) at the start.
-// The output of String is deterministic
-// because it sorts the name-value map.
+// String returns the directive as a formatted string suitable for use in
+// code. It includes two slashes (`//`) at the start.
 func (d *Directive) String() string {
 	if d == nil {
 		return "<nil>"
 	}
 	res := "//" + d.Tool + ":" + d.Directive
-	for _, arg := range d.Args {
-		res += " " + arg
-	}
-	keys := make([]string, 0, len(d.NameValue))
-	for key := range d.NameValue {
-		keys = append(keys, key)
-	}
-	sort.Strings(keys)
-	for _, key := range keys {
-		res += " " + key + "=" + d.NameValue[key]
+	if len(d.Args) > 0 {
+		res += " " + strings.Join(d.Args, " ")
 	}
 	return res
 }
 
-// Parse parses the given comment string and returns
-// any [Directive] inside it. If no such directive is
-// found, it returns nil. Directives are of the form:
-// `//tool:directive arg0 key0=value0 arg1 key1=value1`
-// (the two slashes are optional, and the positional
-// and key-value arguments can be in any order).
-func Parse(comment string) *Directive {
-	dir := &Directive{}
+// Parse parses the given comment string and returns any [Directive] inside it.
+// If no such directive is found, it returns nil. Directives are of the form:
+//
+//	//tool:directive arg0 key0=value0 arg1 key1=value1
+//
+// (the two slashes are optional, and the positional and key-value arguments
+// can be in any order).
+func Parse(comment string) (*Directive, error) {
+	comment = strings.TrimPrefix(comment, "//")
+	rs := []rune(comment)
+	if len(rs) == 0 || unicode.IsSpace(rs[0]) { // directives must not have whitespace as their first character
+		return nil, nil
+	}
 	before, after, found := strings.Cut(comment, ":")
 	if !found {
-		return nil
+		return nil, nil
 	}
-	dir.Source = comment
-	before = strings.TrimPrefix(before, "//")
-	dir.Tool = before
-	dir.Args = []string{}
-	dir.NameValue = map[string]string{}
-	fields := strings.Fields(after)
-	for i, field := range fields {
-		if i == 0 {
-			dir.Directive = field
-			continue
-		}
-		before, after, found := strings.Cut(field, "=")
-		if found {
-			dir.NameValue[before] = after
-		} else {
-			dir.Args = append(dir.Args, before)
-		}
+	directive := &Directive{}
+	directive.Tool = before
+	args, err := shellwords.Parse(after)
+	if err != nil {
+		return nil, fmt.Errorf("error parsing args %w", err)
 	}
-	return dir
+	directive.Args = args
+	if len(args) > 0 {
+		directive.Directive = directive.Args[0]
+		directive.Args = directive.Args[1:]
+	}
+	return directive, nil
 }
 
 // ParseComment parses the given AST comment
 // and returns any [Directive] inside it.
 // It is a helper function that calls [Parse]
 // on the text of the comment.
-func ParseComment(comment *ast.Comment) *Directive {
+func ParseComment(comment *ast.Comment) (*Directive, error) {
 	return Parse(comment.Text)
 }
 
@@ -109,13 +88,16 @@ func ParseComment(comment *ast.Comment) *Directive {
 // group and returns a slice of all [Directive]s
 // inside it. It is a helper function that calls
 // [ParseComment] on each comment in the group.
-func ParseCommentGroup(group *ast.CommentGroup) []*Directive {
+func ParseCommentGroup(group *ast.CommentGroup) ([]*Directive, error) {
 	res := []*Directive{}
 	for _, comment := range group.List {
-		dir := ParseComment(comment)
+		dir, err := ParseComment(comment)
+		if err != nil {
+			return nil, err
+		}
 		if dir != nil {
 			res = append(res, dir)
 		}
 	}
-	return res
+	return res, nil
 }
--- go.mod
+++ go.mod
@@ -1,3 +1,5 @@
 module goki.dev/directive
 
 go 1.18
+
+require github.com/mattn/go-shellwords v1.0.12
