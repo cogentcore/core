34e55301759cf09037cfd47f144513fdaac7fb08
Kai O'Reilly <koreilly5297@gmail.com>
2023-09-24 13:44:55 -0700

    added doc and level

---
+++ doc.go
@@ -0,0 +1,6 @@
+// Copyright (c) 2023, The GoKi Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style
+// license that can be found in the LICENSE file.
+
+// Package grog implements structured log handling and provides global log and print verbosity and color options.
+package grog
