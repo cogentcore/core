adf7d8c13a98737b0d1f38c3370ced0714d0b710
Kai O'Reilly <koreilly5297@gmail.com>
2023-09-24 19:07:22 -0700

    updated version to v0.9.18

--- version.go
+++ version.go
@@ -4,9 +4,9 @@ package cam
 
 const (
 	// Version is the version of this package being used
-	Version = "v0.9.17"
+	Version = "v0.9.18"
 	// GitCommit is the commit just before the latest version commit
-	GitCommit = "d6f2137"
+	GitCommit = "92c1c68"
 	// VersionDate is the date-time of the latest version commit in UTC (in the format 'YYYY-MM-DD HH:MM', which is the Go format '2006-01-02 15:04')
-	VersionDate = "2023-09-25 02:02"
+	VersionDate = "2023-09-25 02:07"
 )
