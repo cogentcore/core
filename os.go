dc95e6d63c9829bf9276499ea9949bbd80cbde01
Kai O'Reilly <koreilly5297@gmail.com>
2023-09-27 12:19:09 -0700

    added os

---
+++ os.go
@@ -0,0 +1,24 @@
+// Copyright (c) 2023, The GoKi Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style
+// license that can be found in the LICENSE file.
+
+package xe
+
+import (
+	"fmt"
+	"os"
+)
+
+// RemoveAll is a simple helper function that calls [os.RemoveAll]
+// after printing an equivalent command to [Config.Commands].
+func (c *Config) RemoveAll(path string) error {
+	err := os.RemoveAll(path)
+	cmds := c.Commands
+	if cmds == nil && err != nil {
+		cmds = c.Stderr
+	}
+	if cmds != nil {
+		fmt.Fprintf(cmds, "rm -rf %q\n", path)
+	}
+	return err
+}
