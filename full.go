0acd2538a74b6f7d1cb2824da41376485c50ab5f
Kai O'Reilly <koreilly5297@gmail.com>
2023-09-26 17:33:45 -0700

    added methods to full

--- full.go
+++ full.go
@@ -4,7 +4,9 @@
 
 package colors
 
-import "image/color"
+import (
+	"image/color"
+)
 
 // Full represents a fully specified color that can either be a solid color or
 // a gradient. If Gradient is nil, it is a solid color; otherwise, it is a gradient.
@@ -13,4 +15,40 @@ import "image/color"
 type Full struct {
 	Solid    color.RGBA
 	Gradient *Gradient
+}
+
+// IsNil returns whether the color is nil, checking both the gradient
+// and the solid color.
+func (f *Full) IsNil() bool {
+	return f.Gradient == nil && IsNil(f.Solid)
+}
+
+// SolidOrNil returns the solid color if it is not non-nil, or nil otherwise.
+// It is should be used by consumers that explicitly handle nil colors.
+func (f *Full) SolidOrNil() color.Color {
+	if IsNil(f.Solid) {
+		return nil
+	}
+	return f.Solid
+}
+
+// SetSolid sets the color to the given solid color,
+// also setting the gradient to nil.
+func (f *Full) SetSolid(solid color.RGBA) {
+	f.Solid = solid
+	f.Gradient = nil
+}
+
+// SetSolid sets the color to the given solid [color.Color],
+// also setting the gradient to nil.
+func (f *Full) SetColor(clr color.Color) {
+	f.Solid = AsRGBA(clr)
+	f.Gradient = nil
+}
+
+// SetSolid sets the color to the solid color with the given name,
+// also setting the gradient to nil.
+func (f *Full) SetName(name string) {
+	f.Solid = LogFromName(name)
+	f.Gradient = nil
 }
