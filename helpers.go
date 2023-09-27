1f20c8f49f3f3d83598ba5190334a20632237c5b
Kai O'Reilly <koreilly5297@gmail.com>
2023-09-27 10:25:03 -0700

    added must helpers

--- helpers.go
+++ helpers.go
@@ -4,7 +4,9 @@
 
 package grr
 
-import "log/slog"
+import (
+	"log/slog"
+)
 
 // Log takes the given value and error and returns the value if
 // the error is nil, and logs the error and returns a zero value
@@ -50,6 +52,50 @@ func Log3[T1, T2, T3 any](v1 T1, v2 T2, v3 T3, err error) (T1, T2, T3) {
 func Log4[T1, T2, T3, T4 any](v1 T1, v2 T2, v3 T3, v4 T4, err error) (T1, T2, T3, T4) {
 	if err != nil {
 		slog.Error(err.Error())
+	}
+	return v1, v2, v3, v4
+}
+
+// Must takes the given value and error and returns the value if
+// the error is nil, and panics if the error is non-nil. The intended usage is:
+//
+//	a := grr.Must(MyFunc(v))
+func Must[T any](v T, err error) T {
+	if err != nil {
+		panic(err)
+	}
+	return v
+}
+
+// Must2 takes the given two values and error and returns the values if
+// the error is nil, and panics if the error is non-nil. The intended usage is:
+//
+//	a, b := grr.Must2(MyFunc(v))
+func Must2[T1, T2 any](v1 T1, v2 T2, err error) (T1, T2) {
+	if err != nil {
+		panic(err)
+	}
+	return v1, v2
+}
+
+// Must3 takes the given three values and error and returns the values if
+// the error is nil, and panics if the error is non-nil. The intended usage is:
+//
+//	a, b, c := grr.Must3(MyFunc(v))
+func Must3[T1, T2, T3 any](v1 T1, v2 T2, v3 T3, err error) (T1, T2, T3) {
+	if err != nil {
+		panic(err)
+	}
+	return v1, v2, v3
+}
+
+// Must4 takes the given four values and error and returns the values if
+// the error is nil, and panics if the error is non-nil. The intended usage is:
+//
+//	a, b, c, d := grr.Must4(MyFunc(v))
+func Must4[T1, T2, T3, T4 any](v1 T1, v2 T2, v3 T3, v4 T4, err error) (T1, T2, T3, T4) {
+	if err != nil {
+		panic(err)
 	}
 	return v1, v2, v3, v4
 }
