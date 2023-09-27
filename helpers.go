ba1a4b281464e91b65c6cb1bc3dfcdc29c2a6b0c
Kai O'Reilly <koreilly5297@gmail.com>
2023-09-27 10:22:37 -0700

    added log helpers

---
+++ helpers.go
@@ -0,0 +1,55 @@
+// Copyright (c) 2023, The GoKi Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style
+// license that can be found in the LICENSE file.
+
+package grr
+
+import "log/slog"
+
+// Log takes the given value and error and returns the value if
+// the error is nil, and logs the error and returns a zero value
+// if the error is non-nil. The intended usage is:
+//
+//	a := grr.Log(MyFunc(v))
+func Log[T any](v T, err error) T {
+	if err != nil {
+		slog.Error(err.Error())
+	}
+	return v
+}
+
+// Log2 takes the given two values and error and returns the values if
+// the error is nil, and logs the error and returns zero values
+// if the error is non-nil. The intended usage is:
+//
+//	a, b := grr.Log2(MyFunc(v))
+func Log2[T1, T2 any](v1 T1, v2 T2, err error) (T1, T2) {
+	if err != nil {
+		slog.Error(err.Error())
+	}
+	return v1, v2
+}
+
+// Log3 takes the given three values and error and returns the values if
+// the error is nil, and logs the error and returns zero values
+// if the error is non-nil. The intended usage is:
+//
+//	a, b, c := grr.Log3(MyFunc(v))
+func Log3[T1, T2, T3 any](v1 T1, v2 T2, v3 T3, err error) (T1, T2, T3) {
+	if err != nil {
+		slog.Error(err.Error())
+	}
+	return v1, v2, v3
+}
+
+// Log4 takes the given four values and error and returns the values if
+// the error is nil, and logs the error and returns zero values
+// if the error is non-nil. The intended usage is:
+//
+//	a, b, c, d := grr.Log4(MyFunc(v))
+func Log4[T1, T2, T3, T4 any](v1 T1, v2 T2, v3 T3, v4 T4, err error) (T1, T2, T3, T4) {
+	if err != nil {
+		slog.Error(err.Error())
+	}
+	return v1, v2, v3, v4
+}
