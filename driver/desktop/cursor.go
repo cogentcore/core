68118063f687a3eac56e1b7be7750f5fcc032e68
Kai O'Reilly <koreilly5297@gmail.com>
2023-09-28 21:14:17 -0700

    finished cursor impl for desktop

--- driver/desktop/cursor.go
+++ driver/desktop/cursor.go
@@ -5,22 +5,38 @@
 package desktop
 
 import (
+	"sync"
+
 	"github.com/go-gl/glfw/v3.3/glfw"
 	"goki.dev/cursors/cursorimg"
 	"goki.dev/enums"
 	"goki.dev/goosi/cursor"
 )
 
-var theCursor = &cursorImpl{}
+var theCursor = cursorImpl{CursorBase: cursor.CursorBase{Vis: true}, cursors: map[string]map[int]*glfw.Cursor{}}
 
 type cursorImpl struct {
 	cursor.CursorBase
+	cursors map[string]map[int]*glfw.Cursor // cached cursors
+	mu      sync.Mutex
 }
 
 func (c *cursorImpl) Set(cursor enums.Enum) error {
-	ci, err := cursorimg.Get(cursor.String(), c.Size)
+	nm := cursor.String()
+	sm := c.cursors[nm]
+	if sm == nil {
+		c.cursors[nm] = map[int]*glfw.Cursor{}
+	}
+	if cur, ok := sm[c.Size]; ok {
+		theApp.ctxtwin.glw.SetCursor(cur)
+	}
+
+	ci, err := cursorimg.Get(nm, c.Size)
 	if err != nil {
 		return err
 	}
 	gc := glfw.CreateCursor(ci.Image, ci.HotSpot.X, ci.HotSpot.Y)
+	sm[c.Size] = gc
+	theApp.ctxtwin.glw.SetCursor(gc)
+	return nil
 }
