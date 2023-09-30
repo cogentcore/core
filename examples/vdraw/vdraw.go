7eca66066e444f74f2af2263be48284ab3785fad
Kai O'Reilly <koreilly5297@gmail.com>
2023-09-30 13:58:05 -0700

    reorganized examples

--- examples/vdraw/vdraw.go
+++ examples/vdraw/vdraw.go
@@ -1,25 +1,201 @@
-// Copyright (c) 2023, The GoKi Authors. All rights reserved.
-// Use of this source code is governed by a BSD-style
-// license that can be found in the LICENSE file.
-
 package main
 
 import (
+	"bytes"
+	"encoding/binary"
 	"fmt"
 	"image"
-	_ "image/jpeg"
-	_ "image/png"
-	"log"
 	"runtime"
 	"time"
 
 	vk "github.com/goki/vulkan"
 
+	"github.com/faiface/beep"
+	"github.com/faiface/beep/speaker"
 	"github.com/go-gl/glfw/v3.3/glfw"
+	"github.com/zergon321/reisen"
 	"goki.dev/vgpu/v2/vdraw"
 	"goki.dev/vgpu/v2/vgpu"
-	"goki.dev/video"
 )
+
+const (
+	frameBufferSize                   = 1024
+	sampleRate                        = 44100
+	channelCount                      = 2
+	bitDepth                          = 8
+	sampleBufferSize                  = 32 * channelCount * bitDepth * 1024
+	SpeakerSampleRate beep.SampleRate = 44100
+)
+
+// readVideoAndAudio reads video and audio frames
+// from the opened media and sends the decoded
+// data to che channels to be played.
+func readVideoAndAudio(media *reisen.Media) (<-chan *image.RGBA, <-chan [2]float64, chan error, error) {
+	frameBuffer := make(chan *image.RGBA,
+		frameBufferSize)
+	sampleBuffer := make(chan [2]float64, sampleBufferSize)
+	errs := make(chan error)
+
+	err := media.OpenDecode()
+
+	if err != nil {
+		return nil, nil, nil, err
+	}
+
+	videoStream := media.VideoStreams()[0]
+	err = videoStream.Open()
+
+	if err != nil {
+		return nil, nil, nil, err
+	}
+
+	audioStream := media.AudioStreams()[0]
+	err = audioStream.Open()
+
+	if err != nil {
+		return nil, nil, nil, err
+	}
+
+	/*err = media.Streams()[0].Rewind(60 * time.Second)
+
+	if err != nil {
+		return nil, nil, nil, err
+	}*/
+
+	/*err = media.Streams()[0].ApplyFilter("h264_mp4toannexb")
+
+	if err != nil {
+		return nil, nil, nil, err
+	}*/
+
+	go func() {
+		for {
+			packet, gotPacket, err := media.ReadPacket()
+
+			if err != nil {
+				go func(err error) {
+					errs <- err
+				}(err)
+			}
+
+			if !gotPacket {
+				break
+			}
+
+			/*hash := sha256.Sum256(packet.Data())
+			fmt.Println(base58.Encode(hash[:]))*/
+
+			switch packet.Type() {
+			case reisen.StreamVideo:
+				s := media.Streams()[packet.StreamIndex()].(*reisen.VideoStream)
+				videoFrame, gotFrame, err := s.ReadVideoFrame()
+
+				if err != nil {
+					go func(err error) {
+						errs <- err
+					}(err)
+				}
+
+				if !gotFrame {
+					break
+				}
+
+				if videoFrame == nil {
+					continue
+				}
+
+				frameBuffer <- videoFrame.Image()
+
+			case reisen.StreamAudio:
+				s := media.Streams()[packet.StreamIndex()].(*reisen.AudioStream)
+				audioFrame, gotFrame, err := s.ReadAudioFrame()
+
+				if err != nil {
+					go func(err error) {
+						errs <- err
+					}(err)
+				}
+
+				if !gotFrame {
+					break
+				}
+
+				if audioFrame == nil {
+					continue
+				}
+
+				// Turn the raw byte data into
+				// audio samples of type [2]float64.
+				reader := bytes.NewReader(audioFrame.Data())
+
+				// See the README.md file for
+				// detailed scheme of the sample structure.
+				for reader.Len() > 0 {
+					sample := [2]float64{0, 0}
+					var result float64
+					err = binary.Read(reader, binary.LittleEndian, &result)
+
+					if err != nil {
+						go func(err error) {
+							errs <- err
+						}(err)
+					}
+
+					sample[0] = result
+
+					err = binary.Read(reader, binary.LittleEndian, &result)
+
+					if err != nil {
+						go func(err error) {
+							errs <- err
+						}(err)
+					}
+
+					sample[1] = result
+					sampleBuffer <- sample
+				}
+			}
+		}
+
+		videoStream.Close()
+		audioStream.Close()
+		media.CloseDecode()
+		close(frameBuffer)
+		close(sampleBuffer)
+		close(errs)
+	}()
+
+	return frameBuffer, sampleBuffer, errs, nil
+}
+
+// streamSamples creates a new custom streamer for
+// playing audio samples provided by the source channel.
+//
+// See https://github.com/faiface/beep/wiki/Making-own-streamers
+// for reference.
+func streamSamples(sampleSource <-chan [2]float64) beep.Streamer {
+	return beep.StreamerFunc(func(samples [][2]float64) (n int, ok bool) {
+		numRead := 0
+
+		for i := 0; i < len(samples); i++ {
+			sample, ok := <-sampleSource
+
+			if !ok {
+				numRead = i + 1
+				break
+			}
+
+			samples[i] = sample
+			numRead++
+		}
+
+		if numRead < len(samples) {
+			return numRead, false
+		}
+
+		return numRead, true
+	})
+}
 
 func init() {
 	// must lock main thread for gpu!
@@ -46,12 +222,57 @@ func main() {
 
 	surfPtr, err := window.CreateWindowSurface(gp.Instance, nil)
 	if err != nil {
-		log.Println(err)
-		return
+		panic(err)
 	}
 	sf := vgpu.NewSurface(gp, vk.SurfaceFromPointer(surfPtr))
 
 	fmt.Printf("format: %s\n", sf.Format.String())
+
+	// Audio and video logic
+
+	// Initialize the audio speaker.
+	err = speaker.Init(sampleRate,
+		SpeakerSampleRate.N(time.Second/10))
+
+	if err != nil {
+		panic(err)
+	}
+
+	// Open the media file.
+	media, err := reisen.NewMedia("../videos/deer.mp4")
+
+	if err != nil {
+		panic(err)
+
+	}
+
+	// Get the FPS for playing
+	// video frames.
+	videoFPS, _ := media.Streams()[0].FrameRate()
+
+	if err != nil {
+		panic(err)
+
+	}
+
+	// SPF for frame ticker.
+	spf := 1.0 / float64(videoFPS)
+	frameDuration, err := time.ParseDuration(fmt.Sprintf("%fs", spf))
+
+	if err != nil {
+		panic(err)
+	}
+
+	// Start decoding streams.
+	var sampleSource <-chan [2]float64
+	frameBuffer, sampleSource, errChan, err := readVideoAndAudio(media)
+
+	if err != nil {
+		panic(err)
+	}
+
+	// Start playing audio samples.
+	speaker.Play(streamSamples(sampleSource))
 
 	drw := &vdraw.Drawer{}
 	drw.YIsDown = true
@@ -70,13 +291,24 @@ func main() {
 
 	stoff := 15 // causes images to wrap around sets, so this tests that..
 
-	rendImgs := func(idx int) {
-		img, err := video.ReadFrame("../videos/deer.mp4", idx)
-		if err != nil {
-			panic(err)
+	update := func(idx int) {
+		// Check for incoming errors.
+		select {
+		case err, ok := <-errChan:
+			if ok {
+				panic(err)
+			}
+		default:
 		}
-		drw.SetGoImage(stoff, 0, img, vgpu.NoFlipY)
-		drw.SyncImages()
+
+		// Read video frames and draw them.
+		frame, ok := <-frameBuffer
+
+		if ok {
+			drw.SetGoImage(stoff, 0, frame, vgpu.NoFlipY)
+			drw.SyncImages()
+		}
+
 		descIdx := 0
 		if stoff >= vgpu.MaxTexturesPerSet {
 			descIdx = 1
@@ -87,13 +319,11 @@ func main() {
 		drw.EndDraw()
 	}
 
-	_ = rendImgs
-
 	frameCount := 0
 	stTime := time.Now()
 
 	renderFrame := func() {
-		rendImgs(frameCount)
+		update(frameCount)
 		frameCount++
 
 		eTime := time.Now()
@@ -112,8 +342,7 @@ func main() {
 
 	exitC := make(chan struct{}, 2)
 
-	fpsDelay := time.Second / 60
-	fpsTicker := time.NewTicker(fpsDelay)
+	fpsTicker := time.NewTicker(frameDuration)
 	for {
 		select {
 		case <-exitC:
