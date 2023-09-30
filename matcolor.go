0f695ef011ed369430c8f8268c933d172379a1fe
Kai O'Reilly <koreilly5297@gmail.com>
2023-09-30 08:14:06 -0700

    added SetSchemes function

--- matcolor.go
+++ matcolor.go
@@ -20,7 +20,8 @@ var Palette = matcolor.NewPalette(matcolor.KeyFromPrimary(color.RGBA{66, 133, 24
 // Schemes are the main global Material Design 3 color schemes.
 // They should not be used for accessing the current color scheme;
 // see [Scheme] for that. Instead, they should be set if you want
-// to define your own custom color schemes for your app.
+// to define your own custom color schemes for your app. The recommended
+// way to set the Schemes is through the [SetSchemes] function.
 var Schemes = matcolor.NewSchemes(Palette)
 
 // Scheme is the main currently active global Material Design 3
@@ -28,9 +29,18 @@ var Schemes = matcolor.NewSchemes(Palette)
 // access the color scheme; ideally, almost all color values should
 // be set to something in here. For more specific tones of colors,
 // see [Palette]. For setting the color schemes of your app, see
-// [Schemes]. For setting whether this scheme to light or dark,
-// see [SetScheme].
+// [Schemes] and [SetSchemes]. For setting whether this scheme to
+// light or dark, see [SetScheme].
 var Scheme = &Schemes.Light
+
+// SetSchemes sets [Schemes], [Scheme], and [Palette] based on the
+// given [matcolor.Key]. It is the main way that end-user code should
+// set the color schemes to something custom.
+func SetSchemes(key *matcolor.Key) {
+	Palette = matcolor.NewPalette(key)
+	Schemes = matcolor.NewSchemes(Palette)
+	SetScheme(matcolor.SchemeIsDark)
+}
 
 // SetScheme sets the value of [Scheme] to either [Schemes.Dark]
 // or [Schemes.Light], based on the given value of whether the
