943fb8741ecd40ac64a94ebbdde37aa912762baa
Kai O'Reilly <koreilly5297@gmail.com>
2023-09-30 20:41:42 -0700

    added keyboard events

--- driver/android/event.go
+++ driver/android/event.go
@@ -5,3 +5,24 @@
 //go:build android
 
 package android
+
+import "C"
+import (
+	"goki.dev/goosi/events"
+	"goki.dev/goosi/events/key"
+)
+
+//export keyboardTyped
+func keyboardTyped(str *C.char) {
+	for _, r := range C.GoString(str) {
+		code := convAndroidKeyCode(r)
+		theApp.window.EvMgr.Key(events.KeyDown, r, code, 0) // TODO: modifiers
+		theApp.window.EvMgr.Key(events.KeyUp, r, code, 0)   // TODO: modifiers
+	}
+}
+
+//export keyboardDelete
+func keyboardDelete() {
+	theApp.window.EvMgr.Key(events.KeyDown, 0, key.CodeDeleteBackspace, 0) // TODO: modifiers
+	theApp.window.EvMgr.Key(events.KeyUp, 0, key.CodeDeleteBackspace, 0)   // TODO: modifiers
+}
