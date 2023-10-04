8dcf6067208a0e9159d41337648022290e5c3570
Kai O'Reilly <koreilly5297@gmail.com>
2023-10-04 13:36:34 -0700

    started run command

---
+++ packman/run.go
@@ -0,0 +1,12 @@
+// Copyright (c) 2023, The GoKi Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style
+// license that can be found in the LICENSE file.
+
+package packman
+
+import "goki.dev/goki/config"
+
+func Run(c *config.Config) error {
+	// TODO(kai): implement run
+	return nil
+}
