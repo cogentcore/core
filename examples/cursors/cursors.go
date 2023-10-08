1d1197c13f7acb34b5f50a5c637237556ecccabf
Kai O'Reilly <koreilly5297@gmail.com>
2023-10-08 11:44:24 -0700

    started cursors example

---
+++ examples/cursors/cursors.go
@@ -0,0 +1,18 @@
+// Copyright (c) 2023, The GoKi Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style
+// license that can be found in the LICENSE file.
+
+package main
+
+import (
+	"goki.dev/gi/v2/gi"
+	"goki.dev/gi/v2/gimain"
+)
+
+func main() { gimain.Run(app) }
+
+func app() {
+	scene := gi.StageScene().SetTitle("Cursors")
+	gi.NewLabel(scene).SetText("The GoGi Standard Cursors").SetType(gi.LabelHeadlineSmall)
+	gi.NewWindow(scene).Run().Wait()
+}
