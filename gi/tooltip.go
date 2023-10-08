f695e296715b58b69aa05d246810788124b76f34
Kai O'Reilly <koreilly5297@gmail.com>
2023-10-08 15:57:08 -0700

    fixed Stage *Type functions, improved tooltip styling, fixed button tooltips, cleaned up tooltip code

--- gi/tooltip.go
+++ gi/tooltip.go
@@ -12,20 +12,6 @@ import (
 	"goki.dev/girl/units"
 )
 
-// TooltipConfigStyles configures the default styles
-// for the given tooltip frame with the given parent.
-// It should be called on tooltips when they are created.
-func TooltipConfigStyles(tooltip *Scene) {
-	tooltip.AddStyles(func(s *styles.Style) {
-		s.Border.Style.Set(styles.BorderNone)
-		s.Border.Radius = styles.BorderRadiusExtraSmall
-		s.Padding.Set(units.Dp(8 * Prefs.DensityMul()))
-		s.BackgroundColor.SetSolid(colors.Scheme.InverseSurface)
-		s.Color = colors.Scheme.InverseOnSurface
-		s.BoxShadow = styles.BoxShadow1() // STYTODO: not sure whether we should have this
-	})
-}
-
 // NewTooltipScene returns a new Tooltip stage with given scene contents,
 // in connection with given widget (which provides key context).
 // Make further configuration choices using Set* methods, which
@@ -35,19 +21,24 @@ func NewTooltipScene(sc *Scene, ctx Widget) *PopupStage {
 	return NewPopupStage(Tooltip, sc, ctx)
 }
 
-// NewTooltip pops up a scene displaying the tooltip text for the given widget
-// at the given position.
+// NewTooltip returns a new scene displaying the tooltip text
+// for the given widget at the given position.
 func NewTooltip(w Widget, pos image.Point) *PopupStage {
+	return NewTooltipText(w, w.AsWidget().Tooltip, pos)
+}
+
+// NewTooltipText returns a new scene displaying the given tooltip text
+// for the given widget at the given position.
+func NewTooltipText(w Widget, tooltip string, pos image.Point) *PopupStage {
 	sc := StageScene(w.Name() + "-tooltip")
 	sc.Geom.Pos = pos
 	sc.AddStyles(func(s *styles.Style) {
 		s.Border.Radius = styles.BorderRadiusExtraSmall
+		s.Padding.Set(units.Dp(8 * Prefs.DensityMul()))
 		s.BackgroundColor.SetSolid(colors.Scheme.InverseSurface)
+		s.Color = colors.Scheme.InverseOnSurface
+		s.BoxShadow = styles.BoxShadow1()
 	})
-	NewLabel(sc, "tooltip").SetText("Hello, World!").
-		SetType(LabelBodyMedium).
-		AddStyles(func(s *styles.Style) {
-			s.Color = colors.Scheme.InverseOnSurface
-		})
+	NewLabel(sc, "tooltip").SetText(tooltip).SetType(LabelBodyMedium)
 	return NewTooltipScene(sc, w)
 }
