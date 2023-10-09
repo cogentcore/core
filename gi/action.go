4eace73c41c3460f092acb70988061ae48aff02f
Kai O'Reilly <koreilly5297@gmail.com>
2023-10-08 19:45:21 -0700

    disable selection on button labels

--- gi/action.go
+++ gi/action.go
@@ -156,6 +156,8 @@ func (ac *Action) OnChildAdded(child ki.Ki) {
 			})
 		case "label":
 			w.AddStyles(func(s *styles.Style) {
+				s.SetAbilities(false, states.Selectable)
+				s.Cursor = cursors.None
 				s.Margin.Set()
 				s.Padding.Set()
 			})
