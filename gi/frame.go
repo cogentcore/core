7c2501ebe9c893158a7fc752861d5e5152aefcae
Kai O'Reilly <koreilly5297@gmail.com>
2023-10-11 10:23:48 -0700

    set horizontal padding on toolbar to fix broken border radius

--- gi/frame.go
+++ gi/frame.go
@@ -69,7 +69,6 @@ func (fr *Frame) FrameStdRender(sc *Scene) {
 	defer fr.RenderUnlock(rs)
 
 	// fmt.Println(fr.Nm)
-
 	fr.RenderStdBox(sc, st)
 	//	if fr.Lay == LayoutGrid && fr.Stripes != NoStripes && Prefs.Params.ZebraStripeWeight != 0 {
 	//		fr.RenderStripes(sc)
