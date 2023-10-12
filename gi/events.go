c7f350b923b275c15e018af670c950c1605a8da7
Kai O'Reilly <koreilly5297@gmail.com>
2023-10-11 19:36:34 -0700

    fixed more event sending issues

--- gi/events.go
+++ gi/events.go
@@ -73,7 +73,7 @@ func (wb *WidgetBase) OnSelect(fun func(e events.Event)) Widget {
 // call HandleEvent directly for any existing events.
 func (wb *WidgetBase) Send(typ events.Types, orig ...events.Event) {
 	var e events.Event
-	if len(orig) > 0 {
+	if len(orig) > 0 && orig[0] != nil {
 		e = orig[0].Clone()
 		e.AsBase().Typ = typ
 	} else {
