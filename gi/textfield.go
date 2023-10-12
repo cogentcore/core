2695cfb0539396e3695370cb0bd2d482ae446e37
Kai O'Reilly <koreilly5297@gmail.com>
2023-10-12 15:47:52 -0700

    try increased state layer for text field style

--- gi/textfield.go
+++ gi/textfield.go
@@ -179,7 +179,7 @@ func (tf *TextField) TextFieldStyles() {
 			s.Border.Width.Set()
 			s.Border.Color.Set()
 			s.Border.Radius = styles.BorderRadiusExtraSmallTop
-			s.BackgroundColor.SetSolid(colors.Scheme.SurfaceContainer)
+			s.StateLayer += 0.06
 
 			s.MaxBorder = s.Border
 			s.MaxBorder.Width.Bottom = units.Dp(2)
