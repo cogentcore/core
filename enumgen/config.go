72d6332c9feeed29cc3afd76ec3a54817197d021
Kai O'Reilly <koreilly5297@gmail.com>
2023-10-12 22:56:12 -0700

    removed desc struct tags

--- enumgen/config.go
+++ enumgen/config.go
@@ -15,39 +15,39 @@ package enumgen
 // used by enumgen
 type Config struct {
 
-	// [def: .] the source directory to run enumgen on (can be set to multiple through paths like ./...)
-	Dir string `def:"." posarg:"0" required:"-" desc:"the source directory to run enumgen on (can be set to multiple through paths like ./...)"`
+	// the source directory to run enumgen on (can be set to multiple through paths like ./...)
+	Dir string `def:"." posarg:"0" required:"-"`
 
-	// [def: enumgen.go] the output file location relative to the package on which enumgen is being called
-	Output string `def:"enumgen.go" desc:"the output file location relative to the package on which enumgen is being called"`
+	// the output file location relative to the package on which enumgen is being called
+	Output string `def:"enumgen.go"`
 
 	// if specified, the enum item transformation method (eg: snake, kebab, lower)
-	Transform string `desc:"if specified, the enum item transformation method (eg: snake, kebab, lower)"`
+	Transform string
 
 	// if specified, a comma-separated list of prefixes to trim from each item
-	TrimPrefix string `desc:"if specified, a comma-separated list of prefixes to trim from each item"`
+	TrimPrefix string
 
 	// if specified, the prefix to add to each item
-	AddPrefix string `desc:"if specified, the prefix to add to each item"`
+	AddPrefix string
 
 	// whether to use line comment text as printed text when present
-	LineComment bool `desc:"whether to use line comment text as printed text when present"`
+	LineComment bool
 
-	// [def: true] whether to accept lowercase versions of enum names in SetString
-	AcceptLower bool `def:"true" desc:"whether to accept lowercase versions of enum names in SetString"`
+	// whether to accept lowercase versions of enum names in SetString
+	AcceptLower bool `def:"true"`
 
-	// [def: true] whether to generate text marshaling methods
-	Text bool `def:"true" desc:"whether to generate text marshaling methods"`
+	// whether to generate text marshaling methods
+	Text bool `def:"true"`
 
 	// whether to generate JSON marshaling methods (note that text marshaling methods will also work for JSON, so this should be unnecessary in almost all cases; see the text option)
-	JSON bool `desc:"whether to generate JSON marshaling methods (note that text marshaling methods will also work for JSON, so this should be unnecessary in almost all cases; see the text option)"`
+	JSON bool
 
 	// whether to generate YAML marshaling methods
-	YAML bool `desc:"whether to generate YAML marshaling methods"`
+	YAML bool
 
 	// whether to generate methods that implement the SQL Scanner and Valuer interfaces
-	SQL bool `desc:"whether to generate methods that implement the SQL Scanner and Valuer interfaces"`
+	SQL bool
 
 	// whether to generate GraphQL marshaling methods for gqlgen
-	GQL bool `desc:"whether to generate GraphQL marshaling methods for gqlgen"`
+	GQL bool
 }
