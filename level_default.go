a23e8ad6673a6eb5e0c6d5421e7bd56bbda91749
Kai O'Reilly <koreilly5297@gmail.com>
2023-10-13 17:14:52 -0700

    updated level build tags

--- level_default.go
+++ level_default.go
@@ -2,10 +2,10 @@
 // Use of this source code is governed by a BSD-style
 // license that can be found in the LICENSE file.
 
-//go:build !debug && !release
+//go:build !develop && !debug
 
 package grog
 
 import "log/slog"
 
-var defaultUserLevel = slog.LevelInfo
+var defaultUserLevel = slog.LevelWarn
--- level_develop.go
+++ level_develop.go
@@ -2,10 +2,10 @@
 // Use of this source code is governed by a BSD-style
 // license that can be found in the LICENSE file.
 
-//go:build release
+//go:build develop
 
 package grog
 
 import "log/slog"
 
-var defaultUserLevel = slog.LevelWarn
+var defaultUserLevel = slog.LevelInfo
