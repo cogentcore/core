66d3d4060bb8cf7345b4d8f687170ac0a05dab7f
Kai O'Reilly <koreilly5297@gmail.com>
2023-10-14 11:11:33 -0700

    added basic test

---
+++ sentencecase/sentencecase_test.go
@@ -0,0 +1,17 @@
+// Copyright (c) 2023, The GoKi Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style
+// license that can be found in the LICENSE file.
+
+package sentencecase
+
+import (
+	"fmt"
+	"testing"
+)
+
+func TestSentenceCase(t *testing.T) {
+	AddProperNouns("Google")
+	src := "ThisIsAStringInSentenceCaseThatIWroteWithTheHelpOfURLsAndGoogle"
+	have := Of(src)
+	fmt.Println(have)
+}
