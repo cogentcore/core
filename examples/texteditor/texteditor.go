da324b648e5f191d4d6922a93ca256abe7f90789
Kai O'Reilly <koreilly5297@gmail.com>
2023-10-15 11:50:54 -0700

    renamed texteditor example

--- examples/texteditor/texteditor.go
+++ examples/texteditor/texteditor.go
@@ -24,15 +24,15 @@ func app() {
 
 	goosi.ZoomFactor = 2
 
-	gi.SetAppName("textview")
-	gi.SetAppAbout(`This is a demo of the textview.View in the <b>GoGi</b> graphical interface system, within the <b>GoKi</b> tree framework.  See <a href="https://github.com/goki">GoKi on GitHub</a>`)
+	gi.SetAppName("texteditor")
+	gi.SetAppAbout(`This is a demo of the texteditor.Editor in the <b>GoGi</b> graphical interface system, within the <b>GoKi</b> tree framework.  See <a href="https://github.com/goki">GoKi on GitHub</a>`)
 
-	sc := gi.NewScene("gogi-textview-test").SetTitle("GoGi textview.View Test")
+	sc := gi.NewScene("gogi-texteditor-test").SetTitle("GoGi texteditor.Editor Test")
 
 	trow := gi.NewLayout(sc, "trow").SetLayout(gi.LayoutHoriz)
 	trow.SetStretchMaxWidth()
 
-	hdrText := `This is a <b>test</b> of the textview.View`
+	hdrText := `This is a <b>test</b> of the texteditor.Editor`
 	title := gi.NewLabel(trow, "title").SetText(hdrText).SetType(gi.LabelHeadlineSmall)
 	title.Style(func(s *styles.Style) {
 		s.Text.WhiteSpace = styles.WhiteSpaceNowrap
@@ -52,14 +52,14 @@ func app() {
 
 	// generally need to put text view within its own layout for scrolling
 
-	txed1 := texteditor.NewView(splt, "textview-1")
+	txed1 := texteditor.NewView(splt, "texteditor-1")
 	txed1.Style(func(s *styles.Style) {
 		s.SetStretchMax()
 		s.SetMinPrefWidth(units.Ch(20))
 		s.SetMinPrefHeight(units.Ch(10))
 	})
 
-	txed2 := texteditor.NewView(splt, "textview-2")
+	txed2 := texteditor.NewView(splt, "texteditor-2")
 	txed2.Style(func(s *styles.Style) {
 		s.SetStretchMax()
 		s.SetMinPrefWidth(units.Ch(20))
