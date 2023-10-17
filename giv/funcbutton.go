c42e95edcd10e031868af044e9eaf208f97b5064
Kai O'Reilly <koreilly5297@gmail.com>
2023-10-16 18:10:06 -0700

    improved FuncButton structure

--- giv/funcbutton.go
+++ giv/funcbutton.go
@@ -6,6 +6,7 @@ package giv
 
 import (
 	"log/slog"
+	"reflect"
 	"strings"
 
 	"github.com/iancoleman/strcase"
@@ -28,14 +29,19 @@ func NewFuncButton(par ki.Ki, fun any) *gi.Button {
 		slog.Error("programmer error: cannot use giv.NewFuncButton with a function that has not been added to gti", "function", fnm)
 		return nil
 	}
+	return NewFuncButtonImpl(par, f, reflect.ValueOf(fun))
+}
+
+// NewFuncButtonImpl is the underlying implementation of [NewFuncButton].
+func NewFuncButtonImpl(par ki.Ki, gfun *gti.Func, rfun reflect.Value) *gi.Button {
 	// get name without package
-	snm := f.Name
+	snm := gfun.Name
 	li := strings.LastIndex(snm, ".")
 	if li >= 0 {
 		snm = snm[li+1:] // must also get rid of "."
 	}
 	bt := gi.NewButton(par, snm).SetText(sentencecase.Of(snm))
-	bt.SetTooltip(f.Doc)
+	bt.SetTooltip(gfun.Doc)
 	// we default to the icon with the same name as
 	// the function, if it exists
 	ic := icons.Icon(strcase.ToSnake(snm))
@@ -43,4 +49,15 @@ func NewFuncButton(par ki.Ki, fun any) *gi.Button {
 		bt.SetIcon(ic)
 	}
 	return bt
+}
+
+// NewMethodButtonImpl is the underlying implementation of [NewMethodButton].
+func NewMethodButtonImpl(par ki.Ki, gmet *gti.Method, rmet reflect.Value) *gi.Button {
+	return NewFuncButtonImpl(par, &gti.Func{
+		Name:       gmet.Name,
+		Doc:        gmet.Doc,
+		Directives: gmet.Directives,
+		Args:       gmet.Args,
+		Returns:    gmet.Returns,
+	}, rmet)
 }
