933c820b52a6214b672d31cadc3319dbc2c8bc76
Kai O'Reilly <koreilly5297@gmail.com>
2023-10-18 17:15:10 -0700

    got FuncButton function calling working

--- giv/funcbutton.go
+++ giv/funcbutton.go
@@ -11,10 +11,12 @@ import (
 
 	"github.com/iancoleman/strcase"
 	"goki.dev/gi/v2/gi"
+	"goki.dev/girl/states"
 	"goki.dev/glop/sentencecase"
 	"goki.dev/goosi/events"
 	"goki.dev/gti"
 	"goki.dev/icons"
+	"goki.dev/laser"
 )
 
 // FuncButton is a button that is set up to call a function when it
@@ -42,10 +44,21 @@ type FuncButton struct {
 	Confirm bool
 	// ShowReturn is whether to display the return values of
 	// the function (and a success message if there are none).
-	ShowReturn bool
+	// The way that the return values are shown is determined
+	// by ShowReturnAsDialog. ShowReturn is on by default.
+	ShowReturn bool `def:"true"`
 	// ShowReturnAsDialog, if and only if ShowReturn is true,
-	// indicates to show the return values of the function
+	// indicates to show the return values of the function in
+	// a dialog, instead of in a snackbar, as they are by default.
+	// If there is a return value from the function of a complex
+	// type (struct, slice, map), then ShowReturnAsDialog will
+	// automatically be set to true.
 	ShowReturnAsDialog bool
+}
+
+func (fb *FuncButton) OnInit() {
+	fb.Button.OnInit()
+	fb.ShowReturn = true
 }
 
 // SetFunc sets the function associated with the FuncButton to the
@@ -112,62 +125,62 @@ func (fb *FuncButton) SetFuncImpl(gfun *gti.Func, rfun reflect.Value) *FuncButto
 // CallFunc calls the function or method associated with this button,
 // prompting the user for any arguments.
 func (fb *FuncButton) CallFunc() {
-	// if fb.Func.Args.Len() == 0 {
-	// 	if !fb.Confirm {
-	// 		rets := fb.ReflectFunc.Call(nil)
-	// 		if !c.ShowResult {
-	// 			return
-	// 		}
-	// 		ShowReturnsDialog(ctx, rets, c)
-	// 		return
-	// 	}
-	// 	gi.NewStdDialog(ctx, gi.DlgOpts{Title: c.Label + "?", Prompt: "Are you sure you want to run " + c.Label + "? " + c.Doc, Ok: true, Cancel: true},
-	// 		func(dlg *gi.Dialog) {
-	// 			if !dlg.Accepted {
-	// 				return
-	// 			}
-	// 			rets := rfun.Call(nil)
-	// 			if !c.ShowResult {
-	// 				return
-	// 			}
-	// 			ShowReturnsDialog(ctx, rets, c)
-	// 		}).Run()
-	// 	return
-	// }
-	// args := ArgsForFunc(rfun, c)
-	// ArgViewDialog(
-	// 	ctx,
-	// 	DlgOpts{Title: c.Label, Prompt: c.Doc, Ok: true, Cancel: true},
-	// 	args,
-	// 	func(dlg *gi.Dialog) {
-	// 		if !dlg.Accepted {
-	// 			return
-	// 		}
-	// 		rargs := make([]reflect.Value, len(args))
-	// 		for i, arg := range args {
-	// 			rargs[i] = laser.NonPtrValue(arg.Val)
-	// 		}
+	if fb.Func.Args.Len() == 0 {
+		if !fb.Confirm {
+			rets := fb.ReflectFunc.Call(nil)
+			if !fb.ShowReturn {
+				return
+			}
+			fb.ShowReturnsDialog(rets)
+			return
+		}
+		gi.NewStdDialog(fb.This().(gi.Widget), gi.DlgOpts{Title: fb.Text + "?", Prompt: "Are you sure you want to run " + fb.Text + "? " + fb.Tooltip, Ok: true, Cancel: true},
+			func(dlg *gi.Dialog) {
+				if !dlg.Accepted {
+					return
+				}
+				rets := fb.ReflectFunc.Call(nil)
+				if !fb.ShowReturn {
+					return
+				}
+				fb.ShowReturnsDialog(rets)
+			}).Run()
+		return
+	}
+	args := fb.Args()
+	ArgViewDialog(
+		fb.This().(gi.Widget),
+		DlgOpts{Title: fb.Text, Prompt: fb.Tooltip, Ok: true, Cancel: true},
+		args,
+		func(dlg *gi.Dialog) {
+			if !dlg.Accepted {
+				return
+			}
+			rargs := make([]reflect.Value, len(args))
+			for i, arg := range args {
+				rargs[i] = laser.NonPtrValue(arg.Val)
+			}
 
-	// 		if !c.Confirm {
-	// 			rets := rfun.Call(rargs)
-	// 			if !c.ShowResult {
-	// 				return
-	// 			}
-	// 			ShowReturnsDialog(ctx, rets, c)
-	// 		}
-	// 		gi.NewStdDialog(ctx, gi.DlgOpts{Title: c.Label + "?", Prompt: "Are you sure you want to run " + c.Label + "? " + c.Doc, Ok: true, Cancel: true},
-	// 			func(dlg *gi.Dialog) {
-	// 				if !dlg.Accepted {
-	// 					return
-	// 				}
-	// 				rets := rfun.Call(rargs)
-	// 				if !c.ShowResult {
-	// 					return
-	// 				}
-	// 				ShowReturnsDialog(ctx, rets, c)
-	// 			}).Run()
-	// 	},
-	// ).Run()
+			if !fb.Confirm {
+				rets := fb.ReflectFunc.Call(rargs)
+				if !fb.ShowReturn {
+					return
+				}
+				fb.ShowReturnsDialog(rets)
+			}
+			gi.NewStdDialog(fb.This().(gi.Widget), gi.DlgOpts{Title: fb.Text + "?", Prompt: "Are you sure you want to run " + fb.Text + "? " + fb.Tooltip, Ok: true, Cancel: true},
+				func(dlg *gi.Dialog) {
+					if !dlg.Accepted {
+						return
+					}
+					rets := fb.ReflectFunc.Call(rargs)
+					if !fb.ShowReturn {
+						return
+					}
+					fb.ShowReturnsDialog(rets)
+				}).Run()
+		},
+	).Run()
 }
 
 // SetMethodImpl is the underlying implementation of [FuncButton.SetFunc] for methods.
@@ -180,4 +193,61 @@ func (fb *FuncButton) SetMethodImpl(gmet *gti.Method, rmet reflect.Value) *FuncB
 		Args:       gmet.Args,
 		Returns:    gmet.Returns,
 	}, rmet)
+}
+
+// ShowReturnsDialog runs a dialog displaying the given function return
+// values for the function associated with the function button.
+func (fb *FuncButton) ShowReturnsDialog(rets []reflect.Value) {
+	if len(rets) == 0 {
+		gi.NewSnackbar(fb.This().(gi.Widget), gi.SnackbarOpts{Text: fb.Text + " succeeded"}).Run()
+		return
+	}
+	ac := fb.Returns(rets)
+	ArgViewDialog(fb.This().(gi.Widget), DlgOpts{Title: "Result of " + fb.Text, Prompt: fb.Tooltip, Ok: true}, ac, nil).Run()
+}
+
+// ArgsForFunc returns the appropriate [ArgConfig] objects for the
+// arguments of the function associated with the function button.
+func (fb *FuncButton) Args() []ArgConfig {
+	res := make([]ArgConfig, fb.Func.Args.Len())
+	for i, kv := range fb.Func.Args.Order {
+		arg := kv.Val
+		ra := ArgConfig{
+			Name:  arg.Name,
+			Label: sentencecase.Of(arg.Name),
+			Doc:   arg.Doc,
+		}
+
+		atyp := fb.ReflectFunc.Type().In(i)
+		ra.Val = reflect.New(atyp)
+
+		ra.View = ToValue(ra.Val.Interface(), "")
+		ra.View.SetSoloValue(ra.Val)
+		ra.View.SetName(ra.Name)
+		res[i] = ra
+	}
+	return res
+}
+
+// ReturnsForFunc returns the appropriate [ArgConfig] objects for the given return values
+// of the function associated with the function button.
+func (fb *FuncButton) Returns(rets []reflect.Value) []ArgConfig {
+	res := make([]ArgConfig, fb.Func.Returns.Len())
+	for i, kv := range fb.Func.Returns.Order {
+		ret := kv.Val
+		ra := ArgConfig{
+			Name:  ret.Name,
+			Label: sentencecase.Of(ret.Name),
+			Doc:   ret.Doc,
+		}
+
+		ra.Val = rets[i]
+
+		ra.View = ToValue(ra.Val.Interface(), "")
+		ra.View.SetSoloValue(ra.Val)
+		ra.View.SetName(ra.Name)
+		ra.View.SetFlag(true, states.ReadOnly)
+		res[i] = ra
+	}
+	return res
 }
