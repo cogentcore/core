72f032b5d97e73771cde8f79fd2f501b33c42849
Kai O'Reilly <koreilly5297@gmail.com>
2023-10-22 10:27:49 -0700

    updated version to v0.0.4

--- go.mod
+++ go.mod
@@ -2,4 +2,7 @@ module goki.dev/icons
 
 go 1.21.0
 
-require github.com/iancoleman/strcase v0.3.0
+require (
+	github.com/iancoleman/strcase v0.3.0
+	goki.dev/glop v0.1.2
+)
--- version.go
+++ version.go
@@ -4,9 +4,9 @@ package icons
 
 const (
 	// Version is the version of this package being used
-	Version = "v0.0.3"
+	Version = "v0.0.4"
 	// GitCommit is the commit just before the latest version commit
-	GitCommit = "e4454f2"
+	GitCommit = "d755bf9"
 	// VersionDate is the date-time of the latest version commit in UTC (in the format 'YYYY-MM-DD HH:MM', which is the Go format '2006-01-02 15:04')
-	VersionDate = "2023-09-25 02:00"
+	VersionDate = "2023-10-22 17:27"
 )
