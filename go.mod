98bbd81d21b24e65e8da38452da89e1b705cf59f
Kai O'Reilly <koreilly5297@gmail.com>
2023-10-22 10:40:58 -0700

    updated version to v0.8.16

--- go.mod
+++ go.mod
@@ -4,7 +4,7 @@ go 1.21.0
 
 require (
 	github.com/srwiley/rasterx v0.0.0-20220730225603-2ab79fcdd4ef
-	goki.dev/cam v0.9.19
+	goki.dev/cam v0.9.20
 	goki.dev/grows v0.3.4
 	goki.dev/laser v0.1.7
 	goki.dev/mat32/v2 v2.0.0-dev0.0.7
@@ -12,7 +12,7 @@ require (
 )
 
 require (
-	goki.dev/enums v0.9.21 // indirect
+	goki.dev/enums v0.9.22 // indirect
 	goki.dev/glop v0.1.2 // indirect
 	golang.org/x/image v0.13.0 // indirect
 	golang.org/x/text v0.13.0 // indirect
--- version.go
+++ version.go
@@ -4,9 +4,9 @@ package colors
 
 const (
 	// Version is the version of this package being used
-	Version = "v0.8.15"
+	Version = "v0.8.16"
 	// GitCommit is the commit just before the latest version commit
-	GitCommit = "3de4d61"
+	GitCommit = "d3c79bb"
 	// VersionDate is the date-time of the latest version commit in UTC (in the format 'YYYY-MM-DD HH:MM', which is the Go format '2006-01-02 15:04')
-	VersionDate = "2023-10-22 17:39"
+	VersionDate = "2023-10-22 17:40"
 )
