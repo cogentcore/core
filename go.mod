64b488ddaeb9c43aae4af7e3b94eb638e2319742
Kai O'Reilly <koreilly5297@gmail.com>
2023-10-22 13:25:18 -0700

    updated version to v2.0.0-dev0.0.8

--- go.mod
+++ go.mod
@@ -6,10 +6,10 @@ require (
 	github.com/anthonynsimon/bild v0.13.0
 	github.com/go-gl/glfw/v3.3/glfw v0.0.0-20221017161538-93cebf72946b
 	github.com/goki/vulkan v1.0.7
-	goki.dev/enums v0.9.28
+	goki.dev/enums v0.9.31
 	goki.dev/glop v0.1.3
-	goki.dev/gti v0.1.7
-	goki.dev/laser v0.1.9
+	goki.dev/gti v0.1.8
+	goki.dev/laser v0.1.10
 	goki.dev/mat32/v2 v2.0.0-dev0.0.7
 	goki.dev/mobile v0.5.4
 	goki.dev/ordmap v0.5.6
--- vgpu/version.go
+++ vgpu/version.go
@@ -4,9 +4,9 @@ package vgpu
 
 const (
 	// Version is the version of this package being used
-	Version = "v2.0.0-dev0.0.7"
+	Version = "v2.0.0-dev0.0.8"
 	// GitCommit is the commit just before the latest version commit
-	GitCommit = "df87a48"
+	GitCommit = "36f8557"
 	// VersionDate is the date-time of the latest version commit in UTC (in the format 'YYYY-MM-DD HH:MM', which is the Go format '2006-01-02 15:04')
-	VersionDate = "2023-10-22 18:10"
+	VersionDate = "2023-10-22 20:25"
 )
