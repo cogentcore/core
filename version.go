241e67d92b1c98b1e60f58334356f7817b2d8415
Kai O'Reilly <koreilly5297@gmail.com>
2023-10-22 11:08:46 -0700

    updated version to v0.8.21

--- version.go
+++ version.go
@@ -4,9 +4,9 @@ package colors
 
 const (
 	// Version is the version of this package being used
-	Version = "v0.8.20"
+	Version = "v0.8.21"
 	// GitCommit is the commit just before the latest version commit
-	GitCommit = "7767ba4"
+	GitCommit = "b34ded1"
 	// VersionDate is the date-time of the latest version commit in UTC (in the format 'YYYY-MM-DD HH:MM', which is the Go format '2006-01-02 15:04')
-	VersionDate = "2023-10-22 18:03"
+	VersionDate = "2023-10-22 18:08"
 )
