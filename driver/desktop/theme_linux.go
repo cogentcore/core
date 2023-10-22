a94b9674272ac05a1961af231bc8dc65ae032ca1
Kai O'Reilly <koreilly5297@gmail.com>
2023-10-22 13:32:02 -0700

    added placeholder linux IsDark impl

---
+++ driver/desktop/theme_linux.go
@@ -0,0 +1,14 @@
+// Copyright (c) 2023, The GoKi Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style
+// license that can be found in the LICENSE file.
+
+//go:build linux
+
+package desktop
+
+// TODO(kai): implement IsDark on linux
+
+// IsDark returns whether the system color theme is dark (as opposed to light).
+func (app *appImpl) IsDark() bool {
+	return false
+}
