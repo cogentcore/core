d3c79bb31d3def14bf0915b761e9186262b336fd
Kai O'Reilly <koreilly5297@gmail.com>
2023-10-22 10:39:27 -0700

    updated version to v0.8.15

--- go.mod
+++ go.mod
@@ -4,10 +4,10 @@ go 1.21.0
 
 require (
 	github.com/srwiley/rasterx v0.0.0-20220730225603-2ab79fcdd4ef
-	goki.dev/cam v0.9.18
+	goki.dev/cam v0.9.19
 	goki.dev/grows v0.3.4
 	goki.dev/laser v0.1.7
-	goki.dev/mat32/v2 v2.0.0-dev0.0.6
+	goki.dev/mat32/v2 v2.0.0-dev0.0.7
 	golang.org/x/net v0.17.0
 )
 
--- version.go
+++ version.go
@@ -4,9 +4,9 @@ package colors
 
 const (
 	// Version is the version of this package being used
-	Version = "v0.8.14"
+	Version = "v0.8.15"
 	// GitCommit is the commit just before the latest version commit
-	GitCommit = "a6b4a26"
+	GitCommit = "3de4d61"
 	// VersionDate is the date-time of the latest version commit in UTC (in the format 'YYYY-MM-DD HH:MM', which is the Go format '2006-01-02 15:04')
-	VersionDate = "2023-10-22 17:28"
+	VersionDate = "2023-10-22 17:39"
 )
