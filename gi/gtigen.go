b7c0f1035cbc20926aeca20cdf45e8e96f6d167d
Kai O'Reilly <koreilly5297@gmail.com>
2023-10-26 17:14:36 -0700

    updated generated code

--- gi/gtigen.go
+++ gi/gtigen.go
@@ -618,87 +618,6 @@ func (t *Image) SetCustomContextMenu(v func(m *Scene)) *Image {
 	return t
 }
 
-var _ = gti.AddType(&gti.Type{
-	Name:      "goki.dev/gi/v2/gi.KeyMapsItem",
-	ShortName: "gi.KeyMapsItem",
-	IDName:    "key-maps-item",
-	Doc:       "KeyMapsItem is an entry in a KeyMaps list",
-	Directives: gti.Directives{
-		&gti.Directive{Tool: "gti", Directive: "add", Args: []string{"-setters"}},
-	},
-	Fields: ordmap.Make([]ordmap.KeyVal[string, *gti.Field]{
-		{"Name", &gti.Field{Name: "Name", Type: "string", LocalType: "string", Doc: "name of keymap", Directives: gti.Directives{}, Tag: "width:\"20\""}},
-		{"Desc", &gti.Field{Name: "Desc", Type: "string", LocalType: "string", Doc: "description of keymap -- good idea to include source it was derived from", Directives: gti.Directives{}, Tag: ""}},
-		{"Map", &gti.Field{Name: "Map", Type: "goki.dev/gi/v2/gi.KeyMap", LocalType: "KeyMap", Doc: "to edit key sequence click button and type new key combination; to edit function mapped to key sequence choose from menu", Directives: gti.Directives{}, Tag: ""}},
-	}),
-	Embeds:  ordmap.Make([]ordmap.KeyVal[string, *gti.Field]{}),
-	Methods: ordmap.Make([]ordmap.KeyVal[string, *gti.Method]{}),
-})
-
-// SetName sets the [KeyMapsItem.Name]:
-// name of keymap
-func (t *KeyMapsItem) SetName(v string) *KeyMapsItem {
-	t.Name = v
-	return t
-}
-
-// SetDesc sets the [KeyMapsItem.Desc]:
-// description of keymap -- good idea to include source it was derived from
-func (t *KeyMapsItem) SetDesc(v string) *KeyMapsItem {
-	t.Desc = v
-	return t
-}
-
-// SetMap sets the [KeyMapsItem.Map]:
-// to edit key sequence click button and type new key combination; to edit function mapped to key sequence choose from menu
-func (t *KeyMapsItem) SetMap(v KeyMap) *KeyMapsItem {
-	t.Map = v
-	return t
-}
-
-var _ = gti.AddType(&gti.Type{
-	Name:      "goki.dev/gi/v2/gi.KeyMaps",
-	ShortName: "gi.KeyMaps",
-	IDName:    "key-maps",
-	Doc:       "KeyMaps is a list of KeyMap's -- users can edit these in Prefs -- to create\na custom one, just duplicate an existing map, rename, and customize",
-	Directives: gti.Directives{
-		&gti.Directive{Tool: "gti", Directive: "add", Args: []string{}},
-	},
-
-	Methods: ordmap.Make([]ordmap.KeyVal[string, *gti.Method]{
-		{"OpenJSON", &gti.Method{Name: "OpenJSON", Doc: "OpenJSON opens keymaps from a JSON-formatted file.\nYou can save and open key maps to / from files to share, experiment, transfer, etc", Directives: gti.Directives{
-			&gti.Directive{Tool: "gti", Directive: "add", Args: []string{}},
-		}, Args: ordmap.Make([]ordmap.KeyVal[string, *gti.Field]{
-			{"filename", &gti.Field{Name: "filename", Type: "goki.dev/gi/v2/gi.FileName", LocalType: "FileName", Doc: "", Directives: gti.Directives{}, Tag: ""}},
-		}), Returns: ordmap.Make([]ordmap.KeyVal[string, *gti.Field]{
-			{"error", &gti.Field{Name: "error", Type: "error", LocalType: "error", Doc: "", Directives: gti.Directives{}, Tag: ""}},
-		})}},
-		{"SaveJSON", &gti.Method{Name: "SaveJSON", Doc: "SaveJSON saves keymaps to a JSON-formatted file.\nYou can save and open key maps to / from files to share, experiment, transfer, etc", Directives: gti.Directives{
-			&gti.Directive{Tool: "gti", Directive: "add", Args: []string{}},
-		}, Args: ordmap.Make([]ordmap.KeyVal[string, *gti.Field]{
-			{"filename", &gti.Field{Name: "filename", Type: "goki.dev/gi/v2/gi.FileName", LocalType: "FileName", Doc: "", Directives: gti.Directives{}, Tag: ""}},
-		}), Returns: ordmap.Make([]ordmap.KeyVal[string, *gti.Field]{
-			{"error", &gti.Field{Name: "error", Type: "error", LocalType: "error", Doc: "", Directives: gti.Directives{}, Tag: ""}},
-		})}},
-		{"OpenPrefs", &gti.Method{Name: "OpenPrefs", Doc: "OpenPrefs opens KeyMaps from GoGi standard prefs directory, in file key_maps_prefs.json.\nThis is called automatically, so calling it manually should not be necessary in most cases.", Directives: gti.Directives{
-			&gti.Directive{Tool: "gti", Directive: "add", Args: []string{}},
-		}, Args: ordmap.Make([]ordmap.KeyVal[string, *gti.Field]{}), Returns: ordmap.Make([]ordmap.KeyVal[string, *gti.Field]{
-			{"error", &gti.Field{Name: "error", Type: "error", LocalType: "error", Doc: "", Directives: gti.Directives{}, Tag: ""}},
-		})}},
-		{"SavePrefs", &gti.Method{Name: "SavePrefs", Doc: "SavePrefs saves KeyMaps to GoGi standard prefs directory, in file key_maps_prefs.json,\nwhich will be loaded automatically at startup if prefs SaveKeyMaps is checked\n(should be if you're using custom keymaps)", Directives: gti.Directives{
-			&gti.Directive{Tool: "gti", Directive: "add", Args: []string{}},
-		}, Args: ordmap.Make([]ordmap.KeyVal[string, *gti.Field]{}), Returns: ordmap.Make([]ordmap.KeyVal[string, *gti.Field]{
-			{"error", &gti.Field{Name: "error", Type: "error", LocalType: "error", Doc: "", Directives: gti.Directives{}, Tag: ""}},
-		})}},
-		{"RevertToStd", &gti.Method{Name: "RevertToStd", Doc: "RevertToStd reverts the keymaps to using the StdKeyMaps that are compiled into the program\nand have all the lastest key functions defined.  If you have edited your maps, and are finding\nthings not working, it is a good idea to save your current maps and try this, or at least do\nViewStdMaps to see the current standards. Your current map edits will be lost if you proceed!", Directives: gti.Directives{
-			&gti.Directive{Tool: "gti", Directive: "add", Args: []string{}},
-		}, Args: ordmap.Make([]ordmap.KeyVal[string, *gti.Field]{}), Returns: ordmap.Make([]ordmap.KeyVal[string, *gti.Field]{})}},
-		{"ViewStd", &gti.Method{Name: "ViewStd", Doc: "ViewStd shows the standard maps that are compiled into the program and have\nall the lastest key functions bound to standard values.  Useful for\ncomparing against custom maps.", Directives: gti.Directives{
-			&gti.Directive{Tool: "gti", Directive: "add", Args: []string{}},
-		}, Args: ordmap.Make([]ordmap.KeyVal[string, *gti.Field]{}), Returns: ordmap.Make([]ordmap.KeyVal[string, *gti.Field]{})}},
-	}),
-})
-
 // LabelType is the [gti.Type] for [Label]
 var LabelType = gti.AddType(&gti.Type{
 	Name:      "goki.dev/gi/v2/gi.Label",
@@ -1088,7 +1007,7 @@ var _ = gti.AddType(&gti.Type{
 		{"ScreenPrefs", &gti.Field{Name: "ScreenPrefs", Type: "map[string]goki.dev/gi/v2/gi.ScreenPrefs", LocalType: "map[string]ScreenPrefs", Doc: "screen-specific preferences -- will override overall defaults if set", Directives: gti.Directives{}, Tag: ""}},
 		{"Params", &gti.Field{Name: "Params", Type: "goki.dev/gi/v2/gi.ParamPrefs", LocalType: "ParamPrefs", Doc: "parameters controlling GUI behavior", Directives: gti.Directives{}, Tag: "view:\"inline\""}},
 		{"Editor", &gti.Field{Name: "Editor", Type: "goki.dev/gi/v2/gi.EditorPrefs", LocalType: "EditorPrefs", Doc: "editor preferences -- for TextView etc", Directives: gti.Directives{}, Tag: "view:\"inline\""}},
-		{"KeyMap", &gti.Field{Name: "KeyMap", Type: "goki.dev/gi/v2/gi.KeyMapName", LocalType: "KeyMapName", Doc: "select the active keymap from list of available keymaps -- see Edit KeyMaps for editing / saving / loading that list", Directives: gti.Directives{}, Tag: ""}},
+		{"KeyMap", &gti.Field{Name: "KeyMap", Type: "goki.dev/gi/v2/keyfun.MapName", LocalType: "keyfun.MapName", Doc: "select the active keymap from list of available keymaps -- see Edit KeyMaps for editing / saving / loading that list", Directives: gti.Directives{}, Tag: ""}},
 		{"SaveKeyMaps", &gti.Field{Name: "SaveKeyMaps", Type: "bool", LocalType: "bool", Doc: "if set, the current available set of key maps is saved to your preferences directory, and automatically loaded at startup -- this should be set if you are using custom key maps, but it may be safer to keep it <i>OFF</i> if you are <i>not</i> using custom key maps, so that you'll always have the latest compiled-in standard key maps with all the current key functions bound to standard key chords", Directives: gti.Directives{}, Tag: ""}},
 		{"SaveDetailed", &gti.Field{Name: "SaveDetailed", Type: "bool", LocalType: "bool", Doc: "if set, the detailed preferences are saved and loaded at startup -- only", Directives: gti.Directives{}, Tag: ""}},
 		{"CustomStyles", &gti.Field{Name: "CustomStyles", Type: "goki.dev/ki/v2.Props", LocalType: "ki.Props", Doc: "a custom style sheet -- add a separate Props entry for each type of object, e.g., button, or class using .classname, or specific named element using #name -- all are case insensitive", Directives: gti.Directives{}, Tag: ""}},
