b8d8db84cc4d2277c337dff8668527b15cac2e4a
Kai O'Reilly <koreilly5297@gmail.com>
2023-10-30 21:47:00 -0700

    fixed views demo

--- examples/views/views.go
+++ examples/views/views.go
@@ -155,12 +155,16 @@ func app() {
 	but.Tooltip = "open a SliceViewDialog slice view with a lot of elments, for performance testing"
 	but.OnClick(func(e events.Event) {
 		sl := make([]float32, 2880)
-		giv.SliceViewDialog(gi.NewDialog(but).Title("SliceView Test").Prompt("It should open quickly."), &sl, nil, false, false).Run()
+		d := gi.NewDialog(but).Title("SliceView Test").Prompt("It should open quickly.").FullWindow(true)
+		giv.NewSliceView(d).SetSlice(&sl)
+		d.Run()
 	})
 	but = gi.NewButton(trow, "table-test").SetText("TableDialog")
 	but.Tooltip = "open a TableViewDialog view "
 	but.OnClick(func(e events.Event) {
-		giv.TableViewDialog(gi.NewDialog(but).Title("TableView Test").Prompt("how does it resize."), &tsttable, nil, false, false).Run()
+		d := gi.NewDialog(but).Title("TableView Test").Prompt("how does it resize.").FullWindow(true)
+		giv.NewTableView(d).SetSlice(&tsttable)
+		d.Run()
 	})
 
 	lab1 := gi.NewLabel(trow, "lab1").SetText("<large>This is a test of the <tt>Slice</tt> and <tt>Map</tt> Views reflect-ive GUI</large>")
