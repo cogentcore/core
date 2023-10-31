035ab23bc58b28a8c316c4dcac75a7768ac519af
Kai O'Reilly <koreilly5297@gmail.com>
2023-10-31 16:09:56 -0700

    updated version to v2.0.0-dev0.0.12

--- vgpu/version.go
+++ vgpu/version.go
@@ -4,9 +4,9 @@ package vgpu
 
 const (
 	// Version is the version of this package being used
-	Version = "v2.0.0-dev0.0.11"
+	Version = "v2.0.0-dev0.0.12"
 	// GitCommit is the commit just before the latest version commit
-	GitCommit = "be4d7fd"
+	GitCommit = "b12bc3a"
 	// VersionDate is the date-time of the latest version commit in UTC (in the format 'YYYY-MM-DD HH:MM', which is the Go format '2006-01-02 15:04')
-	VersionDate = "2023-10-31 23:05"
+	VersionDate = "2023-10-31 23:09"
 )
