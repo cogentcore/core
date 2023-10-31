9e3af781682262caa742d9662790dd16f8914183
Kai O'Reilly <koreilly5297@gmail.com>
2023-10-31 12:47:25 -0700

    updated version to v0.1.11

--- version.go
+++ version.go
@@ -4,9 +4,9 @@ package gti
 
 const (
 	// Version is the version of this package being used
-	Version = "v0.1.10"
+	Version = "v0.1.11"
 	// GitCommit is the commit just before the latest version commit
-	GitCommit = "41d41b9"
+	GitCommit = "6245814"
 	// VersionDate is the date-time of the latest version commit in UTC (in the format 'YYYY-MM-DD HH:MM', which is the Go format '2006-01-02 15:04')
-	VersionDate = "2023-10-22 20:36"
+	VersionDate = "2023-10-31 19:47"
 )
