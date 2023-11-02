27dc9489e1b90dfb4631b53780ced54bd29543d6
Kai O'Reilly <koreilly5297@gmail.com>
2023-11-02 07:54:39 -0700

    updated version to v0.0.15

--- go.mod
+++ go.mod
@@ -3,8 +3,8 @@ module goki.dev/cursors
 go 1.21.0
 
 require (
-	goki.dev/enums v0.9.34
-	goki.dev/grows v0.3.11
+	goki.dev/enums v0.9.35
+	goki.dev/grows v0.3.12
 )
 
 require golang.org/x/image v0.13.0 // indirect
--- version.go
+++ version.go
@@ -4,9 +4,9 @@ package cursors
 
 const (
 	// Version is the version of this package being used
-	Version = "v0.0.14"
+	Version = "v0.0.15"
 	// GitCommit is the commit just before the latest version commit
-	GitCommit = "9a2ca93"
+	GitCommit = "19acb09"
 	// VersionDate is the date-time of the latest version commit in UTC (in the format 'YYYY-MM-DD HH:MM', which is the Go format '2006-01-02 15:04')
-	VersionDate = "2023-10-31 23:07"
+	VersionDate = "2023-11-02 14:54"
 )
