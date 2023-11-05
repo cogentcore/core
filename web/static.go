9da884675d3aa4f5a96b7a139837ad1127ccaf76
Kai O'Reilly <koreilly5297@gmail.com>
2023-11-04 23:21:48 -0700

    started static; added back wasmexec

---
+++ web/static.go
@@ -0,0 +1,143 @@
+// Copyright 2023 The GoKi Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style
+// license that can be found in the LICENSE file.
+
+package web
+
+import (
+	"io/ioutil"
+	"net/http"
+	"net/http/httptest"
+	"os"
+	"path/filepath"
+	"strings"
+
+	"github.com/maxence-charriere/go-app/v9/pkg/errors"
+)
+
+// GenerateStaticWebsite generates the files to run a PWA built with go-app as a
+// static website in the specified directory. Static websites can be used with
+// hosts such as Github Pages.
+//
+// Note that app.wasm must still be built separately and put into the web
+// directory.
+func GenerateStaticWebsite(dir string, h *builder, pages ...string) error {
+	if dir == "" {
+		dir = "."
+	}
+
+	resources := map[string]struct{}{
+		"/":                     {},
+		"/wasm_exec.js":         {},
+		"/app.js":               {},
+		"/app-worker.js":        {},
+		"/manifest.webmanifest": {},
+		"/app.css":              {},
+		"/web":                  {},
+	}
+
+	// for path := range routes.routes {
+	// 	resources[path] = struct{}{}
+	// }
+
+	for _, p := range pages {
+		if p == "" {
+			continue
+		}
+		if !strings.HasPrefix(p, "/") {
+			p = "/" + p
+		}
+		resources[p] = struct{}{}
+	}
+
+	server := httptest.NewServer(h)
+	defer server.Close()
+
+	for path := range resources {
+		switch path {
+		case "/web":
+			if err := createStaticDir(filepath.Join(dir, path), ""); err != nil {
+				return errors.New("creating web directory failed").Wrap(err)
+			}
+
+		default:
+			filename := path
+			if filename == "/" {
+				filename = "/index.html"
+			}
+
+			f, err := createStaticFile(dir, filename)
+			if err != nil {
+				return errors.New("creating file failed").
+					WithTag("path", path).
+					WithTag("filename", filename).
+					Wrap(err)
+			}
+			defer f.Close()
+
+			page, err := createStaticPage(server.URL + path)
+			if err != nil {
+				return errors.New("creating page failed").
+					WithTag("path", path).
+					WithTag("filename", filename).
+					Wrap(err)
+			}
+
+			if n, err := f.Write(page); err != nil {
+				return errors.New("writing page failed").
+					WithTag("path", path).
+					WithTag("filename", filename).
+					WithTag("bytes-written", n).
+					Wrap(err)
+			}
+		}
+	}
+
+	return nil
+}
+
+func createStaticDir(dir, path string) error {
+	dir = filepath.Join(dir, filepath.Dir(path))
+	if _, err := os.Stat(dir); !os.IsNotExist(err) {
+		return nil
+	}
+	return os.MkdirAll(filepath.Join(dir), 0755)
+}
+
+func createStaticFile(dir, path string) (*os.File, error) {
+	if err := createStaticDir(dir, path); err != nil {
+		return nil, errors.New("creating file directory failed").Wrap(err)
+	}
+
+	filename := filepath.Join(dir, path)
+	if filepath.Ext(filename) == "" {
+		filename += ".html"
+	}
+
+	return os.Create(filename)
+}
+
+func createStaticPage(path string) ([]byte, error) {
+	req, err := http.NewRequest(http.MethodGet, path, nil)
+	if err != nil {
+		return nil, errors.New("creating http request failed").
+			WithTag("path", path).
+			Wrap(err)
+	}
+
+	res, err := http.DefaultClient.Do(req)
+	if err != nil {
+		return nil, errors.New("http request failed").
+			WithTag("path", path).
+			Wrap(err)
+	}
+	defer res.Body.Close()
+
+	body, err := ioutil.ReadAll(res.Body)
+	if err != nil {
+		return nil, errors.New("reading request body failed").
+			WithTag("path", path).
+			Wrap(err)
+	}
+	return body, nil
+}
