7030614a1a77b7accbfeb6c8ea2f79c5aaf08ba6
Kai O'Reilly <koreilly5297@gmail.com>
2023-11-04 22:18:08 -0700

    copied web/gen from go-app

---
+++ web/gen/app.js
@@ -0,0 +1,290 @@
+// -----------------------------------------------------------------------------
+// go-app
+// -----------------------------------------------------------------------------
+var goappNav = function () {};
+var goappOnUpdate = function () {};
+var goappOnAppInstallChange = function () {};
+
+const goappEnv = {{.Env}};
+const goappLoadingLabel = "{{.LoadingLabel}}";
+const goappWasmContentLengthHeader = "{{.WasmContentLengthHeader}}";
+
+let goappServiceWorkerRegistration;
+let deferredPrompt = null;
+
+goappInitServiceWorker();
+goappWatchForUpdate();
+goappWatchForInstallable();
+goappInitWebAssembly();
+
+// -----------------------------------------------------------------------------
+// Service Worker
+// -----------------------------------------------------------------------------
+async function goappInitServiceWorker() {
+  if ("serviceWorker" in navigator) {
+    try {
+      const registration = await navigator.serviceWorker.register(
+        "{{.WorkerJS}}"
+      );
+
+      goappServiceWorkerRegistration = registration;
+      goappSetupNotifyUpdate(registration);
+      goappSetupAutoUpdate(registration);
+      goappSetupPushNotification();
+    } catch (err) {
+      console.error("goapp service worker registration failed", err);
+    }
+  }
+}
+
+// -----------------------------------------------------------------------------
+// Update
+// -----------------------------------------------------------------------------
+function goappWatchForUpdate() {
+  window.addEventListener("beforeinstallprompt", (e) => {
+    e.preventDefault();
+    deferredPrompt = e;
+    goappOnAppInstallChange();
+  });
+}
+
+function goappSetupNotifyUpdate(registration) {
+  registration.addEventListener("updatefound", (event) => {
+    const newSW = registration.installing;
+    newSW.addEventListener("statechange", (event) => {
+      if (!navigator.serviceWorker.controller) {
+        return;
+      }
+      if (newSW.state != "installed") {
+        return;
+      }
+      goappOnUpdate();
+    });
+  });
+}
+
+function goappSetupAutoUpdate(registration) {
+  const autoUpdateInterval = "{{.AutoUpdateInterval}}";
+  if (autoUpdateInterval == 0) {
+    return;
+  }
+
+  window.setInterval(() => {
+    registration.update();
+  }, autoUpdateInterval);
+}
+
+// -----------------------------------------------------------------------------
+// Install
+// -----------------------------------------------------------------------------
+function goappWatchForInstallable() {
+  window.addEventListener("appinstalled", () => {
+    deferredPrompt = null;
+    goappOnAppInstallChange();
+  });
+}
+
+function goappIsAppInstallable() {
+  return !goappIsAppInstalled() && deferredPrompt != null;
+}
+
+function goappIsAppInstalled() {
+  const isStandalone = window.matchMedia("(display-mode: standalone)").matches;
+  return isStandalone || navigator.standalone;
+}
+
+async function goappShowInstallPrompt() {
+  deferredPrompt.prompt();
+  await deferredPrompt.userChoice;
+  deferredPrompt = null;
+}
+
+// -----------------------------------------------------------------------------
+// Environment
+// -----------------------------------------------------------------------------
+function goappGetenv(k) {
+  return goappEnv[k];
+}
+
+// -----------------------------------------------------------------------------
+// Notifications
+// -----------------------------------------------------------------------------
+function goappSetupPushNotification() {
+  navigator.serviceWorker.addEventListener("message", (event) => {
+    const msg = event.data.goapp;
+    if (!msg) {
+      return;
+    }
+
+    if (msg.type !== "notification") {
+      return;
+    }
+
+    goappNav(msg.path);
+  });
+}
+
+async function goappSubscribePushNotifications(vapIDpublicKey) {
+  try {
+    const subscription =
+      await goappServiceWorkerRegistration.pushManager.subscribe({
+        userVisibleOnly: true,
+        applicationServerKey: vapIDpublicKey,
+      });
+    return JSON.stringify(subscription);
+  } catch (err) {
+    console.error(err);
+    return "";
+  }
+}
+
+function goappNewNotification(jsonNotification) {
+  let notification = JSON.parse(jsonNotification);
+
+  const title = notification.title;
+  delete notification.title;
+
+  let path = notification.path;
+  if (!path) {
+    path = "/";
+  }
+
+  const webNotification = new Notification(title, notification);
+
+  webNotification.onclick = () => {
+    goappNav(path);
+    webNotification.close();
+  };
+}
+
+// -----------------------------------------------------------------------------
+// Keep Clean Body
+// -----------------------------------------------------------------------------
+function goappKeepBodyClean() {
+  const body = document.body;
+  const bodyChildrenCount = body.children.length;
+
+  const mutationObserver = new MutationObserver(function (mutationList) {
+    mutationList.forEach((mutation) => {
+      switch (mutation.type) {
+        case "childList":
+          while (body.children.length > bodyChildrenCount) {
+            body.removeChild(body.lastChild);
+          }
+          break;
+      }
+    });
+  });
+
+  mutationObserver.observe(document.body, {
+    childList: true,
+  });
+
+  return () => mutationObserver.disconnect();
+}
+
+// -----------------------------------------------------------------------------
+// Web Assembly
+// -----------------------------------------------------------------------------
+async function goappInitWebAssembly() {
+  const loader = document.getElementById("app-wasm-loader");
+
+  if (!goappCanLoadWebAssembly()) {
+    loader.remove();
+    return;
+  }
+
+  let instantiateStreaming = WebAssembly.instantiateStreaming;
+  if (!instantiateStreaming) {
+    instantiateStreaming = async (resp, importObject) => {
+      const source = await (await resp).arrayBuffer();
+      return await WebAssembly.instantiate(source, importObject);
+    };
+  }
+
+  const loaderIcon = document.getElementById("app-wasm-loader-icon");
+  const loaderLabel = document.getElementById("app-wasm-loader-label");
+
+  try {
+    const showProgress = (progress) => {
+      loaderLabel.innerText = goappLoadingLabel.replace("{progress}", progress);
+    };
+    showProgress(0);
+
+    const go = new Go();
+    const wasm = await instantiateStreaming(
+      fetchWithProgress("{{.Wasm}}", showProgress),
+      go.importObject
+    );
+
+    go.run(wasm.instance);
+    loader.remove();
+  } catch (err) {
+    loaderIcon.className = "goapp-logo";
+    loaderLabel.innerText = err;
+    console.error("loading wasm failed: ", err);
+  }
+}
+
+function goappCanLoadWebAssembly() {
+  if (
+    /bot|googlebot|crawler|spider|robot|crawling/i.test(navigator.userAgent)
+  ) {
+    return false;
+  }
+
+  const urlParams = new URLSearchParams(window.location.search);
+  return urlParams.get("wasm") !== "false";
+}
+
+async function fetchWithProgress(url, progess) {
+  const response = await fetch(url);
+
+  let contentLength;
+  try {
+    contentLength = response.headers.get(goappWasmContentLengthHeader);
+  } catch {}
+  if (!goappWasmContentLengthHeader || !contentLength) {
+    contentLength = response.headers.get("Content-Length");
+  }
+
+  const total = parseInt(contentLength, 10);
+  let loaded = 0;
+
+  const progressHandler = function (loaded, total) {
+    progess(Math.round((loaded * 100) / total));
+  };
+
+  var res = new Response(
+    new ReadableStream(
+      {
+        async start(controller) {
+          var reader = response.body.getReader();
+          for (;;) {
+            var { done, value } = await reader.read();
+
+            if (done) {
+              progressHandler(total, total);
+              break;
+            }
+
+            loaded += value.byteLength;
+            progressHandler(loaded, total);
+            controller.enqueue(value);
+          }
+          controller.close();
+        },
+      },
+      {
+        status: response.status,
+        statusText: response.statusText,
+      }
+    )
+  );
+
+  for (var pair of response.headers.entries()) {
+    res.headers.set(pair[0], pair[1]);
+  }
+
+  return res;
+}
