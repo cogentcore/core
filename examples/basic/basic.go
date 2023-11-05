3968e50ce19eaba7f84151f89ce44cd22c162b72
Kai O'Reilly <koreilly5297@gmail.com>
2023-11-04 21:51:29 -0700

    updated basic example for debugging mobile icon sizing

--- examples/basic/basic.go
+++ examples/basic/basic.go
@@ -1,11 +1,8 @@
 package main
 
 import (
-	"fmt"
-
 	"goki.dev/gi/v2/gi"
 	"goki.dev/gi/v2/gimain"
-	"goki.dev/goosi/events"
 	"goki.dev/icons"
 )
 
@@ -17,19 +14,22 @@ func app() {
 	// gi.LayoutTrace = true
 	// gi.RenderTrace = true
 
-	sc := gi.NewScene().SetTitle("Basic")
-	gi.NewLabel(sc).SetText("Hello, World!")
+	sc := gi.NewScene().SetTitle("Basic").SetTopAppBar(func(tb *gi.TopAppBar) {})
 
-	gi.NewButton(sc).
-		SetText("Open Dialog").SetIcon(icons.OpenInNew).
-		OnClick(func(e events.Event) {
-			fmt.Println("button clicked")
-			// dialog := gi.NewScene("dialog")
-			// gi.NewLabel(&dialog.Frame, "dialog").SetText("Dialog!")
-			// gi.NewDialog(dialog, but).SetModal().SetMovable().SetCloseable().Run()
-		})
+	gi.NewIcon(sc).SetIcon(icons.Add)
 
-	gi.NewSwitch(sc)
+	// gi.NewLabel(sc).SetText("Hello, World!")
+
+	// gi.NewButton(sc).
+	// 	SetText("Open Dialog").SetIcon(icons.OpenInNew).
+	// 	OnClick(func(e events.Event) {
+	// 		fmt.Println("button clicked")
+	// 		// dialog := gi.NewScene("dialog")
+	// 		// gi.NewLabel(&dialog.Frame, "dialog").SetText("Dialog!")
+	// 		// gi.NewDialog(dialog, but).SetModal().SetMovable().SetCloseable().Run()
+	// 	})
+
+	// gi.NewSwitch(sc)
 
 	gi.NewWindow(sc).Run().Wait()
 }
