f98ab78a028dccbb9af290844f0d403baa9db58b
Kai O'Reilly <koreilly5297@gmail.com>
2023-11-05 10:54:59 -0800

    got web driver building

---
+++ driver/web/web.go
@@ -0,0 +1,21 @@
+// Copyright 2023 The GoKi Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style
+// license that can be found in the LICENSE file.
+
+//go:build js
+
+package web
+
+import "goki.dev/goosi"
+
+func (app *appImpl) ShowVirtualKeyboard(typ goosi.VirtualKeyboardTypes) {
+	// TODO(kai)
+}
+
+func (app *appImpl) HideVirtualKeyboard() {
+	// TODO(kai)
+}
+
+func (app *appImpl) mainLoop() {
+
+}
