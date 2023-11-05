79ae8955b4ece7f10498e1260630ff60d7568be0
Kai O'Reilly <koreilly5297@gmail.com>
2023-11-04 18:01:24 -0700

    improved tone doc

--- hct/hct.go
+++ hct/hct.go
@@ -38,7 +38,7 @@ type HCT struct {
 	// chroma (C) is the colorfulness or saturation of the color -- greyscale colors have no chroma, and fully saturated ones have high chroma.  The maximum varies as a function of hue and tone, but 150 is an upper bound.
 	Chroma float32 `min:"0" max:"150"`
 
-	// tone is the L* component from the LAB (L*a*b*) color system, which is linear in human perception of lightness
+	// tone is the L* component from the LAB (L*a*b*) color system, which is linear in human perception of lightness. It ranges from 0 to 100.
 	Tone float32 `min:"0" max:"100"`
 
 	// sRGB standard gamma-corrected 0-1 normalized RGB representation of the color.  Critically, components are not premultiplied by alpha
