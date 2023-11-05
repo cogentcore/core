fa9f2c67f39e43352d00b32a4486c48b1da5c95e
Kai O'Reilly <koreilly5297@gmail.com>
2023-11-05 00:15:32 -0700

    added basic web example

---
+++ examples/web/web.go
@@ -0,0 +1,11 @@
+// Copyright 2023 The GoKi Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style
+// license that can be found in the LICENSE file.
+
+package main
+
+import "fmt"
+
+func main() {
+	fmt.Println("Hello, web!")
+}
