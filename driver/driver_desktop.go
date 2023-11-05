20a84c62c2621ac0e27f99f8d2a63bf2a0f2a3fb
Kai O'Reilly <koreilly5297@gmail.com>
2023-11-05 10:56:15 -0800

    added web driverMain

--- driver/driver_desktop.go
+++ driver/driver_desktop.go
@@ -7,7 +7,7 @@
 // Use of this source code is governed by a BSD-style
 // license that can be found in the LICENSE file.
 
-//go:build !(android || ios)
+//go:build !(android || ios || js)
 
 package driver
 
