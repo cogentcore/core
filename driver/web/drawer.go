8ba6e22a1574a75919ab9595b64f4da47a46a793
Kai O'Reilly <koreilly5297@gmail.com>
2023-11-05 12:29:01 -0800

    added placeholder implementation of web drawer

--- driver/web/drawer.go
+++ driver/web/drawer.go
@@ -6,7 +6,81 @@
 
 package web
 
+import (
+	"image"
+	"image/draw"
+)
+
 // drawerImpl is a TEMPORARY, low-performance implementation of [goosi.Drawer].
 // It will be replaced with a full WebGPU based drawer at some point.
 // TODO: replace drawerImpl with WebGPU
 type drawerImpl struct{}
+
+// SetMaxTextures updates the max number of textures for drawing
+// Must call this prior to doing any allocation of images.
+func (dw *drawerImpl) SetMaxTextures(maxTextures int) {}
+
+// MaxTextures returns the max number of textures for drawing
+func (dw *drawerImpl) MaxTextures() int {
+	return 16
+}
+
+// DestBounds returns the bounds of the render destination
+func (dw *drawerImpl) DestBounds() image.Rectangle {
+	return image.Rect(0, 0, 500, 500)
+}
+
+// SetGoImage sets given Go image as a drawing source to given image index,
+// and layer, used in subsequent Draw methods.
+// A standard Go image is rendered upright on a standard surface.
+// Set flipY to true to flip.
+func (dw *drawerImpl) SetGoImage(idx, layer int, img image.Image, flipY bool) {}
+
+// ConfigImageDefaultFormat configures the draw image at the given index
+// to fit the default image format specified by the given width, height,
+// and number of layers.
+func (dw *drawerImpl) ConfigImageDefaultFormat(idx int, width int, height int, layers int) {}
+
+// ConfigImage configures the draw image at given index
+// to fit the given image format and number of layers as a drawing source.
+// ConfigImage(idx int, fmt *vgpu.ImageFormat)
+
+// SyncImages must be called after images have been updated, to sync
+// memory up to the GPU.
+func (dw *drawerImpl) SyncImages() {}
+
+// Scale copies texture at given index and layer to render target,
+// scaling the region defined by src and sr to the destination
+// such that sr in src-space is mapped to dr in dst-space.
+// dr is the destination rectangle
+// sr is the source region (set to image.ZR zero rect for all),
+// op is the drawing operation: Src = copy source directly (blit),
+// Over = alpha blend with existing
+// flipY = flipY axis when drawing this image
+func (dw *drawerImpl) Scale(idx, layer int, dr image.Rectangle, sr image.Rectangle, op draw.Op, flipY bool) error {
+	return nil
+}
+
+// Copy copies texture at given index and layer to render target.
+// dp is the destination point,
+// sr is the source region (set to image.ZR zero rect for all),
+// op is the drawing operation: Src = copy source directly (blit),
+// Over = alpha blend with existing
+// flipY = flipY axis when drawing this image
+func (dw *drawerImpl) Copy(idx, layer int, dp image.Point, sr image.Rectangle, op draw.Op, flipY bool) error {
+	return nil
+}
+
+// UseTextureSet selects the descriptor set to use --
+// choose this based on the bank of 16
+// texture values if number of textures > MaxTexturesPerSet.
+func (dw *drawerImpl) UseTextureSet(descIdx int) {}
+
+// StartDraw starts image drawing rendering process on render target
+// No images can be added or set after this point.
+// descIdx is the descriptor set to use -- choose this based on the bank of 16
+// texture values if number of textures > MaxTexturesPerSet.
+func (dw *drawerImpl) StartDraw(descIdx int) {}
+
+// EndDraw ends image drawing rendering process on render target
+func (dw *drawerImpl) EndDraw() {}
