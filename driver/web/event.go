e9c64db8a5c47e20d7c0c9f3e50110341152b6fb
Kai O'Reilly <koreilly5297@gmail.com>
2023-11-06 17:43:26 -0800

    handle touch start

--- driver/web/event.go
+++ driver/web/event.go
@@ -17,7 +17,7 @@ import (
 func (app *appImpl) addEventListeners() {
 	g := js.Global()
 	g.Call("addEventListener", "mousedown", js.FuncOf(app.onMouseDown))
-	g.Call("addEventListener", "touchstart", js.FuncOf(app.onMouseDown))
+	g.Call("addEventListener", "touchstart", js.FuncOf(app.onTouchStart))
 	g.Call("addEventListener", "mouseup", js.FuncOf(app.onMouseUp))
 	g.Call("addEventListener", "touchend", js.FuncOf(app.onMouseUp))
 	g.Call("addEventListener", "mousemove", js.FuncOf(app.onMouseMove))
@@ -30,7 +30,7 @@ func (app *appImpl) addEventListeners() {
 
 func (app *appImpl) onMouseDown(this js.Value, args []js.Value) any {
 	e := args[0]
-	x, y := e.Get("clientX").Int(), args[0].Get("clientY").Int()
+	x, y := e.Get("clientX").Int(), e.Get("clientY").Int()
 	but := e.Get("button").Int()
 	var ebut events.Buttons
 	switch but {
@@ -46,9 +46,21 @@ func (app *appImpl) onMouseDown(this js.Value, args []js.Value) any {
 	return nil
 }
 
+func (app *appImpl) onTouchStart(this js.Value, args []js.Value) any {
+	e := args[0]
+	touches := e.Get("changedTouches")
+	for i := 0; i < touches.Length(); i++ {
+		touch := touches.Index(i)
+		x, y := touch.Get("clientX").Int(), touch.Get("clientY").Int()
+		app.window.EvMgr.MouseButton(events.MouseDown, events.Left, image.Pt(x, y), 0)
+	}
+	e.Call("preventDefault")
+	return nil
+}
+
 func (app *appImpl) onMouseUp(this js.Value, args []js.Value) any {
 	e := args[0]
-	x, y := e.Get("clientX").Int(), args[0].Get("clientY").Int()
+	x, y := e.Get("clientX").Int(), e.Get("clientY").Int()
 	but := e.Get("button").Int()
 	var ebut events.Buttons
 	switch but {
@@ -66,7 +78,7 @@ func (app *appImpl) onMouseUp(this js.Value, args []js.Value) any {
 
 func (app *appImpl) onMouseMove(this js.Value, args []js.Value) any {
 	e := args[0]
-	x, y := e.Get("clientX").Int(), args[0].Get("clientY").Int()
+	x, y := e.Get("clientX").Int(), e.Get("clientY").Int()
 	app.window.EvMgr.MouseMove(image.Pt(x, y))
 	e.Call("preventDefault")
 	return nil
