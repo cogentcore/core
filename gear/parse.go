1ee4f6f8693c1bb67cf8be889a2a040871b3fc3b
Kai O'Reilly <koreilly5297@gmail.com>
2023-11-11 10:27:14 -0800

    handle no names

--- gear/parse.go
+++ gear/parse.go
@@ -54,10 +54,12 @@ func (cm *Cmd) Parse() error {
 				f.Names = append(f.Names, name)
 			}
 		}
+		if len(f.Names) == 0 {
+			continue
+		}
 		slices.SortFunc(f.Names, func(a, b string) int {
 			return cmp.Compare(len(a), len(b))
 		})
-		// fmt.Println(f.Names)
 		f.Name = f.Names[len(f.Names)-1]
 		cm.Flags = append(cm.Flags, f)
 	}
