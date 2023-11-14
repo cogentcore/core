14c7d076640044d67bc5a3f9ed9f36b97d0b8518
Kai O'Reilly <koreilly5297@gmail.com>
2023-11-14 07:58:10 -0800

    updated version to v2.0.0-dev0.0.19

--- go.mod
+++ go.mod
@@ -3,16 +3,16 @@ module goki.dev/ki/v2
 go 1.21.0
 
 require (
-	goki.dev/enums v0.9.37
-	goki.dev/gti v0.1.15
+	goki.dev/enums v0.9.38
+	goki.dev/gti v0.1.16
 )
 
 require (
-	goki.dev/grows v0.3.14
-	goki.dev/ordmap v0.5.6
+	goki.dev/grows v0.3.15
+	goki.dev/ordmap v0.5.7
 )
 
 require (
 	goki.dev/glop v0.1.4 // indirect
-	goki.dev/laser v0.1.17 // indirect
+	goki.dev/laser v0.1.18 // indirect
 )
--- version.go
+++ version.go
@@ -4,9 +4,9 @@ package ki
 
 const (
 	// Version is the version of this package being used
-	Version = "v2.0.0-dev0.0.18"
+	Version = "v2.0.0-dev0.0.19"
 	// GitCommit is the commit just before the latest version commit
-	GitCommit = "cc2c188"
+	GitCommit = "c4d5eaf"
 	// VersionDate is the date-time of the latest version commit in UTC (in the format 'YYYY-MM-DD HH:MM', which is the Go format '2006-01-02 15:04')
-	VersionDate = "2023-11-08 00:31"
+	VersionDate = "2023-11-14 15:58"
 )
