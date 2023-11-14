35e2b97bd3adceb4de3c3a63129dc59a36b2bc76
Kai O'Reilly <koreilly5297@gmail.com>
2023-11-14 07:52:06 -0800

    updated version to v0.0.9

--- version.go
+++ version.go
@@ -4,9 +4,9 @@ package icons
 
 const (
 	// Version is the version of this package being used
-	Version = "v0.0.8"
+	Version = "v0.0.9"
 	// GitCommit is the commit just before the latest version commit
-	GitCommit = "a8bd245"
+	GitCommit = "4a28cf3"
 	// VersionDate is the date-time of the latest version commit in UTC (in the format 'YYYY-MM-DD HH:MM', which is the Go format '2006-01-02 15:04')
-	VersionDate = "2023-11-08 00:26"
+	VersionDate = "2023-11-14 15:52"
 )
