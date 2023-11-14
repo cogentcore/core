3d129f8aca9e4b34e339084048a9220a4d56c2c8
Kai O'Reilly <koreilly5297@gmail.com>
2023-11-14 07:53:15 -0800

    updated version to v0.1.18

--- go.mod
+++ go.mod
@@ -3,6 +3,6 @@ module goki.dev/laser
 go 1.21.0
 
 require (
-	goki.dev/enums v0.9.37
+	goki.dev/enums v0.9.38
 	goki.dev/glop v0.1.4
 )
--- version.go
+++ version.go
@@ -4,9 +4,9 @@ package laser
 
 const (
 	// Version is the version of this package being used
-	Version = "v0.1.17"
+	Version = "v0.1.18"
 	// GitCommit is the commit just before the latest version commit
-	GitCommit = "10eeacf"
+	GitCommit = "764edfd"
 	// VersionDate is the date-time of the latest version commit in UTC (in the format 'YYYY-MM-DD HH:MM', which is the Go format '2006-01-02 15:04')
-	VersionDate = "2023-11-08 00:27"
+	VersionDate = "2023-11-14 15:53"
 )
