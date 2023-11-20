fe6df324c460cda74e76ecf85947e11e733880d0
Kai O'Reilly <koreilly5297@gmail.com>
2023-11-19 23:52:20 -0800

    added TestTextWidgets

--- gi/widget_test.go
+++ gi/widget_test.go
@@ -26,3 +26,22 @@ func TestBasicWidgets(t *testing.T) {
 		sc.AssertPixelsOnShow(t, strcase.ToSnake(typ.IDName))
 	}
 }
+
+func TestTextWidgets(t *testing.T) {
+	strings := []string{"test", "Hello, world!", "123.456", "This is a really long test sentence with a lot of words in it."}
+	funcs := map[string]func(par Widget, text string){
+		"button": func(par Widget, text string) {
+			NewButton(par).SetText(text)
+		},
+		"label": func(par Widget, text string) {
+			NewLabel(par).SetText(text)
+		},
+	}
+	for nm, f := range funcs {
+		for _, str := range strings {
+			sc := NewEmptyScene()
+			f(sc, str)
+			sc.AssertPixelsOnShow(t, "text_"+nm+"_"+strcase.ToSnake(str))
+		}
+	}
+}
