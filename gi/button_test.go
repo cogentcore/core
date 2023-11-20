eab175ac932361f9a83b40b4dd99321ab883776e
Kai O'Reilly <koreilly5297@gmail.com>
2023-11-20 12:52:58 -0800

    tried running button tests in parallel

--- gi/button_test.go
+++ gi/button_test.go
@@ -4,17 +4,27 @@
 
 package gi
 
-import "testing"
+import (
+	"testing"
+
+	"goki.dev/girl/states"
+)
 
 func TestButton(t *testing.T) {
 	for _, typ := range ButtonTypesValues() {
 		for _, str := range testStrings {
 			for _, ic := range testIcons {
 				for _, st := range testStates {
-					sc := NewEmptyScene()
-					bt := NewButton(sc).SetType(typ).SetText(str).SetIcon(ic).SetState(true, st...)
-					nm := testName("button", typ, str, ic, bt.Styles.State)
-					sc.AssertPixelsOnShow(t, nm)
+					st := st
+					var stf states.States
+					stf.SetFlag(true, st...)
+					nm := testName("button", typ, str, ic, stf)
+					t.Run(nm, func(t *testing.T) {
+						t.Parallel()
+						sc := NewEmptyScene()
+						NewButton(sc).SetType(typ).SetText(str).SetIcon(ic).SetState(true, st...)
+						sc.AssertPixelsOnShow(t, nm)
+					})
 				}
 			}
 		}
