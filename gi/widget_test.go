dc1309e1f1336ca803916b754a55625f68d4a5df
Kai O'Reilly <koreilly5297@gmail.com>
2023-11-20 00:09:02 -0800

    removed basic elements test

--- gi/widget_test.go
+++ gi/widget_test.go
@@ -12,22 +12,12 @@ import (
 
 	"github.com/iancoleman/strcase"
 	"goki.dev/grr"
-	"goki.dev/gti"
 )
 
 func TestMain(m *testing.M) {
 	RunTest(func() {
 		os.Exit(m.Run())
 	})
-}
-
-func TestBasicWidgets(t *testing.T) {
-	types := gti.AllEmbeddersOf(WidgetBaseType)
-	for _, typ := range types {
-		sc := NewEmptyScene()
-		sc.NewChild(typ)
-		sc.AssertPixelsOnShow(t, strcase.ToSnake(typ.IDName))
-	}
 }
 
 func TestTextWidgets(t *testing.T) {
