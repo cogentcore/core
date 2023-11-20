89818d153ccfbf2da2327c988cc8c9b8ba1c8942
Kai O'Reilly <koreilly5297@gmail.com>
2023-11-20 14:04:49 -0800

    removed TestTextWidgets

--- gi/widget_test.go
+++ gi/widget_test.go
@@ -56,30 +56,3 @@ func testName(elems ...any) string {
 	}
 	return filepath.Join(strs...)
 }
-
-func TestTextWidgets(t *testing.T) {
-	strs := []string{"test", "Hello, world!", "123.456", "This is a really long test sentence with a lot of words in it."}
-
-	funcs := map[string]func(par Widget, text string){
-		"button": func(par Widget, text string) {
-			NewButton(par).SetText(text)
-		},
-		"label": func(par Widget, text string) {
-			NewLabel(par).SetText(text)
-		},
-		"switch": func(par Widget, text string) {
-			NewSwitch(par).SetText(text)
-		},
-		"tab": func(par Widget, text string) {
-			NewTab(par).SetText(text)
-		},
-	}
-	for nm, f := range funcs {
-		for _, str := range strs {
-			sc := NewEmptyScene()
-			f(sc, str)
-			fw, _, _ := strings.Cut(str, " ")
-			sc.AssertPixelsOnShow(t, filepath.Join(nm, "text_"+strcase.ToSnake(fw)))
-		}
-	}
-}
