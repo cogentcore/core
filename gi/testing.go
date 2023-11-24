d86a6d6d73b1eee8424ed866fc77c7de86ad7c5b
Kai O'Reilly <koreilly5297@gmail.com>
2023-11-24 11:19:47 -0800

    updated to use new paint testing functions

--- gi/testing.go
+++ gi/testing.go
@@ -5,29 +5,26 @@
 package gi
 
 import (
-	"errors"
-	"io/fs"
-	"os"
-	"path/filepath"
-	"strings"
-
-	"goki.dev/colors"
+	"goki.dev/girl/paint"
 	"goki.dev/goosi"
 	"goki.dev/goosi/driver"
 	"goki.dev/goosi/events"
-	"goki.dev/grows/images"
 )
 
-// RunTest is a simple helper function that runs the given
-// function after calling [driver.Main] and [Init]. It should
-// only be used in tests. For example:
+// RunTest runs the given function after calling [driver.Main] and [Init].
+// It should only be used in tests, and it should typically be called in
+// TestMain. For example:
+//
+//	func TestMain(m *testing.M) {
+//		gi.RunTest(func() {
+//			os.Exit(m.Run())
+//		})
+//	}
 //
 //	func TestSomething(t *testing.T) {
-//		gi.RunTest(func() {
-//			sc := gi.NewScene()
-//			gi.NewLabel(sc).SetText("Something")
-//			sc.AssertPixelsOnShow(t, "something")
-//		})
+//		sc := gi.NewScene()
+//		gi.NewLabel(sc).SetText("Something")
+//		sc.AssertPixelsOnShow(t, "something")
 //	}
 func RunTest(test func()) {
 	driver.Main(func(a goosi.App) {
@@ -35,19 +32,6 @@ func RunTest(test func()) {
 		test()
 	})
 }
-
-// TestingT is an interface wrapper around *testing.T
-type TestingT interface {
-	Errorf(format string, args ...any)
-}
-
-// UpdateTestImages indicates whether to update currently saved test
-// images in [AssertCaptureIs] instead of comparing against them.
-// It is automatically set if the env variable "UPDATE_TEST_IMAGES" is "true",
-// and it should typically only be set through that. It should only be
-// set when behavior has been updated that causes test images to change,
-// and it should only be set once and then turned back off.
-var UpdateTestImages = os.Getenv("UPDATE_TEST_IMAGES") == "true"
 
 // AssertPixelsOnShow is a helper function that makes a new window from
 // the scene, waits until it is shown, calls [Scene.AssertPixels]
@@ -57,7 +41,7 @@ var UpdateTestImages = os.Getenv("UPDATE_TEST_IMAGES") == "true"
 // scene is shown, right before [Scene.AssertPixels] is called. Also,
 // if a function is passed, [Scene.DoNeedsRender] is also called before
 // [Scene.AssertPixels].
-func (sc *Scene) AssertPixelsOnShow(t TestingT, filename string, fun ...func()) {
+func (sc *Scene) AssertPixelsOnShow(t paint.TestingT, filename string, fun ...func()) {
 	showed := make(chan struct{})
 	sc.OnShow(func(e events.Event) {
 		if len(fun) > 0 {
@@ -78,82 +62,7 @@ func (sc *Scene) AssertPixelsOnShow(t TestingT, filename string, fun ...func()) 
 // (eg: "button" becomes "testdata/button.png").
 // If it is not, it fails the test with an error, but continues its
 // execution. If there is no image at the given filename in the testdata
-// directory, it creates the image
-func (sc *Scene) AssertPixels(t TestingT, filename string) {
-	capture := sc.Pixels
-
-	filename = filepath.Join("testdata", filename)
-	if filepath.Ext(filename) == "" {
-		filename += ".png"
-	}
-
-	err := os.MkdirAll(filepath.Dir(filename), 0750)
-	if err != nil {
-		t.Errorf("error making testdata directory: %v", err)
-	}
-
-	ext := filepath.Ext(filename)
-	failFilename := strings.TrimSuffix(filename, ext) + ".fail" + ext
-
-	if UpdateTestImages {
-		err := images.Save(capture, filename)
-		if err != nil {
-			t.Errorf("Scene.AssertPixels: error saving updated image: %v", err)
-		}
-		err = os.RemoveAll(failFilename)
-		if err != nil {
-			t.Errorf("Scene.AssertPixels: error removing old fail image: %v", err)
-		}
-		return
-	}
-
-	img, _, err := images.Open(filename)
-	if err != nil {
-		if !errors.Is(err, fs.ErrNotExist) {
-			t.Errorf("Scene.AssertPixels: error opening saved image: %v", err)
-			return
-		}
-		// we don't have the file yet, so we make it
-		err := images.Save(capture, filename)
-		if err != nil {
-			t.Errorf("Scene.AssertPixels: error saving new image: %v", err)
-		}
-		return
-	}
-
-	failed := false
-
-	cbounds := capture.Bounds()
-	ibounds := img.Bounds()
-	if cbounds != ibounds {
-		t.Errorf("Scene.AssertPixels: expected bounds %v for image for %s, but got bounds %v; see %s", ibounds, filename, cbounds, failFilename)
-		failed = true
-	} else {
-		for y := cbounds.Min.Y; y < cbounds.Max.Y; y++ {
-			for x := cbounds.Min.X; x < cbounds.Max.X; x++ {
-				cc := colors.AsRGBA(capture.At(x, y))
-				ic := colors.AsRGBA(img.At(x, y))
-				if cc != ic {
-					t.Errorf("Scene.AssertPixels: image for %s is not the same as expected; see %s; expected color %v at (%d, %d), but got %v", filename, failFilename, ic, x, y, cc)
-					failed = true
-					break
-				}
-			}
-			if failed {
-				break
-			}
-		}
-	}
-
-	if failed {
-		err := images.Save(capture, failFilename)
-		if err != nil {
-			t.Errorf("Scene.AssertPixels: error saving fail image: %v", err)
-		}
-	} else {
-		err := os.RemoveAll(failFilename)
-		if err != nil {
-			t.Errorf("Scene.AssertPixels: error removing old fail image: %v", err)
-		}
-	}
+// directory, it creates the image.
+func (sc *Scene) AssertPixels(t paint.TestingT, filename string) {
+	paint.AssertImage(t, sc.Pixels, filename)
 }
