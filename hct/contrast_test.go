2ac1a4ad001a362ea3dae5c96908bf4d8e7fea87
Kai O'Reilly <koreilly5297@gmail.com>
2023-11-26 11:12:36 -0800

    added TestContrastRatio

--- hct/contrast_test.go
+++ hct/contrast_test.go
@@ -5,8 +5,31 @@
 package hct
 
 import (
+	"image/color"
 	"testing"
+
+	"goki.dev/mat32/v2"
 )
+
+func TestContrastRatio(t *testing.T) {
+	type data struct {
+		a    color.Color
+		b    color.Color
+		want float32
+	}
+	tests := []data{
+		{color.White, color.Black, 21},
+		{color.Black, color.White, 21},
+		{color.RGBA{100, 100, 100, 255}, color.RGBA{100, 100, 100, 255}, 1},
+		{color.RGBA{0, 0, 255, 255}, color.RGBA{255, 255, 255, 255}, 8.59},
+	}
+	for i, test := range tests {
+		res := ContrastRatio(test.a, test.b)
+		if mat32.Abs(res-test.want) > 0.1 {
+			t.Errorf("%d: expected %g but got %g", i, test.want, res)
+		}
+	}
+}
 
 func TestToneContrastRatio(t *testing.T) {
 	type data struct {
