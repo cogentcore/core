84c74d2c37bb0258b60e96ba7da4d7c9c28b3406
Kai O'Reilly <koreilly5297@gmail.com>
2023-11-26 20:16:12 -0800

    added background test

---
+++ paint/background_test.go
@@ -0,0 +1,84 @@
+// Copyright (c) 2023, The GoKi Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style
+// license that can be found in the LICENSE file.
+
+package paint
+
+import (
+	"image"
+	"os"
+	"testing"
+
+	"goki.dev/colors"
+	"goki.dev/girl/styles"
+	"goki.dev/grows/images"
+	"goki.dev/mat32/v2"
+)
+
+func TestBackgroundColor(t *testing.T) {
+	imgsz := image.Point{320, 240}
+	szrec := image.Rectangle{Max: imgsz}
+	img := image.NewRGBA(szrec)
+
+	rs := &State{}
+	pc := &Paint{}
+
+	pc.Defaults()               // zeros are not good defaults for paint
+	pc.SetUnitContextExt(imgsz) // initialize units
+
+	rs.Init(imgsz.X, imgsz.Y, img)
+	rs.PushBounds(szrec)
+	rs.Lock()
+
+	st := &styles.Style{}
+	st.Defaults()
+	st.BackgroundColor.SetSolid(colors.Blue)
+
+	st.ToDots()
+
+	sbg := &colors.Full{Solid: colors.White}
+
+	sz := st.BoxSpace().Size().Add(mat32.Vec2{200, 100})
+	pc.DrawStdBox(rs, st, mat32.Vec2{50, 75}, sz, sbg, 0)
+
+	rs.Unlock()
+
+	images.Assert(t, img, "background_color")
+}
+
+func TestBackgroundImage(t *testing.T) {
+	imgsz := image.Point{320, 240}
+	szrec := image.Rectangle{Max: imgsz}
+	img := image.NewRGBA(szrec)
+
+	rs := &State{}
+	pc := &Paint{}
+
+	pc.Defaults()               // zeros are not good defaults for paint
+	pc.SetUnitContextExt(imgsz) // initialize units
+
+	rs.Init(imgsz.X, imgsz.Y, img)
+	rs.PushBounds(szrec)
+	rs.Lock()
+
+	st := &styles.Style{}
+	st.Defaults()
+	f, err := os.Open("test.png")
+	if err != nil {
+		t.Error(err)
+	} else {
+		defer f.Close()
+		st.BackgroundImage = f
+	}
+
+	st.ToDots()
+
+	sbg := &colors.Full{Solid: colors.White}
+
+	sz := st.BoxSpace().Size().Add(mat32.Vec2{200, 100})
+	pc.DrawStdBox(rs, st, mat32.Vec2{50, 75}, sz, sbg, 0)
+
+	rs.Unlock()
+
+	images.Assert(t, img, "background_image")
+}
