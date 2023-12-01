120398316fa06e1c55ae69862f3dcd76785f7503
Kai O'Reilly <koreilly5297@gmail.com>
2023-12-01 11:56:02 -0800

    updated version to v0.1.6

--- version.go
+++ version.go
@@ -4,9 +4,9 @@ package glop
 
 const (
 	// Version is the version of this package being used
-	Version = "v0.1.5"
+	Version = "v0.1.6"
 	// GitCommit is the commit just before the latest version commit
-	GitCommit = "c1ed70a"
+	GitCommit = "74a25ec"
 	// VersionDate is the date-time of the latest version commit in UTC (in the format 'YYYY-MM-DD HH:MM', which is the Go format '2006-01-02 15:04')
-	VersionDate = "2023-11-14 15:50"
+	VersionDate = "2023-12-01 19:56"
 )
