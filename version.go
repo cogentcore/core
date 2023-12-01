df496528cb33d232642839fe25237f3cd7eae837
Kai O'Reilly <koreilly5297@gmail.com>
2023-12-01 11:56:06 -0800

    updated version to v0.0.7

--- version.go
+++ version.go
@@ -4,9 +4,9 @@ package grr
 
 const (
 	// Version is the version of this package being used
-	Version = "v0.0.6"
+	Version = "v0.0.7"
 	// GitCommit is the commit just before the latest version commit
-	GitCommit = "7e126dc"
+	GitCommit = "c1a0445"
 	// VersionDate is the date-time of the latest version commit in UTC (in the format 'YYYY-MM-DD HH:MM', which is the Go format '2006-01-02 15:04')
-	VersionDate = "2023-11-14 15:51"
+	VersionDate = "2023-12-01 19:56"
 )
