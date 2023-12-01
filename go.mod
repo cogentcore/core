8b734551889c8b61ccc4c686f3d629feac53a1ec
Kai O'Reilly <koreilly5297@gmail.com>
2023-12-01 11:55:55 -0800

    updated version to v0.3.9

--- go.mod
+++ go.mod
@@ -2,9 +2,6 @@ module goki.dev/gengo
 
 go 1.21.0
 
-require golang.org/x/tools v0.15.0
+require golang.org/x/tools v0.16.0
 
-require (
-	golang.org/x/mod v0.14.0 // indirect
-	golang.org/x/sys v0.14.0 // indirect
-)
+require golang.org/x/mod v0.14.0 // indirect
--- version.go
+++ version.go
@@ -4,9 +4,9 @@ package gengo
 
 const (
 	// Version is the version of this package being used
-	Version = "v0.3.8"
+	Version = "v0.3.9"
 	// GitCommit is the commit just before the latest version commit
-	GitCommit = "b7811b0"
+	GitCommit = "1b321da"
 	// VersionDate is the date-time of the latest version commit in UTC (in the format 'YYYY-MM-DD HH:MM', which is the Go format '2006-01-02 15:04')
-	VersionDate = "2023-11-14 15:49"
+	VersionDate = "2023-12-01 19:55"
 )
