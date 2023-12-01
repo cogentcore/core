a713831054230628051416398c6c4503399bdae3
Kai O'Reilly <koreilly5297@gmail.com>
2023-12-01 12:36:53 -0800

    updated version to v2.0.0-dev0.0.14

--- gi/version.go
+++ gi/version.go
@@ -4,9 +4,9 @@ package gi
 
 const (
 	// Version is the version of this package being used
-	Version = "v2.0.0-dev0.0.13"
+	Version = "v2.0.0-dev0.0.14"
 	// GitCommit is the commit just before the latest version commit
-	GitCommit = "5c9b95ef"
+	GitCommit = "19c185f9"
 	// VersionDate is the date-time of the latest version commit in UTC (in the format 'YYYY-MM-DD HH:MM', which is the Go format '2006-01-02 15:04')
-	VersionDate = "2023-11-16 04:27"
+	VersionDate = "2023-12-01 20:36"
 )
