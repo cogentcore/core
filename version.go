b2779c32a7dd3b6b5cc2c9b7342d671ac7e7a38a
Kai O'Reilly <koreilly5297@gmail.com>
2023-12-01 17:30:00 -0800

    updated version to v0.1.16

--- version.go
+++ version.go
@@ -4,9 +4,9 @@ package svg
 
 const (
 	// Version is the version of this package being used
-	Version = "v0.1.15"
+	Version = "v0.1.16"
 	// GitCommit is the commit just before the latest version commit
-	GitCommit = "ea76fb1"
+	GitCommit = "4f49fbb"
 	// VersionDate is the date-time of the latest version commit in UTC (in the format 'YYYY-MM-DD HH:MM', which is the Go format '2006-01-02 15:04')
-	VersionDate = "2023-12-02 01:23"
+	VersionDate = "2023-12-02 01:30"
 )
