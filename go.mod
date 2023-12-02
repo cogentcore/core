72cc3fe5c5fffae125268ef80ee117780f4e0c76
Kai O'Reilly <koreilly5297@gmail.com>
2023-12-01 17:17:48 -0800

    updated version to v0.1.19

--- go.mod
+++ go.mod
@@ -3,8 +3,8 @@ module goki.dev/laser
 go 1.21.0
 
 require (
-	goki.dev/enums v0.9.38
-	goki.dev/glop v0.1.4
+	goki.dev/enums v0.9.39
+	goki.dev/glop v0.1.6
 )
 
 require github.com/fatih/camelcase v1.0.0 // indirect
--- version.go
+++ version.go
@@ -4,9 +4,9 @@ package laser
 
 const (
 	// Version is the version of this package being used
-	Version = "v0.1.18"
+	Version = "v0.1.19"
 	// GitCommit is the commit just before the latest version commit
-	GitCommit = "764edfd"
+	GitCommit = "223df9e"
 	// VersionDate is the date-time of the latest version commit in UTC (in the format 'YYYY-MM-DD HH:MM', which is the Go format '2006-01-02 15:04')
-	VersionDate = "2023-11-14 15:53"
+	VersionDate = "2023-12-02 01:17"
 )
