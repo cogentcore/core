4317e3e0527c094452d8881e5c4fca15b9f62648
Kai O'Reilly <koreilly5297@gmail.com>
2023-12-05 19:45:32 -0800

    added base.App functions

--- driver/base/app.go
+++ driver/base/app.go
@@ -13,10 +13,14 @@ package base
 
 import (
 	"sync"
+
+	"goki.dev/goosi"
 )
 
 // App contains the data and logic common to all implementations of [goosi.App].
 type App struct {
+	Ths goosi.App
+
 	// Mu is the main mutex protecting access to app operations, including [App.RunOnMain] calls.
 	Mu sync.Mutex
 
@@ -50,4 +54,74 @@ type App struct {
 type FuncRun struct {
 	F    func()
 	Done chan struct{}
+}
+
+// RunOnMain runs the given function on the main thread
+func (a *App) RunOnMain(f func()) {
+	if a.MainQueue == nil {
+		f()
+	} else {
+		done := make(chan struct{})
+		a.MainQueue <- FuncRun{F: f, Done: done}
+		<-done
+	}
+}
+
+// GoRunOnMain runs the given function on the main thread and returns immediately
+func (a *App) GoRunOnMain(f func()) {
+	go func() {
+		a.MainQueue <- FuncRun{F: f, Done: nil}
+	}()
+}
+
+// StopMain stops the main loop and thus terminates the app
+func (a *App) StopMain() {
+	a.MainDone <- struct{}{}
+}
+
+func (a *App) Name() string {
+	return a.Nm
+}
+
+func (a *App) SetName(name string) {
+	a.Nm = name
+}
+
+func (a *App) About() string {
+	return a.Abt
+}
+
+func (a *App) SetAbout(about string) {
+	a.Abt = about
+}
+
+func (a *App) SetQuitReqFunc(fun func()) {
+	a.QuitReqFunc = fun
+}
+
+func (a *App) SetQuitCleanFunc(fun func()) {
+	a.QuitCleanFunc = fun
+}
+
+func (a *App) QuitReq() {
+	if a.Quitting {
+		return
+	}
+	if a.QuitReqFunc != nil {
+		a.QuitReqFunc()
+	} else {
+		a.Quit()
+	}
+}
+
+func (a *App) IsQuitting() bool {
+	return a.Quitting
+}
+
+func (a *App) Quit() {
+	if a.Quitting {
+		return
+	}
+	a.Ths.QuitClean()
+	a.StopMain()
 }
