13c8ac255aaaea4527e40ea32788657fc7202f32
Kai O'Reilly <koreilly5297@gmail.com>
2023-12-05 20:23:19 -0800

    started on AppSingle

--- driver/base/app.go
+++ driver/base/app.go
@@ -2,7 +2,7 @@
 // Use of this source code is governed by a BSD-style
 // license that can be found in the LICENSE file.
 
-// based on golang.org/x/exp/shiny:
+// Based on golang.org/x/exp/shiny:
 // Copyright 2015 The Go Authors. All rights reserved.
 // Use of this source code is governed by a BSD-style
 // license that can be found in the LICENSE file.
---
+++ driver/base/app_single.go
@@ -0,0 +1,30 @@
+// Copyright 2023 The GoKi Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style
+// license that can be found in the LICENSE file.
+
+// Based on golang.org/x/exp/shiny:
+// Copyright 2015 The Go Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style
+// license that can be found in the LICENSE file.
+
+package base
+
+import "goki.dev/goosi"
+
+// AppSingle contains the data and logic common to all implementations of [goosi.App]
+// on single-window platforms (mobile, web, and offscreen), as opposed to multi-window
+// platforms (desktop), for which you should use [AppMulti]. An AppSingle is associated
+// with a corresponding type of [goosi.Drawer] and [goosi.Window]. The [goosi.Window]
+// type should embed [WindowSingle].
+type AppSingle[D goosi.Drawer, W goosi.Window] struct {
+	App
+
+	// Drawer is the single [goosi.Drawer] used for the app.
+	Drawer D
+
+	// Window is the single [goosi.Window] associated with the app.
+	Window W
+
+	// Screen is the single [goosi.Screen] associated with the app.
+	Screen *goosi.Screen
+}
