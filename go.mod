576ed844ee061a8f3297cf90b6d805cafaa280c2
Kai O'Reilly <koreilly5297@gmail.com>
2023-12-09 11:28:10 -0800

    updated version to v2.0.0-dev0.0.25

--- go.mod
+++ go.mod
@@ -3,17 +3,17 @@ module goki.dev/ki/v2
 go 1.21.0
 
 require (
-	goki.dev/enums v0.9.44
-	goki.dev/gti v0.1.22
+	goki.dev/enums v0.9.45
+	goki.dev/gti v0.1.23
 )
 
 require (
 	goki.dev/glop v0.1.7
-	goki.dev/grows v0.3.21
+	goki.dev/grows v0.3.22
 	goki.dev/ordmap v0.5.7
 )
 
 require (
 	github.com/fatih/camelcase v1.0.0 // indirect
-	goki.dev/laser v0.1.23 // indirect
+	goki.dev/laser v0.1.24 // indirect
 )
--- version.go
+++ version.go
@@ -4,9 +4,9 @@ package ki
 
 const (
 	// Version is the version of this package being used
-	Version = "v2.0.0-dev0.0.24"
+	Version = "v2.0.0-dev0.0.25"
 	// GitCommit is the commit just before the latest version commit
-	GitCommit = "8593bc5"
+	GitCommit = "418e379"
 	// VersionDate is the date-time of the latest version commit in UTC (in the format 'YYYY-MM-DD HH:MM', which is the Go format '2006-01-02 15:04')
-	VersionDate = "2023-12-09 19:12"
+	VersionDate = "2023-12-09 19:28"
 )
