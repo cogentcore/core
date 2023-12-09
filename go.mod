56609cf2e6c48d5bf47766f96184bd6a1046fc0d
Kai O'Reilly <koreilly5297@gmail.com>
2023-12-09 10:31:05 -0800

    updated version to v0.0.12

--- go.mod
+++ go.mod
@@ -4,6 +4,6 @@ go 1.21.0
 
 require (
 	github.com/iancoleman/strcase v0.3.0
-	goki.dev/glop v0.1.6
-	goki.dev/grr v0.0.7
+	goki.dev/glop v0.1.7
+	goki.dev/grr v0.0.8
 )
--- version.go
+++ version.go
@@ -4,9 +4,9 @@ package icons
 
 const (
 	// Version is the version of this package being used
-	Version = "v0.0.11"
+	Version = "v0.0.12"
 	// GitCommit is the commit just before the latest version commit
-	GitCommit = "05ffac5"
+	GitCommit = "b377b83"
 	// VersionDate is the date-time of the latest version commit in UTC (in the format 'YYYY-MM-DD HH:MM', which is the Go format '2006-01-02 15:04')
-	VersionDate = "2023-12-01 20:32"
+	VersionDate = "2023-12-09 18:31"
 )
