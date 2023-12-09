10593237bcd2ac78d93d495efb8f10f71ba0c695
Kai O'Reilly <koreilly5297@gmail.com>
2023-12-09 10:33:09 -0800

    updated version to v2.0.0-dev0.0.18

--- version.go
+++ version.go
@@ -4,9 +4,9 @@ package mat32
 
 const (
 	// Version is the version of this package being used
-	Version = "v2.0.0-dev0.0.17"
+	Version = "v2.0.0-dev0.0.18"
 	// GitCommit is the commit just before the latest version commit
-	GitCommit = "50e00cc"
+	GitCommit = "d836ebb"
 	// VersionDate is the date-time of the latest version commit in UTC (in the format 'YYYY-MM-DD HH:MM', which is the Go format '2006-01-02 15:04')
-	VersionDate = "2023-12-02 01:41"
+	VersionDate = "2023-12-09 18:33"
 )
