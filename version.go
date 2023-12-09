07bf174e9efd4e518531a157d4544f5a16701e63
Kai O'Reilly <koreilly5297@gmail.com>
2023-12-09 11:13:30 -0800

    updated version to v2.0.0-dev0.0.22

--- version.go
+++ version.go
@@ -4,9 +4,9 @@ package vci
 
 const (
 	// Version is the version of this package being used
-	Version = "v2.0.0-dev0.0.21"
+	Version = "v2.0.0-dev0.0.22"
 	// GitCommit is the commit just before the latest version commit
-	GitCommit = "daf406f"
+	GitCommit = "496272f"
 	// VersionDate is the date-time of the latest version commit in UTC (in the format 'YYYY-MM-DD HH:MM', which is the Go format '2006-01-02 15:04')
-	VersionDate = "2023-12-09 18:34"
+	VersionDate = "2023-12-09 19:13"
 )
