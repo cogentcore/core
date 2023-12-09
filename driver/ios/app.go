e986339d631239729055efd3820fd9e86a9f3859
Kai O'Reilly <koreilly5297@gmail.com>
2023-12-08 21:37:28 -0800

    removed insets from ios app

--- driver/ios/app.go
+++ driver/ios/app.go
@@ -13,7 +13,6 @@ import (
 	"path/filepath"
 
 	vk "github.com/goki/vulkan"
-	"goki.dev/girl/styles"
 	"goki.dev/goosi"
 	"goki.dev/goosi/clip"
 	"goki.dev/goosi/driver/base"
@@ -35,9 +34,6 @@ type App struct {
 
 	// Winptr is the pointer to the underlying system window
 	Winptr uintptr
-
-	// Insets are the screen insets
-	Insets styles.SideFloats
 }
 
 // Main is called from main thread when it is time to start running the
