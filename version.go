ca7c686c86dcdab0df7c02b6ecd08bcbec247e3e
Kai O'Reilly <koreilly5297@gmail.com>
2023-12-09 11:30:21 -0800

    updated version to v0.0.20

--- version.go
+++ version.go
@@ -4,9 +4,9 @@ package xe
 
 const (
 	// Version is the version of this package being used
-	Version = "v0.0.19"
+	Version = "v0.0.20"
 	// GitCommit is the commit just before the latest version commit
-	GitCommit = "3a3e738"
+	GitCommit = "27b5190"
 	// VersionDate is the date-time of the latest version commit in UTC (in the format 'YYYY-MM-DD HH:MM', which is the Go format '2006-01-02 15:04')
-	VersionDate = "2023-12-09 19:14"
+	VersionDate = "2023-12-09 19:30"
 )
