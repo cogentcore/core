c101e9b4ed648ef18a474026ad5ebb09d56b3d90
Kai O'Reilly <koreilly5297@gmail.com>
2023-12-12 07:55:16 -0800

    updated version to v2.0.0-dev0.0.21

--- go.mod
+++ go.mod
@@ -2,12 +2,14 @@ module goki.dev/mat32/v2
 
 go 1.21.0
 
+toolchain go1.21.4
+
 retract (
 	v2.0.1 // Retracts v2.0.0
 	v2.0.0 // Published accidentally
 )
 
 require (
-	goki.dev/enums v0.9.45
+	goki.dev/enums v0.9.47
 	golang.org/x/image v0.14.0
 )
--- version.go
+++ version.go
@@ -4,9 +4,9 @@ package mat32
 
 const (
 	// Version is the version of this package being used
-	Version = "v2.0.0-dev0.0.20"
+	Version = "v2.0.0-dev0.0.21"
 	// GitCommit is the commit just before the latest version commit
-	GitCommit = "c775710"
+	GitCommit = "abf14e1"
 	// VersionDate is the date-time of the latest version commit in UTC (in the format 'YYYY-MM-DD HH:MM', which is the Go format '2006-01-02 15:04')
-	VersionDate = "2023-12-09 19:28"
+	VersionDate = "2023-12-12 15:55"
 )
