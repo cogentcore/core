8c88f6401e802f7e05215d4617be259499a6afcc
Kai O'Reilly <koreilly5297@gmail.com>
2023-12-12 07:53:15 -0800

    updated version to v0.5.8

--- version.go
+++ version.go
@@ -4,9 +4,9 @@ package ordmap
 
 const (
 	// Version is the version of this package being used
-	Version = "v0.5.7"
+	Version = "v0.5.8"
 	// GitCommit is the commit just before the latest version commit
-	GitCommit = "a6028be"
+	GitCommit = "20e8853"
 	// VersionDate is the date-time of the latest version commit in UTC (in the format 'YYYY-MM-DD HH:MM', which is the Go format '2006-01-02 15:04')
-	VersionDate = "2023-11-14 15:51"
+	VersionDate = "2023-12-12 15:53"
 )
