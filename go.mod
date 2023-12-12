287bc4988b516beb80cc945ebf15cf056215c9af
Kai O'Reilly <koreilly5297@gmail.com>
2023-12-12 07:55:36 -0800

    updated version to v2.0.0-dev0.0.24

--- go.mod
+++ go.mod
@@ -2,8 +2,10 @@ module goki.dev/vci/v2
 
 go 1.21.0
 
+toolchain go1.21.4
+
 require (
 	github.com/Masterminds/vcs v1.13.3
-	goki.dev/enums v0.9.45
-	goki.dev/glop v0.1.7
+	goki.dev/enums v0.9.47
+	goki.dev/glop v0.1.8
 )
--- version.go
+++ version.go
@@ -4,9 +4,9 @@ package vci
 
 const (
 	// Version is the version of this package being used
-	Version = "v2.0.0-dev0.0.23"
+	Version = "v2.0.0-dev0.0.24"
 	// GitCommit is the commit just before the latest version commit
-	GitCommit = "07bf174"
+	GitCommit = "fada814"
 	// VersionDate is the date-time of the latest version commit in UTC (in the format 'YYYY-MM-DD HH:MM', which is the Go format '2006-01-02 15:04')
-	VersionDate = "2023-12-09 19:29"
+	VersionDate = "2023-12-12 15:55"
 )
