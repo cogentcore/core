eae3dc2ee8fd4d4df5fcb4ed3b89c9dcbbd27563
Kai O'Reilly <koreilly5297@gmail.com>
2023-12-12 12:48:24 -0800

    updated version to v0.1.27

--- version.go
+++ version.go
@@ -4,9 +4,9 @@ package laser
 
 const (
 	// Version is the version of this package being used
-	Version = "v0.1.26"
+	Version = "v0.1.27"
 	// GitCommit is the commit just before the latest version commit
-	GitCommit = "69df185"
+	GitCommit = "373333b"
 	// VersionDate is the date-time of the latest version commit in UTC (in the format 'YYYY-MM-DD HH:MM', which is the Go format '2006-01-02 15:04')
-	VersionDate = "2023-12-12 15:55"
+	VersionDate = "2023-12-12 20:48"
 )
