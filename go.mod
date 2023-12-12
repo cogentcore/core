b0929fbe631aeb57521fe3ea36692cd372a7de00
Kai O'Reilly <koreilly5297@gmail.com>
2023-12-12 12:47:58 -0800

    updated version to v0.0.19

--- go.mod
+++ go.mod
@@ -1,8 +1,6 @@
 module goki.dev/goosi
 
-go 1.21.0
-
-toolchain go1.21.4
+go 1.21
 
 require (
 	github.com/fsnotify/fsnotify v1.7.0
--- version.go
+++ version.go
@@ -4,9 +4,9 @@ package goosi
 
 const (
 	// Version is the version of this package being used
-	Version = "v0.0.18"
+	Version = "v0.0.19"
 	// GitCommit is the commit just before the latest version commit
-	GitCommit = "011e3df"
+	GitCommit = "5adb179"
 	// VersionDate is the date-time of the latest version commit in UTC (in the format 'YYYY-MM-DD HH:MM', which is the Go format '2006-01-02 15:04')
-	VersionDate = "2023-12-12 16:00"
+	VersionDate = "2023-12-12 20:47"
 )
