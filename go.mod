2247d2ceaba72c617c5ede6e6cee6f1bb905c720
Kai O'Reilly <koreilly5297@gmail.com>
2023-12-12 07:56:09 -0800

    updated version to v0.9.42

--- go.mod
+++ go.mod
@@ -2,9 +2,13 @@ module goki.dev/cam
 
 go 1.21.0
 
+toolchain go1.21.4
+
 require (
-	goki.dev/enums v0.9.45
-	goki.dev/mat32/v2 v2.0.0-dev0.0.20
+	goki.dev/enums v0.9.47
+	goki.dev/glop v0.1.8
+	goki.dev/grows v0.3.22
+	goki.dev/mat32/v2 v2.0.0-dev0.0.21
 )
 
 require golang.org/x/image v0.14.0 // indirect
--- version.go
+++ version.go
@@ -4,9 +4,9 @@ package cam
 
 const (
 	// Version is the version of this package being used
-	Version = "v0.9.41"
+	Version = "v0.9.42"
 	// GitCommit is the commit just before the latest version commit
-	GitCommit = "3bdfb7d"
+	GitCommit = "051747c"
 	// VersionDate is the date-time of the latest version commit in UTC (in the format 'YYYY-MM-DD HH:MM', which is the Go format '2006-01-02 15:04')
-	VersionDate = "2023-12-09 19:30"
+	VersionDate = "2023-12-12 15:56"
 )
