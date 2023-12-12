78b6d2b711e9f232700146e3c155a19a6f0671d1
Kai O'Reilly <koreilly5297@gmail.com>
2023-12-12 07:53:12 -0800

    updated version to v0.0.9

--- go.mod
+++ go.mod
@@ -1,3 +1,3 @@
 module goki.dev/grr
 
-go 1.21.0
+go 1.21
--- version.go
+++ version.go
@@ -4,9 +4,9 @@ package grr
 
 const (
 	// Version is the version of this package being used
-	Version = "v0.0.8"
+	Version = "v0.0.9"
 	// GitCommit is the commit just before the latest version commit
-	GitCommit = "124b28d"
+	GitCommit = "8275852"
 	// VersionDate is the date-time of the latest version commit in UTC (in the format 'YYYY-MM-DD HH:MM', which is the Go format '2006-01-02 15:04')
-	VersionDate = "2023-12-09 18:30"
+	VersionDate = "2023-12-12 15:53"
 )
