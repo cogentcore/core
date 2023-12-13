030de5b9637319c10a3d89dd1bc9d03ae22fd84c
Kai O'Reilly <koreilly5297@gmail.com>
2023-12-12 16:31:00 -0800

    updated version to v0.3.26

--- version.go
+++ version.go
@@ -4,9 +4,9 @@ package grows
 
 const (
 	// Version is the version of this package being used
-	Version = "v0.3.25"
+	Version = "v0.3.26"
 	// GitCommit is the commit just before the latest version commit
-	GitCommit = "72dd2a1"
+	GitCommit = "b224f74"
 	// VersionDate is the date-time of the latest version commit in UTC (in the format 'YYYY-MM-DD HH:MM', which is the Go format '2006-01-02 15:04')
-	VersionDate = "2023-12-13 00:07"
+	VersionDate = "2023-12-13 00:31"
 )
