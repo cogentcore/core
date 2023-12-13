e314de77d4d04f17c89a50b9fd2f167a35dcf890
Randall C. O'Reilly <oreilly@ucdavis.edu>
2023-12-12 21:02:18 -0800

    box.go

---
+++ gi/box.go
@@ -0,0 +1,29 @@
+// Copyright (c) 2023, The GoKi Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style
+// license that can be found in the LICENSE file.
+
+package gi
+
+// Box is a simple base [Widget] that renders the Std Box model
+type Box struct {
+	WidgetBase
+}
+
+func (bx *Box) OnInit() {
+	bx.WidgetBase.OnInit()
+}
+
+// RenderBox does the standard box model rendering
+func (bx *Box) RenderBox() {
+	_, st := bx.RenderLock()
+	bx.RenderStdBox(st)
+	bx.RenderUnlock()
+}
+
+func (bx *Box) Render() {
+	if bx.PushBounds() {
+		bx.RenderBox()
+		bx.RenderChildren()
+		bx.PopBounds()
+	}
+}
