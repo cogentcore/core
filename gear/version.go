a0ef5920b38726ede9bd3662fb1c7e327466d111
Kai O'Reilly <koreilly5297@gmail.com>
2023-12-12 16:30:25 -0800

    updated version to v0.0.5

--- gear/version.go
+++ gear/version.go
@@ -4,9 +4,9 @@ package gear
 
 const (
 	// Version is the version of this package being used
-	Version = "v0.0.4"
+	Version = "v0.0.5"
 	// GitCommit is the commit just before the latest version commit
-	GitCommit = "fd14cf6"
+	GitCommit = "8842a4c"
 	// VersionDate is the date-time of the latest version commit in UTC (in the format 'YYYY-MM-DD HH:MM', which is the Go format '2006-01-02 15:04')
-	VersionDate = "2023-12-13 00:16"
+	VersionDate = "2023-12-13 00:30"
 )
