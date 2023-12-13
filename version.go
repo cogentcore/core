313f90640c221571c932cfabcd80d1ddaff838fa
Kai O'Reilly <koreilly5297@gmail.com>
2023-12-12 16:31:44 -0800

    updated version to v0.0.17

--- version.go
+++ version.go
@@ -4,9 +4,9 @@ package video
 
 const (
 	// Version is the version of this package being used
-	Version = "v0.0.16"
+	Version = "v0.0.17"
 	// GitCommit is the commit just before the latest version commit
-	GitCommit = "ecd111e"
+	GitCommit = "0d2def6"
 	// VersionDate is the date-time of the latest version commit in UTC (in the format 'YYYY-MM-DD HH:MM', which is the Go format '2006-01-02 15:04')
-	VersionDate = "2023-12-13 00:14"
+	VersionDate = "2023-12-13 00:31"
 )
