bd770ceda983b3e13b4076bc22a2c0b3c75a10c1
Kai O'Reilly <koreilly5297@gmail.com>
2023-12-12 16:30:54 -0800

    updated version to v0.8.18

--- version.go
+++ version.go
@@ -4,9 +4,9 @@ package greasi
 
 const (
 	// Version is the version of this package being used
-	Version = "v0.8.17"
+	Version = "v0.8.18"
 	// GitCommit is the commit just before the latest version commit
-	GitCommit = "65f79f7"
+	GitCommit = "0f29aab"
 	// VersionDate is the date-time of the latest version commit in UTC (in the format 'YYYY-MM-DD HH:MM', which is the Go format '2006-01-02 15:04')
-	VersionDate = "2023-12-13 00:13"
+	VersionDate = "2023-12-13 00:30"
 )
