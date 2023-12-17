aa3d0ea6dd2b756011e5ae011f596da19d118111
Kai O'Reilly <koreilly5297@gmail.com>
2023-12-17 13:52:27 -0800

    started on TestColorAt

--- gradient/gradient_test.go
+++ gradient/gradient_test.go
@@ -4,7 +4,13 @@
 
 package gradient
 
-import "goki.dev/colors"
+import (
+	"image"
+	"image/color"
+	"testing"
+
+	"goki.dev/colors"
+)
 
 func ExampleLinear() {
 	NewLinear().AddStop(colors.White, 0).AddStop(colors.Black, 1)
@@ -12,4 +18,32 @@ func ExampleLinear() {
 
 func ExampleRadial() {
 	NewRadial().AddStop(colors.Green, 0).AddStop(colors.Yellow, 0.5).AddStop(colors.Red, 1)
+}
+
+func TestColorAt(t *testing.T) {
+	type value struct {
+		x    int
+		y    int
+		want color.RGBA
+	}
+	type test struct {
+		gr   Gradient
+		want []value
+	}
+	tests := []test{
+		{NewLinear().
+			AddStop(colors.White, 0).
+			AddStop(colors.Black, 1),
+			[]value{
+				{7, 3, colors.Gray},
+			}},
+	}
+	for i, test := range tests {
+		for j, v := range test.want {
+			have := test.gr.At(v.x, v.y)
+			if have != v.want {
+				t.Errorf("%d.%d: expected %v at %v but got %v", i, j, v.want, image.Pt(v.x, v.y), have)
+			}
+		}
+	}
 }
