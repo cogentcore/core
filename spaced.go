8cc72d52bb7729d79c2b322d6d44849dc53d30e3
Randall C. O'Reilly <oreilly@ucdavis.edu>
2023-12-18 15:30:53 -0800

    rename list -> spaced; turn yellow down by 5 tone

--- spaced.go
+++ spaced.go
@@ -6,29 +6,10 @@ package colors
 
 import (
 	"image/color"
-	"math"
 
 	"goki.dev/cam/hct"
 	"goki.dev/colors/matcolor"
-	"goki.dev/mat32/v2"
 )
-
-// BinarySpacedNumber returns a floating point number in the 0-1 range based on the
-// binary representation of the given input number, such that the biggest differences
-// are in the lowest-order bits, with progressively smaller differences for higher powers.
-// 0 = 0; 1 = 0.5; 2 = 0.25; 3 = 0.75; 4 = 0.125; 5 = 0.625...
-func BinarySpacedNumber(idx int) float32 {
-	nb := int(mat32.Ceil(mat32.Log(float32(idx)) / math.Ln2))
-	rv := float32(0)
-	for i := 0; i <= nb; i++ {
-		pbase := 1 << i
-		base := 1 << (i + 1)
-		dv := (idx % base) / pbase
-		iv := float32(dv) * (1 / float32(base))
-		rv += iv
-	}
-	return rv
-}
 
 // Spaced returns a maximally widely-spaced sequence of colors
 // for progressive values of the index, using the HCT space.
@@ -46,7 +27,7 @@ func SpacedLight(idx int) color.RGBA {
 	// hues := []float32{30, 280, 140, 110, 330, 200, 70, 305}
 	hues := []float32{25, 255, 150, 105, 340, 210, 60, 300}
 	// even 45:       30, 75, 120, 165, 210, 255, 300, 345,
-	toffs := []float32{0, -10, 0, 10, 0, 0, 5, 0}
+	toffs := []float32{0, -10, 0, 5, 0, 0, 5, 0}
 	tones := []float32{65, 80, 45, 65, 80}
 	chromas := []float32{90, 90, 90, 20, 20}
 	ncats := len(hues)
