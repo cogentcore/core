677386fe6e3ed4578abb1d5d4b5c1179acaa21ae
Kai O'Reilly <koreilly5297@gmail.com>
2023-12-20 15:44:43 -0800

    updated version to v0.3.28

--- go.mod
+++ go.mod
@@ -4,8 +4,8 @@ go 1.21
 
 require (
 	github.com/BurntSushi/toml v1.3.2
-	goki.dev/enums v0.9.52
-	goki.dev/glop v0.1.8
+	goki.dev/enums v0.9.53
+	goki.dev/glop v0.1.9
 	golang.org/x/image v0.14.0
 	gopkg.in/yaml.v3 v3.0.1
 )
--- version.go
+++ version.go
@@ -4,9 +4,9 @@ package grows
 
 const (
 	// Version is the version of this package being used
-	Version = "v0.3.27"
+	Version = "v0.3.28"
 	// GitCommit is the commit just before the latest version commit
-	GitCommit = "030de5b"
+	GitCommit = "4f8d9ae"
 	// VersionDate is the date-time of the latest version commit in UTC (in the format 'YYYY-MM-DD HH:MM', which is the Go format '2006-01-02 15:04')
-	VersionDate = "2023-12-13 01:05"
+	VersionDate = "2023-12-20 23:44"
 )
