80650f11681bd5ddd5061bef3df2392f30a1d653
Kai O'Reilly <koreilly5297@gmail.com>
2023-12-20 15:52:31 -0800

    updated version to v0.5.18

--- version.go
+++ version.go
@@ -4,9 +4,9 @@ package xyz
 
 const (
 	// Version is the version of this package being used
-	Version = "v0.5.17"
+	Version = "v0.5.18"
 	// GitCommit is the commit just before the latest version commit
-	GitCommit = "519ec59"
+	GitCommit = "0bd6d26"
 	// VersionDate is the date-time of the latest version commit in UTC (in the format 'YYYY-MM-DD HH:MM', which is the Go format '2006-01-02 15:04')
-	VersionDate = "2023-12-13 01:08"
+	VersionDate = "2023-12-20 23:52"
 )
