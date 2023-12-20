75999f25e5250fb45e3567dd42cb16b9e2c06263
Kai O'Reilly <koreilly5297@gmail.com>
2023-12-19 22:51:40 -0800

    added more documentation and optimization of math functions

--- math.go
+++ math.go
@@ -243,210 +243,536 @@ func Erf(x float32) float32 {
 	return math32.Erf(x)
 }
 
+// Erfc returns the complementary error function of x.
+//
+// Special cases are:
+//
+//	Erfc(+Inf) = 0
+//	Erfc(-Inf) = 2
+//	Erfc(NaN) = NaN
 func Erfc(x float32) float32 {
 	return math32.Erfc(x)
 }
 
+// Erfcinv returns the inverse of Erfc(x).
+//
+// Special cases are:
+//
+//	Erfcinv(0) = +Inf
+//	Erfcinv(2) = -Inf
+//	Erfcinv(x) = NaN if x < 0 or x > 2
+//	Erfcinv(NaN) = NaN
 func Erfcinv(x float32) float32 {
 	return float32(math.Erfcinv(float64(x)))
 }
 
+// Erfinv returns the inverse error function of x.
+//
+// Special cases are:
+//
+//	Erfinv(1) = +Inf
+//	Erfinv(-1) = -Inf
+//	Erfinv(x) = NaN if x < -1 or x > 1
+//	Erfinv(NaN) = NaN
 func Erfinv(x float32) float32 {
 	return float32(math.Erfinv(float64(x)))
 }
 
+// Exp returns e**x, the base-e exponential of x.
+//
+// Special cases are:
+//
+//	Exp(+Inf) = +Inf
+//	Exp(NaN) = NaN
+//
+// Very large values overflow to 0 or +Inf.
+// Very small values underflow to 1.
 func Exp(x float32) float32 {
 	return math32.Exp(x)
 }
 
+// Exp2 returns 2**x, the base-2 exponential of x.
+//
+// Special cases are the same as Exp.
 func Exp2(x float32) float32 {
 	return math32.Exp2(x)
 }
 
+// Expm1 returns e**x - 1, the base-e exponential of x minus 1.
+// It is more accurate than Exp(x) - 1 when x is near zero.
+//
+// Special cases are:
+//
+//	Expm1(+Inf) = +Inf
+//	Expm1(-Inf) = -1
+//	Expm1(NaN) = NaN
+//
+// Very large values overflow to -1 or +Inf.
 func Expm1(x float32) float32 {
 	return math32.Expm1(x)
 }
 
+// FMA returns x * y + z, computed with only one rounding.
+// (That is, FMA returns the fused multiply-add of x, y, and z.)
 func FMA(x, y, z float32) float32 {
 	return float32(math.FMA(float64(x), float64(y), float64(z)))
 }
 
+// Floor returns the greatest integer value less than or equal to x.
+//
+// Special cases are:
+//
+//	Floor(±0) = ±0
+//	Floor(±Inf) = ±Inf
+//	Floor(NaN) = NaN
 func Floor(x float32) float32 {
 	return math32.Floor(x)
 }
 
+// Frexp breaks f into a normalized fraction
+// and an integral power of two.
+// It returns frac and exp satisfying f == frac × 2**exp,
+// with the absolute value of frac in the interval [½, 1).
+//
+// Special cases are:
+//
+//	Frexp(±0) = ±0, 0
+//	Frexp(±Inf) = ±Inf, 0
+//	Frexp(NaN) = NaN, 0
 func Frexp(f float32) (frac float32, exp int) {
-	var fc float64
-	fc, exp = math.Frexp(float64(f))
-	frac = float32(fc)
-	return
+	return math32.Frexp(f)
 }
 
+// Gamma returns the Gamma function of x.
+//
+// Special cases are:
+//
+//	Gamma(+Inf) = +Inf
+//	Gamma(+0) = +Inf
+//	Gamma(-0) = -Inf
+//	Gamma(x) = NaN for integer x < 0
+//	Gamma(-Inf) = NaN
+//	Gamma(NaN) = NaN
 func Gamma(x float32) float32 {
 	return math32.Gamma(x)
 }
 
+// Hypot returns Sqrt(p*p + q*q), taking care to avoid
+// unnecessary overflow and underflow.
+//
+// Special cases are:
+//
+//	Hypot(±Inf, q) = +Inf
+//	Hypot(p, ±Inf) = +Inf
+//	Hypot(NaN, q) = NaN
+//	Hypot(p, NaN) = NaN
 func Hypot(p, q float32) float32 {
-	return float32(math.Hypot(float64(p), float64(q)))
+	return math32.Hypot(p, q)
 }
 
+// Ilogb returns the binary exponent of x as an integer.
+//
+// Special cases are:
+//
+//	Ilogb(±Inf) = MaxInt32
+//	Ilogb(0) = MinInt32
+//	Ilogb(NaN) = MaxInt32
 func Ilogb(x float32) float32 {
 	return float32(math32.Ilogb(x))
 }
 
+// Inf returns positive infinity if sign >= 0, negative infinity if sign < 0.
 func Inf(sign int) float32 {
-	return float32(math.Inf(sign))
+	return math32.Inf(sign)
 }
 
+// IsInf reports whether f is an infinity, according to sign.
+// If sign > 0, IsInf reports whether f is positive infinity.
+// If sign < 0, IsInf reports whether f is negative infinity.
+// If sign == 0, IsInf reports whether f is either infinity.
 func IsInf(x float32, sign int) bool {
-	return math.IsInf(float64(x), sign)
+	return math32.IsInf(x, sign)
 }
 
+// IsNaN reports whether f is an IEEE 754 “not-a-number” value.
 func IsNaN(x float32) bool {
-	return math.IsNaN(float64(x))
+	return math32.IsNaN(x)
 }
 
+// J0 returns the order-zero Bessel function of the first kind.
+//
+// Special cases are:
+//
+//	J0(±Inf) = 0
+//	J0(0) = 1
+//	J0(NaN) = NaN
 func J0(x float32) float32 {
 	return math32.J0(x)
 }
 
+// J1 returns the order-one Bessel function of the first kind.
+//
+// Special cases are:
+//
+//	J1(±Inf) = 0
+//	J1(NaN) = NaN
 func J1(x float32) float32 {
 	return math32.J1(x)
 }
 
+// Jn returns the order-n Bessel function of the first kind.
+//
+// Special cases are:
+//
+//	Jn(n, ±Inf) = 0
+//	Jn(n, NaN) = NaN
 func Jn(n int, x float32) float32 {
-	return float32(math.Jn(n, float64(x)))
+	return math32.Jn(n, x)
 }
 
+// Ldexp is the inverse of Frexp.
+// It returns frac × 2**exp.
+//
+// Special cases are:
+//
+//	Ldexp(±0, exp) = ±0
+//	Ldexp(±Inf, exp) = ±Inf
+//	Ldexp(NaN, exp) = NaN
 func Ldexp(frac float32, exp int) float32 {
-	return float32(math.Ldexp(float64(frac), exp))
+	return math32.Ldexp(frac, exp)
 }
 
-// Lerp is linear interpolation between start and stop in proportion to amount
+// Lerp returns the linear interpolation between start and stop in proportion to amount
 func Lerp(start, stop, amount float32) float32 {
 	return (1-amount)*start + amount*stop
 }
 
+// Lgamma returns the natural logarithm and sign (-1 or +1) of Gamma(x).
+//
+// Special cases are:
+//
+//	Lgamma(+Inf) = +Inf
+//	Lgamma(0) = +Inf
+//	Lgamma(-integer) = +Inf
+//	Lgamma(-Inf) = -Inf
+//	Lgamma(NaN) = NaN
 func Lgamma(x float32) (lgamma float32, sign int) {
-	var l float64
-	l, sign = math.Lgamma(float64(x))
-	lgamma = float32(l)
-	return
+	return math32.Lgamma(x)
 }
 
+// Log returns the natural logarithm of x.
+//
+// Special cases are:
+//
+//	Log(+Inf) = +Inf
+//	Log(0) = -Inf
+//	Log(x < 0) = NaN
+//	Log(NaN) = NaN
 func Log(x float32) float32 {
 	return math32.Log(x)
 }
 
+// Log10 returns the decimal logarithm of x.
+// The special cases are the same as for Log.
 func Log10(x float32) float32 {
 	return math32.Log10(x)
 }
 
+// Log1p returns the natural logarithm of 1 plus its argument x.
+// It is more accurate than Log(1 + x) when x is near zero.
+//
+// Special cases are:
+//
+//	Log1p(+Inf) = +Inf
+//	Log1p(±0) = ±0
+//	Log1p(-1) = -Inf
+//	Log1p(x < -1) = NaN
+//	Log1p(NaN) = NaN
 func Log1p(x float32) float32 {
 	return math32.Log1p(x)
 }
 
+// Log2 returns the binary logarithm of x.
+// The special cases are the same as for Log.
 func Log2(x float32) float32 {
 	return math32.Log2(x)
 }
 
+// Logb returns the binary exponent of x.
+//
+// Special cases are:
+//
+//	Logb(±Inf) = +Inf
+//	Logb(0) = -Inf
+//	Logb(NaN) = NaN
 func Logb(x float32) float32 {
 	return math32.Logb(x)
 }
 
+// TODO(kai): should we use builtin max and min?
+
+// Max returns the larger of x or y.
+//
+// Special cases are:
+//
+//	Max(x, +Inf) = Max(+Inf, x) = +Inf
+//	Max(x, NaN) = Max(NaN, x) = NaN
+//	Max(+0, ±0) = Max(±0, +0) = +0
+//	Max(-0, -0) = -0
+//
+// Note that this differs from the built-in function max when called
+// with NaN and +Inf.
 func Max(x, y float32) float32 {
-	return float32(math.Max(float64(x), float64(y)))
+	return math32.Max(x, y)
 }
 
+// Min returns the smaller of x or y.
+//
+// Special cases are:
+//
+//	Min(x, -Inf) = Min(-Inf, x) = -Inf
+//	Min(x, NaN) = Min(NaN, x) = NaN
+//	Min(-0, ±0) = Min(±0, -0) = -0
+//
+// Note that this differs from the built-in function min when called
+// with NaN and -Inf.
 func Min(x, y float32) float32 {
-	return float32(math.Min(float64(x), float64(y)))
+	return math32.Min(x, y)
 }
 
+// Mod returns the floating-point remainder of x/y.
+// The magnitude of the result is less than y and its
+// sign agrees with that of x.
+//
+// Special cases are:
+//
+//	Mod(±Inf, y) = NaN
+//	Mod(NaN, y) = NaN
+//	Mod(x, 0) = NaN
+//	Mod(x, ±Inf) = x
+//	Mod(x, NaN) = NaN
 func Mod(x, y float32) float32 {
-	return float32(math.Mod(float64(x), float64(y)))
+	return math32.Mod(x, y)
 }
 
+// Modf returns integer and fractional floating-point numbers
+// that sum to f. Both values have the same sign as f.
+//
+// Special cases are:
+//
+//	Modf(±Inf) = ±Inf, NaN
+//	Modf(NaN) = NaN, NaN
 func Modf(f float32) (it float32, frac float32) {
-	var ii, ff float64
-	ii, ff = math.Modf(float64(f))
-	it = float32(ii)
-	frac = float32(ff)
-	return
+	return math32.Modf(f)
 }
 
+// NaN returns an IEEE 754 “not-a-number” value.
 func NaN() float32 {
-	return float32(math.NaN())
+	return math32.NaN()
 }
 
+// Nextafter returns the next representable float32 value after x towards y.
+//
+// Special cases are:
+//
+//	Nextafter32(x, x)   = x
+//	Nextafter32(NaN, y) = NaN
+//	Nextafter32(x, NaN) = NaN
 func Nextafter(x, y float32) float32 {
-	return math.Nextafter32(x, y)
+	return math32.Nextafter(x, y)
 }
 
+// Pow returns x**y, the base-x exponential of y.
+//
+// Special cases are (in order):
+//
+//	Pow(x, ±0) = 1 for any x
+//	Pow(1, y) = 1 for any y
+//	Pow(x, 1) = x for any x
+//	Pow(NaN, y) = NaN
+//	Pow(x, NaN) = NaN
+//	Pow(±0, y) = ±Inf for y an odd integer < 0
+//	Pow(±0, -Inf) = +Inf
+//	Pow(±0, +Inf) = +0
+//	Pow(±0, y) = +Inf for finite y < 0 and not an odd integer
+//	Pow(±0, y) = ±0 for y an odd integer > 0
+//	Pow(±0, y) = +0 for finite y > 0 and not an odd integer
+//	Pow(-1, ±Inf) = 1
+//	Pow(x, +Inf) = +Inf for |x| > 1
+//	Pow(x, -Inf) = +0 for |x| > 1
+//	Pow(x, +Inf) = +0 for |x| < 1
+//	Pow(x, -Inf) = +Inf for |x| < 1
+//	Pow(+Inf, y) = +Inf for y > 0
+//	Pow(+Inf, y) = +0 for y < 0
+//	Pow(-Inf, y) = Pow(-0, -y)
+//	Pow(x, y) = NaN for finite x < 0 and finite non-integer y
 func Pow(x, y float32) float32 {
-	return float32(math.Pow(float64(x), float64(y)))
+	return math32.Pow(x, y)
 }
 
+// Pow10 returns 10**n, the base-10 exponential of n.
+//
+// Special cases are:
+//
+//	Pow10(n) =    0 for n < -323
+//	Pow10(n) = +Inf for n > 308
 func Pow10(n int) float32 {
-	return float32(math.Pow10(n))
+	return math32.Pow10(n)
 }
 
+// Remainder returns the IEEE 754 floating-point remainder of x/y.
+//
+// Special cases are:
+//
+//	Remainder(±Inf, y) = NaN
+//	Remainder(NaN, y) = NaN
+//	Remainder(x, 0) = NaN
+//	Remainder(x, ±Inf) = x
+//	Remainder(x, NaN) = NaN
 func Remainder(x, y float32) float32 {
-	return float32(math.Remainder(float64(x), float64(y)))
+	return math32.Remainder(x, y)
 }
 
+// Round returns the nearest integer, rounding half away from zero.
+//
+// Special cases are:
+//
+//	Round(±0) = ±0
+//	Round(±Inf) = ±Inf
+//	Round(NaN) = NaN
 func Round(x float32) float32 {
 	return math32.Round(x)
 }
 
+// RoundToEven returns the nearest integer, rounding ties to even.
+//
+// Special cases are:
+//
+//	RoundToEven(±0) = ±0
+//	RoundToEven(±Inf) = ±Inf
+//	RoundToEven(NaN) = NaN
 func RoundToEven(x float32) float32 {
 	return float32(math.RoundToEven(float64(x)))
 }
 
+// Signbit returns true if x is negative or negative zero.
 func Signbit(x float32) bool {
-	return math.Signbit(float64(x))
+	return math32.Signbit(x)
 }
 
+// Sin returns the sine of the radian argument x.
+//
+// Special cases are:
+//
+//	Sin(±0) = ±0
+//	Sin(±Inf) = NaN
+//	Sin(NaN) = NaN
 func Sin(x float32) float32 {
 	return math32.Sin(x)
 }
 
+// Sincos returns Sin(x), Cos(x).
+//
+// Special cases are:
+//
+//	Sincos(±0) = ±0, 1
+//	Sincos(±Inf) = NaN, NaN
+//	Sincos(NaN) = NaN, NaN
 func Sincos(x float32) (sin, cos float32) {
-	s, c := math.Sincos(float64(x))
-	sin = float32(s)
-	cos = float32(c)
-	return
+	return math32.Sincos(x)
 }
 
+// Sinh returns the hyperbolic sine of x.
+//
+// Special cases are:
+//
+//	Sinh(±0) = ±0
+//	Sinh(±Inf) = ±Inf
+//	Sinh(NaN) = NaN
 func Sinh(x float32) float32 {
 	return math32.Sinh(x)
 }
 
+// Sqrt returns the square root of x.
+//
+// Special cases are:
+//
+//	Sqrt(+Inf) = +Inf
+//	Sqrt(±0) = ±0
+//	Sqrt(x < 0) = NaN
+//	Sqrt(NaN) = NaN
 func Sqrt(x float32) float32 {
 	return math32.Sqrt(x)
 }
 
+// Tan returns the tangent of the radian argument x.
+//
+// Special cases are:
+//
+//	Tan(±0) = ±0
+//	Tan(±Inf) = NaN
+//	Tan(NaN) = NaN
 func Tan(x float32) float32 {
 	return math32.Tan(x)
 }
 
+// Tanh returns the hyperbolic tangent of x.
+//
+// Special cases are:
+//
+//	Tanh(±0) = ±0
+//	Tanh(±Inf) = ±1
+//	Tanh(NaN) = NaN
 func Tanh(x float32) float32 {
 	return math32.Tanh(x)
 }
 
+// Trunc returns the integer value of x.
+//
+// Special cases are:
+//
+//	Trunc(±0) = ±0
+//	Trunc(±Inf) = ±Inf
+//	Trunc(NaN) = NaN
 func Trunc(x float32) float32 {
 	return math32.Trunc(x)
 }
 
+// Y0 returns the order-zero Bessel function of the second kind.
+//
+// Special cases are:
+//
+//	Y0(+Inf) = 0
+//	Y0(0) = -Inf
+//	Y0(x < 0) = NaN
+//	Y0(NaN) = NaN
 func Y0(x float32) float32 {
 	return math32.Y0(x)
 }
 
+// Y1 returns the order-one Bessel function of the second kind.
+//
+// Special cases are:
+//
+//	Y1(+Inf) = 0
+//	Y1(0) = -Inf
+//	Y1(x < 0) = NaN
+//	Y1(NaN) = NaN
 func Y1(x float32) float32 {
 	return math32.Y1(x)
 }
 
+// Yn returns the order-n Bessel function of the second kind.
+//
+// Special cases are:
+//
+//	Yn(n, +Inf) = 0
+//	Yn(n ≥ 0, 0) = -Inf
+//	Yn(n < 0, 0) = +Inf if n is odd, -Inf if n is even
+//	Yn(n, x < 0) = NaN
+//	Yn(n, NaN) = NaN
 func Yn(n int, x float32) float32 {
-	return float32(math.Yn(n, float64(x)))
+	return math32.Yn(n, x)
 }
 
 //////////////////////////////////////////////////////////////
@@ -472,16 +798,6 @@ func ClampInt(x, a, b int) int {
 		return b
 	}
 	return x
-}
-
-// SetMax sets a to Max(a,b)
-func SetMax(a *float32, b float32) {
-	*a = Max(*a, b)
-}
-
-// SetMin sets a to Min(a,b)
-func SetMin(a *float32, b float32) {
-	*a = Min(*a, b)
 }
 
 // MinPos returns the minimum of the two values, excluding any that are <= 0
