4749ff11b524021cd030361e454024a8a4af6f1a
Kai O'Reilly <koreilly5297@gmail.com>
2023-12-21 16:50:23 -0800

    replaced GoKi with Goki

--- field.go
+++ field.go
@@ -1,4 +1,4 @@
-// Copyright (c) 2023, The GoKi Authors. All rights reserved.
+// Copyright (c) 2023, The Goki Authors. All rights reserved.
 // Use of this source code is governed by a BSD-style
 // license that can be found in the LICENSE file.
 
