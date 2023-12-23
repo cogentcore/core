ebf758395f2b020ed93f58f86cb1c31fb654a2b6
Kai O'Reilly <koreilly5297@gmail.com>
2023-12-23 10:41:03 -0800

    went back to filer fs

--- web/gen/wasm_exec.js
+++ web/gen/wasm_exec.js
@@ -22,7 +22,8 @@
 		return buf.length;
 	}
 
-	if (!globalThis.fs.constants) {
+	if (!globalThis.fs) {
+		globalThis.fs = new Filer.FileSystem();
 		globalThis.fs.constants = { O_WRONLY: -1, O_RDWR: -1, O_CREAT: -1, O_TRUNC: -1, O_APPEND: -1, O_EXCL: -1 } // unused
 	}
 
