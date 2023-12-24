4f3cd3c9e3cebdf3d53b16306c706f6c5f7bdbb4
Kai O'Reilly <koreilly5297@gmail.com>
2023-12-24 11:43:44 -0800

    added newfs example

---
+++ examples/newfs/newfs.go
@@ -0,0 +1,23 @@
+// Copyright (c) 2023, The Goki Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style
+// license that can be found in the LICENSE file.
+
+// Based on https://github.com/hack-pad/hackpad
+// Licensed under the Apache 2.0 License
+
+//go:build js
+
+package main
+
+import (
+	"syscall/js"
+
+	"goki.dev/grr"
+	"goki.dev/jsfs"
+)
+
+func main() {
+	fs := grr.Must1(jsfs.NewFS())
+	grr.Must1(fs.MkdirAll([]js.Value{js.ValueOf("me"), js.ValueOf(0777)}))
+	js.Global().Get("console").Call("log", "stat file info", grr.Must1(fs.Stat([]js.Value{js.ValueOf("me")})))
+}
