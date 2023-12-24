9fc555bc7e37358ca9c0f450a4eb0b4adf5e6f76
Kai O'Reilly <koreilly5297@gmail.com>
2023-12-23 23:49:31 -0800

    removed unused fs code; set console operation based on fd

--- web/gen/wasm_exec.js
+++ web/gen/wasm_exec.js
@@ -16,7 +16,11 @@
 		outputBuf += decoder.decode(buf);
 		const nl = outputBuf.lastIndexOf("\n");
 		if (nl != -1) {
-			console.log(outputBuf.substring(0, nl));
+			if (fd == 2) {
+				console.error(outputBuf.substring(0, nl));
+			} else {
+				console.log(outputBuf.substring(0, nl));
+			}
 			outputBuf = outputBuf.substring(nl + 1);
 		}
 		return buf.length;
@@ -25,38 +29,6 @@
 	if (!globalThis.fs) {
 		globalThis.fs = {
 			constants: { O_WRONLY: -1, O_RDWR: -1, O_CREAT: -1, O_TRUNC: -1, O_APPEND: -1, O_EXCL: -1 }, // unused
-			writeSync: writeConsole,
-			write(fd, buf, offset, length, position, callback) {
-				if (offset !== 0 || length !== buf.length || position !== null) {
-					callback(enosys());
-					return;
-				}
-				const n = this.writeSync(fd, buf);
-				callback(null, n);
-			},
-			chmod(path, mode, callback) { callback(enosys()); },
-			chown(path, uid, gid, callback) { callback(enosys()); },
-			close(fd, callback) { callback(enosys()); },
-			fchmod(fd, mode, callback) { callback(enosys()); },
-			fchown(fd, uid, gid, callback) { callback(enosys()); },
-			fstat(fd, callback) { callback(enosys()); },
-			fsync(fd, callback) { callback(null); },
-			ftruncate(fd, length, callback) { callback(enosys()); },
-			lchown(path, uid, gid, callback) { callback(enosys()); },
-			link(path, link, callback) { callback(enosys()); },
-			lstat(path, callback) { callback(enosys()); },
-			mkdir(path, perm, callback) { callback(enosys()); },
-			open(path, flags, mode, callback) { callback(enosys()); },
-			read(fd, buffer, offset, length, position, callback) { callback(enosys()); },
-			readdir(path, callback) { callback(enosys()); },
-			readlink(path, callback) { callback(enosys()); },
-			rename(from, to, callback) { callback(enosys()); },
-			rmdir(path, callback) { callback(enosys()); },
-			stat(path, callback) { callback(enosys()); },
-			symlink(path, link, callback) { callback(enosys()); },
-			truncate(path, length, callback) { callback(enosys()); },
-			unlink(path, callback) { callback(enosys()); },
-			utimes(path, atime, mtime, callback) { callback(enosys()); },
 		};
 	}
 
