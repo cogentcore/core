224a345b556c1805dcdaa5bd6008d6582b0f913b
Kai O'Reilly <koreilly5297@gmail.com>
2023-12-26 15:28:38 -0800

    updated version to v0.0.32

--- version.go
+++ version.go
@@ -4,9 +4,9 @@ package cursors
 
 const (
 	// Version is the version of this package being used
-	Version = "v0.0.31"
+	Version = "v0.0.32"
 	// GitCommit is the commit just before the latest version commit
-	GitCommit = "5c024e8"
+	GitCommit = "dd477af"
 	// VersionDate is the date-time of the latest version commit in UTC (in the format 'YYYY-MM-DD HH:MM', which is the Go format '2006-01-02 15:04')
-	VersionDate = "2023-12-21 00:30"
+	VersionDate = "2023-12-26 23:28"
 )
