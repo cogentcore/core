bcc9a765c462fad4d5478a220e3c50c344667ca4
Kai O'Reilly <koreilly5297@gmail.com>
2023-12-27 16:04:21 -0800

    improved basic example

--- examples/basic/basic.go
+++ examples/basic/basic.go
@@ -3,37 +3,17 @@ package main
 import (
 	"goki.dev/gi/v2/gi"
 	"goki.dev/gi/v2/gimain"
+	"goki.dev/goosi/events"
+	"goki.dev/icons"
 )
 
 func main() { gimain.Run(app) }
 
 func app() {
-	// gi.DebugSettings.RenderTrace = true
-	// gi.DebugSettings.WinEventTrace = true
-	// gi.EventTrace = true
-	// gi.DebugSettings.LayoutTrace = true
-	// gi.DebugSettings.LayoutTraceDetail = true
-	// gi.DebugSettings.RenderTrace = true
-
 	b := gi.NewAppBody("basic")
-	b.App().AppBarConfig = nil
-
-	gi.NewButton(b).SetType(gi.ButtonAction).SetText("Action")
-
-	// gi.NewIcon(sc).SetIcon(icons.Add)
-
-	// gi.NewLabel(b).SetText("Hello, World!")
-
-	// gi.NewButton(sc).
-	// 	SetText("Open Dialog").SetIcon(icons.OpenInNew).
-	// 	OnClick(func(e events.Event) {
-	// 		fmt.Println("button clicked")
-	// 		// dialog := gi.NewScene("dialog")
-	// 		// gi.NewLabel(&dialog.Frame, "dialog").SetText("Dialog!")
-	// 		// gi.NewBody(dialog, but).SetModal().SetMovable().SetCloseable().Run()
-	// 	})
-
-	// gi.NewSwitch(sc)
-
+	bt := gi.NewButton(b).SetText("Open dialog").SetIcon(icons.OpenInNew)
+	bt.OnClick(func(e events.Event) {
+		gi.NewBody().AddTitle("Hello, world!").AddOkOnly().NewDialog(bt).Run()
+	})
 	b.NewWindow().Run().Wait()
 }
