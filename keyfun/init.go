ff2dbc8f173af69d6d7e088f9fa670c9b3f25e2c
Kai O'Reilly <koreilly5297@gmail.com>
2024-01-07 14:43:47 -0800

    added keyfun init

---
+++ keyfun/init.go
@@ -0,0 +1,19 @@
+// Copyright (c) 2023, The Goki Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style
+// license that can be found in the LICENSE file.
+
+package keyfun
+
+import "runtime"
+
+func init() {
+	switch runtime.GOOS {
+	case "darwin":
+		DefaultMap = "MacStd"
+	case "windows":
+		DefaultMap = "WindowsStd"
+	default:
+		DefaultMap = "LinuxStd"
+	}
+	SetActiveMapName(DefaultMap)
+}
