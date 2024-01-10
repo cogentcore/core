de20d0925faead34e1e361bc4b434d4d0df67a8f
Randall C. O'Reilly <oreilly@ucdavis.edu>
2024-01-10 10:49:10 -0800

    add Ignore* functions for ignoring errors

--- helpers.go
+++ helpers.go
@@ -187,4 +187,52 @@ func Test4[T1, T2, T3, T4 any](t TestingT, v1 T1, v2 T2, v3 T3, v4 T4, err error
 	return v1, v2, v3, v4
 }
 
+// Ignore1 ignores an error return value for a function returning
+// a value and an error, allowing direct usage of the value.
+// The intended usage is:
+//
+//	a := grr.Ignore1(MyFunc(v))
+func Ignore1[T any](v T, err error) T {
+	if err != nil {
+		slog.Error(err.Error())
+	}
+	return v
+}
+
+// Ignore2 ignores an error return value for a function returning
+// two values and an error, allowing direct usage of the values.
+// The intended usage is:
+//
+//	a, b := grr.Ignore2(MyFunc(v))
+func Ignore2[T1, T2 any](v1 T1, v2 T2, err error) (T1, T2) {
+	if err != nil {
+		slog.Error(err.Error())
+	}
+	return v1, v2
+}
+
+// Ignore3 ignores an error return value for a function returning
+// three values and an error, allowing direct usage of the values.
+// The intended usage is:
+//
+//	a, b, c := grr.Ignore3(MyFunc(v))
+func Ignore3[T1, T2, T3 any](v1 T1, v2 T2, v3 T3, err error) (T1, T2, T3) {
+	if err != nil {
+		slog.Error(err.Error())
+	}
+	return v1, v2, v3
+}
+
+// Ignore4 ignores an error return value for a function returning
+// four values and an error, allowing direct usage of the values.
+// The intended usage is:
+//
+//	a, b, c, d := grr.Ignore4(MyFunc(v))
+func Ignore4[T1, T2, T3, T4 any](v1 T1, v2 T2, v3 T3, v4 T4, err error) (T1, T2, T3, T4) {
+	if err != nil {
+		slog.Error(err.Error())
+	}
+	return v1, v2, v3, v4
+}
+
 // Note: errors.Join is the std way to do AllErrors
