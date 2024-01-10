ec498acba466c734ea947b48a79fcbdb31b233ac
Kai O'Reilly <koreilly5297@gmail.com>
2024-01-09 21:12:56 -0800

    more work on tabs test

--- gi/tabs_test.go
+++ gi/tabs_test.go
@@ -6,15 +6,27 @@ package gi
 
 import (
 	"testing"
+
+	"goki.dev/girl/styles"
+	"goki.dev/girl/units"
+	"goki.dev/icons"
 )
 
 func TestTabs(t *testing.T) {
+	configTab := func(tb *Frame) {
+		NewLabel(tb).SetType(LabelHeadlineLarge).SetText(tb.Name())
+		NewLabel(tb).SetText(testStrings[len(testStrings)-1])
+		NewButton(tb).SetText(tb.Name()).SetIcon(icons.Send)
+	}
 	for _, typ := range TabTypesValues() {
 		sc := NewScene()
+		sc.Style(func(s *styles.Style) {
+			s.Min.Set(units.Dp(800))
+		})
 		ts := NewTabs(sc).SetType(typ)
-		ts.NewTab("Search")
-		ts.NewTab("Discover")
-		ts.NewTab("History")
+		configTab(ts.NewTab("Search"))
+		configTab(ts.NewTab("Discover"))
+		configTab(ts.NewTab("History"))
 		sc.AssertPixelsOnShow(t, testName("tabs", typ))
 	}
 }
