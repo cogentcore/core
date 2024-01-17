0262b71c9debd61ecefc553717870b8721ac9a21
Kai O'Reilly <koreilly5297@gmail.com>
2024-01-17 10:19:13 -0800

    fixed yamls test

--- grows/yamls/yamls_test.go
+++ grows/yamls/yamls_test.go
@@ -4,7 +4,12 @@
 
 package yamls
 
-import "testing"
+import (
+	"path/filepath"
+	"testing"
+
+	"cogentcore.org/core/grr"
+)
 
 type testStruct struct {
 	A string
@@ -12,30 +17,21 @@ type testStruct struct {
 }
 
 func TestYAML(t *testing.T) {
+	tpath := filepath.Join("testdata", "test.yaml")
+
 	s := &testStruct{A: "aaa", B: 3.14}
-	err := Save(s, "testdata/s.yaml")
-	if err != nil {
-		t.Error(err)
-	}
+	grr.Test(t, Save(s, tpath))
 	b, err := WriteBytes(s)
-	if err != nil {
-		t.Error(err)
-	}
+	grr.Test(t, err)
 
 	a := &testStruct{}
-	err = Open(a, "testdata/s.yaml")
-	if err != nil {
-		t.Error(err)
-	}
+	grr.Test(t, Open(a, tpath))
 	if *a != *s {
 		t.Errorf("Open failed to read same data as saved: wanted %v != got %v", s, a)
 	}
 
 	c := &testStruct{}
-	err = ReadBytes(c, b)
-	if err != nil {
-		t.Error(err)
-	}
+	grr.Test(t, ReadBytes(c, b))
 	if *c != *s {
 		t.Errorf("ReadBytes or WriteBytes failed to read same data as saved: wanted %v != got %v", s, c)
 	}
