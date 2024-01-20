5e9dc90ddeb8f2a367fe618a6559dc941d84271b
Kai O'Reilly <koreilly5297@gmail.com>
2024-01-20 14:40:39 -0800

    fixed paint tests to work with the new structure of not calling ComputeActualBackgroundColor automatically

--- paint/background_test.go
+++ paint/background_test.go
@@ -16,12 +16,14 @@ import (
 
 func TestBackgroundColor(t *testing.T) {
 	RunTest(t, "background_color", 300, 300, func(pc *Context) {
+		pabg := colors.C(colors.White)
 		st := styles.NewStyle()
 		st.Background = colors.C(colors.Blue)
+		st.ComputeActualBackground(pabg)
 		st.ToDots()
 
 		sz := st.BoxSpace().Size().Add(mat32.V2(200, 100))
-		pc.DrawStdBox(st, mat32.V2(50, 100), sz, colors.C(colors.White))
+		pc.DrawStdBox(st, mat32.V2(50, 100), sz, pabg)
 	})
 }
 
@@ -29,15 +31,17 @@ func TestBackgroundImage(t *testing.T) {
 	img, _, err := images.Open("test.png")
 	grr.Test(t, err)
 	RunTest(t, "background_image", 750, 400, func(pc *Context) {
+		pabg := colors.C(colors.White)
 		st := styles.NewStyle()
 		st.Background = img
+		st.ComputeActualBackground(pabg)
 		st.ToDots()
 
 		sz := st.BoxSpace().Size().Add(mat32.V2(200, 100))
 
 		test := func(of styles.ObjectFits, pos mat32.Vec2) {
 			st.ObjectFit = of
-			pc.DrawStdBox(st, pos, sz, colors.C(colors.White))
+			pc.DrawStdBox(st, pos, sz, pabg)
 		}
 
 		test(styles.FitFill, mat32.V2(0, 0))
--- paint/boxmodel_test.go
+++ paint/boxmodel_test.go
@@ -17,6 +17,7 @@ import (
 
 func TestBoxModel(t *testing.T) {
 	RunTest(t, "boxmodel", 300, 300, func(pc *Context) {
+		pabg := colors.C(colors.White)
 		s := styles.NewStyle()
 		s.Color = colors.Black
 		s.Background = colors.C(colors.Lightblue)
@@ -24,15 +25,17 @@ func TestBoxModel(t *testing.T) {
 		s.Border.Width.Set(units.Dp(5))
 		s.Border.Color.Set(colors.Red)
 		s.Border.Radius = styles.BorderRadiusFull
+		s.ComputeActualBackground(pabg)
 		s.ToDots()
 
 		sz := s.BoxSpace().Size().Add(mat32.V2(200, 100))
-		pc.DrawStdBox(s, mat32.V2(50, 100), sz, colors.C(colors.White))
+		pc.DrawStdBox(s, mat32.V2(50, 100), sz, pabg)
 	})
 }
 
 func TestBoxShadow(t *testing.T) {
 	RunTest(t, "boxshadow", 300, 300, func(pc *Context) {
+		pabg := colors.C(colors.White)
 		s := styles.NewStyle()
 		s.Color = colors.Black
 		s.Background = colors.C(colors.Lightblue)
@@ -41,34 +44,40 @@ func TestBoxShadow(t *testing.T) {
 		s.Border.Color.Set(colors.Red)
 		s.Border.Radius = styles.BorderRadiusFull
 		s.BoxShadow = styles.BoxShadow1()
+		s.ComputeActualBackground(pabg)
 		s.ToDots()
 
 		sz := s.BoxSpace().Size().Add(mat32.V2(200, 100))
 
-		pc.DrawStdBox(s, mat32.V2(50, 100), sz, colors.C(colors.White))
+		pc.DrawStdBox(s, mat32.V2(50, 100), sz, pabg)
 	})
 }
 
 func TestActualBackgroundColor(t *testing.T) {
 	RunTest(t, "actual-background-color", 300, 300, func(pc *Context) {
+		pabg := colors.C(colors.White)
 		a := styles.NewStyle()
 		a.Background = colors.C(colors.Lightgray)
-		pc.DrawStdBox(a, mat32.Vec2{}, mat32.V2(300, 300), colors.C(colors.White))
+		a.ComputeActualBackground(pabg)
+		pc.DrawStdBox(a, mat32.Vec2{}, mat32.V2(300, 300), pabg)
 
 		b := styles.NewStyle()
 		b.Background = colors.C(colors.Red)
 		b.Opacity = 0.5
+		b.ComputeActualBackground(a.ActualBackground)
 		pc.DrawStdBox(b, mat32.V2(50, 50), mat32.V2(200, 200), a.ActualBackground)
 
 		c := styles.NewStyle()
 		c.Background = colors.C(colors.Blue)
 		c.Opacity = 0.5
 		c.StateLayer = 0.1
+		c.ComputeActualBackground(b.ActualBackground)
 		pc.DrawStdBox(c, mat32.V2(75, 75), mat32.V2(150, 150), b.ActualBackground)
 
 		// d is transparent and thus should not be any different than c
 		d := styles.NewStyle()
 		d.Opacity = 0.5
+		d.ComputeActualBackground(c.ActualBackground)
 		pc.DrawStdBox(d, mat32.V2(100, 100), mat32.V2(100, 100), c.ActualBackground)
 	})
 }
