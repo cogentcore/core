f50e2b02ef962a5b2b9b9284d9f41990d91f32cf
Kai O'Reilly <koreilly5297@gmail.com>
2024-01-19 21:36:09 -0800

    removed DownCircleOutlined and RightCircleOutlined icons; use ExpandCircleDown and ExpandCircleRight instead

--- icons/iconnames.go
+++ icons/iconnames.go
@@ -3,12 +3,6 @@
 package icons
 
 const (
-	// DownCircleOutlined is https://github.com/cogentcore/core/blob/main/icons/svg/DownCircleOutlined.svg
-	DownCircleOutlined Icon = "DownCircleOutlined"
-
-	// RightCircleOutlined is https://github.com/cogentcore/core/blob/main/icons/svg/RightCircleOutlined.svg
-	RightCircleOutlined Icon = "RightCircleOutlined"
-
 	// Abc is https://github.com/cogentcore/core/blob/main/icons/svg/abc.svg
 	Abc Icon = "abc"
 
