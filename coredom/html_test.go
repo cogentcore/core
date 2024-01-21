2ec122752b8cf65e492233ba1b59dc767c398631
Kai O'Reilly <koreilly5297@gmail.com>
2024-01-21 13:57:17 -0800

    added TestHTMLElements

--- coredom/html_test.go
+++ coredom/html_test.go
@@ -5,9 +5,12 @@
 package coredom
 
 import (
+	"path/filepath"
 	"testing"
 
 	"cogentcore.org/core/gi"
+	"cogentcore.org/core/grr"
+	"cogentcore.org/core/ki"
 )
 
 func TestRenderHTML(t *testing.T) {
@@ -17,9 +20,30 @@ func TestRenderHTML(t *testing.T) {
 		<p>This is a demonstration of the various features of coredom</p>
 		<button>Hello, world!</button>
 		`
-	err := ReadHTMLString(NewContext(), b, s)
-	if err != nil {
-		t.Error(err)
-	}
+	grr.Test(t, ReadHTMLString(NewContext(), b, s))
 	b.AssertRender(t, "test-render-html")
+}
+
+func TestHTMLElements(t *testing.T) {
+	tests := []string{
+		`<h1>Test</h1>`,
+		`<h2>Test</h2>`,
+		`<h3>Test</h3>`,
+	}
+	for _, s := range tests {
+		b := gi.NewBody()
+		grr.Test(t, ReadHTMLString(NewContext(), b, s))
+
+		// find the tag of the element
+		tag := ""
+		b.WalkPre(func(k ki.Ki) bool {
+			if tp := k.Prop("tag"); tp != nil && tp != "body" {
+				tag = tp.(string)
+				return ki.Break
+			}
+			return ki.Continue
+		})
+
+		b.AssertRender(t, filepath.Join("html", "elements", tag))
+	}
 }
