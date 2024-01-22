85e931a059e34e92e65307033ca6ff0976d6f378
Kai O'Reilly <koreilly5297@gmail.com>
2024-01-22 12:05:52 -0800

    mark function as helper in grr.Test; removed *4 error helpers

--- grr/helpers.go
+++ grr/helpers.go
@@ -57,18 +57,6 @@ func Log3[T1, T2, T3 any](v1 T1, v2 T2, v3 T3, err error) (T1, T2, T3) {
 	return v1, v2, v3
 }
 
-// Log4 takes the given four values and error and returns the values if
-// the error is nil, and logs the error and returns zero values
-// if the error is non-nil. The intended usage is:
-//
-//	a, b, c, d := grr.Log4(MyFunc(v))
-func Log4[T1, T2, T3, T4 any](v1 T1, v2 T2, v3 T3, v4 T4, err error) (T1, T2, T3, T4) {
-	if err != nil {
-		slog.Error(err.Error())
-	}
-	return v1, v2, v3, v4
-}
-
 // Must takes the given error and panics if it is non-nil.
 // The intended usage is:
 //
@@ -112,20 +100,12 @@ func Must3[T1, T2, T3 any](v1 T1, v2 T2, v3 T3, err error) (T1, T2, T3) {
 	return v1, v2, v3
 }
 
-// Must4 takes the given four values and error and returns the values if
-// the error is nil, and panics if the error is non-nil. The intended usage is:
-//
-//	a, b, c, d := grr.Must4(MyFunc(v))
-func Must4[T1, T2, T3, T4 any](v1 T1, v2 T2, v3 T3, v4 T4, err error) (T1, T2, T3, T4) {
-	if err != nil {
-		panic(err)
-	}
-	return v1, v2, v3, v4
-}
-
-// TestingT is an interface wrapper around *testing.T
+// TestingT is an interface wrapper around [*testing.T]
 type TestingT interface {
 	Error(args ...any)
+
+	// Helper marks the calling function as a test helper function.
+	Helper()
 }
 
 // Test takes the given error and errors the test it if it is non-nil.
@@ -133,58 +113,11 @@ type TestingT interface {
 //
 //	grr.Test(t, MyFunc(v))
 func Test(t TestingT, err error) error {
+	t.Helper()
 	if err != nil {
 		t.Error(err)
 	}
 	return err
-}
-
-// Test1 takes the given value and error and returns the value if
-// the error is nil, and errors the test and returns a zero value
-// if the error is non-nil. The intended usage is:
-//
-//	a := grr.Test1(t, MyFunc(v))
-func Test1[T any](t TestingT, v T, err error) T {
-	if err != nil {
-		t.Error(err)
-	}
-	return v
-}
-
-// Test2 takes the given two values and error and returns the values if
-// the error is nil, and errors the test and returns zero values
-// if the error is non-nil. The intended usage is:
-//
-//	a, b := grr.Test2(t, MyFunc(v))
-func Test2[T1, T2 any](t TestingT, v1 T1, v2 T2, err error) (T1, T2) {
-	if err != nil {
-		t.Error(err)
-	}
-	return v1, v2
-}
-
-// Test3 takes the given three values and error and returns the values if
-// the error is nil, and errors the test and returns zero values
-// if the error is non-nil. The intended usage is:
-//
-//	a, b, c := grr.Test3(t, MyFunc(v))
-func Test3[T1, T2, T3 any](t TestingT, v1 T1, v2 T2, v3 T3, err error) (T1, T2, T3) {
-	if err != nil {
-		t.Error(err)
-	}
-	return v1, v2, v3
-}
-
-// Test4 takes the given four values and error and returns the values if
-// the error is nil, and errors the test and returns zero values
-// if the error is non-nil. The intended usage is:
-//
-//	a, b, c, d := grr.Test4(t, MyFunc(v))
-func Test4[T1, T2, T3, T4 any](t TestingT, v1 T1, v2 T2, v3 T3, v4 T4, err error) (T1, T2, T3, T4) {
-	if err != nil {
-		t.Error(err)
-	}
-	return v1, v2, v3, v4
 }
 
 // Ignore1 ignores an error return value for a function returning
@@ -212,15 +145,6 @@ func Ignore2[T1, T2 any](v1 T1, v2 T2, err error) (T1, T2) {
 //	a, b, c := grr.Ignore3(MyFunc(v))
 func Ignore3[T1, T2, T3 any](v1 T1, v2 T2, v3 T3, err error) (T1, T2, T3) {
 	return v1, v2, v3
-}
-
-// Ignore4 ignores an error return value for a function returning
-// four values and an error, allowing direct usage of the values.
-// The intended usage is:
-//
-//	a, b, c, d := grr.Ignore4(MyFunc(v))
-func Ignore4[T1, T2, T3, T4 any](v1 T1, v2 T2, v3 T3, v4 T4, err error) (T1, T2, T3, T4) {
-	return v1, v2, v3, v4
 }
 
 // Note: errors.Join is the std way to do AllErrors
