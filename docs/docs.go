42b2eb25483eee04b4e60dfbbbc10b4ae127ebfc
Kai O'Reilly <koreilly5297@gmail.com>
2024-01-22 08:07:49 -0800

    started on docs app

---
+++ docs/docs.go
@@ -0,0 +1,26 @@
+// Copyright 2024 Cogent Core. All rights reserved.
+// Use of this source code is governed by a BSD-style
+// license that can be found in the LICENSE file.
+
+package main
+
+import (
+	"embed"
+	"io/fs"
+
+	"cogentcore.org/core/gi"
+	"cogentcore.org/core/grr"
+	"cogentcore.org/core/webcore"
+)
+
+//go:embed content
+var content embed.FS
+
+func main() {
+	b := gi.NewAppBody("Cogent Core Docs")
+	pg := webcore.NewPage(b).SetSource(grr.Log1(fs.Sub(content, "content")))
+	b.AddAppBar(pg.AppBar)
+	w := b.NewWindow().Run()
+	pg.OpenURL("", true)
+	w.Wait()
+}
