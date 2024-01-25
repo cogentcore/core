480928318abc21fe0490b3504e2733e24b29c0c6
Kai O'Reilly <koreilly5297@gmail.com>
2024-01-25 15:36:39 -0800

    started on timed snackbar tests

--- gi/snackbar_test.go
+++ gi/snackbar_test.go
@@ -7,17 +7,27 @@ package gi
 import (
 	"path/filepath"
 	"testing"
+	"time"
 
 	"cogentcore.org/core/styles"
 	"cogentcore.org/core/units"
 )
 
-func TestSnackbar(t *testing.T) {
-	b := NewBody()
-	b.Style(func(s *styles.Style) {
-		s.Min.Set(units.Dp(300))
-	})
-	b.AssertScreenRender(t, filepath.Join("snackbar", "basic"), func() {
-		MessageSnackbar(b, "Test")
-	})
+func TestSnackbarTime(t *testing.T) {
+	SystemSettings.SnackbarTimeout = 50 * time.Millisecond
+	defer func() {
+		SystemSettings.SnackbarTimeout = 7 * time.Second
+	}()
+	times := []time.Duration{0, 25 * time.Millisecond, 25 * time.Millisecond}
+	for _, tm := range times {
+		tm := tm
+		b := NewBody()
+		b.Style(func(s *styles.Style) {
+			s.Min.Set(units.Dp(300))
+		})
+		b.AssertScreenRender(t, filepath.Join("snackbar", tm.String()), func() {
+			MessageSnackbar(b, "Test")
+			time.Sleep(tm)
+		})
+	}
 }
