8bb1431c306c991aa181c2f117bc42761df86e68
Kai O'Reilly <koreilly5297@gmail.com>
2024-01-25 21:54:38 -0800

    fixed app bars showing up on popups

--- gi/bars.go
+++ gi/bars.go
@@ -54,7 +54,7 @@ func (sc *Scene) AddAppBar(fun func(tb *Toolbar)) {
 // ConfigSceneBars configures the side control bars, for main scenes
 func (sc *Scene) ConfigSceneBars() {
 	// at last possible moment, add app-specific app bar config
-	if TheApp.AppBarConfig != nil {
+	if TheApp.AppBarConfig != nil && sc.Stage.Type.IsMain() {
 		if sc.Bars.Top.IsEmpty() {
 			sc.Bars.Top.Add(TheApp.AppBarConfig) // put in the top by default
 		}
