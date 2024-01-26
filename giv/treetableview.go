216675df30afd0dddf19bb88f1c3e9227ae92847
Kai O'Reilly <koreilly5297@gmail.com>
2024-01-25 16:55:22 -0800

    implemented basic tree table config and test

--- giv/treetableview.go
+++ giv/treetableview.go
@@ -5,16 +5,32 @@
 package giv
 
 import (
+	"image/color"
+
 	"cogentcore.org/core/gi"
 )
 
 // TreeTableView combines a [TreeView] and [TableView].
 type TreeTableView struct {
-	*gi.Frame
+	gi.Frame
 
 	// Tree is the tree view component of the tree table view.
-	Tree *TreeView
+	Tree *TreeView `set:"-"`
 
 	// Table is the table view component of the tree table view.
-	Table *TableView
+	Table *TableView `set:"-"`
+}
+
+func (tt *TreeTableView) ConfigWidget() {
+	if tt.HasChildren() {
+		return
+	}
+
+	updt := tt.UpdateStart()
+
+	sp := gi.NewSplits(tt)
+	tt.Tree = NewTreeView(sp)
+	tt.Table = NewTableView(sp).SetSlice(&[]color.RGBA{})
+
+	tt.UpdateEndLayout(updt)
 }
