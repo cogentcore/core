17d7c5b875b5ee3ca28ad85f30cdecbc8b93f0c3
Kai O'Reilly <koreilly5297@gmail.com>
2024-01-27 08:25:49 -0800

    fixed webcore build error

--- webcore/page.go
+++ webcore/page.go
@@ -230,13 +230,13 @@ func (pg *Page) AppBar(tb *gi.Toolbar) {
 	})
 
 	ch.AllowNew = true
-	ch.ItemsFunc = func() {
+	ch.AddItemsFunc(func() {
 		ch.Items = make([]any, len(pg.History))
 		for i, u := range pg.History {
 			// we reverse the order
 			ch.Items[len(pg.History)-i-1] = u
 		}
-	}
+	})
 	ch.OnChange(func(e events.Event) {
 		// we need a slash so that it doesn't think it's a relative URL
 		pg.OpenURL("/"+ch.CurLabel, true)
