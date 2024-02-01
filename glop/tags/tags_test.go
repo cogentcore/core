cb764b9acf6f20d1d2ab2d082615da7a9a1e800d
Kai O'Reilly <koreilly5297@gmail.com>
2024-01-31 16:49:01 -0800

    added tags options demonstration

---
+++ glop/tags/tags_test.go
@@ -0,0 +1,18 @@
+// Copyright (c) 2024, Cogent Core. All rights reserved.
+// Use of this source code is governed by a BSD-style
+// license that can be found in the LICENSE file.
+
+package tags
+
+// func (u *User) TypeTags() map[string]map[string]any {
+// 	return map[string]map[string]any{
+// 		"Name": {"view": u.On},
+// 	}
+// }
+
+// func (u *User) TypeTag(field, tag string) (any, bool) {
+// 	if field == "Name" && tag == "view" {
+// 		return u.On, true
+// 	}
+// 	return nil, false
+// }
