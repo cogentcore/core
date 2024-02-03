d7f7657ad99f6b069b3ca2c3a7c552f5ff6542ad
Kai O'Reilly <koreilly5297@gmail.com>
2024-02-03 15:35:23 -0800

    added basic canvas test for #268

---
+++ gi/canvas_test.go
@@ -0,0 +1,25 @@
+// Copyright (c) 2024, Cogent Core. All rights reserved.
+// Use of this source code is governed by a BSD-style
+// license that can be found in the LICENSE file.
+
+package gi
+
+import (
+	"path/filepath"
+	"testing"
+
+	"cogentcore.org/core/colors"
+	"cogentcore.org/core/paint"
+)
+
+func TestCanvas(t *testing.T) {
+	b := NewBody()
+	c := NewCanvas(b)
+	c.Draw(func(pc *paint.Context) {
+		pc.MoveTo(20, 50)
+		pc.LineTo(50, 20)
+		pc.StrokeStyle.Color = colors.C(colors.Blue)
+		pc.Stroke()
+	})
+	b.AssertRender(t, filepath.Join("canvas", "basic"))
+}
