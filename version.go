2cdfff95ce5e24b1694ecf0cf813138d4e7a9280
Kai O'Reilly <koreilly5297@gmail.com>
2024-02-05 12:43:12 -0800

    updated version to v0.0.4

--- version.go
+++ version.go
@@ -4,9 +4,9 @@ package core
 
 const (
 	// Version is the version of this package being used
-	Version = "v0.0.3"
+	Version = "v0.0.4"
 	// GitCommit is the commit just before the latest version commit
-	GitCommit = "430c445a6"
+	GitCommit = "cc0d4cfc8"
 	// VersionDate is the date-time of the latest version commit in UTC (in the format 'YYYY-MM-DD HH:MM', which is the Go format '2006-01-02 15:04')
-	VersionDate = "2024-01-25 06:24"
+	VersionDate = "2024-02-05 20:43"
 )
