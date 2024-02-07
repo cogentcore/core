d1abef4031c6b1b1647fa56b485113075c139b8a
Kai O'Reilly <koreilly5297@gmail.com>
2024-02-06 17:48:00 -0800

    cleaned up gi strings functions

--- gi/strings.go
+++ gi/strings.go
@@ -4,35 +4,10 @@
 
 package gi
 
-// TODO(kai/menu): should these string functions be in glop?
-
-// StringsInsertFirst inserts the given string at start of a string slice,
-// while keeping overall length to given max value
-// useful for a "recents" kind of string list
-func StringsInsertFirst(strs *[]string, str string, max int) {
-	if strs == nil {
-		*strs = make([]string, 0, max)
-	}
-	sz := len(*strs)
-	if sz > max {
-		*strs = (*strs)[:max]
-	}
-	if sz >= max {
-		copy((*strs)[1:max], (*strs)[0:max-1])
-		(*strs)[0] = str
-	} else {
-		*strs = append(*strs, "")
-		if sz > 0 {
-			copy((*strs)[1:], (*strs)[0:sz])
-		}
-		(*strs)[0] = str
-	}
-}
-
-// StringsInsertFirstUnique inserts the given string at start of a string slice,
-// while keeping overall length to given max value.
-// if item is already on the list, then it is moved to the top and not re-added (unique items only)
-// useful for a "recents" kind of string list
+// StringsInsertFirstUnique inserts the given string at the start of the given string slice
+// while keeping the overall length to the given max value. If the item is already on the list,
+// then it is moved to the top and not re-added (unique items only). This is useful for a list
+// of recent items.
 func StringsInsertFirstUnique(strs *[]string, str string, max int) {
 	if strs == nil {
 		*strs = make([]string, 0, max)
@@ -60,32 +35,5 @@ func StringsInsertFirstUnique(strs *[]string, str string, max int) {
 			copy((*strs)[1:], (*strs)[0:sz])
 		}
 		(*strs)[0] = str
-	}
-}
-
-// StringsAppendIfUnique append str to strs if not already in slice
-func StringsAppendIfUnique(strs *[]string, str string, max int) {
-	if strs == nil {
-		*strs = make([]string, 0, max)
-	}
-	for _, s := range *strs {
-		if s == str {
-			return
-		}
-	}
-	*strs = append(*strs, str)
-}
-
-// StringsRemoveExtras is a generic function for removing items of a slice from another slice
-func StringsRemoveExtras(items *[]string, extras []string) {
-	for _, extra := range extras {
-		i := 0
-		for _, item := range *items {
-			if item != extra {
-				(*items)[i] = item
-				i++
-			}
-		}
-		*items = (*items)[:i]
 	}
 }
