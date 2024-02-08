cogentcore/core

commit a4dfa25acced381be890a1773971b88e1b054984

important: added Grow(1,0) to Button.label -- may cause issues so keep an eye on that; semi-futile attempt to get FileValue to obey width prefs in TableView -- works fine elsewhere but something weird is happening. need to revisit.

gi/button.go
@@ -204,6 +204,7 @@ func (bt *Button) SetStyles() {
 			w.Style(func(s *styles.Style) {
 				s.SetNonSelectable()
 				s.SetTextWrap(false)
+				s.Grow.Set(1, 0)
 				s.Margin.Zero()
 				s.Padding.Zero()
 				s.Max.X.Zero()

giv/values.go
@@ -1504,6 +1504,9 @@ func (vv *FileValue) ConfigWidget(w gi.Widget) {
 	vv.StdConfigWidget(w)
 	bt := vv.Widget.(*gi.Button)
 	bt.SetType(gi.ButtonTonal)
+	bt.Style(func(s *styles.Style) {
+		s.Min.X.Ch(25)
+	})
 	ConfigDialogWidget(vv, bt, false)
 	vv.UpdateWidget()
 }

examples/views/views.go
@@ -104,6 +104,9 @@ type Struct struct { //gti:add
 	Things []*TableStruct
 
 	Stuff []float32
+
+	// a file
+	File gi.Filename
 }
 
 func (st *Struct) ShouldShow(field string) bool {

giv/tableview.go
@@ -430,8 +430,8 @@ func (tv *TableView) ConfigRows() {
 			}
 			if i == 0 {
 				tv.ColMaxWidths[fli] = 0
-				_, isbase := vv.(*ValueBase)
-				if isbase && tv.SliceSize > 0 && fval.Kind() == reflect.String {
+				_, isicon := vv.(*IconValue)
+				if !isicon && tv.SliceSize > 0 && fval.Kind() == reflect.String {
 					mxw := 0
 					for rw := 0; rw < tv.SliceSize; rw++ {
 						sval := laser.OnePtrUnderlyingValue(tv.SliceNPVal.Index(rw))

commit ff7bd4615f5a9b9ef4f90baee207aaa0052d5061

fixed date view and file value button styling that @rcoreilly broke in a4dfa25

gi/button.go
@@ -204,7 +204,6 @@ func (bt *Button) SetStyles() {
 			w.Style(func(s *styles.Style) {
 				s.SetNonSelectable()
 				s.SetTextWrap(false)
-				s.Grow.Set(1, 0)
 				s.Margin.Zero()
 				s.Padding.Zero()
 				s.Max.X.Zero()

giv/values.go
@@ -1504,9 +1504,6 @@ func (vv *FileValue) ConfigWidget(w gi.Widget) {
 	vv.StdConfigWidget(w)
 	bt := vv.Widget.(*gi.Button)
 	bt.SetType(gi.ButtonTonal)
-	bt.Style(func(s *styles.Style) {
-		s.Min.X.Ch(25)
-	})
 	ConfigDialogWidget(vv, bt, false)
 	vv.UpdateWidget()
 }