5419f11dc700e19547c60d24d5c6f6fa7c3851ef
Kai O'Reilly <koreilly5297@gmail.com>
2024-02-14 16:43:14 -0800

    added mixed vertical splits test for #850

---
+++ gi/splits_test.go
@@ -0,0 +1,26 @@
+// Copyright (c) 2024, Cogent Core. All rights reserved.
+// Use of this source code is governed by a BSD-style
+// license that can be found in the LICENSE file.
+
+package gi
+
+import (
+	"testing"
+
+	"cogentcore.org/core/styles"
+)
+
+// For https://github.com/cogentcore/core/issues/850
+func TestMixedVerticalSplits(t *testing.T) {
+	b := NewBody()
+	txt := "This is a long sentence that I wrote for the purpose of testing vertical splits behavior"
+	NewLabel(b).SetText(txt)
+	sp := NewSplits(b)
+	sp.Style(func(s *styles.Style) {
+		s.Direction = styles.Column
+	})
+	NewLabel(sp).SetText(txt)
+	NewLabel(sp).SetText(txt)
+	NewLabel(b).SetText(txt)
+	b.AssertRender(t, "splits/mixed-vertical")
+}
