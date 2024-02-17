944ba450de8467fd418590a019e3bd2d90af40fd
ddkwork <2762713521@qq.com>
2024-02-17 04:45:09 -0800

    ki test

--- ki/io_test.go
+++ ki/io_test.go
@@ -81,23 +81,23 @@ func TestNodeXML(t *testing.T) {
 	typ := parent.KiType()
 	parent.Mbr1 = "bloop"
 	parent.Mbr2 = 32
-	// child1 :=
-	parent.NewChild(typ, "child1")
-	var child2 = parent.NewChild(typ, "child1").(*testdata.NodeEmbed)
-	// child3 :=
-	parent.NewChild(typ, "child1")
+
+	parent.NewChild(typ, "child1") // child1 :=
+	child2 := parent.NewChild(typ, "child1").(*testdata.NodeEmbed)
+
+	parent.NewChild(typ, "child1") // child3 :=
 	child2.NewChild(typ, "subchild1")
 
 	var buf bytes.Buffer
 	assert.NoError(t, parent.WriteXML(&buf, true))
 
 	b := buf.Bytes()
-	tstload := testdata.NodeEmbed{}
-	tstload.InitName(&tstload, "")
-	assert.NoError(t, tstload.ReadXML(bytes.NewReader(b)))
+	nodeEmbed := testdata.NodeEmbed{}
+	nodeEmbed.InitName(&nodeEmbed, "")
+	assert.NoError(t, nodeEmbed.ReadXML(&buf))
 
 	var buf2 bytes.Buffer
-	assert.NoError(t, tstload.WriteXML(&buf2, true))
+	assert.NoError(t, nodeEmbed.WriteXML(&buf2, true))
 	assert.Equal(t, buf2.Bytes(), b)
 	// fmt.Printf("test loaded json output:\n%v\n", string(tstb))
 }
