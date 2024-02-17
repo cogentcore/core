c86ba22892619f476177656cdf60413ccdfb767f
ddkwork <2762713521@qq.com>
2024-02-17 06:15:17 -0800

    add concurrentMapTable.go

--- examples/concurrentMapTable/concurrentMapTable.go
+++ examples/concurrentMapTable/concurrentMapTable.go
@@ -5,7 +5,33 @@ import (
 	"cogentcore.org/core/gi"
 	"cogentcore.org/core/giv"
 	"cogentcore.org/core/icons"
+	"time"
 )
+
+func main() {
+	table := make([]*TableStruct, 0, maxRow)
+	b := gi.NewBody("concurrent-map")
+	tv := giv.NewTableView(b, "tv")
+	tv.SetReadOnly(true)
+	tv.SetSlice(&table)
+
+	b.OnShow(func(e events.Event) {
+		go func() {
+			for i := 0; i < maxRow; i++ {
+				table = append(table, &TableStruct{IntField: i, FloatField: float32(i) / 10.0})
+				updt := tv.UpdateStartAsync()
+				tv.UpdateWidgets()
+				tv.UpdateEndAsyncRender(updt)
+				if len(table) > 0 {
+					tv.ScrollToIdx(len(table) - 1)
+				}
+				time.Sleep(300 * time.Millisecond)
+			}
+		}()
+	})
+
+	b.RunMainWindow()
+}
 
 type TableStruct struct { //gti:add
 	Icon       icons.Icon
@@ -15,26 +41,4 @@ type TableStruct struct { //gti:add
 	File       gi.Filename
 }
 
-func main() {
-	table := make([]*TableStruct, 0, 100000)
-	b := gi.NewBody("concurrent-map")
-	tv := giv.NewTableView(b, "tv")
-	tv.SetReadOnly(true)
-	tv.SetSlice(&table)
-
-	b.OnShow(func(e events.Event) {
-		go func() {
-			for i := 0; i < 100000; i++ {
-				table = append(table, &TableStruct{IntField: i, FloatField: float32(i) / 10.0})
-				updt := tv.UpdateStartAsync()
-				tv.UpdateWidgets()
-				tv.UpdateEndAsyncRender(updt)
-				if len(table) > 0 {
-					tv.ScrollToIdx(len(table) - 1)
-				}
-			}
-		}()
-	})
-
-	b.RunMainWindow()
-}
+const maxRow = 100000
