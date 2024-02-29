5f9a63564f1df0028a9d33f803671ef487b1bdb7
Kai O'Reilly <koreilly5297@gmail.com>
2024-02-29 12:52:40 -0800

    added more hct transform unit tests

--- cam/hct/transform_test.go
+++ cam/hct/transform_test.go
@@ -5,9 +5,7 @@
 package hct
 
 import (
-	"fmt"
 	"image/color"
-	"math/rand"
 	"testing"
 
 	"github.com/stretchr/testify/assert"
@@ -16,19 +14,24 @@ import (
 func TestTransform(t *testing.T) {
 	assert.Equal(t, color.RGBA{131, 140, 255, 255}, Lighten(color.RGBA{0, 0, 255, 255}, 30))
 	assert.Equal(t, color.RGBA{0, 0, 52, 255}, Darken(color.RGBA{0, 0, 255, 255}, 30))
-	assert.Equal(t, color.RGBA{131, 140, 255, 255}, Highlight(color.RGBA{0, 0, 255, 255}, 30))
-	assert.Equal(t, color.RGBA{0, 54, 92, 255}, Highlight(color.RGBA{18, 127, 205, 255}, 30))
+	assert.Equal(t, color.RGBA{80, 90, 255, 255}, Highlight(color.RGBA{0, 0, 255, 255}, 15))
+	assert.Equal(t, color.RGBA{0, 82, 136, 255}, Highlight(color.RGBA{18, 127, 205, 255}, 18))
+
+	assert.Equal(t, color.RGBA{201, 0, 143, 255}, Saturate(color.RGBA{201, 2, 143, 255}, 16))
+	assert.Equal(t, color.RGBA{96, 76, 125, 255}, Desaturate(color.RGBA{112, 35, 206, 255}, 43))
+
+	assert.Equal(t, color.RGBA{107, 66, 106, 255}, Spin(color.RGBA{30, 85, 116, 255}, 91))
+
+	assert.Equal(t, float32(80), MinHueDistance(240, 320))
+	assert.Equal(t, float32(-80), MinHueDistance(320, 240))
+	assert.Equal(t, float32(46), MinHueDistance(320, 6))
+	assert.Equal(t, float32(-46), MinHueDistance(6, 320))
 
 	c := Blend(50, color.RGBA{255, 255, 255, 255}, color.RGBA{0, 0, 0, 255})
 	assert.Equal(t, color.RGBA{119, 119, 119, 255}, c)
-}
 
-func TestMinHueDistance(t *testing.T) {
-	t.Skip("informational confirmation")
-	for i := 0; i < 50; i++ {
-		a := rand.Intn(360)
-		b := rand.Intn(360)
-		d := MinHueDistance(float32(a), float32(b))
-		fmt.Println(a, b, d)
-	}
+	assert.False(t, IsLight(color.RGBA{17, 38, 91, 255}))
+	assert.True(t, IsLight(color.RGBA{178, 89, 203, 255}))
+	assert.True(t, IsDark(color.RGBA{17, 38, 91, 255}))
+	assert.False(t, IsDark(color.RGBA{178, 89, 203, 255}))
 }
