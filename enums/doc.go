bfd99b162a8b4aef349e9c67ee0e65af05e32740
Kai O'Reilly <koreilly5297@gmail.com>
2024-03-04 11:34:33 -0800

    removed old enums doc file

--- enums/doc.go
+++
@@ -1,7 +0,0 @@
-// Copyright (c) 2023, Cogent Core. All rights reserved.
-// Use of this source code is governed by a BSD-style
-// license that can be found in the LICENSE file.
-
-// Package enums provides common interfaces for enums
-// and bit flag enums and utilities for using them
-package enums
