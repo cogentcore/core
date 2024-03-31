8b4e0546eb088ae7a12cbe0fd361c91b6ba5de0c
Kai O'Reilly <koreilly5297@gmail.com>
2024-03-31 15:27:20 -0700

    update canvas test to new structure

--- gi/canvas_test.go
+++ gi/canvas_test.go
@@ -15,15 +15,15 @@ import (
 func TestCanvas(t *testing.T) {
 	b := NewBody()
 	NewCanvas(b).SetDraw(func(pc *paint.Context) {
-		pc.MoveTo(20, 50)
-		pc.LineTo(50, 20)
+		pc.MoveTo(0.25, 0.5)
+		pc.LineTo(0.5, 0.25)
 		pc.StrokeStyle.Color = colors.C(colors.Blue)
 		pc.Stroke()
 
-		pc.FillBoxColor(mat32.V2(164, 58), mat32.V2(50, 112), colors.Scheme.Success.Container)
+		pc.FillBoxColor(mat32.V2(0.3, 0.4), mat32.V2(0.5, 0.8), colors.Scheme.Success.Container)
 
 		pc.FillStyle.Color = colors.C(colors.Orange)
-		pc.DrawCircle(80, 110, 40)
+		pc.DrawCircle(0.4, 0.6, 0.5)
 		pc.Fill()
 	})
 	b.AssertRender(t, "canvas/basic")
