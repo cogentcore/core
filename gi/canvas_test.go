7f096b32a119e483f8becacbd657e0a19b4502e6
Kai O'Reilly <koreilly5297@gmail.com>
2024-04-01 11:59:03 -0700

    fix canvas test

--- gi/canvas_test.go
+++ gi/canvas_test.go
@@ -15,15 +15,15 @@ import (
 func TestCanvas(t *testing.T) {
 	b := NewBody()
 	NewCanvas(b).SetDraw(func(pc *paint.Context) {
-		pc.MoveTo(0.25, 0.5)
-		pc.LineTo(0.5, 0.25)
+		pc.MoveTo(0.15, 0.3)
+		pc.LineTo(0.3, 0.15)
 		pc.StrokeStyle.Color = colors.C(colors.Blue)
 		pc.Stroke()
 
-		pc.FillBox(mat32.V2(0.3, 0.2), mat32.V2(0.5, 0.6), colors.C(colors.Scheme.Success.Container))
+		pc.FillBox(mat32.V2(0.7, 0.3), mat32.V2(0.2, 0.5), colors.C(colors.Scheme.Success.Container))
 
 		pc.FillStyle.Color = colors.C(colors.Orange)
-		pc.DrawCircle(0.4, 0.6, 0.2)
+		pc.DrawCircle(0.4, 0.5, 0.15)
 		pc.Fill()
 	})
 	b.AssertRender(t, "canvas/basic")
