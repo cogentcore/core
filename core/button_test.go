18c0597819581a6c29c3d11aea831c0077e6156a
Kai O'Reilly <koreilly5297@gmail.com>
2024-04-17 12:15:57 -0700

    add TestButtonTypes and remove bad old TestButton

--- core/button_test.go
+++ core/button_test.go
@@ -5,6 +5,7 @@
 package core
 
 import (
+	"strings"
 	"testing"
 
 	"cogentcore.org/core/events"
@@ -65,26 +66,10 @@ func TestButtonMenuClick(t *testing.T) {
 	})
 }
 
-func TestButton(t *testing.T) {
-	for typi, typ := range ButtonTypesValues() {
-		for stri, str := range testStrings {
-			for ici, ic := range testIcons {
-				if stri == 0 && ici == 0 {
-					continue
-				}
-				for sti, st := range testStates {
-					// we do not test other types and states of the rest
-					// of the strings, as that is a waste of
-					// testing time
-					if stri > 1 && (typi > 0 || sti > 0) {
-						continue
-					}
-					b := NewBody()
-					bt := NewButton(b).SetType(typ).SetText(str).SetIcon(ic).SetState(true, st...)
-					nm := testName("button", typ, str, ic, bt.Styles.State)
-					b.AssertRender(t, nm)
-				}
-			}
-		}
+func TestButtonTypes(t *testing.T) {
+	for _, typ := range ButtonTypesValues() {
+		b := NewBody()
+		NewButton(b).SetType(typ).SetText(typ.String())
+		b.AssertRender(t, "button/type-"+strings.ToLower(typ.String()))
 	}
 }
