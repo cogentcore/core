5fb32f0774e1f9bf1f0e573f08c585d1fa9d5a33
Kai O'Reilly <koreilly5297@gmail.com>
2024-04-18 13:52:30 -0700

    test slider value, bounds, and tooltip

--- core/slider_test.go
+++ core/slider_test.go
@@ -4,10 +4,31 @@
 
 package core
 
-import "testing"
+import (
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+)
 
 func TestSlider(t *testing.T) {
 	b := NewBody()
-	NewSlider(b)
+	sr := NewSlider(b)
+	assert.Equal(t, "(value: 0.5, minimum: 0, maximum: 1)", sr.WidgetTooltip())
+	sr.SetTooltip("Rating")
+	assert.Equal(t, "Rating (value: 0.5, minimum: 0, maximum: 1)", sr.WidgetTooltip())
 	b.AssertRender(t, "slider/basic")
+}
+
+func TestSliderValue(t *testing.T) {
+	b := NewBody()
+	sr := NewSlider(b).SetValue(0.7)
+	assert.Equal(t, "(value: 0.7, minimum: 0, maximum: 1)", sr.WidgetTooltip())
+	b.AssertRender(t, "slider/value")
+}
+
+func TestSliderBounds(t *testing.T) {
+	b := NewBody()
+	sr := NewSlider(b).SetMin(5.7).SetMax(18).SetValue(10.2)
+	assert.Equal(t, "(value: 10.2, minimum: 5.7, maximum: 18)", sr.WidgetTooltip())
+	b.AssertRender(t, "slider/bounds")
 }
