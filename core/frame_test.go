90ca117a2c44829f8010a4d1043ea38ee2c4fa13
Kai O'Reilly <koreilly5297@gmail.com>
2024-04-18 12:32:01 -0700

    test frame background and gradient

--- core/frame_test.go
+++ core/frame_test.go
@@ -4,13 +4,43 @@
 
 package core
 
-import "testing"
+import (
+	"testing"
+
+	"cogentcore.org/core/colors"
+	"cogentcore.org/core/colors/gradient"
+	"cogentcore.org/core/styles"
+)
+
+func frameTestButtons(fr *Frame) {
+	NewButton(fr).SetText("First")
+	NewButton(fr).SetText("Second")
+	NewButton(fr).SetText("Third")
+}
 
 func TestFrame(t *testing.T) {
 	b := NewBody()
 	fr := NewFrame(b)
-	NewButton(fr).SetText("First")
-	NewButton(fr).SetText("Second")
-	NewButton(fr).SetText("Third")
+	frameTestButtons(fr)
 	b.AssertRender(t, "frame/basic")
+}
+
+func TestFrameBackground(t *testing.T) {
+	b := NewBody()
+	fr := NewFrame(b)
+	fr.Style(func(s *styles.Style) {
+		s.Background = colors.C(colors.Scheme.Warn.Container)
+	})
+	frameTestButtons(fr)
+	b.AssertRender(t, "frame/background")
+}
+
+func TestFrameGradient(t *testing.T) {
+	b := NewBody()
+	fr := NewFrame(b)
+	fr.Style(func(s *styles.Style) {
+		s.Background = gradient.NewLinear().AddStop(colors.Yellow, 0).AddStop(colors.Orange, 0.5).AddStop(colors.Red, 1)
+	})
+	frameTestButtons(fr)
+	b.AssertRender(t, "frame/gradient")
 }
