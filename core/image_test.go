c2a7e4a33de3022a917ed234a1dac9a339b855ff
Kai O'Reilly <koreilly5297@gmail.com>
2024-04-18 12:51:36 -0700

    test image size and direct go image

--- core/image_test.go
+++ core/image_test.go
@@ -9,10 +9,12 @@ import (
 	"path/filepath"
 	"testing"
 
+	"cogentcore.org/core/colors"
 	"cogentcore.org/core/math32"
 	"cogentcore.org/core/styles"
 	"cogentcore.org/core/units"
 	"github.com/stretchr/testify/assert"
+	"golang.org/x/image/draw"
 )
 
 var testImagePath = Filename(filepath.Join("..", "icon.png"))
@@ -22,6 +24,26 @@ func TestImage(t *testing.T) {
 	img := NewImage(b)
 	assert.NoError(t, img.Open(testImagePath))
 	b.AssertRender(t, "image/basic")
+}
+
+func TestImageSize(t *testing.T) {
+	b := NewBody()
+	img := NewImage(b)
+	assert.NoError(t, img.Open(testImagePath))
+	img.Style(func(s *styles.Style) {
+		s.Min.Set(units.Dp(256))
+	})
+	b.AssertRender(t, "image/size")
+}
+
+func TestImageDirect(t *testing.T) {
+	b := NewBody()
+	img := image.NewRGBA(image.Rect(0, 0, 100, 100))
+	draw.Draw(img, image.Rect(10, 5, 100, 90), colors.C(colors.Scheme.Warn.Container), image.Point{}, draw.Src)
+	draw.Draw(img, image.Rect(20, 20, 60, 50), colors.C(colors.Scheme.Success.Base), image.Point{}, draw.Src)
+	draw.Draw(img, image.Rect(60, 70, 80, 100), colors.C(colors.Scheme.Error.Base), image.Point{}, draw.Src)
+	NewImage(b).SetImage(img)
+	b.AssertRender(t, "image/direct")
 }
 
 func TestImageCropped(t *testing.T) {
