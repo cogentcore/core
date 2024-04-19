9cc5110c7bd0035a1c95a5f5ee77bafac47af821
Kai O'Reilly <koreilly5297@gmail.com>
2024-04-18 20:45:04 -0700

    add more splits tests

--- core/splits_test.go
+++ core/splits_test.go
@@ -10,6 +10,54 @@ import (
 	"cogentcore.org/core/styles"
 )
 
+func TestSplits(t *testing.T) {
+	b := NewBody()
+	sp := NewSplits(b)
+	NewLabel(sp).SetText("First")
+	NewLabel(sp).SetText("Second")
+	b.AssertRender(t, "splits/basic")
+}
+
+func TestSplitsMany(t *testing.T) {
+	b := NewBody()
+	sp := NewSplits(b)
+	NewLabel(sp).SetText("First")
+	NewLabel(sp).SetText("Second")
+	NewLabel(sp).SetText("Third")
+	NewLabel(sp).SetText("Fourth")
+	b.AssertRender(t, "splits/many")
+}
+
+func TestSplitsSet(t *testing.T) {
+	b := NewBody()
+	sp := NewSplits(b).SetSplits(0.2, 0.8)
+	NewLabel(sp).SetText("First")
+	NewLabel(sp).SetText("Second")
+	b.AssertRender(t, "splits/set")
+}
+
+func TestSplitsColumn(t *testing.T) {
+	b := NewBody()
+	sp := NewSplits(b)
+	sp.Style(func(s *styles.Style) {
+		s.Direction = styles.Column
+	})
+	NewLabel(sp).SetText("First")
+	NewLabel(sp).SetText("Second")
+	b.AssertRender(t, "splits/column")
+}
+
+func TestSplitsRow(t *testing.T) {
+	b := NewBody()
+	sp := NewSplits(b)
+	sp.Style(func(s *styles.Style) {
+		s.Direction = styles.Row
+	})
+	NewLabel(sp).SetText("First")
+	NewLabel(sp).SetText("Second")
+	b.AssertRender(t, "splits/row")
+}
+
 // For https://github.com/cogentcore/core/issues/850
 func TestMixedVerticalSplits(t *testing.T) {
 	b := NewBody()
