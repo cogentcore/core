cd72b908081556e7eae89558694a0590b202574e
Kai O'Reilly <koreilly5297@gmail.com>
2024-04-18 21:20:04 -0700

    add more tab tests

--- core/tabs_test.go
+++ core/tabs_test.go
@@ -8,26 +8,66 @@ import (
 	"testing"
 
 	"cogentcore.org/core/icons"
-	"cogentcore.org/core/strcase"
-	"cogentcore.org/core/styles"
-	"cogentcore.org/core/units"
 )
 
 func TestTabs(t *testing.T) {
-	configTab := func(tb *Frame) {
-		NewLabel(tb).SetType(LabelHeadlineLarge).SetText(tb.Name())
-		NewLabel(tb).SetText("This is a label contained within a tab called " + tb.Name())
-		NewButton(tb).SetText(tb.Name()).SetIcon(icons.Send)
-	}
-	for _, typ := range TabTypesValues() {
-		b := NewBody()
-		b.Style(func(s *styles.Style) {
-			s.Min.Set(units.Dp(800))
-		})
-		ts := NewTabs(b).SetType(typ)
-		configTab(ts.NewTab("Search", icons.Search))
-		configTab(ts.NewTab("Discover", icons.Explore))
-		configTab(ts.NewTab("History", icons.History))
-		b.AssertRender(t, "tabs/"+strcase.ToKebab(typ.String()))
-	}
+	b := NewBody()
+	ts := NewTabs(b)
+	ts.NewTab("First")
+	ts.NewTab("Second")
+	b.AssertRender(t, "tabs/basic")
+}
+
+func TestTabsWidgets(t *testing.T) {
+	b := NewBody()
+	ts := NewTabs(b)
+	first := ts.NewTab("First")
+	NewLabel(first).SetText("I am first!")
+	second := ts.NewTab("Second")
+	NewLabel(second).SetText("I am second!")
+	b.AssertRender(t, "tabs/widgets")
+}
+
+func TestTabsMany(t *testing.T) {
+	b := NewBody()
+	ts := NewTabs(b)
+	ts.NewTab("First")
+	ts.NewTab("Second")
+	ts.NewTab("Third")
+	ts.NewTab("Fourth")
+	b.AssertRender(t, "tabs/many")
+}
+
+func TestTabsIcons(t *testing.T) {
+	b := NewBody()
+	ts := NewTabs(b)
+	ts.NewTab("First", icons.Home)
+	ts.NewTab("Second", icons.Explore)
+	b.AssertRender(t, "tabs/icons")
+}
+
+func TestTabsFunctional(t *testing.T) {
+	b := NewBody()
+	ts := NewTabs(b).SetType(FunctionalTabs)
+	ts.NewTab("First")
+	ts.NewTab("Second")
+	ts.NewTab("Third")
+	b.AssertRender(t, "tabs/functional")
+}
+
+func TestTabsNavigation(t *testing.T) {
+	b := NewBody()
+	ts := NewTabs(b).SetType(NavigationAuto)
+	ts.NewTab("First", icons.Home)
+	ts.NewTab("Second", icons.Explore)
+	ts.NewTab("Third", icons.History)
+	b.AssertRender(t, "tabs/navigation")
+}
+
+func TestTabsNew(t *testing.T) {
+	b := NewBody()
+	ts := NewTabs(b).SetNewTabButton(true)
+	ts.NewTab("First")
+	ts.NewTab("Second")
+	b.AssertRender(t, "tabs/new")
 }
