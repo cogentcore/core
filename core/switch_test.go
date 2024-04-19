9059ee3eaf46fb4234a1fd02e0d2c98a06144818
Kai O'Reilly <koreilly5297@gmail.com>
2024-04-18 20:55:43 -0700

    add more switch tests

--- core/switch_test.go
+++ core/switch_test.go
@@ -4,10 +4,50 @@
 
 package core
 
-import "testing"
+import (
+	"testing"
+
+	"cogentcore.org/core/events"
+	"github.com/stretchr/testify/assert"
+)
 
 func TestSwitch(t *testing.T) {
 	b := NewBody()
 	NewSwitch(b)
 	b.AssertRender(t, "switch/basic")
+}
+
+func TestSwitchLabel(t *testing.T) {
+	b := NewBody()
+	NewSwitch(b).SetText("Remember me")
+	b.AssertRender(t, "switch/label")
+}
+
+func TestSwitchCheckbox(t *testing.T) {
+	b := NewBody()
+	NewSwitch(b).SetType(SwitchCheckbox).SetText("Remember me")
+	b.AssertRender(t, "switch/checkbox")
+}
+
+func TestSwitchRadioButton(t *testing.T) {
+	b := NewBody()
+	NewSwitch(b).SetType(SwitchRadioButton).SetText("Remember me")
+	b.AssertRender(t, "switch/radio-button")
+}
+
+func TestSwitchChange(t *testing.T) {
+	b := NewBody()
+	sw := NewSwitch(b)
+	checked := false
+	sw.OnChange(func(e events.Event) {
+		checked = sw.IsChecked()
+	})
+	b.AssertRender(t, "switch/change", func() {
+		sw.Send(events.Click)
+		assert.True(t, checked)
+		sw.Send(events.Click)
+		assert.False(t, checked)
+		sw.Send(events.Click)
+		assert.True(t, checked)
+	})
 }
