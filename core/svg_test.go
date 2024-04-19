cf33fe614b2ee49b51ebde76d07abb7731fdbbb3
Kai O'Reilly <koreilly5297@gmail.com>
2024-04-18 20:50:46 -0700

    improve svg tests

--- core/svg_test.go
+++ core/svg_test.go
@@ -5,20 +5,38 @@
 package core
 
 import (
+	"path/filepath"
 	"testing"
 
+	"cogentcore.org/core/errors"
 	"cogentcore.org/core/math32"
 	"cogentcore.org/core/styles"
 	"cogentcore.org/core/svg"
 	"cogentcore.org/core/units"
 )
 
+var testSVGPath = Filename(filepath.Join("..", "icon.svg"))
+
 func TestSVG(t *testing.T) {
 	b := NewBody()
-	sv := NewSVG(b)
-	sv.SVG.Root.ViewBox.Size.SetScalar(10)
-	svg.NewCircle(&sv.SVG.Root).SetPos(math32.Vec2(5, 5)).SetRadius(5)
-	b.AssertRender(t, "svg/basic-circle")
+	errors.Log(NewSVG(b).Open(testSVGPath))
+	b.AssertRender(t, "svg/basic")
+}
+
+func TestSVGSize(t *testing.T) {
+	b := NewBody()
+	svg := NewSVG(b)
+	errors.Log(svg.Open(testSVGPath))
+	svg.Style(func(s *styles.Style) {
+		s.Min.Set(units.Dp(128))
+	})
+	b.AssertRender(t, "svg/size")
+}
+
+func TestSVGString(t *testing.T) {
+	b := NewBody()
+	errors.Log(NewSVG(b).ReadString(`<rect width="100" height="100" fill="red"/>`))
+	b.AssertRender(t, "svg/string")
 }
 
 // For https://github.com/cogentcore/core/issues/729
