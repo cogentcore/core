8ac1adbcbd0e8cb4c228dcf36fa883ccf6cbbef9
Kai O'Reilly <koreilly5297@gmail.com>
2024-04-22 11:30:53 -0700

    start improving home header

--- docs/docs.go
+++ docs/docs.go
@@ -45,27 +45,7 @@ func main() {
 	pg.Context.WikilinkResolver = htmlview.PkgGoDevWikilink("cogentcore.org/core")
 	b.AddAppBar(pg.AppBar)
 
-	htmlview.ElementHandlers["home-header"] = func(ctx *htmlview.Context) bool {
-		ly := core.NewLayout(ctx.BlockParent).Style(func(s *styles.Style) {
-			s.Direction = styles.Column
-			s.Justify.Content = styles.Center
-			s.Align.Content = styles.Center
-			s.Align.Items = styles.Center
-			s.Text.Align = styles.Center
-		})
-		errors.Log(core.NewSVG(ly).ReadBytes(icon))
-		img := core.NewImage(ly)
-		errors.Log(img.OpenFS(name, "name.png"))
-		img.Style(func(s *styles.Style) {
-			x := func(uc *units.Context) float32 {
-				return min(uc.Dp(612), uc.Vw(90))
-			}
-			s.Min.Set(units.Custom(x), units.Custom(func(uc *units.Context) float32 {
-				return x(uc) * (128.0 / 612.0)
-			}))
-		})
-		return true
-	}
+	htmlview.ElementHandlers["home-header"] = homeHeader
 	htmlview.ElementHandlers["get-started"] = func(ctx *htmlview.Context) bool {
 		core.NewButton(ctx.BlockParent).SetText("Get Started").OnClick(func(e events.Event) {
 			pg.OpenURL("/getting-started", true)
@@ -76,4 +56,27 @@ func main() {
 	}
 
 	b.RunMainWindow()
+}
+
+func homeHeader(ctx *htmlview.Context) bool {
+	ly := core.NewLayout(ctx.BlockParent).Style(func(s *styles.Style) {
+		s.Direction = styles.Column
+		s.Justify.Content = styles.Center
+		s.Align.Content = styles.Center
+		s.Align.Items = styles.Center
+		s.Text.Align = styles.Center
+	})
+	errors.Log(core.NewSVG(ly).ReadBytes(icon))
+	img := core.NewImage(ly)
+	errors.Log(img.OpenFS(name, "name.png"))
+	img.Style(func(s *styles.Style) {
+		x := func(uc *units.Context) float32 {
+			return min(uc.Dp(612), uc.Vw(90))
+		}
+		s.Min.Set(units.Custom(x), units.Custom(func(uc *units.Context) float32 {
+			return x(uc) * (128.0 / 612.0)
+		}))
+	})
+	core.NewLabel(ly).SetType(core.LabelHeadlineMedium).SetText("A cross-platform framework for building powerful, fast, and cogent 2D and 3D apps")
+	return true
 }
