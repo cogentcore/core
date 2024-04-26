cef1b019cfa35be1122138f7193c7fd19826bd69
Randall C. O'Reilly <oreilly@ucdavis.edu>
2024-04-26 14:33:23 -0700

    fix clust test to use tolassert

--- tensor/stats/clust/clust_test.go
+++ tensor/stats/clust/clust_test.go
@@ -7,10 +7,10 @@ package clust
 import (
 	"testing"
 
+	"cogentcore.org/core/gox/tolassert"
 	"cogentcore.org/core/tensor/stats/metric"
 	"cogentcore.org/core/tensor/stats/simat"
 	"cogentcore.org/core/tensor/table"
-	"github.com/stretchr/testify/assert"
 )
 
 var clustres = `
@@ -40,7 +40,21 @@ func TestClust(t *testing.T) {
 	// fmt.Printf("%v\n", smat.Mat)
 	// cl := Glom(smat, MinDist)
 	cl := Glom(smat, AvgDist)
-	s := cl.Sprint(smat, 0)
+	// s := cl.Sprint(smat, 0)
 	// fmt.Println(s)
-	assert.Equal(t, clustres, s)
+
+	var dists []float64
+
+	var gather func(n *Node)
+	gather = func(n *Node) {
+		dists = append(dists, n.Dist)
+		for _, kn := range n.Kids {
+			gather(kn)
+		}
+	}
+	gather(cl)
+
+	exdists := []float64{0, 9.181170003996987, 5.534356399283667, 4.859933131085473, 3.4641016151377544, 0, 0, 3.4641016151377544, 0, 0, 3.4641016151377544, 0, 0, 5.111664626761644, 4.640135790634417, 4, 0, 0, 3.4641016151377544, 0, 0, 3.605551275463989, 0, 0}
+
+	tolassert.EqualTolSlice(t, exdists, dists, 1.0e-8)
 }
