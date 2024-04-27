9a66348ce979d2c62e320e119c711022c9a3a668
Kai O'Reilly <koreilly5297@gmail.com>
2024-04-26 18:33:21 -0700

    increase TestSliderInput tolerance even more

--- core/slider_test.go
+++ core/slider_test.go
@@ -152,7 +152,7 @@ func TestSliderInput(t *testing.T) {
 		}
 		sr.SystemEvents().MouseButton(events.MouseUp, events.Left, image.Pt(200, 20), 0)
 	}, func() {
-		tolassert.EqualTol(t, 4, float32(n), 2)
+		tolassert.EqualTol(t, 4, float32(n), 3)
 		tolassert.Equal(t, 0.5690789, value)
 	})
 }
