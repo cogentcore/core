3098f8baadc7c23a541a8170999d6d503613e74a
Kai O'Reilly <koreilly5297@gmail.com>
2024-05-01 07:23:18 -0700

    add more Vector2 tests

--- math32/vector2_test.go
+++ math32/vector2_test.go
@@ -17,4 +17,14 @@ func TestVector2(t *testing.T) {
 	assert.Equal(t, Vector2{20, 20}, Vector2Scalar(20))
 	assert.Equal(t, Vector2{15, -5}, Vector2FromPoint(image.Pt(15, -5)))
 	assert.Equal(t, Vector2{8, 3}, Vector2FromFixed(fixed.P(8, 3)))
+
+	v := Vector2{}
+	v.Set(-1, 7)
+	assert.Equal(t, Vector2{-1, 7}, v)
+
+	v.SetScalar(8.12)
+	assert.Equal(t, Vector2{8.12, 8.12}, v)
+
+	v.SetFromVector2i(Vector2i{8, 9})
+	assert.Equal(t, Vector2{8, 9}, v)
 }
