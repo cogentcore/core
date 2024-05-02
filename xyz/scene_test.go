3b40bdab070ca557620c826e23290934cec5b868
Kai O'Reilly <koreilly5297@gmail.com>
2024-05-02 13:08:27 -0700

    add basic scene test to xyz

---
+++ xyz/scene_test.go
@@ -0,0 +1,14 @@
+// Copyright (c) 2024, Cogent Core. All rights reserved.
+// Use of this source code is governed by a BSD-style
+// license that can be found in the LICENSE file.
+
+package xyz
+
+import (
+	"testing"
+)
+
+func TestScene(t *testing.T) {
+	sc := NewOffscreenScene()
+	sc.AssertImage(t, "scene")
+}
