1aba6040880f63815ce6e375cad0338783089faa
Kai O'Reilly <koreilly5297@gmail.com>
2024-05-03 06:59:00 -0700

    clean up vecint

--- math32/vecint/vector2i.go
+++ math32/vecint/vector2i.go
@@ -24,42 +24,34 @@ type Vector2i struct {
 	Y int
 }
 
-// NewVector2i returns a new Vector2i with the specified x and y components.
-func NewVector2i(x, y int) Vector2i {
+// Vec2i returns a new [Vector2i] with the given x and y components.
+func Vec2i(x, y int) Vector2i {
 	return Vector2i{X: x, Y: y}
 }
 
-// NewVector2iScalar returns a new Vector2i with all components set to scalar.
-func NewVector2iScalar(s int) Vector2i {
-	return Vector2i{X: s, Y: s}
+// Vector2iScalar returns a new Vector2i with all components set to scalar.
+func Vector2iScalar(scalar int) Vector2i {
+	return Vector2i{X: scalar, Y: scalar}
 }
 
-// NewVector2iFromVector2Round converts from floating point math32.Vector2 vector to int, using rounding
-func NewVector2iFromVector2Round(v math32.Vector2) Vector2i {
+// Vector2iFromVector2Round converts from floating point math32.Vector2 vector to int, using rounding
+func Vector2iFromVector2Round(v math32.Vector2) Vector2i {
 	return Vector2i{int(math32.Round(v.X)), int(math32.Round(v.Y))}
 }
 
-// NewVector2iFromVector2Floor converts from floating point math32.Vector2 vector to int, using floor
-func NewVector2iFromVector2Floor(v math32.Vector2) Vector2i {
+// Vector2iFromVector2Floor converts from floating point math32.Vector2 vector to int, using floor
+func Vector2iFromVector2Floor(v math32.Vector2) Vector2i {
 	return Vector2i{int(math32.Floor(v.X)), int(math32.Floor(v.Y))}
 }
 
-// NewVector2iFromVector2Ceil converts from floating point math32.Vector2 vector to int, using ceil
-func NewVector2iFromVector2Ceil(v math32.Vector2) Vector2i {
+// Vector2iFromVector2Ceil converts from floating point math32.Vector2 vector to int, using ceil
+func Vector2iFromVector2Ceil(v math32.Vector2) Vector2i {
 	return Vector2i{X: int(math32.Ceil(v.X)), Y: int(math32.Ceil(v.Y))}
 }
 
-// ToVector2 returns floating point math32.Vector2 from int
+// ToVector2 returns floating point [math32.Vector2] from int.
 func (v Vector2i) ToVector2() math32.Vector2 {
 	return math32.Vec2(float32(v.X), float32(v.Y))
-}
-
-// IsNil returns true if all values are 0 (uninitialized).
-func (v Vector2i) IsNil() bool {
-	if v.X == 0 && v.Y == 0 {
-		return true
-	}
-	return false
 }
 
 // Set sets this vector X and Y components.
@@ -69,9 +61,9 @@ func (v *Vector2i) Set(x, y int) {
 }
 
 // SetScalar sets all vector components to same scalar value.
-func (v *Vector2i) SetScalar(s int) {
-	v.X = s
-	v.Y = s
+func (v *Vector2i) SetScalar(scalar int) {
+	v.X = scalar
+	v.Y = scalar
 }
 
 // SetDim sets this vector component value by its dimension index
@@ -98,39 +90,26 @@ func (v Vector2i) Dim(dim math32.Dims) int {
 	}
 }
 
-// SetByName sets this vector component value by its case insensitive name: "x" or "y".
-func (v *Vector2i) SetByName(name string, value int) {
-	switch name {
-	case "x", "X":
-		v.X = value
-	case "y", "Y":
-		v.Y = value
-	default:
-		panic("Invalid Vector2i component name: " + name)
-	}
-}
-
-// SetZero sets this vector X and Y components to be zero.
+// SetZero sets all of the vector's components to zero.
 func (v *Vector2i) SetZero() {
 	v.SetScalar(0)
 }
 
-// FromArray sets this vector's components from the specified array and offset.
-func (v *Vector2i) FromArray(array []int, offset int) {
+// FromSlice sets this vector's components from the given slice, starting at offset.
+func (v *Vector2i) FromSlice(array []int, offset int) {
 	v.X = array[offset]
 	v.Y = array[offset+1]
 }
 
-// ToArray copies this vector's components to array starting at offset.
-func (v Vector2i) ToArray(array []int, offset int) {
+// ToSlice copies this vector's components to the given slice, starting at offset.
+func (v Vector2i) ToSlice(array []int, offset int) {
 	array[offset] = v.X
 	array[offset+1] = v.Y
 }
 
-///////////////////////////////////////////////////////////////////////
-//  Basic math operations
+// Basic math operations:
 
-// Add adds other vector to this one and returns result in a new vector.
+// Add adds the other given vector to this one and returns the result as a new vector.
 func (v Vector2i) Add(other Vector2i) Vector2i {
 	return Vector2i{v.X + other.X, v.Y + other.Y}
 }
@@ -220,55 +199,40 @@ func (v *Vector2i) SetDiv(other Vector2i) {
 }
 
 // SetDivScalar sets this to division by scalar.
-func (v *Vector2i) SetDivScalar(s int) {
-	if s != 0 {
-		v.SetMulScalar(1 / s)
+func (v *Vector2i) SetDivScalar(scalar int) {
+	if scalar != 0 {
+		v.X /= scalar
+		v.Y /= scalar
 	} else {
 		v.SetZero()
 	}
 }
 
-// Min32i returns the min of the two int numbers.
-func Min32i(a, b int) int {
-	if a < b {
-		return a
-	}
-	return b
-}
-
-// Max32i returns the max of the two int numbers.
-func Max32i(a, b int) int {
-	if a > b {
-		return a
-	}
-	return b
-}
-
 // Min returns min of this vector components vs. other vector.
 func (v Vector2i) Min(other Vector2i) Vector2i {
-	return Vector2i{Min32i(v.X, other.X), Min32i(v.Y, other.Y)}
+	return Vector2i{min(v.X, other.X), min(v.Y, other.Y)}
 }
 
 // SetMin sets this vector components to the minimum values of itself and other vector.
 func (v *Vector2i) SetMin(other Vector2i) {
-	v.X = Min32i(v.X, other.X)
-	v.Y = Min32i(v.Y, other.Y)
+	v.X = min(v.X, other.X)
+	v.Y = min(v.Y, other.Y)
 }
 
 // Max returns max of this vector components vs. other vector.
 func (v Vector2i) Max(other Vector2i) Vector2i {
-	return Vector2i{Max32i(v.X, other.X), Max32i(v.Y, other.Y)}
+	return Vector2i{max(v.X, other.X), max(v.Y, other.Y)}
 }
 
 // SetMax sets this vector components to the maximum value of itself and other vector.
 func (v *Vector2i) SetMax(other Vector2i) {
-	v.X = Max32i(v.X, other.X)
-	v.Y = Max32i(v.Y, other.Y)
+	v.X = max(v.X, other.X)
+	v.Y = max(v.Y, other.Y)
 }
 
-// Clamp sets this vector components to be no less than the corresponding components of min
-// and not greater than the corresponding component of max.
-// Assumes min < max, if this assumption isn't true it will not operate correctly.
+// Clamp sets this vector's components to be no less than the corresponding
+// components of min and not greater than the corresponding component of max.
+// Assumes min < max; if this assumption isn't true, it will not operate correctly.
 func (v *Vector2i) Clamp(min, max Vector2i) {
 	if v.X < min.X {
 		v.X = min.X
@@ -282,23 +246,7 @@ func (v *Vector2i) Clamp(min, max Vector2i) {
 	}
 }
 
-// ClampScalar sets this vector components to be no less than minVal and not greater than maxVal.
-func (v *Vector2i) ClampScalar(minVal, maxVal int) {
-	v.Clamp(NewVector2iScalar(minVal), NewVector2iScalar(maxVal))
-}
-
-// Negate returns vector with each component negated.
+// Negate returns the vector with each component negated.
 func (v Vector2i) Negate() Vector2i {
 	return Vector2i{-v.X, -v.Y}
-}
-
-// SetNegate negates each of this vector's components.
-func (v *Vector2i) SetNegate() {
-	v.X = -v.X
-	v.Y = -v.Y
-}
-
-// IsEqual returns if this vector is equal to other.
-func (v Vector2i) IsEqual(other Vector2i) bool {
-	return (other.X == v.X) && (other.Y == v.Y)
 }
