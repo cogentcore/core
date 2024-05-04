87d6623e2db22030b0ab6e3bcf8f8a077b67652c
Kai O'Reilly <koreilly5297@gmail.com>
2024-05-04 14:58:32 -0700

    add shell.Exec

--- shell/exec.go
+++ shell/exec.go
@@ -6,9 +6,13 @@ package shell
 
 import (
 	"fmt"
+	"log"
+	"os"
 	"strings"
 
+	"cogentcore.org/core/base/exec"
 	"cogentcore.org/core/base/reflectx"
+	"github.com/mattn/go-shellwords"
 )
 
 func (sh *Shell) ExecLine(ln string) error {
@@ -42,4 +46,34 @@ func (sh *Shell) ExecLine(ln string) error {
 
 	_, err := sh.Exec.Exec(cmd, args...)
 	return err
+}
+
+// Exec executes the given command string, parsing and separating any arguments.
+// If there is any error, it fatally logs it. It returns the stdout of the command,
+// in addition to forwarding output to [os.Stdout] and [os.Stderr] appropriately.
+func Exec(cmd string) string {
+	args, err := shellwords.Parse(cmd)
+	if err != nil {
+		log.Fatalln("shell.Exec: error parsing arguments:", err)
+	}
+	if len(args) == 0 {
+		log.Fatalln("shell.Exec: no arguments")
+	}
+
+	ecmd := args[0]
+	var eargs []string
+	if len(args) > 1 {
+		eargs = args[1:]
+	}
+	out, _ := ExecConfig.Output(ecmd, eargs...)
+	return out
+}
+
+// ExecConfig is the [exec.Config] used in [Exec].
+var ExecConfig = &exec.Config{
+	Fatal:  true,
+	Env:    map[string]string{},
+	Stdout: os.Stdout,
+	Stderr: os.Stderr,
+	Stdin:  os.Stdin,
 }
