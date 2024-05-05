0a03d0f48df5f5b508b877114539d3fb6324bd05
Randall C. O'Reilly <oreilly@ucdavis.edu>
2024-05-04 21:46:23 -0700

    updating

--- shell/interpreter/interpreter.go
+++ shell/interpreter/interpreter.go
@@ -5,22 +5,63 @@
 package interpreter
 
 import (
+	"reflect"
+	"strings"
+
 	"cogentcore.org/core/shell"
 	"github.com/traefik/yaegi/interp"
 	"github.com/traefik/yaegi/stdlib"
 )
 
-// Interp represents one running shell context
-type Interp struct {
+// Interpreter represents one running shell context
+type Interpreter struct {
 	Shell *shell.Shell
 
 	Interp *interp.Interpreter
 }
 
-func NewInterp() *Interp {
-	sh := &Interp{}
-	sh.Interp = interp.New(interp.Options{})
-	sh.Interp.Use(stdlib.Symbols) // this causes symbols to crash
-	sh.Shell = shell.NewShell()
-	return sh
+func NewInterpreter() *Interpreter {
+	in := &Interpreter{}
+	in.Interp = interp.New(interp.Options{})
+	in.Interp.Use(stdlib.Symbols) // this causes symbols to crash
+	in.Shell = shell.NewShell()
+	return in
+}
+
+// SymbolByName returns the reflect.Value for given symbol name
+// from the current Globals, Symbols (must call GetSymbols first)
+func (in *Shell) SymbolByName(name string) (bool, reflect.Value) {
+	globs := sh.Interp.Globals()
+	syms := sh.Interp.Symbols("main") // note: cannot use ""
+
+	nmorig := name
+	nmpath := ""
+	dotIdx := strings.Index(name, ".")
+	if dotIdx > 0 {
+		nmpath = name[:dotIdx]
+		name = name[dotIdx+1:]
+	}
+	for path, sy := range syms {
+		in.DebugPrintln(2, "	searching symbol path:", path)
+		if nmpath != "" && path != nmpath {
+			continue
+		}
+		for nm, v := range sy {
+			in.DebugPrintln(2, "	checking symbol name:", nm)
+			if nm == name {
+				in.DebugPrintln(1, "	got symbol:", path+"."+nm)
+				return true, v
+			}
+		}
+	}
+
+	for nm, v := range globs {
+		in.DebugPrintln(2, "	searching global name:", nm)
+		if nm == name {
+			in.DebugPrintln(1, "	got global:", nm)
+			return true, v
+		}
+	}
+	in.DebugPrintln(1, "	symbol not found:", nmorig)
+	return false, reflect.Value{}
 }
--- shell/shell.go
+++ shell/shell.go
@@ -4,15 +4,6 @@
 
 package shell
 
-import (
-	"reflect"
-	"strings"
-
-	"cogentcore.org/core/base/exec"
-	"github.com/traefik/yaegi/interp"
-	"github.com/traefik/yaegi/stdlib"
-)
-
 // Shell represents one running shell context
 type Shell struct {
 	// debug levels: 2 = full detail, 1 = summary, 0 = none
@@ -21,50 +12,5 @@ type Shell struct {
 
 func NewShell() *Shell {
 	sh := &Shell{}
-	sh.Interp = interp.New(interp.Options{})
-	sh.Interp.Use(stdlib.Symbols) // this causes symbols to crash
-	sh.Exec = exec.Major()
-	sh.Exec.Commands = nil
 	return sh
-}
-
-// GetSymbols gets the current symbols from the interpreter
-func (sh *Shell) GetSymbols() {
-	sh.Globals = sh.Interp.Globals()
-	sh.Symbols = sh.Interp.Symbols("main") // note: cannot use ""
-}
-
-// SymbolByName returns the reflect.Value for given symbol name
-// from the current Globals, Symbols (must call GetSymbols first)
-func (sh *Shell) SymbolByName(name string) (bool, reflect.Value) {
-	nmorig := name
-	nmpath := ""
-	dotIdx := strings.Index(name, ".")
-	if dotIdx > 0 {
-		nmpath = name[:dotIdx]
-		name = name[dotIdx+1:]
-	}
-	for path, sy := range sh.Symbols {
-		sh.DebugPrintln(2, "	searching symbol path:", path)
-		if nmpath != "" && path != nmpath {
-			continue
-		}
-		for nm, v := range sy {
-			sh.DebugPrintln(2, "	checking symbol name:", nm)
-			if nm == name {
-				sh.DebugPrintln(1, "	got symbol:", path+"."+nm)
-				return true, v
-			}
-		}
-	}
-
-	for nm, v := range sh.Globals {
-		sh.DebugPrintln(2, "	searching global name:", nm)
-		if nm == name {
-			sh.DebugPrintln(1, "	got global:", nm)
-			return true, v
-		}
-	}
-	sh.DebugPrintln(1, "	symbol not found:", nmorig)
-	return false, reflect.Value{}
 }
