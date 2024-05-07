55e63c37be48b7176c07b360802e76d0c3755999
Kai O'Reilly <koreilly5297@gmail.com>
2024-05-07 11:29:02 -0700

    do homedir expansion in shell.execArgs

--- shell/exec.go
+++ shell/exec.go
@@ -9,13 +9,14 @@ import (
 	"strings"
 
 	"cogentcore.org/core/base/reflectx"
+	"github.com/mitchellh/go-homedir"
 )
 
 // Exec executes the given command string, handling the given arguments appropriately.
 // If there is any error, it adds it to the shell. It forwards output to
 // [exec.Config.Stdout] and [exec.Config.Stderr] appropriately.
 func (sh *Shell) Exec(cmd any, args ...any) {
-	scmd, sargs := execArgs(cmd, args...)
+	scmd, sargs := sh.execArgs(cmd, args...)
 	if !sh.RunBuiltin(scmd, sargs...) {
 		sh.AddError(sh.Config.Run(scmd, sargs...))
 	}
@@ -25,7 +26,7 @@ func (sh *Shell) Exec(cmd any, args ...any) {
 // appropriately. If there is any error, it adds it to the shell. It returns
 // the stdout as a string and forwards stderr to [exec.Config.Stderr] appropriately.
 func (sh *Shell) Output(cmd any, args ...any) string {
-	scmd, sargs := execArgs(cmd, args...)
+	scmd, sargs := sh.execArgs(cmd, args...)
 	oldStdout := sh.Config.Stdout
 	buf := &bytes.Buffer{}
 	sh.Config.Stdout = buf
@@ -47,11 +48,14 @@ func (sh *Shell) RunBuiltin(cmd string, args ...string) bool {
 }
 
 // execArgs converts the given command and arguments into strings.
-func execArgs(cmd any, args ...any) (string, []string) {
+func (sh *Shell) execArgs(cmd any, args ...any) (string, []string) {
 	scmd := reflectx.ToString(cmd)
 	sargs := make([]string, len(args))
 	for i, a := range args {
-		sargs[i] = reflectx.ToString(a)
+		s := reflectx.ToString(a)
+		s, err := homedir.Expand(s)
+		sh.AddError(err)
+		sargs[i] = s
 	}
 	return scmd, sargs
 }
