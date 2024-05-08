2aa68521522806d0be73af6e9c1b69473ad4a8e0
Kai O'Reilly <koreilly5297@gmail.com>
2024-05-07 21:00:38 -0700

    add atomctr test

--- base/atomctr/atomctr.go
+++ base/atomctr/atomctr.go
@@ -2,7 +2,7 @@
 // Use of this source code is governed by a BSD-style
 // license that can be found in the LICENSE file.
 
-// Package atomctr implements basic atomic int64 counter
+// Package atomctr implements a basic atomic int64 counter.
 package atomctr
 
 import (
---
+++ base/atomctr/atomctr_test.go
@@ -0,0 +1,47 @@
+// Copyright (c) 2024, Cogent Core. All rights reserved.
+// Use of this source code is governed by a BSD-style
+// license that can be found in the LICENSE file.
+
+package atomctr
+
+import "testing"
+
+func TestCtr(t *testing.T) {
+	var a Ctr
+	if a.Value() != 0 {
+		t.Error("Ctr.Value() != 0")
+	}
+	if a.Inc() != 1 {
+		t.Error("Ctr.Inc() != 1")
+	}
+	if a.Value() != 1 {
+		t.Error("Ctr.Value() != 1")
+	}
+	if a.Add(5) != 6 {
+		t.Error("Ctr.Add(5) != 6")
+	}
+	if a.Value() != 6 {
+		t.Error("Ctr.Value() != 6")
+	}
+	if a.Sub(2) != 4 {
+		t.Error("Ctr.Sub(2) != 4")
+	}
+	if a.Value() != 4 {
+		t.Error("Ctr.Value() != 4")
+	}
+	if a.Dec() != 3 {
+		t.Error("Ctr.Dec() != 3")
+	}
+	if a.Value() != 3 {
+		t.Error("Ctr.Value() != 3")
+	}
+	if a.Swap(7) != 3 {
+		t.Error("Ctr.Swap(7) != 3")
+	}
+	if a.Value() != 7 {
+		t.Error("Ctr.Value() != 7")
+	}
+	if a.Set(9); a.Value() != 9 {
+		t.Error("Ctr.Set(9); Ctr.Value() != 9")
+	}
+}
