9c300b9916d7b3da5e4e00aa0d742847abbe8ee1
Kai O'Reilly <koreilly5297@gmail.com>
2024-05-09 16:18:32 -0700

    add basic pages test

---
+++ pages/page_test.go
@@ -0,0 +1,28 @@
+// Copyright (c) 2024, Cogent Core. All rights reserved.
+// Use of this source code is governed by a BSD-style
+// license that can be found in the LICENSE file.
+
+package pages
+
+import (
+	"embed"
+	"io/fs"
+	"testing"
+
+	"cogentcore.org/core/base/errors"
+	"cogentcore.org/core/core"
+	"cogentcore.org/core/styles"
+	"cogentcore.org/core/styles/units"
+)
+
+//go:embed examples/basic/content
+var content embed.FS
+
+func TestPage(t *testing.T) {
+	b := core.NewBody("Pages Example")
+	b.Style(func(s *styles.Style) {
+		s.Min.Set(units.Em(50))
+	})
+	NewPage(b).SetSource(errors.Log1(fs.Sub(content, "examples/basic/content")))
+	b.AssertRender(t, "basic")
+}
