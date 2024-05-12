4d14d04519cab9ae34274f216349d066cc54d28e
Randall C. O'Reilly <oreilly@ucdavis.edu>
2024-05-11 22:03:02 -0700

    config progress

--- core/config.go
+++ core/config.go
@@ -4,12 +4,15 @@
 
 package core
 
-import "cogentcore.org/core/base/ordmap"
-
 // ConfigItem represents a Widget configuration element,
 // with one New function to create and configure a new child Widget,
 // and an Update function to update the state of that child Widget.
+// Contains a list of Config for its children, which is all sorted
+// out as items are added.
 type ConfigItem struct {
+	// Path is the / delimited path to the element
+	Path string
+
 	// New returns a new Widget of the correct type for this element,
 	// fully configured and ready for use.
 	New func() Widget
@@ -17,17 +20,29 @@ type ConfigItem struct {
 	// Update updates the widget based on current state, so that it
 	// propertly renders the correct information.
 	Update func(w Widget)
+
+	//	Config for Children elements.
+	Children Config
 }
 
 // Config is an ordered list of [ConfigItem]s,
 // ordered in progressive hierarchical order
 // so that parents are listed before any children.
 // The Key of the map is the path of the element.
-type Config ordmap.Map[string, ConfigItem]
+type Config []ConfigItem
 
-// Add
+// Add adds given config.  This should be called on the root level Config
+// list -- any items with nested paths are added to Children lists as
+// appropriate.
 func (c *Config) Add(path string, nw func() Widget, updt func(w Widget)) {
-
+	itm := ConfigItem{Path: path, New: nw, Update: updt}
+	plist := strings.Split(path, "/")
+	if len(plist) == 1 {
+		*c = append(*c, itm)
+		return
+	}
+	next := c.FindMakeChild(plist[0])
+	next.AddSubItem(plist[1:], itm]
 }
 
 // ConfigWidget is the base implementation of [Widget.ConfigWidget] that
@@ -39,3 +54,4 @@ func (wb *WidgetBase) ConfigWidget() {
 	}
 	wb.This().(Widget).Config()
 }
+
