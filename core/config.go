7fa28ea87c3fff018ae13b4f2274336337448f09
Randall C. O'Reilly <oreilly@ucdavis.edu>
2024-05-11 22:08:09 -0700

    building

--- core/config.go
+++ core/config.go
@@ -4,6 +4,8 @@
 
 package core
 
+import "strings"
+
 // ConfigItem represents a Widget configuration element,
 // with one New function to create and configure a new child Widget,
 // and an Update function to update the state of that child Widget.
@@ -21,7 +23,7 @@ type ConfigItem struct {
 	// propertly renders the correct information.
 	Update func(w Widget)
 
-	//	Config for Children elements.
+	// Config for Children elements.
 	Children Config
 }
 
@@ -29,20 +31,28 @@ type ConfigItem struct {
 // ordered in progressive hierarchical order
 // so that parents are listed before any children.
 // The Key of the map is the path of the element.
-type Config []ConfigItem
+type Config []*ConfigItem
 
 // Add adds given config.  This should be called on the root level Config
-// list -- any items with nested paths are added to Children lists as
+// list.  Any items with nested paths are added to Children lists as
 // appropriate.
 func (c *Config) Add(path string, nw func() Widget, updt func(w Widget)) {
-	itm := ConfigItem{Path: path, New: nw, Update: updt}
+	itm := &ConfigItem{Path: path, New: nw, Update: updt}
 	plist := strings.Split(path, "/")
 	if len(plist) == 1 {
 		*c = append(*c, itm)
 		return
 	}
 	next := c.FindMakeChild(plist[0])
-	next.AddSubItem(plist[1:], itm]
+	next.AddSubItem(plist[1:], itm)
+}
+
+// AddSubItem adds given sub item
+func (c *ConfigItem) AddSubItem(path []string, itm *ConfigItem) {
+}
+
+func (c *Config) FindMakeChild(name string) *ConfigItem {
+	return nil
 }
 
 // ConfigWidget is the base implementation of [Widget.ConfigWidget] that
@@ -54,4 +64,3 @@ func (wb *WidgetBase) ConfigWidget() {
 	}
 	wb.This().(Widget).Config()
 }
-
