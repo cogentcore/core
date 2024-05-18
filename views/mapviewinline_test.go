7681c875dfb87def299021d9e9bab27196d47f44
Kai O'Reilly <koreilly5297@gmail.com>
2024-05-18 12:30:32 -0700

    add map view inline change and read only tests

--- views/mapviewinline_test.go
+++ views/mapviewinline_test.go
@@ -5,13 +5,62 @@
 package views
 
 import (
+	"maps"
 	"testing"
 
 	"cogentcore.org/core/core"
+	"cogentcore.org/core/events"
+	"github.com/stretchr/testify/assert"
 )
 
 func TestMapViewInline(t *testing.T) {
 	b := core.NewBody()
 	NewMapViewInline(b).SetMap(&map[string]int{"Go": 1, "C++": 3})
 	b.AssertRender(t, "map-view-inline/basic")
+}
+
+func TestMapViewInlineReadOnly(t *testing.T) {
+	b := core.NewBody()
+	NewMapViewInline(b).SetMap(&map[string]int{"Go": 1, "C++": 3}).SetReadOnly(true)
+	b.AssertRender(t, "map-view-inline/read-only")
+}
+
+func TestMapViewInlineChange(t *testing.T) {
+	b := core.NewBody()
+	m := map[string]int{"Go": 1, "C++": 3}
+
+	n := 0
+	value := map[string]int{}
+	mv := NewMapViewInline(b).SetMap(&m)
+	mv.OnChange(func(e events.Event) {
+		n++
+		maps.Copy(value, m)
+	})
+	b.AssertRender(t, "map-view-inline/change", func() {
+		// [3] is value of second row, which is "Go" since it is sorted alphabetically
+		mv.Child(3).(*core.Spinner).TrailingIconButton().Send(events.Click)
+		assert.Equal(t, 1, n)
+		assert.Equal(t, m, value)
+		assert.Equal(t, map[string]int{"Go": 2, "C++": 3}, m)
+	})
+}
+
+func TestMapViewInlineChangeKey(t *testing.T) {
+	b := core.NewBody()
+	m := map[string]int{"Go": 1, "C++": 3}
+
+	n := 0
+	value := map[string]int{}
+	mv := NewMapViewInline(b).SetMap(&m)
+	mv.OnChange(func(e events.Event) {
+		n++
+		maps.Copy(value, m)
+	})
+	b.AssertRender(t, "map-view-inline/change-key", func() {
+		// [0] is key of first row, which is "C++" since it is sorted alphabetically
+		mv.Child(0).(*core.TextField).SetText("JavaScript").SendChange()
+		assert.Equal(t, 1, n)
+		assert.Equal(t, m, value)
+		assert.Equal(t, map[string]int{"Go": 1, "JavaScript": 3}, m)
+	})
 }
