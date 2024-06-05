db3225837ed4630643c140616ad504b05ac6d002
Kai O'Reilly <koreilly5297@gmail.com>
2024-06-04 18:32:15 -0700

    update slboolview to new value structure

--- vgpu/gosl/slboolview/slboolview.go
+++ vgpu/gosl/slboolview/slboolview.go
@@ -5,39 +5,10 @@
 package slboolview
 
 import (
-	"cogentcore.org/core/base/reflectx"
 	"cogentcore.org/core/core"
-	"cogentcore.org/core/events"
 	"cogentcore.org/core/vgpu/gosl/slbool"
-	"cogentcore.org/core/views"
 )
 
 func init() {
-	views.AddValue(slbool.Bool(0), func() views.Value {
-		return &BoolValue{}
-	})
-}
-
-// BoolValue presents a checkbox for a boolean
-type BoolValue struct {
-	views.ValueBase[*core.Switch]
-}
-
-func (v *BoolValue) Config() {
-	v.Widget.OnFinal(events.Change, func(e events.Event) {
-		v.SetValue(v.Widget.IsChecked())
-	})
-}
-
-func (v *BoolValue) Update() {
-	npv := reflectx.NonPointerValue(v.Value)
-	sb, ok := npv.Interface().(slbool.Bool)
-	if ok {
-		v.Widget.SetChecked(sb.IsTrue())
-	} else {
-		sb, ok := npv.Interface().(*slbool.Bool)
-		if ok {
-			v.Widget.SetChecked(sb.IsTrue())
-		}
-	}
+	core.AddValueType[slbool.Bool, *core.Switch]()
 }
