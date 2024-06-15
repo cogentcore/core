5a11ebef6fae4433921559d2cdc47189b2cc5877
Kai O'Reilly <koreilly5297@gmail.com>
2024-06-15 16:50:34 -0700

    start on file picker docs

---
+++ docs/content/2-widgets/4-pickers/file-pickers.md
@@ -0,0 +1,15 @@
+# File pickers
+
+Cogent Core provides powerful file pickers for selecting files that support sorting, navigation, searching, favorites, and history.
+
+You can make a file picker:
+
+```Go
+core.NewFilePicker(parent)
+```
+
+You can make a button that opens a file picker:
+
+```Go
+core.NewFileButton(parent)
+```
--- docs/pagegen.go
+++ docs/pagegen.go
@@ -949,6 +949,12 @@ func main() {
 			core.MessageSnackbar(parent, colors.AsHex(cb.Color))
 		})
 	},
+	"widgets/pickers/file-pickers-0": func(parent core.Widget) {
+		core.NewFilePicker(parent)
+	},
+	"widgets/pickers/file-pickers-1": func(parent core.Widget) {
+		core.NewFileButton(parent)
+	},
 	"widgets/media/canvases-0": func(parent core.Widget) {
 		core.NewCanvas(parent).SetDraw(func(pc *paint.Context) {
 			pc.FillBox(math32.Vector2{}, math32.Vec2(1, 1), colors.C(colors.Scheme.Primary.Base))
