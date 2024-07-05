a1935932d14fb89d97333696a1f9c1d065a465b8
Kai O'Reilly <koreilly5297@gmail.com>
2024-07-05 11:52:36 -0700

    move icons/gen.go logic to icongen

--- cmd/core/generate/icons.go
+++ cmd/core/generate/icons.go
@@ -4,12 +4,64 @@
 
 package generate
 
-import "cogentcore.org/core/cmd/core/config"
+import (
+	"bytes"
+	"io/fs"
+	"os"
+	"strings"
+	"text/template"
+	"unicode"
+
+	"cogentcore.org/core/base/generate"
+	"cogentcore.org/core/base/strcase"
+	"cogentcore.org/core/cmd/core/config"
+)
+
+// iconData contains the data for an icon
+type iconData struct {
+	Dir   string // Dir is the directory in which the icon is contained
+	Snake string // Snake is the snake_case name of the icon
+	Camel string // Camel is the CamelCase name of the icon
+}
+
+var iconTmpl = template.Must(template.New("icon").Parse(
+	`
+	//go:embed {{.Dir}}/{{.Snake}}.svg
+	{{.Camel}} Icon`,
+))
 
 // Icons does any necessary generation for icons.
 func Icons(c *config.Config) error {
 	if c.Generate.Icons == "" {
 		return nil
 	}
-	return nil
+	b := &bytes.Buffer{}
+	generate.PrintHeader(b, "icons")
+	b.WriteString(`import _ "embed"
+
+var (`)
+
+	fs.WalkDir(os.DirFS(c.Generate.Icons), ".", func(path string, d fs.DirEntry, err error) error {
+		if d.IsDir() {
+			return nil
+		}
+		name := strings.TrimSuffix(path, ".svg")
+		// ignore blank icon, as we define the constant for that separately
+		if name == "blank" {
+			return nil
+		}
+		camel := strcase.ToCamel(name)
+		// identifier names can't start with a digit
+		if unicode.IsDigit([]rune(camel)[0]) {
+			camel = "X" + camel
+		}
+		data := iconData{
+			Dir:   c.Generate.Icons,
+			Snake: name,
+			Camel: camel,
+		}
+		return iconTmpl.Execute(b, data)
+	})
+	b.WriteString("\n)\n")
+	return generate.Write("icongen.go", b.Bytes(), nil)
 }
