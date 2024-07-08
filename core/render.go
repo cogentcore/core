7e36782ff6c522563fff7cb4d2c8b46f77d74cdc
Randall C. O'Reilly <oreilly@ucdavis.edu>
2024-07-07 22:04:46 -0700

    disable first render logic until we can figure out how to fix tree nodes

--- core/render.go
+++ core/render.go
@@ -343,7 +343,7 @@ func (wb *WidgetBase) PushBounds() bool {
 	if pc.State == nil || pc.Image == nil {
 		return false
 	}
-	if len(pc.BoundsStack) == 0 && wb.Parent != nil {
+	if false && len(pc.BoundsStack) == 0 && wb.Parent != nil {
 		wb.firstRender = true
 		// push our parent's bounds if we are the first to render
 		pw := wb.ParentWidget()
