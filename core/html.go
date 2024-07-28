0b60d760a43b9474678d1cf691e74ad3243a2288
Kai O'Reilly <koreilly5297@gmail.com>
2024-07-28 14:44:41 -0700

    start on core.ToHTML

---
+++ core/html.go
@@ -0,0 +1,13 @@
+// Copyright (c) 2024, Cogent Core. All rights reserved.
+// Use of this source code is governed by a BSD-style
+// license that can be found in the LICENSE file.
+
+package core
+
+// ToHTML converts the given widget and all of its children to an HTML string.
+// This is not guaranteed to be perfect HTML, and it should not be used as a
+// replacement for a Cogent Core app. However, it is good enough to be used as
+// a preview or for SEO purposes.
+func ToHTML(w Widget) string {
+	return ""
+}
