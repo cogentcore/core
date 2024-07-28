8e84597aa3bce10d443c19073beca9e9c97f29e7
Kai O'Reilly <koreilly5297@gmail.com>
2024-07-28 16:41:02 -0700

    exclude more default values from ToCSS

--- styles/css.go
+++ styles/css.go
@@ -15,17 +15,30 @@ import (
 func ToCSS(s *Style) string {
 	parts := []string{}
 	add := func(key, value string) {
+		// exclude default values
+		switch value {
+		case "#00000000", "0px", "none", "solid":
+			return
+		case "16px":
+			if key == "font-size" {
+				return
+			}
+		}
 		parts = append(parts, key+":"+value)
 	}
 
 	add("color", colors.AsHex(colors.ToUniform(s.Color)))
 	add("background", colors.AsHex(colors.ToUniform(s.Background)))
 	add("font-size", s.Font.Size.StringCSS())
-	add("border-style", s.Border.Style.Top.String())
-	add("border-width", s.Border.Width.Top.StringCSS())
-	add("border-radius", s.Border.Radius.Top.StringCSS())
-	add("border-offset", s.Border.Offset.Top.StringCSS())
-	add("border-color", colors.AsHex(colors.ToUniform(s.Border.Color.Top)))
+	if s.Font.Family != "Roboto" {
+		add("font-family", s.Font.Family)
+	}
+	if s.Border.Width.Top.Value > 0 {
+		add("border-style", s.Border.Style.Top.String())
+		add("border-width", s.Border.Width.Top.StringCSS())
+		add("border-radius", s.Border.Radius.Top.StringCSS())
+		add("border-color", colors.AsHex(colors.ToUniform(s.Border.Color.Top)))
+	}
 
 	return strings.Join(parts, ";")
 }
