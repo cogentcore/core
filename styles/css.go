05ceb63fe07791a905803a953f69b2d483f7e41e
Kai O'Reilly <koreilly5297@gmail.com>
2024-07-28 21:45:22 -0700

    add more styling support

--- styles/css.go
+++ styles/css.go
@@ -30,12 +30,22 @@ func ToCSS(s *Style) string {
 	}
 	add("width", s.Min.X.StringCSS())
 	add("height", s.Min.Y.StringCSS())
+	add("max-width", s.Max.X.StringCSS())
+	add("max-height", s.Max.Y.StringCSS())
+	add("padding", s.Padding.Top.StringCSS())
+	add("margin", s.Margin.Top.StringCSS())
 	if s.Font.Size.Value != 16 || s.Font.Size.Unit != units.UnitDp {
 		add("font-size", s.Font.Size.StringCSS())
 	}
 	if s.Font.Family != "" && s.Font.Family != "Roboto" {
 		add("font-family", s.Font.Family)
 	}
+	if s.Font.Weight == WeightMedium {
+		add("font-weight", "500")
+	} else {
+		add("font-weight", s.Font.Weight.String())
+	}
+	add("line-height", s.Text.LineHeight.StringCSS())
 	if s.Border.Width.Top.Value > 0 {
 		add("border-style", s.Border.Style.Top.String())
 		add("border-width", s.Border.Width.Top.StringCSS())
