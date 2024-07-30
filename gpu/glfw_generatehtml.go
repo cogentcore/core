df399af0e78dd999ca21fbe2eeccec228f31caff
Kai O'Reilly <koreilly5297@gmail.com>
2024-07-29 17:56:59 -0700

    add clean quit fallback in gpu glfw for generatehtml

--- gpu/glfw_generatehtml.go
+++ gpu/glfw_generatehtml.go
@@ -2,19 +2,19 @@
 // Use of this source code is governed by a BSD-style
 // license that can be found in the LICENSE file.
 
-//go:build (offscreen || ios || android || js) && !generatehtml
+//go:build generatehtml
 
 package gpu
 
 import (
+	"os"
+
 	"github.com/rajveermalviya/go-webgpu/wgpu"
 )
 
 // GLFWCreateWindow is a helper function intended only for use in simple examples that makes a
 // new window with glfw on platforms that support it and is largely a no-op on other platforms.
 func GLFWCreateWindow(gp *GPU, width, height int, title string) (surface *wgpu.Surface, terminate func(), pollEvents func() bool, err error) {
-	surface = gp.Instance.CreateSurface(&wgpu.SurfaceDescriptor{})
-	terminate = func() {}
-	pollEvents = func() bool { return true }
+	os.Exit(0) // TODO: needs clean quit for generatehtml; is there any better solution?
 	return
 }
