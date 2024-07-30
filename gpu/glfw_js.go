438b6753d87d07c4b40854551a3736cf77677244
Kai O'Reilly <koreilly5297@gmail.com>
2024-07-29 18:11:19 -0700

    config jsfs in gpu glfw

--- gpu/glfw_js.go
+++ gpu/glfw_js.go
@@ -2,17 +2,22 @@
 // Use of this source code is governed by a BSD-style
 // license that can be found in the LICENSE file.
 
-//go:build (offscreen || ios || android || js) && !generatehtml
+//go:build js
 
 package gpu
 
 import (
+	"syscall/js"
+
+	"cogentcore.org/core/base/errors"
+	"cogentcore.org/core/system/driver/web/jsfs"
 	"github.com/rajveermalviya/go-webgpu/wgpu"
 )
 
 // GLFWCreateWindow is a helper function intended only for use in simple examples that makes a
 // new window with glfw on platforms that support it and is largely a no-op on other platforms.
 func GLFWCreateWindow(gp *GPU, width, height int, title string) (surface *wgpu.Surface, terminate func(), pollEvents func() bool, err error) {
+	errors.Log1(jsfs.Config(js.Global().Get("fs"))) // needed for printing etc to work
 	surface = gp.Instance.CreateSurface(&wgpu.SurfaceDescriptor{})
 	terminate = func() {}
 	pollEvents = func() bool { return true }
