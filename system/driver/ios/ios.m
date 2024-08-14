0cae8ba9b6c4521e4fee4b0b5255690e7c29af97
Kai O'Reilly <koreilly5297@gmail.com>
2024-08-13 17:40:31 -0700

    convert CALayer to void * in ios; still not working

--- system/driver/ios/ios.m
+++ system/driver/ios/ios.m
@@ -127,7 +127,7 @@ struct utsname sysInfo;
 	UIInterfaceOrientation orientation = [[UIApplication sharedApplication] statusBarOrientation];
 	updateConfig((int)size.width, (int)size.height, orientation);
 
-    setWindowPtr([self.view layerClass]);
+    setWindowPtr((void *)[self.view layerClass]);
 	
     CADisplayLink* displayLink = [CADisplayLink displayLinkWithTarget:self selector:@selector(render:)];
     [displayLink addToRunLoop:[NSRunLoop currentRunLoop] forMode:NSDefaultRunLoopMode];
