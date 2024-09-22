4d039c1e6e37c037b59130233abc2b3973152223
Randall C. O'Reilly <oreilly@ucdavis.edu>
2024-09-22 16:24:43 -0700

    testing, etc

--- tensor/funcs_test.go
+++ tensor/funcs_test.go
@@ -8,11 +8,14 @@ import (
 	"math"
 	"testing"
 
+	"cogentcore.org/core/base/errors"
 	"github.com/stretchr/testify/assert"
 )
 
 func abs(in, out Tensor) {
-	SetShapeFrom(out, in)
+	if err := SetShapeFromMustBeValues(out, in); errors.Log(err) != nil {
+		return
+	}
 	VectorizeThreaded(1, NFirstLen, func(idx int, tsr ...Tensor) {
 		tsr[1].SetFloat1D(math.Abs(tsr[0].Float1D(idx)), idx)
 	}, in, out)
@@ -44,4 +47,29 @@ func TestFuncs(t *testing.T) {
 	out := CallOut("Abs", oned)
 	// assert.NoError(t, err)
 	assert.Equal(t, AsFloat64Scalar(oneout), AsFloat64Scalar(out))
+}
+
+func TestAlign(t *testing.T) {
+	a := NewFloat64(3, 4)
+	b := NewFloat64(1, 3, 4)
+	as, bs, os, err := AlignShapes(a, b)
+	assert.NoError(t, err)
+	assert.Equal(t, []int{1, 3, 4}, os.Sizes)
+	assert.Equal(t, []int{1, 3, 4}, as.Sizes)
+	assert.Equal(t, []int{1, 3, 4}, bs.Sizes)
+
+	ars := NewReshaped(a, 12)
+	as, bs, os, err = AlignShapes(ars, b)
+	assert.Error(t, err)
+
+	brs := NewReshaped(b, 12)
+	as, bs, os, err = AlignShapes(ars, brs)
+	assert.NoError(t, err)
+
+	ars = NewReshaped(a, 3, 1, 4)
+	as, bs, os, err = AlignShapes(ars, b)
+	assert.NoError(t, err)
+	assert.Equal(t, []int{3, 3, 4}, os.Sizes)
+	assert.Equal(t, []int{3, 1, 4}, as.Sizes)
+	assert.Equal(t, []int{1, 3, 4}, bs.Sizes)
 }
