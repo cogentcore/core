cea91d80f0cd16d3aef6cb75e7ff79493e8d9b34
Kai O'Reilly <koreilly5297@gmail.com>
2024-11-14 21:14:29 -0800

    add Cogent Content readme

--- content/content.go
+++ content/content.go
@@ -2,6 +2,6 @@
 // Use of this source code is governed by a BSD-style
 // license that can be found in the LICENSE file.
 
-// Package content provides a framework for making content-focused
+// Package content provides a system for making content-focused
 // apps and websites consisting of Markdown, HTML, and Cogent Core.
 package content
