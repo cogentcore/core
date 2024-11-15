393852360c55e57d267132d3b5ba09ef410d2864
Kai O'Reilly <koreilly5297@gmail.com>
2024-11-14 21:13:35 -0800

    start on content

---
+++ content/content.go
@@ -0,0 +1,7 @@
+// Copyright (c) 2023, Cogent Core. All rights reserved.
+// Use of this source code is governed by a BSD-style
+// license that can be found in the LICENSE file.
+
+// Package content provides a framework for making content-focused
+// apps and websites consisting of Markdown, HTML, and Cogent Core.
+package content
