576a595d7ca11ca72e39f62c9e2d272302064f4f
Kai O'Reilly <koreilly5297@gmail.com>
2024-11-14 23:16:25 -0800

    start on content.Content

--- content/content.go
+++ content/content.go
@@ -5,3 +5,24 @@
 // Package content provides a system for making content-focused
 // apps and websites consisting of Markdown, HTML, and Cogent Core.
 package content
+
+import (
+	"io/fs"
+
+	"cogentcore.org/core/core"
+)
+
+// Content manages and displays the content of a set of pages.
+type Content struct {
+	core.Frame
+
+	// Source is the source filesystem for the content.
+	Source fs.FS
+
+	// Pages are the pages that constitute the content.
+	Pages []*Page `set:"-"`
+}
+
+func (ct *Content) Init() {
+	ct.Frame.Init()
+}
