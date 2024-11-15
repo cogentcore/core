14749f433a13ca98761083445ab22aab227dda21
Kai O'Reilly <koreilly5297@gmail.com>
2024-11-15 10:38:19 -0800

    start on content.Content.SetSource and SetContent

--- content/content.go
+++ content/content.go
@@ -7,8 +7,11 @@
 package content
 
 import (
+	"fmt"
 	"io/fs"
 
+	"cogentcore.org/core/base/errors"
+	"cogentcore.org/core/base/fsx"
 	"cogentcore.org/core/core"
 )
 
@@ -17,12 +20,41 @@ type Content struct {
 	core.Frame
 
 	// Source is the source filesystem for the content.
-	Source fs.FS
+	// It should be set using [Content.SetSource] or [Content.SetContent].
+	Source fs.FS `set:"-"`
 
-	// Pages are the pages that constitute the content.
-	Pages []*Page `set:"-"`
+	// pages are the pages that constitute the content.
+	pages []*Page `set:"-"`
 }
 
 func (ct *Content) Init() {
 	ct.Frame.Init()
+}
+
+// SetSource sets the source filesystem for the content.
+func (ct *Content) SetSource(source fs.FS) *Content {
+	ct.Source = source
+	ct.pages = []*Page{}
+	errors.Log(fs.WalkDir(ct.Source, ".", func(path string, d fs.DirEntry, err error) error {
+		if err != nil {
+			return err
+		}
+		if path == "" || path == "." {
+			return nil
+		}
+		pg, err := NewPage(ct.Source, path)
+		if err != nil {
+			return err
+		}
+		ct.pages = append(ct.pages, pg)
+		return nil
+	}))
+	fmt.Printf("%#v\n", ct.pages)
+	return ct
+}
+
+// SetContent is a helper function that calls [Content.SetSource]
+// with the "content" subdirectory of the given filesystem.
+func (ct *Content) SetContent(content fs.FS) *Content {
+	return ct.SetSource(fsx.Sub(content, "content"))
 }
