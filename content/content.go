093351113babb4070e452cbdc6b2a2b7c4babab5
Kai O'Reilly <koreilly5297@gmail.com>
2024-11-15 10:49:22 -0800

    start on current page rendering logic in content

--- content/content.go
+++ content/content.go
@@ -14,6 +14,7 @@ import (
 	"cogentcore.org/core/base/errors"
 	"cogentcore.org/core/base/fsx"
 	"cogentcore.org/core/core"
+	"cogentcore.org/core/tree"
 )
 
 // Content manages and displays the content of a set of pages.
@@ -25,11 +26,27 @@ type Content struct {
 	Source fs.FS `set:"-"`
 
 	// pages are the pages that constitute the content.
-	pages []*Page `set:"-"`
+	pages []*Page
+
+	// currentPage is the currently open page.
+	currentPage *Page
 }
 
 func (ct *Content) Init() {
 	ct.Frame.Init()
+	ct.Maker(func(p *tree.Plan) {
+		if ct.currentPage == nil {
+			return
+		}
+		if ct.currentPage.Name != "" {
+			tree.Add(p, func(w *core.Text) {
+				w.SetType(core.TextHeadlineSmall)
+				w.Updater(func() {
+					w.SetText(ct.currentPage.Name)
+				})
+			})
+		}
+	})
 }
 
 // SetSource sets the source filesystem for the content.
@@ -47,6 +64,7 @@ func (ct *Content) SetSource(source fs.FS) *Content {
 		if err != nil {
 			return err
 		}
+		ct.currentPage = pg
 		ct.pages = append(ct.pages, pg)
 		return nil
 	}))
