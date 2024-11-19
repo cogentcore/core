3310c14ff9c261d9a50a1a860f66bfdc5dd2e5e6
Kai O'Reilly <koreilly5297@gmail.com>
2024-11-19 15:37:39 -0800

    start on web URL logic in content

---
+++ content/url.go
@@ -0,0 +1,8 @@
+// Copyright (c) 2024, Cogent Core. All rights reserved.
+// Use of this source code is governed by a BSD-style
+// license that can be found in the LICENSE file.
+
+package content
+
+// saveWebURL, if non-nil, saves the current page URL to the user's address bar and history.
+var saveWebURL func(ct *Content)
---
+++ content/url_js.go
@@ -0,0 +1,13 @@
+// Copyright (c) 2024, Cogent Core. All rights reserved.
+// Use of this source code is governed by a BSD-style
+// license that can be found in the LICENSE file.
+
+//go:build js
+
+package content
+
+func init() {
+	saveWebURL = func(ct *Content) {
+
+	}
+}
