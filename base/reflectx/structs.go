247cc8b553060f6d0cd33cd931d5d6947a669a1b
Randall C. O'Reilly <oreilly@ucdavis.edu>
2024-12-01 02:16:18 -0800

    move SetFields to structs -- just one func.

--- base/reflectx/structs.go
+++ base/reflectx/structs.go
@@ -6,6 +6,7 @@ package reflectx
 
 import (
 	"fmt"
+	"log"
 	"log/slog"
 	"reflect"
 	"strconv"
@@ -305,4 +306,34 @@ func FieldAtPath(val reflect.Value, path string) (reflect.Value, error) {
 		return fld.Addr(), nil
 	}
 	return FieldAtPath(fld.Addr(), strings.Join(paths[1:], "."))
+}
+
+// SetFieldsFromMap sets given map[string]any values to fields of given object,
+// where the map keys are field paths (with . delimiters for sub-field paths).
+// The value can be any appropriate type that applies to the given field.
+// It prints a message if a parameter fails to be set, and returns an error.
+func SetFieldsFromMap(obj any, vals map[string]any) error {
+	objv := reflect.ValueOf(obj)
+	npv := NonPointerValue(objv)
+	if npv.Kind() == reflect.Map {
+		err := CopyMapRobust(obj, vals)
+		if err != nil {
+			log.Println(err)
+			return err
+		}
+	}
+	var errs []error
+	for k, v := range vals {
+		fld, err := FieldAtPath(objv, k)
+		if err != nil {
+			errs = append(errs, err)
+		}
+		err = SetRobust(fld.Interface(), v)
+		if err != nil {
+			err = fmt.Errorf("SetFieldsFromMap: was not able to apply value: %v to field: %s", v, k)
+			log.Println(err)
+			errs = append(errs, err)
+		}
+	}
+	return errors.Join(errs...)
 }
