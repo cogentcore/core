a0a2dd46e24755313e2c51a46efa51084108857b
Kai O'Reilly <koreilly5297@gmail.com>
2024-12-08 11:35:39 -0800

    add Scene.IsFullscreen

--- core/scene.go
+++ core/scene.go
@@ -318,10 +318,23 @@ func (sc *Scene) ResizeToContent(extra ...image.Point) {
 	}()
 }
 
-// UpdateFullscreen requests that the window associated with this Scene
+// IsFullscreen returns whether the window associated with this [Scene]
+// is in fullscreen mode (true) or window mode (false). This is implemented
+// on desktop and web platforms. See [Scene.UpdateFullscreen] to update the
+// current fullscreen state and [Stage.Fullscreen] to set the initial state.
+func (sc *Scene) IsFullscreen() bool {
+	rw := sc.RenderWindow()
+	if rw == nil {
+		return false
+	}
+	return rw.SystemWindow.Is(system.Fullscreen)
+}
+
+// UpdateFullscreen requests that the window associated with this [Scene]
 // be updated to either fullscreen mode (true) or window mode (false).
-// This is implemented on desktop and web platforms. See [Stage.Fullscreen]
-// to set the initial fullscreen state.
+// This is implemented on desktop and web platforms. See [Scene.IsFullscreen]
+// to get the current fullscreen state and [Stage.Fullscreen] to set the
+// initial state.
 func (sc *Scene) UpdateFullscreen(fullscreen bool) {
 	rw := sc.RenderWindow()
 	if rw == nil {
--- core/stage.go
+++ core/stage.go
@@ -139,8 +139,8 @@ type Stage struct { //types:add -setters
 	// the window truly fullscreen without decorations (such as for a video player),
 	// whereas maximized keeps decorations and just makes it fill the available space.
 	// Not to be confused with [Stage.FullWindow], which is for stages contained within
-	// another system window. See [Scene.UpdateFullscreen] to update fullscreen state
-	// dynamically.
+	// another system window. See [Scene.IsFullscreen] and [Scene.UpdateFullscreen] to
+	// check and update fullscreen state dynamically on desktop and web platforms.
 	Fullscreen bool
 
 	// UseMinSize uses a minimum size as a function of the total available size
