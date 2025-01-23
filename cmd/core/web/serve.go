69b8d7567956552341e7be197d0581fd0623285b
Kai O'Reilly <koreilly5297@gmail.com>
2025-01-23 12:12:56 -0800

    create unified HandleFunc in web.Serve

--- cmd/core/web/serve.go
+++ cmd/core/web/serve.go
@@ -5,7 +5,9 @@
 package web
 
 import (
+	"fmt"
 	"net/http"
+	"strings"
 
 	"cogentcore.org/core/base/logx"
 	"cogentcore.org/core/cmd/core/config"
@@ -14,11 +16,14 @@ import (
 // Serve serves the build output directory on the default network address at the config port.
 func Serve(c *config.Config) error {
 	fs := http.FileServer(http.Dir(c.Build.Output))
-	http.Handle("/", fs)
-	http.HandleFunc("/app.wasm", func(w http.ResponseWriter, r *http.Request) {
-		w.Header().Set("Content-Type", "application/wasm")
-		if c.Web.Gzip {
-			w.Header().Set("Content-Encoding", "gzip")
+	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+		trim := strings.Trim(r.URL.Path, "/")
+		fmt.Println(trim)
+		if trim == "app.wasm" {
+			w.Header().Set("Content-Type", "application/wasm")
+			if c.Web.Gzip {
+				w.Header().Set("Content-Encoding", "gzip")
+			}
 		}
 		fs.ServeHTTP(w, r)
 	})
