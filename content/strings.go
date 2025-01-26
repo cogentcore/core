fc15c25b0055d5a8e5a97322baa3a0b395771351
Kai O'Reilly <koreilly5297@gmail.com>
2025-01-25 19:31:30 -0800

    content: improve automatic redirection similar page logic

--- content/strings.go
+++ content/strings.go
@@ -5,51 +5,22 @@
 package content
 
 import (
-	"strings"
-
 	"cogentcore.org/core/content/bcontent"
+	"github.com/adrg/strutil/metrics"
 )
-
-// hamming computes the Hamming distance between the two given strings.
-// It is based on https://github.com/adrg/strutil/blob/master/metrics/hamming.go.
-func hamming(a, b string) int {
-	a = strings.ToLower(a)
-	b = strings.ToLower(b)
-
-	runesA, runesB := []rune(a), []rune(b)
-
-	// Check if both terms are empty.
-	lenA, lenB := len(runesA), len(runesB)
-	if lenA == 0 && lenB == 0 {
-		return 0
-	}
-
-	// If the lengths of the sequences are not equal, the distance is
-	// initialized to their absolute difference. Otherwise, it is set to 0.
-	if lenA > lenB {
-		lenA, lenB = lenB, lenA
-	}
-	distance := lenB - lenA
-
-	// Calculate Hamming distance.
-	for i := 0; i < lenA; i++ {
-		if runesA[i] != runesB[i] {
-			distance++
-		}
-	}
-
-	return distance
-}
 
 // similarPage returns the page most similar to the given URL, used for automatic 404 redirects.
 func (ct *Content) similarPage(url string) *bcontent.Page {
+	m := metrics.NewJaccard()
+	m.CaseSensitive = false
+
 	var best *bcontent.Page
-	bestDistance := 1000000
+	bestSimilarity := -1.0
 	for _, page := range ct.pages {
-		distance := hamming(url, page.URL)
-		if distance < bestDistance {
+		similarity := m.Compare(url, page.URL)
+		if similarity > bestSimilarity {
 			best = page
-			bestDistance = distance
+			bestSimilarity = similarity
 		}
 	}
 	return best
