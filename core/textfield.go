c689c1f570f0b9a9a0f0b98316d9473a37790d04
Randall C. O'Reilly <oreilly@ucdavis.edu>
2025-01-26 00:20:14 -0800

    scroll: textfield is not scrollableunfocused by default

--- core/textfield.go
+++ core/textfield.go
@@ -214,7 +214,7 @@ func (tf *TextField) Init() {
 
 	tf.Styler(func(s *styles.Style) {
 		s.SetAbilities(true, abilities.Activatable, abilities.Focusable, abilities.Hoverable, abilities.Slideable, abilities.DoubleClickable, abilities.TripleClickable)
-		// s.SetAbilities(false, abilities.ScrollableUnfocused)
+		s.SetAbilities(false, abilities.ScrollableUnfocused)
 		tf.CursorWidth.Dp(1)
 		tf.SelectColor = colors.Scheme.Select.Container
 		tf.PlaceholderColor = colors.Scheme.OnSurfaceVariant
