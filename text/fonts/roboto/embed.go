e6441a20a4025a9c17b861c6aada6952c4a5f470
Randall C. O'Reilly <oreilly@ucdavis.edu>
2025-04-08 21:12:15 -0700

    newpaint: add roboto embed

---
+++ text/fonts/roboto/embed.go
@@ -0,0 +1,10 @@
+// Copyright (c) 2025, Cogent Core. All rights reserved.
+// Use of this source code is governed by a BSD-style
+// license that can be found in the LICENSE file.
+
+package roboto
+
+import "embed"
+
+//go:embed *.ttf
+var EmbeddedFonts embed.FS
