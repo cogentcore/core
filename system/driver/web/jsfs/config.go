d288344bc0ad6406b21f16fd350150f683149ed0
Kai O'Reilly <koreilly5297@gmail.com>
2025-05-15 16:38:55 -0700

    jsfs: fix os.ReadDir on web with go1.24 by setting O_DIRECTORY constant in wasm_exec.js and jsfs.Config; need to set it immediately in wasm_exec.js so that syscall does not reject it on it by thinking that we are on Windows

--- system/driver/web/jsfs/config.go
+++ system/driver/web/jsfs/config.go
@@ -32,6 +32,7 @@ func Config(jfs js.Value) (*FS, error) {
 	constants.Set("O_TRUNC", syscall.O_TRUNC)
 	constants.Set("O_APPEND", syscall.O_APPEND)
 	constants.Set("O_EXCL", syscall.O_EXCL)
+	constants.Set("O_DIRECTORY", syscall.O_DIRECTORY)
 
 	SetFunc(jfs, "chmod", fs.Chmod)
 	SetFunc(jfs, "chown", fs.Chown)
