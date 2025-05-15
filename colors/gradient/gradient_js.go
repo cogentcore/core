85d1e3a68fab7a431da80983dab727d8dffffabf
Kai O'Reilly <koreilly5297@gmail.com>
2025-05-15 15:33:21 -0700

    gradient: skip NaN stops on js to fix crashing

--- colors/gradient/gradient_js.go
+++ colors/gradient/gradient_js.go
@@ -10,12 +10,16 @@ import (
 	"syscall/js"
 
 	"cogentcore.org/core/colors"
+	"cogentcore.org/core/math32"
 )
 
 // ToJS converts the gradient to a JavaScript object.
 func (l *Linear) ToJS(ctx js.Value) js.Value {
 	grad := ctx.Call("createLinearGradient", l.rStart.X, l.rStart.Y, l.rEnd.X, l.rEnd.Y)
 	for _, stop := range l.Stops {
+		if math32.IsNaN(stop.Pos) {
+			continue
+		}
 		grad.Call("addColorStop", stop.Pos, colors.AsHex(stop.Color))
 	}
 	return grad
@@ -25,6 +29,9 @@ func (l *Linear) ToJS(ctx js.Value) js.Value {
 func (r *Radial) ToJS(ctx js.Value) js.Value {
 	grad := ctx.Call("createRadialGradient", r.rCenter.X, r.rCenter.Y, 0, r.rFocal.X, r.rFocal.Y, r.rRadius.X) // TODO: is this the right use of center and focal?
 	for _, stop := range r.Stops {
+		if math32.IsNaN(stop.Pos) {
+			continue
+		}
 		grad.Call("addColorStop", stop.Pos, colors.AsHex(stop.Color))
 	}
 	return grad
