58f7a4f64a16e8c94dde1b555ff4d1e6ecf32f78
Kai O'Reilly <koreilly5297@gmail.com>
2025-05-16 18:09:46 -0700

    htmlcore: remove debug statement

--- htmlcore/md.go
+++ htmlcore/md.go
@@ -44,7 +44,6 @@ func mdToHTML(ctx *Context, md []byte) []byte {
 // Cogent Core widgets to the given [core.Widget], using the given context.
 func ReadMD(ctx *Context, parent core.Widget, b []byte) error {
 	htm := mdToHTML(ctx, b)
-	// os.WriteFile("htmlcore_tmp.html", htm, 0666)
 	buf := bytes.NewBuffer(htm)
 	return ReadHTML(ctx, parent, buf)
 }
