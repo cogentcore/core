0effd2414f569e9e311a788b4682aafe7873c896
Randall C. O'Reilly <oreilly@ucdavis.edu>
2025-05-17 16:00:18 -0700

    newpaint: csl posarg, required arg tags

--- text/csl/mdcite/mdcite.go
+++ text/csl/mdcite/mdcite.go
@@ -14,28 +14,28 @@ import (
 type Config struct {
 
 	// CSL JSON formatted file with the library of references to lookup citations in.
-	Refs string `flag:"r,refs" required:"+"`
+	Refs string `flag:"r,refs" posarg:"0"`
 
 	// Directory with markdown files to extract citations from.
 	// Defaults to current directory if empty.
-	Dir string `flag:"d,dir" required:"-"`
+	Dir string `flag:"d,dir"`
 
 	// File name to write the formatted references to.
 	// Defaults to references.md if empty.
-	Output string `flag:"o,output" required:"-"`
+	Output string `flag:"o,output"`
 
 	// File name to write the subset of cited reference data to.
 	// Defaults to citedrefs.json if empty.
-	CitedData string `flag:"c,cited" required:"-"`
+	CitedData string `flag:"c,cited"`
 
 	// heading to add to the top of the references file.
 	// Include markdown heading syntax, e.g., ##
 	// Defaults to ## References if empty.
-	Heading string `flag:"h,heading" required:"-"`
+	Heading string `flag:"h,heading"`
 
 	// style is the citation style to generate.
 	// Defaults to APA if empty.
-	Style csl.Styles `flag:"s,style"  required:"-"`
+	Style csl.Styles `flag:"s,style"`
 }
 
 // Generate extracts citations and generates resulting references file.
