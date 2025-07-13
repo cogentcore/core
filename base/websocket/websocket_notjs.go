a6ff012a956b0cdc2438fc08008b0ee462ac603e
Kai O'Reilly <koreilly5297@gmail.com>
2025-07-13 15:58:44 -0700

    websocket: implement OnMessage for notjs

--- base/websocket/websocket_notjs.go
+++ base/websocket/websocket_notjs.go
@@ -6,7 +6,10 @@
 
 package websocket
 
-import "github.com/gorilla/websocket"
+import (
+	"cogentcore.org/core/base/errors"
+	"github.com/gorilla/websocket"
+)
 
 // Client represents a WebSocket client connection.
 // You can use [Connect] to create a new Client.
@@ -14,6 +17,9 @@ type Client struct {
 
 	// conn is the underlying WebSocket connection.
 	conn *websocket.Conn
+
+	// done is a channel that is closed when the connection is closed.
+	done chan struct{}
 }
 
 // Connect connects to a WebSocket server and returns a [Client].
@@ -22,5 +28,20 @@ func Connect(url string) (*Client, error) {
 	if err != nil {
 		return nil, err
 	}
-	return &Client{conn: conn}, nil
+	return &Client{conn: conn, done: make(chan struct{})}, nil
+}
+
+// OnMessage sets a callback function to be called when a message is received.
+// This function can only be called once.
+func (c *Client) OnMessage(f func(typ int, msg []byte)) {
+	go func() {
+		for {
+			typ, msg, err := c.conn.ReadMessage()
+			if errors.Log(err) != nil {
+				close(c.done)
+				return
+			}
+			f(typ, msg)
+		}
+	}()
 }
