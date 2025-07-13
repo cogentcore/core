f1cc864581559e61f1021592551f884e5d82b021
Kai O'Reilly <koreilly5297@gmail.com>
2025-07-13 16:20:41 -0700

    websocket: Connect and OnMessage on js

--- base/websocket/websocket_js.go
+++ base/websocket/websocket_js.go
@@ -16,3 +16,27 @@ type Client struct {
 	// See https://developer.mozilla.org/en-US/docs/Web/API/WebSocket
 	ws js.Value
 }
+
+// Connect connects to a WebSocket server and returns a [Client].
+func Connect(url string) (*Client, error) {
+	ws := js.Global().Get("WebSocket").New(url)
+	ws.Set("binaryType", "arraybuffer")
+	return &Client{ws: ws}, nil
+}
+
+// OnMessage sets a callback function to be called when a message is received.
+// This function can only be called once on native.
+func (c *Client) OnMessage(f func(typ MessageTypes, msg []byte)) {
+	c.ws.Call("addEventListener", "message", js.FuncOf(func(this js.Value, args []js.Value) any {
+		data := args[0].Get("data")
+		if data.Type() == js.TypeString {
+			f(TextMessage, []byte(data.String()))
+			return nil
+		}
+		array := js.Global().Get("Uint8ClampedArray").New(data)
+		b := make([]byte, array.Length())
+		js.CopyBytesToGo(b, array) // TODO: more performant way to do this, perhaps with gopherjs/goscript?
+		f(BinaryMessage, b)
+		return nil
+	}))
+}
